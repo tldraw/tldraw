61a8401d29d89c97b665376fe3c23437dd3500ce
Steve Ruiz <steveruizok@gmail.com>
2021-06-19 16:38:23 +0100

    sentry

--- next.config.js
+++ next.config.js
@@ -2,13 +2,6 @@ const withPWA = require('next-pwa')
 const { withSentryConfig } = require('@sentry/nextjs')
 
 const SentryWebpackPluginOptions = {
-  // Additional config options for the Sentry Webpack plugin. Keep in mind that
-  // the following options are set automatically, and overriding them is not
-  // recommended:
-  //   release, url, org, project, authToken, configFile, stripPrefix,
-  //   urlPrefix, include, ignore
-  // For all available options, see:
-  // https://github.com/getsentry/sentry-webpack-plugin#options.
   silent: process.env.NODE_ENV === 'development',
 }
 
