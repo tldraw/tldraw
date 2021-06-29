f12c18e9bef6b6bd8c44b212c0b3b89b34ca42cb
Steve Ruiz <steveruizok@gmail.com>
2021-06-29 20:48:06 +0100

    Update meta

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import useGtag from 'hooks/useGtag'
+import Head from 'next/head'
 import { AppProps } from 'next/app'
 import { globalStyles } from 'styles'
 import { Provider } from 'next-auth/client'
@@ -10,9 +11,18 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   useGtag()
 
   return (
-    <Provider session={pageProps.session}>
-      <Component {...pageProps} />
-    </Provider>
+    <>
+      <Head>
+        <title>tldraw</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+        />
+      </Head>
+      <Provider session={pageProps.session}>
+        <Component {...pageProps} />
+      </Provider>
+    </>
   )
 }
 
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -37,35 +37,42 @@ class MyDocument extends NextDocument {
   }
 
   render(): JSX.Element {
+    const APP_NAME = 'tldraw'
+    const APP_DESCRIPTION = 'A tiny little drawing app.'
+    const APP_URL = 'https://tldraw.com'
+
     return (
       <Html lang="en">
         <Head>
-          <link rel="manifest" href="/manifest.json" />
-          <link rel="shortcut icon" href="/favicon.ico" />
-          <meta name="application-name" content="tldraw" />
+          <meta name="application-name" content={APP_NAME} />
           <meta name="apple-mobile-web-app-capable" content="yes" />
           <meta name="apple-mobile-web-app-status-bar-style" content="black" />
-          <meta name="apple-mobile-web-app-title" content="tldraw" />
-          <meta name="description" content="A tiny little drawing app." />
+          <meta name="apple-mobile-web-app-title" content={APP_NAME} />
+          <meta name="description" content={APP_DESCRIPTION} />
           <meta name="format-detection" content="telephone=no" />
           <meta name="mobile-web-app-capable" content="yes" />
           <meta name="theme-color" content="#fafafa" />
+
           <meta name="twitter:card" content="summary" />
-          <meta name="twitter:url" content="https://tldraw.com" />
-          <meta name="twitter:title" content="tldraw" />
-          <meta
-            name="twitter:description"
-            content="A tiny little drawing app."
-          />
+          <meta name="twitter:url" content={APP_URL} />
+          <meta name="twitter:title" content={APP_NAME} />
+          <meta name="twitter:description" content={APP_DESCRIPTION} />
           <meta name="twitter:creator" content="@steveruizok" />
+
           <meta property="og:type" content="website" />
-          <meta property="og:title" content="tldraw" />
-          <meta
-            property="og:description"
-            content="A tiny little drawing app."
+          <meta property="og:title" content={APP_NAME} />
+          <meta property="og:description" content={APP_DESCRIPTION} />
+          <meta property="og:site_name" content={APP_NAME} />
+          <meta property="og:url" content={APP_URL} />
+
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="shortcut icon" href="/favicon.ico" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/icons/apple-touch-icon.png"
           />
-          <meta property="og:site_name" content="tldraw" />
-          <meta property="og:url" content="https://tldraw.com" />
+
           <script
             async
             src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
