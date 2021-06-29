0dff86be66806d21ba5460ff9a4827b9bd775619
Steve Ruiz <steveruizok@gmail.com>
2021-06-29 20:32:11 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,11 +3,9 @@ const withPWA = require('next-pwa')
 
 const isProduction = process.env.NODE_ENV === 'production'
 
-module.exports = withPWA(
-  withNx({
-    pwa: {
-      disable: !isProduction,
-      dest: 'public',
-    },
-  })
-)
+module.exports = withPWA({
+  pwa: {
+    disable: !isProduction,
+    dest: 'public',
+  },
+})
