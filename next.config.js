a84b77157169a945b9fb27f2aaa8773ecf93906e
Steve Ruiz <steveruizok@gmail.com>
2021-07-07 10:23:31 +0100

    Reconfigures sentry

--- next.config.js
+++ next.config.js
@@ -1,11 +1,76 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const withPWA = require('next-pwa')
 
-const isProduction = process.env.NODE_ENV === 'production'
+const {
+  GITHUB_ID,
+  GITHUB_SECRET,
+  GITHUB_API_SECRET,
+  NEXT_PUBLIC_SENTRY_DSN: SENTRY_DSN,
+  SENTRY_ORG,
+  SENTRY_PROJECT,
+  SENTRY_AUTH_TOKEN,
+  NODE_ENV,
+  VERCEL_GIT_COMMIT_SHA,
+  GA_MEASUREMENT_ID,
+} = process.env
+
+process.env.SENTRY_DSN = SENTRY_DSN
+
+const isProduction = NODE_ENV === 'production'
 
 module.exports = withPWA({
   pwa: {
     disable: !isProduction,
     dest: 'public',
   },
+})
+
+const basePath = ''
+
+module.exports = withPWA({
+  productionBrowserSourceMaps: true,
+  env: {
+    NEXT_PUBLIC_COMMIT_SHA: VERCEL_GIT_COMMIT_SHA,
+    GA_MEASUREMENT_ID,
+    GITHUB_ID,
+    GITHUB_SECRET,
+    GITHUB_API_SECRET,
+  },
+  webpack: (config, options) => {
+    if (!options.isServer) {
+      config.resolve.alias['@sentry/node'] = '@sentry/browser'
+    }
+
+    config.plugins.push(
+      new options.webpack.DefinePlugin({
+        'process.env.NEXT_IS_SERVER': JSON.stringify(
+          options.isServer.toString()
+        ),
+      })
+    )
+
+    if (
+      SENTRY_DSN &&
+      SENTRY_ORG &&
+      SENTRY_PROJECT &&
+      SENTRY_AUTH_TOKEN &&
+      VERCEL_GIT_COMMIT_SHA &&
+      isProduction
+    ) {
+      config.plugins.push(
+        new SentryWebpackPlugin({
+          include: '.next',
+          ignore: ['node_modules'],
+          stripPrefix: ['webpack://_N_E/'],
+          urlPrefix: `~${basePath}/_next`,
+          release: VERCEL_GIT_COMMIT_SHA,
+          authToken: SENTRY_AUTH_TOKEN,
+          org: SENTRY_PROJECT,
+          project: SENTRY_ORG,
+        })
+      )
+    }
+    return config
+  },
+  basePath,
 })
