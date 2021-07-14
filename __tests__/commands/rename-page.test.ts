6ce1f0b5f1a692607f9e06e72d5a198e8d8704aa
Steve Ruiz <steveruizok@gmail.com>
2021-07-14 12:40:18 +0100

    Fixes tests

--- __tests__/commands/rename-page.test.ts
+++ __tests__/commands/rename-page.test.ts
@@ -26,30 +26,30 @@ describe('rename page command', () => {
     })
   })
 
-  // describe('renames a page other than the current page', () => {
-  //   tt.restore()
-  //     .reset()
-  //     .send('CREATED_PAGE')
-  //     .send('CHANGED_PAGE', { id: 'page1' })
+  describe('renames a page other than the current page', () => {
+    tt.restore()
+      .reset()
+      .send('CREATED_PAGE')
+      .send('CHANGED_PAGE', { id: 'page1' })
 
-  //   expect(Object.keys(tt.data.document.pages).length).toBe(2)
+    expect(Object.keys(tt.data.document.pages).length).toBe(2)
 
-  //   expect(tt.data.currentPageId).toBe('page1')
+    expect(tt.data.currentPageId).toBe('page1')
 
-  //   const secondPageId = Object.keys(tt.data.document.pages)[1]
+    const secondPageId = Object.keys(tt.data.document.pages)[1]
 
-  //   expect(tt.data.document.pages[secondPageId].name).toBe('New Page')
+    expect(tt.data.document.pages[secondPageId].name).toBe('Page 2')
 
-  //   tt.send('RENAMED_PAGE', { id: secondPageId, name: 'My Second Page' })
+    tt.send('RENAMED_PAGE', { id: secondPageId, name: 'My Second Page' })
 
-  //   expect(tt.data.document.pages[secondPageId].name).toBe('My Second Page')
+    expect(tt.data.document.pages[secondPageId].name).toBe('My Second Page')
 
-  //   tt.undo()
+    tt.undo()
 
-  //   expect(tt.data.document.pages[secondPageId].name).toBe('New Page')
+    expect(tt.data.document.pages[secondPageId].name).toBe('Page 2')
 
-  //   tt.redo()
+    tt.redo()
 
-  //   expect(tt.data.document.pages[secondPageId].name).toBe('My Second Page')
-  // })
+    expect(tt.data.document.pages[secondPageId].name).toBe('My Second Page')
+  })
 })
