4cdf7c5a76b3ebf690983801908e49f1647d638f
Steve Ruiz <steveruizok@gmail.com>
2021-07-29 19:36:37 +0100

    Update next.config.js

--- packages/site/next.config.js
+++ packages/site/next.config.js
@@ -5,9 +5,6 @@ const withNx = require('@nrwl/next/plugins/with-nx')
  * @type {import('@nrwl/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
-  future: {
-    webpack5: true,
-  },
   nx: {
     // Set this to false if you do not want to use SVGR
     // See: https://github.com/gregberge/svgr
