391ea4d172bb64c272c374bb9ac71d7478a7ddcb
Steve Ruiz <steveruizok@gmail.com>
2021-08-05 11:23:16 +0100

    Update useGtag.tsx

--- packages/www/hooks/useGtag.tsx
+++ packages/www/hooks/useGtag.tsx
@@ -1,14 +1,13 @@
-/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import router from 'next/router'
 import { useEffect } from 'react'
 import * as gtag from '../utils/gtag'
 
 export default function useGtag() {
   useEffect(() => {
-    if (process.env.NODE_ENV !== 'production') return
-
     function handleRouteChange(url: URL) {
-      gtag.pageview(url)
+      if (process.env.NODE_ENV !== 'production') {
+        gtag.pageview(url)
+      }
     }
 
     router.events.on('routeChangeComplete', handleRouteChange)
