150bf56f62d7f5cde4097c217ad42e0a3b18512d
Steve Ruiz <steveruizok@gmail.com>
2021-08-07 23:00:17 +0100

    Update context-menu.tsx

--- packages/tldraw/src/lib/components/context-menu/context-menu.tsx
+++ packages/tldraw/src/lib/components/context-menu/context-menu.tsx
@@ -29,7 +29,7 @@ import {
   SpaceEvenlyVerticallyIcon,
   StretchHorizontallyIcon,
   StretchVerticallyIcon,
-} from '../icons'
+} from '@radix-ui/react-icons'
 
 const has1SelectedIdsSelector = (s: Data) => {
   return s.pageState.selectedIds.length > 0
