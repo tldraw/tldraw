3d164851be4b5dc36bc6d4ab80f5d41783fb21a5
Tais Massaro <tais@hey.com>
2021-08-28 23:12:44 +0200

    test(align-command): cover states where command is disabled

--- __tests__/commands/align.test.ts
+++ __tests__/commands/align.test.ts
@@ -1,40 +1,69 @@
+import { ShapeType } from 'types'
 import TestState from '../test-utils'
 
 describe('align command', () => {
   const tt = new TestState()
   tt.resetDocumentState()
+    .createShape(
+      {
+        type: ShapeType.Rectangle,
+        point: [0, 0],
+        size: [100, 100],
+      },
+      'rectangleShape'
+    )
+    .createShape(
+      {
+        type: ShapeType.Ellipse,
+        point: [150, 150],
+        radiusX: 50,
+        radiusY: 50,
+      },
+      'ellipseShape'
+    )
+    .save()
+
+  describe('read-only mode', () => {
+    it('is disabled', () => {
+      tt.restore()
+      tt.state.send('TOGGLED_READ_ONLY')
+
+      tt.clickShape('rectangleShape').clickShape('ellipseShape', {
+        shiftKey: true,
+      })
+
+      expect(tt.state.can('ALIGNED')).toBe(false)
+    })
+  })
 
   describe('when one item is selected', () => {
-    it('does command', () => {
-      // TODO
-      null
-    })
+    it('is disabled', () => {
+      tt.restore()
 
-    it('un-does command', () => {
-      // TODO
-      null
-    })
+      tt.clickShape('rectangleShape').send('ALIGNED')
 
-    it('re-does command', () => {
-      // TODO
-      null
+      expect(tt.state.can('ALIGNED')).toBe(false)
     })
   })
 
   describe('when multiple items are selected', () => {
-    it('does command', () => {
-      // TODO
-      null
+    describe('top', () => {
+      it.todo('does, undoes and redoes command')
     })
-
-    it('un-does command', () => {
-      // TODO
-      null
+    describe('center vertical', () => {
+      it.todo('does, undoes and redoes command')
     })
-
-    it('re-does command', () => {
-      // TODO
-      null
+    describe('bottom', () => {
+      it.todo('does, undoes and redoes command')
+    })
+    describe('left', () => {
+      it.todo('does, undoes and redoes command')
+    })
+    describe('center horizontal', () => {
+      it.todo('does, undoes and redoes command')
+    })
+    describe('right', () => {
+      it.todo('does, undoes and redoes command')
     })
   })
 })
