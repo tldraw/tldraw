d05807bbfbb350d83146120a5e4d93efac467405
Steve Ruiz <steveruizok@gmail.com>
2021-09-03 15:41:41 +0100

    Update index.tsx

--- packages/www/pages/index.tsx
+++ packages/www/pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import dynamic from 'next/dynamic'
-import type { GetServerSideProps } from 'next'
-import { getSession } from 'next-auth/client'
+// import type { GetServerSideProps } from 'next'
+// import { getSession } from 'next-auth/client'
 
 const Editor = dynamic(() => import('components/editor'), { ssr: false })
 
@@ -18,17 +18,17 @@ export default function Home(): JSX.Element {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const session = await getSession(context)
+// export const getServerSideProps: GetServerSideProps = async (context) => {
+//   const session = await getSession(context)
 
-  if (!session?.user && process.env.NODE_ENV !== 'development') {
-    context.res.setHeader('Location', `/sponsorware`)
-    context.res.statusCode = 307
-  }
+//   if (!session?.user && process.env.NODE_ENV !== 'development') {
+//     context.res.setHeader('Location', `/sponsorware`)
+//     context.res.statusCode = 307
+//   }
 
-  return {
-    props: {
-      session,
-    },
-  }
-}
+//   return {
+//     props: {
+//       session,
+//     },
+//   }
+// }
