b223515b8a293bbdb9a0a9e48722bc2f5b314321
Steve Ruiz <steveruizok@gmail.com>
2021-09-04 11:33:48 +0100

    Removes references to old font

--- packages/tldraw/src/hooks/useCustomFonts.ts
+++ packages/tldraw/src/hooks/useCustomFonts.ts
@@ -1,14 +1,5 @@
 import { useStyle, css } from '~hooks/useStyle'
 
-// const customFonts = css`
-//   @font-face {
-//     font-family: 'Verveine Regular';
-//     font-style: normal;
-//     font-weight: normal;
-//     src: local('Verveine Regular'), url('/VerveineRegular.woff') format('woff');
-//   }
-// `
-
 const customFonts = css`
   @import url('https://fonts.googleapis.com/css2?family=Caveat+Brush&display=swap');
 `
