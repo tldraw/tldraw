ca9e1981e6fa4bc91f7442f58bbdb3b37771b7e0
Steve Ruiz <steveruizok@gmail.com>
2021-09-04 11:21:53 +0100

    Revert "More cleanup on www"
    This reverts commit e4ae0170824147b2311537cc8c61b2f81d7f87ed.

--- packages/www/next.config.js
+++ packages/www/next.config.js
@@ -1,5 +1,7 @@
-module.exports = {
+const withTM = require('next-transpile-modules')(['@tldraw/tldraw'])
+
+module.exports = withTM({
   reactStrictMode: true,
   webpack5: true,
   symlinks: false,
-}
+})
