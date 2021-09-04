dbed5f6bf4c78c54dafc72a35b9b294e42d28147
Steve Ruiz <steveruizok@gmail.com>
2021-09-04 13:18:44 +0100

    Adds room pages, auth api routes

---
+++ packages/www/pages/r/[id].tsx
@@ -0,0 +1,37 @@
+import * as React from 'react'
+import type { GetServerSideProps } from 'next'
+import { getSession } from 'next-auth/client'
+import dynamic from 'next/dynamic'
+const Editor = dynamic(() => import('components/editor'), { ssr: false })
+
+interface RoomProps {
+  id: string
+}
+
+export default function Room({ id }: RoomProps): JSX.Element {
+  return <Editor id={id} />
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const session = await getSession(context)
+
+  if (!session?.user && process.env.NODE_ENV !== 'development') {
+    context.res.setHeader('Location', `/sponsorware`)
+    context.res.statusCode = 307
+  }
+
+  const id = context.query.id?.toString()
+
+  // Get document from database
+
+  // If document does not exist, create an empty document
+
+  // Return the document
+
+  return {
+    props: {
+      id,
+      session,
+    },
+  }
+}
