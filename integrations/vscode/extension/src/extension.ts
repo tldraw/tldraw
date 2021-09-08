29fc4eced445c0353627182f8e742b88c7e8c29e
Francois Laberge <francoislaberge@gmail.com>
2021-09-08 12:18:43 -0400

    Have onChange working with latest fix. Back to iframed for a few mom

--- integrations/vscode/extension/src/extension.ts
+++ integrations/vscode/extension/src/extension.ts
@@ -1,11 +1,7 @@
 import * as vscode from 'vscode'
-import { CatScratchEditorProvider } from './catScratchEditor'
-import { PawDrawEditorProvider } from './pawDrawEditor'
 import { TldrawEditorProvider } from './tldraw-editor'
 
 export function activate(context: vscode.ExtensionContext) {
   // Register our custom editor providers
   context.subscriptions.push(TldrawEditorProvider.register(context))
-  // context.subscriptions.push(CatScratchEditorProvider.register(context))
-  // context.subscriptions.push(PawDrawEditorProvider.register(context))
 }
--- integrations/vscode/extension/src/tldraw-editor.ts
+++ integrations/vscode/extension/src/tldraw-editor.ts
@@ -175,10 +175,34 @@ export class TldrawEditorProvider implements vscode.CustomTextEditorProvider {
       )
     )
 
+    const cssUrl = webview.asWebviewUri(
+      vscode.Uri.joinPath(
+        this.context.extensionUri,
+        'build/static/css',
+        'main.c353a27c.chunk.css'
+      )
+    )
+
+    const jsUrl1 = webview.asWebviewUri(
+      vscode.Uri.joinPath(
+        this.context.extensionUri,
+        'build/static/js',
+        '2.6640e0ac.chunk.js'
+      )
+    )
+
+    const jsUrl2 = webview.asWebviewUri(
+      vscode.Uri.joinPath(
+        this.context.extensionUri,
+        'build/static/js',
+        'main.34325131.chunk.js'
+      )
+    )
+
     // Use a nonce to whitelist which scripts can be run
     const nonce = getNonce()
 
-    return /* html */ `
+    const older = /* html */ `
 			<!DOCTYPE html>
 			<html lang="en">
 			<head>
@@ -203,6 +227,26 @@ export class TldrawEditorProvider implements vscode.CustomTextEditorProvider {
 				<script nonce="${nonce}" src="${scriptUri}"></script>
 			</body>
 			</html>`
+      return older;
+    const newer = `<!doctype html>
+      <html lang="en">
+        <head>
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
+            <meta name="theme-color" content="#000000">
+            <!-- <link rel="shortcut icon" href="./favicon.ico"> -->
+            <title>React App</title>
+            <link href="${cssUrl}" rel="stylesheet">
+        </head>
+        <body>
+            <noscript>You need to enable JavaScript to run this app.</noscript>
+            <div id="root"></div>
+            <script>!function(e){function r(r){for(var n,l,a=r[0],f=r[1],i=r[2],p=0,s=[];p<a.length;p++)l=a[p],Object.prototype.hasOwnProperty.call(o,l)&&o[l]&&s.push(o[l][0]),o[l]=0;for(n in f)Object.prototype.hasOwnProperty.call(f,n)&&(e[n]=f[n]);for(c&&c(r);s.length;)s.shift()();return u.push.apply(u,i||[]),t()}function t(){for(var e,r=0;r<u.length;r++){for(var t=u[r],n=!0,a=1;a<t.length;a++){var f=t[a];0!==o[f]&&(n=!1)}n&&(u.splice(r--,1),e=l(l.s=t[0]))}return e}var n={},o={1:0},u=[];function l(r){if(n[r])return n[r].exports;var t=n[r]={i:r,l:!1,exports:{}};return e[r].call(t.exports,t,t.exports,l),t.l=!0,t.exports}l.m=e,l.c=n,l.d=function(e,r,t){l.o(e,r)||Object.defineProperty(e,r,{enumerable:!0,get:t})},l.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},l.t=function(e,r){if(1&r&&(e=l(e)),8&r)return e;if(4&r&&"object"==typeof e&&e&&e.__esModule)return e;var t=Object.create(null);if(l.r(t),Object.defineProperty(t,"default",{enumerable:!0,value:e}),2&r&&"string"!=typeof e)for(var n in e)l.d(t,n,function(r){return e[r]}.bind(null,n));return t},l.n=function(e){var r=e&&e.__esModule?function(){return e.default}:function(){return e};return l.d(r,"a",r),r},l.o=function(e,r){return Object.prototype.hasOwnProperty.call(e,r)},l.p="./";var a=this["webpackJsonptldraw-vscode"]=this["webpackJsonptldraw-vscode"]||[],f=a.push.bind(a);a.push=r,a=a.slice();for(var i=0;i<a.length;i++)r(a[i]);var c=f;t()}([])</script>
+            <script src="${jsUrl1}"></script>
+            <script src="${jsUrl2}"></script>
+        </body>
+      </html>`
+     return newer;
   }
 
   /**
