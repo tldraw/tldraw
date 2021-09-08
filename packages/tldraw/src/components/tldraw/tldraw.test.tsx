784a77fe26b576a7f4cd07bc6e7420f09aa690ec
Steve Ruiz <steveruizok@gmail.com>
2021-09-08 13:02:58 +0100

    Update tldraw.test.tsx

--- packages/tldraw/src/components/tldraw/tldraw.test.tsx
+++ packages/tldraw/src/components/tldraw/tldraw.test.tsx
@@ -6,4 +6,24 @@ describe('tldraw', () => {
   test('mounts component without crashing', () => {
     render(<TLDraw />)
   })
+
+  test('mounts component and calls onMount', (done) => {
+    const onMount = jest.fn()
+    render(<TLDraw onMount={onMount} />)
+    // The call is asynchronous: it won't be called until the next tick.
+    setTimeout(() => {
+      expect(onMount).toHaveBeenCalled()
+      done()
+    }, 100)
+  })
+
+  test('mounts component and calls onMount when id is present', (done) => {
+    const onMount = jest.fn()
+    render(<TLDraw id="someId" onMount={onMount} />)
+    // The call is asynchronous: it won't be called until the next tick.
+    setTimeout(() => {
+      expect(onMount).toHaveBeenCalled()
+      done()
+    }, 100)
+  })
 })
