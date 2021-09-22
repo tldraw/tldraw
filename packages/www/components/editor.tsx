1d988f132a4f99947f6718f4eae5a80fef7c16e8
Steve Ruiz <steveruizok@gmail.com>
2021-09-22 13:17:52 +0100

    [feature] Add Sentry tracking, restore installable PWA (#106)
    * This PR restores the sentry and pwa features
    
    * Add gtag to editor in events.

--- packages/www/components/editor.tsx
+++ packages/www/components/editor.tsx
@@ -1,4 +1,5 @@
-import { TLDraw } from '@tldraw/tldraw'
+import { TLDraw, TLDrawState, Data } from '@tldraw/tldraw'
+import * as gtag from '-utils/gtag'
 import React from 'react'
 
 interface EditorProps {
@@ -6,15 +7,26 @@ interface EditorProps {
 }
 
 export default function Editor({ id = 'home' }: EditorProps) {
-  const handleMount = React.useCallback((tlstate) => {
+  // Put the tlstate into the window, for debugging.
+  const handleMount = React.useCallback((tlstate: TLDrawState) => {
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
     window.tlstate = tlstate
   }, [])
 
+  // Send events to gtag as actions.
+  const handleChange = React.useCallback((_tlstate: TLDrawState, _state: Data, reason: string) => {
+    gtag.event({
+      action: reason,
+      category: 'editor',
+      label: `page:${id}`,
+      value: 0,
+    })
+  }, [])
+
   return (
     <div className="tldraw">
-      <TLDraw id={id} onMount={handleMount} autofocus />
+      <TLDraw id={id} onMount={handleMount} onChange={handleChange} autofocus />
     </div>
   )
 }
--- packages/www/pages/_app.tsx
+++ packages/www/pages/_app.tsx
@@ -1,6 +1,12 @@
 import '../styles/globals.css'
+import { init } from '-utils/sentry'
+import useGtag from '-utils/useGtag'
+
+init()
 
 function MyApp({ Component, pageProps }) {
+  useGtag()
+
   return <Component {...pageProps} />
 }
 
