b8e95acbd704fb384ce7557d3b601d6318d41671
Francois Laberge <francoislaberge@gmail.com>
2021-09-25 07:57:57 -0400

    Quick fix

---
+++ integrations/vscode/editor/.eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  overrides: [
+    {
+      // enable the rule specifically for TypeScript files
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        '@typescript-eslint/explicit-module-boundary-types': [0],
+      },
+    },
+  ],
+}
