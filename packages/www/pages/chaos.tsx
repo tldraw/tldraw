dbb65e15cdcd8770e62c774d64d137b958b7c480
Steve Ruiz <steveruizok@gmail.com>
2021-10-08 16:54:28 +0100

    Create chaos.tsx

---
+++ packages/www/pages/chaos.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react'
+import Head from 'next/head'
+import dynamic from 'next/dynamic'
+const MultiplayerEditor = dynamic(() => import('components/multiplayer-editor'), { ssr: false })
+
+export default function ChaosRoom(): JSX.Element {
+  return (
+    <>
+      <Head>
+        <title>tldraw</title>
+      </Head>
+      <MultiplayerEditor id={'chaos'} />
+    </>
+  )
+}
