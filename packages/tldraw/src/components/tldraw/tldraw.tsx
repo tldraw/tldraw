23952243149c0adb7872b8f7c05808e645daaf82
Steve Ruiz <steveruizok@gmail.com>
2021-10-15 13:24:34 +0100

    Removes context menu

--- packages/tldraw/src/components/tldraw/tldraw.tsx
+++ packages/tldraw/src/components/tldraw/tldraw.tsx
@@ -12,7 +12,6 @@ import {
   useTLDrawContext,
 } from '~hooks'
 import { tldrawShapeUtils } from '~shape'
-import { ContextMenu } from '~components/context-menu'
 import { StylePanel } from '~components/style-panel'
 import { ToolsPanel } from '~components/tools-panel'
 import { PagePanel } from '~components/page-panel'
@@ -215,72 +214,72 @@ function InnerTldraw({
     <div ref={rWrapper} tabIndex={0}>
       <div className={layout()}>
         <OneOff focusableRef={rWrapper} autofocus={autofocus} />
-        <ContextMenu>
-          <Renderer
-            id={id}
-            containerRef={rWrapper}
-            page={page}
-            pageState={pageState}
-            users={users}
-            userId={tlstate.state.room?.userId}
-            shapeUtils={tldrawShapeUtils}
-            theme={theme}
-            meta={meta}
-            hideBounds={hideBounds}
-            hideHandles={hideHandles}
-            hideIndicators={hideIndicators}
-            onPinchStart={tlstate.onPinchStart}
-            onPinchEnd={tlstate.onPinchEnd}
-            onPinch={tlstate.onPinch}
-            onPan={tlstate.onPan}
-            onZoom={tlstate.onZoom}
-            onPointerDown={tlstate.onPointerDown}
-            onPointerMove={tlstate.onPointerMove}
-            onPointerUp={tlstate.onPointerUp}
-            onPointCanvas={tlstate.onPointCanvas}
-            onDoubleClickCanvas={tlstate.onDoubleClickCanvas}
-            onRightPointCanvas={tlstate.onRightPointCanvas}
-            onDragCanvas={tlstate.onDragCanvas}
-            onReleaseCanvas={tlstate.onReleaseCanvas}
-            onPointShape={tlstate.onPointShape}
-            onDoubleClickShape={tlstate.onDoubleClickShape}
-            onRightPointShape={tlstate.onRightPointShape}
-            onDragShape={tlstate.onDragShape}
-            onHoverShape={tlstate.onHoverShape}
-            onUnhoverShape={tlstate.onUnhoverShape}
-            onReleaseShape={tlstate.onReleaseShape}
-            onPointBounds={tlstate.onPointBounds}
-            onDoubleClickBounds={tlstate.onDoubleClickBounds}
-            onRightPointBounds={tlstate.onRightPointBounds}
-            onDragBounds={tlstate.onDragBounds}
-            onHoverBounds={tlstate.onHoverBounds}
-            onUnhoverBounds={tlstate.onUnhoverBounds}
-            onReleaseBounds={tlstate.onReleaseBounds}
-            onPointBoundsHandle={tlstate.onPointBoundsHandle}
-            onDoubleClickBoundsHandle={tlstate.onDoubleClickBoundsHandle}
-            onRightPointBoundsHandle={tlstate.onRightPointBoundsHandle}
-            onDragBoundsHandle={tlstate.onDragBoundsHandle}
-            onHoverBoundsHandle={tlstate.onHoverBoundsHandle}
-            onUnhoverBoundsHandle={tlstate.onUnhoverBoundsHandle}
-            onReleaseBoundsHandle={tlstate.onReleaseBoundsHandle}
-            onPointHandle={tlstate.onPointHandle}
-            onDoubleClickHandle={tlstate.onDoubleClickHandle}
-            onRightPointHandle={tlstate.onRightPointHandle}
-            onDragHandle={tlstate.onDragHandle}
-            onHoverHandle={tlstate.onHoverHandle}
-            onUnhoverHandle={tlstate.onUnhoverHandle}
-            onReleaseHandle={tlstate.onReleaseHandle}
-            onError={tlstate.onError}
-            onRenderCountChange={tlstate.onRenderCountChange}
-            onShapeChange={tlstate.onShapeChange}
-            onShapeBlur={tlstate.onShapeBlur}
-            onShapeClone={tlstate.onShapeClone}
-            onBoundsChange={tlstate.updateBounds}
-            onKeyDown={tlstate.onKeyDown}
-            onKeyUp={tlstate.onKeyUp}
-          />
-        </ContextMenu>
-        <div className={ui()}>
+        {/* <ContextMenu> */}
+        <Renderer
+          id={id}
+          containerRef={rWrapper}
+          page={page}
+          pageState={pageState}
+          users={users}
+          userId={tlstate.state.room?.userId}
+          shapeUtils={tldrawShapeUtils}
+          theme={theme}
+          meta={meta}
+          hideBounds={hideBounds}
+          hideHandles={hideHandles}
+          hideIndicators={hideIndicators}
+          onPinchStart={tlstate.onPinchStart}
+          onPinchEnd={tlstate.onPinchEnd}
+          onPinch={tlstate.onPinch}
+          onPan={tlstate.onPan}
+          onZoom={tlstate.onZoom}
+          onPointerDown={tlstate.onPointerDown}
+          onPointerMove={tlstate.onPointerMove}
+          onPointerUp={tlstate.onPointerUp}
+          onPointCanvas={tlstate.onPointCanvas}
+          onDoubleClickCanvas={tlstate.onDoubleClickCanvas}
+          onRightPointCanvas={tlstate.onRightPointCanvas}
+          onDragCanvas={tlstate.onDragCanvas}
+          onReleaseCanvas={tlstate.onReleaseCanvas}
+          onPointShape={tlstate.onPointShape}
+          onDoubleClickShape={tlstate.onDoubleClickShape}
+          onRightPointShape={tlstate.onRightPointShape}
+          onDragShape={tlstate.onDragShape}
+          onHoverShape={tlstate.onHoverShape}
+          onUnhoverShape={tlstate.onUnhoverShape}
+          onReleaseShape={tlstate.onReleaseShape}
+          onPointBounds={tlstate.onPointBounds}
+          onDoubleClickBounds={tlstate.onDoubleClickBounds}
+          onRightPointBounds={tlstate.onRightPointBounds}
+          onDragBounds={tlstate.onDragBounds}
+          onHoverBounds={tlstate.onHoverBounds}
+          onUnhoverBounds={tlstate.onUnhoverBounds}
+          onReleaseBounds={tlstate.onReleaseBounds}
+          onPointBoundsHandle={tlstate.onPointBoundsHandle}
+          onDoubleClickBoundsHandle={tlstate.onDoubleClickBoundsHandle}
+          onRightPointBoundsHandle={tlstate.onRightPointBoundsHandle}
+          onDragBoundsHandle={tlstate.onDragBoundsHandle}
+          onHoverBoundsHandle={tlstate.onHoverBoundsHandle}
+          onUnhoverBoundsHandle={tlstate.onUnhoverBoundsHandle}
+          onReleaseBoundsHandle={tlstate.onReleaseBoundsHandle}
+          onPointHandle={tlstate.onPointHandle}
+          onDoubleClickHandle={tlstate.onDoubleClickHandle}
+          onRightPointHandle={tlstate.onRightPointHandle}
+          onDragHandle={tlstate.onDragHandle}
+          onHoverHandle={tlstate.onHoverHandle}
+          onUnhoverHandle={tlstate.onUnhoverHandle}
+          onReleaseHandle={tlstate.onReleaseHandle}
+          onError={tlstate.onError}
+          onRenderCountChange={tlstate.onRenderCountChange}
+          onShapeChange={tlstate.onShapeChange}
+          onShapeBlur={tlstate.onShapeBlur}
+          onShapeClone={tlstate.onShapeClone}
+          onBoundsChange={tlstate.updateBounds}
+          onKeyDown={tlstate.onKeyDown}
+          onKeyUp={tlstate.onKeyUp}
+        />
+        {/* </ContextMenu> */}
+        {/* <div className={ui()}>
           {isFocusMode ? (
             <div className={unfocusButton()}>
               <button className={iconButton({ bp: breakpoints })} onClick={tlstate.toggleFocusMode}>
@@ -298,7 +297,7 @@ function InnerTldraw({
               <ToolsPanel />
             </>
           )}
-        </div>
+        </div> */}
       </div>
     </div>
   )
