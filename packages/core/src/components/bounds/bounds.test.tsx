af3eae8651694a7a79f40e642e7dab7f366138b3
Steve Ruiz <steveruizok@gmail.com>
2021-10-19 14:09:53 +0100

    Update bounds.test.tsx

--- packages/core/src/components/bounds/bounds.test.tsx
+++ packages/core/src/components/bounds/bounds.test.tsx
@@ -1,10 +1,10 @@
-import { render } from '@testing-library/react'
+import { renderWithContext } from '+test'
 import * as React from 'react'
 import { Bounds } from './bounds'
 
 describe('bounds', () => {
   test('mounts component without crashing', () => {
-    render(
+    renderWithContext(
       <Bounds
         zoom={1}
         bounds={{ minX: 0, minY: 0, maxX: 100, maxY: 100, width: 100, height: 100 }}
