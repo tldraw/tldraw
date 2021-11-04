abb200439ac2de77c67b1d73febf97713de0f604
Steve Ruiz <steveruizok@gmail.com>
2021-11-04 14:30:01 +0000

    rename dev to example

--- dev/README.md
+++
@@ -1,6 +0,0 @@
-# @tldraw/dev
-
-A very fast dev server.
-
-You probably do not need to start the server here: it is started as
-part of `yarn start` in the root directory.
--- packages/tldraw/src/components/TopPanel/PageOptionsDialog.tsx
+++ packages/tldraw/src/components/TopPanel/PageOptionsDialog.tsx
@@ -128,6 +128,8 @@ export const StyledDialogOverlay = styled(Dialog.Overlay, {
   right: 0,
   bottom: 0,
   left: 0,
+  width: '100%',
+  height: '100%',
 })
 
 function DialogAction({ onSelect, ...rest }: RowButtonProps) {
