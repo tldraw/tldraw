58b7038600f86668a3f4aa55b642c1b7f7855a95
Steve Ruiz <steveruizok@gmail.com>
2021-11-09 08:53:01 +0000

    Remove logic around saving pageState

--- vscode/editor/src/utils/sanitizeDocument.ts
+++ vscode/editor/src/utils/sanitizeDocument.ts
@@ -1,14 +1,17 @@
 import type { TLDrawDocument } from '@tldraw/tldraw'
 
 export function sanitizeDocument(prev: TLDrawDocument, next: TLDrawDocument): TLDrawDocument {
-  const final = { ...next, pageStates: { ...next.pageStates } }
+  return next
 
-  Object.values(prev.pageStates).forEach((pageState) => {
-    // Ensure that the previous page state is preserved, if possible
-    if (next.pages[pageState.id] !== undefined) {
-      final.pageStates[pageState.id] = pageState
-    }
-  })
+  // // Don't save changes to page state?
 
-  return final
+  // const final = { ...next, pageStates: { ...next.pageStates } }
+
+  // Object.values(prev.pageStates).forEach((pageState) => {
+  //   if (next.pages[pageState.id] !== undefined) {
+  //     final.pageStates[pageState.id] = pageState
+  //   }
+  // })
+
+  // return final
 }
