fecc6b99c95e2128a081cc4fbaed9fb02d7cf8e7
Steve Ruiz <steveruizok@gmail.com>
2021-11-16 16:17:33 +0000

    add social image

--- www/pages/_app.tsx
+++ www/pages/_app.tsx
@@ -5,16 +5,50 @@ import useGtag from '-utils/useGtag'
 
 init()
 
+const APP_NAME = 'Tldraw'
+const APP_DESCRIPTION = 'A tiny little drawing app.'
+const APP_URL = 'https://tldraw.com'
+const IMAGE = 'https://tldraw.com/social-image.png'
+
 function MyApp({ Component, pageProps }) {
   useGtag()
 
   return (
     <>
       <Head>
+        <meta name="application-name" content={APP_NAME} />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+        <meta name="apple-mobile-web-app-title" content={APP_NAME} />
+        <meta name="description" content={APP_DESCRIPTION} />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="theme-color" content="#fafafa" />
+
+        <meta name="twitter:url" content={APP_URL} />
+        <meta name="twitter:title" content={APP_NAME} />
+        <meta name="twitter:description" content={APP_DESCRIPTION} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:creator" content="@tldraw" />
+        <meta name="twitter:site" content="@tldraw" />
+        <meta name="twitter:image" content={IMAGE} />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={APP_NAME} />
+        <meta property="og:description" content={APP_DESCRIPTION} />
+        <meta property="og:site_name" content={APP_NAME} />
+        <meta property="og:url" content={APP_URL} />
+        <meta property="og:image" content={IMAGE} />
+
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no"
         />
+
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png" />
+
         <title>Tldraw</title>
       </Head>
       <Component {...pageProps} />
--- www/pages/_document.tsx
+++ www/pages/_document.tsx
@@ -2,10 +2,6 @@ import NextDocument, { Html, Head, Main, NextScript, DocumentContext } from 'nex
 import { getCssText } from '../styles'
 import { GA_TRACKING_ID } from '../utils/gtag'
 
-const APP_NAME = 'Tldraw'
-const APP_DESCRIPTION = 'A tiny little drawing app.'
-const APP_URL = 'https://tldraw.com'
-
 class MyDocument extends NextDocument {
   static async getInitialProps(ctx: DocumentContext): Promise<{
     styles: JSX.Element
@@ -35,31 +31,6 @@ class MyDocument extends NextDocument {
     return (
       <Html lang="en">
         <Head>
-          <meta name="application-name" content={APP_NAME} />
-          <meta name="apple-mobile-web-app-capable" content="yes" />
-          <meta name="apple-mobile-web-app-status-bar-style" content="black" />
-          <meta name="apple-mobile-web-app-title" content={APP_NAME} />
-          <meta name="description" content={APP_DESCRIPTION} />
-          <meta name="format-detection" content="telephone=no" />
-          <meta name="mobile-web-app-capable" content="yes" />
-          <meta name="theme-color" content="#fafafa" />
-
-          <meta name="twitter:card" content="summary" />
-          <meta name="twitter:url" content={APP_URL} />
-          <meta name="twitter:title" content={APP_NAME} />
-          <meta name="twitter:description" content={APP_DESCRIPTION} />
-          <meta name="twitter:creator" content="@steveruizok" />
-
-          <meta property="og:type" content="website" />
-          <meta property="og:title" content={APP_NAME} />
-          <meta property="og:description" content={APP_DESCRIPTION} />
-          <meta property="og:site_name" content={APP_NAME} />
-          <meta property="og:url" content={APP_URL} />
-
-          <link rel="manifest" href="/manifest.json" />
-          <link rel="shortcut icon" href="/favicon.ico" />
-          <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png" />
-
           <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
           <script
             dangerouslySetInnerHTML={{
