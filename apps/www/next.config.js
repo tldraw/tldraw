8426a649794d42869bf57235b3cdbd3345359c77
Steve Ruiz <steveruizok@gmail.com>
2021-11-18 16:13:27 +0000

    switches to sentry's next.js library

--- apps/www/next.config.js
+++ apps/www/next.config.js
@@ -1,75 +1,30 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const withPWA = require('next-pwa')
-const SentryWebpackPlugin = require('@sentry/webpack-plugin')
 const withTM = require('next-transpile-modules')(['@tldraw/tldraw', '@tldraw/core'])
+const { withSentryConfig } = require('@sentry/nextjs')
+const { GITHUB_ID, GITHUB_SECRET, GITHUB_API_SECRET, NODE_ENV, GA_MEASUREMENT_ID } = process.env
 
-const {
-  GITHUB_ID,
-  GITHUB_SECRET,
-  GITHUB_API_SECRET,
-  NEXT_PUBLIC_SENTRY_DSN: SENTRY_DSN,
-  SENTRY_ORG,
-  SENTRY_PROJECT,
-  SENTRY_AUTH_TOKEN,
-  NODE_ENV,
-  VERCEL_GIT_COMMIT_SHA,
-  GA_MEASUREMENT_ID,
-} = process.env
+const moduleExports = {
+  reactStrictMode: true,
+  pwa: {
+    disable: NODE_ENV !== 'production',
+    dest: 'public',
+  },
+  env: {
+    GA_MEASUREMENT_ID,
+    GITHUB_ID,
+    GITHUB_SECRET,
+    GITHUB_API_SECRET,
+  },
+}
 
-process.env.SENTRY_DSN = SENTRY_DSN
+const sentryWebpackPluginOptions = {
+  silent: true,
+}
 
-const isProduction = NODE_ENV === 'production'
-
-const basePath = ''
-
-module.exports = withPWA(
-  withTM({
-    reactStrictMode: true,
-    pwa: {
-      disable: !isProduction,
-      dest: 'public',
-    },
-    productionBrowserSourceMaps: true,
-    env: {
-      NEXT_PUBLIC_COMMIT_SHA: VERCEL_GIT_COMMIT_SHA,
-      GA_MEASUREMENT_ID,
-      GITHUB_ID,
-      GITHUB_SECRET,
-      GITHUB_API_SECRET,
-    },
-    webpack: (config, options) => {
-      if (!options.isServer) {
-        config.resolve.alias['@sentry/node'] = '@sentry/browser'
-      }
-
-      config.plugins.push(
-        new options.webpack.DefinePlugin({
-          'process.env.NEXT_IS_SERVER': JSON.stringify(options.isServer.toString()),
-        })
-      )
-
-      if (
-        SENTRY_DSN &&
-        SENTRY_ORG &&
-        SENTRY_PROJECT &&
-        SENTRY_AUTH_TOKEN &&
-        VERCEL_GIT_COMMIT_SHA &&
-        isProduction
-      ) {
-        config.plugins.push(
-          new SentryWebpackPlugin({
-            include: '.next',
-            ignore: ['node_modules'],
-            stripPrefix: ['webpack://_N_E/'],
-            urlPrefix: `~${basePath}/_next`,
-            release: VERCEL_GIT_COMMIT_SHA,
-            authToken: SENTRY_AUTH_TOKEN,
-            org: SENTRY_PROJECT,
-            project: SENTRY_ORG,
-          })
-        )
-      }
-      return config
-    },
-  })
+// Make sure adding Sentry options is the last code to run before exporting, to
+// ensure that your source maps include changes from all other Webpack plugins
+module.exports = module.exports = withSentryConfig(
+  withPWA(withTM(moduleExports)),
+  sentryWebpackPluginOptions
 )
