839b389f410a7d2e6f0653f9d13e2c26a4a55058
Steve Ruiz <steveruizok@gmail.com>
2021-11-22 16:27:19 +0000

    Update SelectTool.ts

--- packages/tldraw/src/state/tools/SelectTool/SelectTool.ts
+++ packages/tldraw/src/state/tools/SelectTool/SelectTool.ts
@@ -551,7 +551,6 @@ export class SelectTool extends BaseTool<Status> {
 
   onRightPointShape: TLPointerEventHandler = (info) => {
     if (!this.app.isSelected(info.target)) {
-      console.log(info.target)
       this.app.select(info.target)
     }
   }
