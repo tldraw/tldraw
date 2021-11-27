2d93f84a87f0c709e55fb2766519bfde03f8e854
Steve Ruiz <steveruizok@gmail.com>
2021-11-27 20:43:29 +0000

    Drop mobx as a dependency for core

--- packages/core/src/hooks/useCameraCss.tsx
+++ packages/core/src/hooks/useCameraCss.tsx
@@ -1,5 +1,4 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
-import { autorun } from 'mobx'
 import * as React from 'react'
 import type { TLPageState } from '../types'
 
@@ -13,36 +12,34 @@ export function useCameraCss(
   const rPoint = React.useRef<number[]>()
 
   React.useLayoutEffect(() => {
-    return autorun(() => {
-      const { zoom, point } = pageState.camera
+    const { zoom, point } = pageState.camera
 
-      const didZoom = zoom !== rZoom.current
-      const didPan = point !== rPoint.current
+    const didZoom = zoom !== rZoom.current
+    const didPan = point !== rPoint.current
 
-      rZoom.current = zoom
-      rPoint.current = point
+    rZoom.current = zoom
+    rPoint.current = point
 
-      if (didZoom || didPan) {
-        const layer = layerRef.current
-        if (containerRef && 'current' in containerRef) {
-          const container = containerRef.current
+    if (didZoom || didPan) {
+      const layer = layerRef.current
+      if (containerRef && 'current' in containerRef) {
+        const container = containerRef.current
 
-          // If we zoomed, set the CSS variable for the zoom
-          if (didZoom) {
-            if (container) {
-              container.style.setProperty('--tl-zoom', zoom.toString())
-            }
-          }
-
-          // Either way, position the layer
-          if (layer) {
-            layer.style.setProperty(
-              'transform',
-              `scale(${zoom}) translateX(${point[0]}px) translateY(${point[1]}px)`
-            )
+        // If we zoomed, set the CSS variable for the zoom
+        if (didZoom) {
+          if (container) {
+            container.style.setProperty('--tl-zoom', zoom.toString())
           }
         }
+
+        // Either way, position the layer
+        if (layer) {
+          layer.style.setProperty(
+            'transform',
+            `scale(${zoom}) translateX(${point[0]}px) translateY(${point[1]}px)`
+          )
+        }
       }
-    })
-  }, [pageState])
+    }
+  }, [pageState.camera.zoom, pageState.camera.point])
 }
