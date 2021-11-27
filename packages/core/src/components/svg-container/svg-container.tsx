c66527cffbe4d54facbb2be5a3c5caa815a6d87b
Noah Shipley <nshipley@centricsoftware.com>
2021-11-26 23:24:27 -0600

    Support className property on SVGContainer

--- packages/core/src/components/svg-container/svg-container.tsx
+++ packages/core/src/components/svg-container/svg-container.tsx
@@ -3,14 +3,15 @@ import * as React from 'react'
 
 interface SvgContainerProps extends React.SVGProps<SVGSVGElement> {
   children: React.ReactNode
+  className?: string
 }
 
 export const SVGContainer = React.forwardRef<SVGSVGElement, SvgContainerProps>(
-  function SVGContainer({ id, children, ...rest }, ref) {
+  function SVGContainer({ id, className, children, ...rest }, ref) {
     return (
       <Observer>
         {() => (
-          <svg ref={ref} className="tl-positioned-svg" {...rest}>
+          <svg ref={ref} className={`tl-positioned-svg ${className}`} {...rest}>
             <g id={id} className="tl-centered-g">
               {children}
             </g>
