a29745f5c6b367ab6920d4a8375bb452f6c286ac
Steve Ruiz <steveruizok@gmail.com>
2021-12-21 23:59:04 +0000

    Update useCursorAnimation.ts

--- packages/core/src/hooks/useCursorAnimation.ts
+++ packages/core/src/hooks/useCursorAnimation.ts
@@ -56,7 +56,9 @@ export function useCursorAnimation(ref: any, point: number[]) {
     const now = performance.now()
     const spline = rSpline.current
     if (rState.current === 'stopped') {
+      console.log('yep!')
       rTimestamp.current = now
+      rPrevPoint.current = point
       spline.clear()
     }
     spline.addPoint(point)
@@ -67,7 +69,7 @@ export function useCursorAnimation(ref: any, point: number[]) {
       from: rPrevPoint.current,
       to: point,
       timeStamp: now,
-      duration: Math.min(now - rTimestamp.current, 500),
+      duration: Math.min(now - rTimestamp.current, 300),
     }
     rPrevPoint.current = point
     rTimestamp.current = now
