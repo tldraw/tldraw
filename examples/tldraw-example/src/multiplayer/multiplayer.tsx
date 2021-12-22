5d0093fd8288b91a3dcf2801173bbce3299caf0f
Faraz Shaikh <farazzshaikh@gmail.com>
2021-12-22 23:22:14 +0400

    Added firebase storage to multiplayer for media

--- examples/tldraw-example/src/multiplayer/multiplayer.tsx
+++ examples/tldraw-example/src/multiplayer/multiplayer.tsx
@@ -1,11 +1,17 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 import * as React from 'react'
-import { Tldraw } from '@tldraw/tldraw'
+import { TDShape, Tldraw } from '@tldraw/tldraw'
 import { createClient } from '@liveblocks/client'
 import { LiveblocksProvider, RoomProvider } from '@liveblocks/react'
 import { useMultiplayerState } from './useMultiplayerState'
+import { initializeApp } from 'firebase/app'
+import firebaseConfig from '../firebase.config'
+import { useEffect } from 'react'
+import { useMemo } from 'react'
+import { getStorage, ref, uploadBytes, getDownloadURL, deleteObject } from 'firebase/storage'
 
 const client = createClient({
+  // @ts-ignore
   publicApiKey: process.env.LIVEBLOCKS_PUBLIC_API_KEY || '',
   throttle: 100,
 })
@@ -24,12 +30,28 @@ export function Multiplayer() {
 
 function Editor({ roomId }: { roomId: string }) {
   const { error, ...events } = useMultiplayerState(roomId)
+  const app = useMemo(() => initializeApp(firebaseConfig), [firebaseConfig])
+  const storage = useMemo(() => getStorage(app, firebaseConfig.storageBucket), [])
 
   if (error) return <div>Error: {error.message}</div>
 
   return (
     <div className="tldraw">
-      <Tldraw showPages={false} {...events} />
+      <Tldraw
+        showPages={false}
+        {...events}
+        onImageUpload={async (file: File, id: string) => {
+          const imageRef = ref(storage, id)
+          const snapshot = await uploadBytes(imageRef, file)
+          const url = await getDownloadURL(snapshot.ref)
+          return url
+        }}
+        onImageDelete={async (id: string) => {
+          console.log(id)
+          const imageRef = ref(storage, id)
+          await deleteObject(imageRef)
+        }}
+      />
     </div>
   )
 }
