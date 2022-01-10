cc065a72d240180114de7ada2faa8f6cdec38fc2
Steve Ruiz <steveruizok@gmail.com>
2022-01-10 12:09:28 +0000

    bump upload size to 5mb

--- apps/www/pages/api/upload.ts
+++ apps/www/pages/api/upload.ts
@@ -17,7 +17,7 @@ export default async function handler(req, res) {
     },
     Expires: 60, // seconds
     Conditions: [
-      ['content-length-range', 0, 1048576], // up to 1 MB
+      ['content-length-range', 0, 5242880], // up to 5 MB
     ],
   })
 
