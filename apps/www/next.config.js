de57658fe8653d7b03e77e51f938258822467239
Jason Laster <jason.laster.11@gmail.com>
2022-01-24 17:21:30 -0800

    Add sourcemap-loader to build step

--- apps/www/next.config.js
+++ apps/www/next.config.js
@@ -46,6 +46,12 @@ module.exports = withPWA(
         })
       )
 
+      config.module.rules.push({
+        test: /\.js$/,
+        use: ['source-map-loader'],
+        enforce: 'pre',
+      })
+
       if (
         SENTRY_DSN &&
         SENTRY_ORG &&
