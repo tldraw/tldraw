1ba2399f93aca440a08323af6372235c0eda3679
sb <sb@example.com>
2022-02-07 02:45:12 +0000

    [feature] Transparent or white background on export

--- apps/www/pages/api/export.ts
+++ apps/www/pages/api/export.ts
@@ -74,6 +74,10 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         app.selectAll()
         app.zoomToSelection()
         app.selectNone()
+        const tlContainer = document.getElementsByClassName('tl-container').item(0) as HTMLElement;
+        if (tlContainer) {
+          tlContainer.style.background = 'transparent';
+        }
       } catch (e) {
         err = e.message
       }
@@ -83,6 +87,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     }
     const imageBuffer = await page.screenshot({
       type,
+      omitBackground: true
     })
     await browser.close()
     res.status(200).send(imageBuffer)
