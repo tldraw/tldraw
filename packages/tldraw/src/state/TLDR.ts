76160b202703e308840ec66d4b98c3920f9b7a9e
Steve Ruiz <steveruizok@gmail.com>
2022-02-13 08:56:52 +0000

    Update TLDR.ts

--- packages/tldraw/src/state/TLDR.ts
+++ packages/tldraw/src/state/TLDR.ts
@@ -241,51 +241,6 @@ export class TLDR {
     return Array.from(visited.values())
   }
 
-  // static updateBindings(
-  //   data: TDSnapshot,
-  //   id: string,
-  //   beforeShapes: Record<string, Partial<TDShape>> = {},
-  //   afterShapes: Record<string, Partial<TDShape>> = {},
-  //   pageId: string
-  // ): TDSnapshot {
-  //   const page = { ...TLDR.getPage(data, pageId) }
-  //   return Object.values(page.bindings)
-  //     .filter((binding) => binding.fromId === id || binding.toId === id)
-  //     .reduce((cTDSnapshot, binding) => {
-  //       let oppositeShape: TDShape | undefined = undefined
-
-  //       if (!beforeShapes[binding.fromId]) {
-  //         const arrowShape = TLDR.getShape<ArrowShape>(cTDSnapshot, binding.fromId, pageId)
-  //         beforeShapes[binding.fromId] = Utils.deepClone(arrowShape)
-  //         const oppositeHandle = arrowShape.handles[binding.handleId === 'start' ? 'end' : 'start']
-  //         if (oppositeHandle.bindingId) {
-  //           const oppositeBinding = page.bindings[oppositeHandle.bindingId]
-  //           oppositeShape = TLDR.getShape(data, oppositeBinding.toId, data.appState.currentPageId)
-  //         }
-  //       }
-
-  //       if (!beforeShapes[binding.toId]) {
-  //         beforeShapes[binding.toId] = Utils.deepClone(
-  //           TLDR.getShape(cTDSnapshot, binding.toId, pageId)
-  //         )
-  //       }
-
-  //       // Hmm
-
-  //       // updateArrowBindings ?
-
-  //       afterShapes[binding.fromId] = Utils.deepClone(
-  //         TLDR.getShape(cTDSnapshot, binding.fromId, pageId)
-  //       )
-
-  //       afterShapes[binding.toId] = Utils.deepClone(
-  //         TLDR.getShape(cTDSnapshot, binding.toId, pageId)
-  //       )
-
-  //       return cTDSnapshot
-  //     }, data)
-  // }
-
   static getLinkedShapeIds(
     data: TDSnapshot,
     pageId: string,
