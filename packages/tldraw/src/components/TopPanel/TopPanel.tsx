d81bcc1055d4c947de516b45eebad34bd0ac51cd
Steve Ruiz <steveruizok@gmail.com>
2022-04-16 12:22:24 +0100

    [improvement] add undo redo at all screen sizes (#643)
    * Adds undo redo at all viewport sizes
    
    * Update TopPanel.tsx

--- packages/tldraw/src/components/TopPanel/TopPanel.tsx
+++ packages/tldraw/src/components/TopPanel/TopPanel.tsx
@@ -45,14 +45,12 @@ export function TopPanel({
       {(showStyles || showZoom) && (
         <Panel side="right">
           {showStyles && !readOnly && <StyleMenu />}
-          <MobileOnly bp={breakpoints}>
-            <ToolButton>
-              <UndoIcon onClick={app.undo} />
-            </ToolButton>
-            <ToolButton>
-              <RedoIcon onClick={app.redo} />
-            </ToolButton>
-          </MobileOnly>
+          <ToolButton>
+            <UndoIcon onClick={app.undo} />
+          </ToolButton>
+          <ToolButton>
+            <RedoIcon onClick={app.redo} />
+          </ToolButton>
           {showZoom && <ZoomMenu />}
         </Panel>
       )}
@@ -77,19 +75,4 @@ const StyledTopPanel = styled('div', {
 const StyledSpacer = styled('div', {
   flexGrow: 2,
   pointerEvents: 'none',
-})
-
-const MobileOnly = styled('div', {
-  display: 'flex',
-  flexDirection: 'row',
-  variants: {
-    bp: {
-      small: {
-        display: 'inherit',
-      },
-      large: {
-        display: 'none',
-      },
-    },
-  },
 })
