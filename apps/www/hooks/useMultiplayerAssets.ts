d30a2e4fcd43bbfd82905bd3d0a4b9df1febc60f
Steve Ruiz <steveruizok@gmail.com>
2022-05-11 09:35:15 +0100

    Fix onAssetCreate and onAssetDelete APIs

--- apps/www/hooks/useMultiplayerAssets.ts
+++ apps/www/hooks/useMultiplayerAssets.ts
@@ -1,27 +1,38 @@
+import { TldrawApp } from '@tldraw/tldraw'
 import { useCallback } from 'react'
 
 export function useMultiplayerAssets() {
-  const onAssetCreate = useCallback(async (file: File, id: string): Promise<string | false> => {
-    const filename = encodeURIComponent(file.name)
-    const fileType = encodeURIComponent(file.type)
-    const res = await fetch(`/api/upload?file=${filename}&fileType=${fileType}`)
-    const { url, fields } = await res.json()
-    const formData = new FormData()
-    Object.entries({ ...fields, file }).forEach(([key, value]) => {
-      formData.append(key, value as any)
-    })
-    const upload = await fetch(url, {
-      method: 'POST',
-      body: formData,
-    })
-    if (upload.ok) {
-      return url + '/' + filename
-    } else {
-      return false
-    }
-  }, [])
+  const onAssetCreate = useCallback(
+    // Send the asset to our upload enpoint, which in turn will send it to AWS and
+    // respond with the URL of the uploaded file.
+    async (app: TldrawApp, file: File, id: string): Promise<string | false> => {
+      const filename = encodeURIComponent(file.name)
 
-  const onAssetDelete = useCallback(async (id: string): Promise<boolean> => {
+      const fileType = encodeURIComponent(file.type)
+
+      const res = await fetch(`/api/upload?file=${filename}&fileType=${fileType}`)
+
+      const { url, fields } = await res.json()
+
+      const formData = new FormData()
+
+      Object.entries({ ...fields, file }).forEach(([key, value]) => {
+        formData.append(key, value as any)
+      })
+
+      const upload = await fetch(url, {
+        method: 'POST',
+        body: formData,
+      })
+
+      if (!upload.ok) return false
+
+      return url + '/' + filename
+    },
+    []
+  )
+
+  const onAssetDelete = useCallback(async (app: TldrawApp, id: string): Promise<boolean> => {
     // noop
     return true
   }, [])
