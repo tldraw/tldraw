e32815eaec70ee4f73843e5b2fd68cbe8cc6ab20
Steve Ruiz <steveruizok@gmail.com>
2022-05-15 10:40:22 +0100

    Fix scaled text (#687)

--- packages/tldraw/src/state/shapes/shared/getTextSvgElement.ts
+++ packages/tldraw/src/state/shapes/shared/getTextSvgElement.ts
@@ -7,17 +7,21 @@ import { LINE_HEIGHT } from '~constants'
 export function getTextSvgElement(text: string, style: ShapeStyles, bounds: TLBounds) {
   const fontSize = getFontSize(style.size, style.font)
   const g = document.createElementNS('http://www.w3.org/2000/svg', 'g')
+  const scale = style.scale ?? 1
 
   const textLines = text.split('\n').map((line, i) => {
     const textElm = document.createElementNS('http://www.w3.org/2000/svg', 'text')
     textElm.textContent = line
-    textElm.setAttribute('y', LINE_HEIGHT * fontSize * (0.5 + i) + '')
+    textElm.setAttribute('y', fontSize * (0.5 + i * LINE_HEIGHT) + '')
     textElm.setAttribute('letter-spacing', fontSize * -0.03 + '')
     textElm.setAttribute('font-size', fontSize + 'px')
     textElm.setAttribute('font-family', getFontFace(style.font).slice(1, -1))
     textElm.setAttribute('text-align', getTextAlign(style.textAlign))
     textElm.setAttribute('text-align', getTextAlign(style.textAlign))
     textElm.setAttribute('alignment-baseline', 'central')
+    if (style.scale !== 1) {
+      textElm.setAttribute('transform', `scale(${style.scale})`)
+    }
     g.appendChild(textElm)
 
     return textElm
@@ -28,14 +32,14 @@ export function getTextSvgElement(text: string, style: ShapeStyles, bounds: TLBo
       g.setAttribute('text-align', 'center')
       g.setAttribute('text-anchor', 'middle')
       textLines.forEach((textElm) => {
-        textElm.setAttribute('x', bounds.width / 2 + '')
+        textElm.setAttribute('x', bounds.width / 2 / scale + '')
       })
       break
     }
     case AlignStyle.End: {
       g.setAttribute('text-align', 'right')
       g.setAttribute('text-anchor', 'end')
-      textLines.forEach((textElm) => textElm.setAttribute('x', bounds.width + ''))
+      textLines.forEach((textElm) => textElm.setAttribute('x', bounds.width / scale + ''))
       break
     }
     case AlignStyle.Start: {
