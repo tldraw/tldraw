2ff9eca9a2d954bda3f2d0c99fe9b3d973c43fa7
Steve Ruiz <steveruizok@gmail.com>
2022-05-18 13:00:07 +0100

    Update menu

--- packages/tldraw/src/components/TopPanel/TopPanel.tsx
+++ packages/tldraw/src/components/TopPanel/TopPanel.tsx
@@ -18,7 +18,7 @@ interface TopPanelProps {
   showStyles: boolean
   showZoom: boolean
   showMultiplayerMenu: boolean
-  showSponsorLink: boolean
+  sponsor?: boolean
 }
 
 export function TopPanel({
@@ -27,7 +27,7 @@ export function TopPanel({
   showMenu,
   showStyles,
   showZoom,
-  showSponsorLink,
+  sponsor,
   showMultiplayerMenu,
 }: TopPanelProps) {
   const app = useTldrawApp()
@@ -36,7 +36,7 @@ export function TopPanel({
     <StyledTopPanel>
       {(showMenu || showPages) && (
         <Panel side="left" id="TD-MenuPanel">
-          {showMenu && <Menu showSponsorLink={showSponsorLink} readOnly={readOnly} />}
+          {showMenu && <Menu sponsor={sponsor} readOnly={readOnly} />}
           {showMultiplayerMenu && <MultiplayerMenu />}
           {showPages && <PageMenu />}
         </Panel>
