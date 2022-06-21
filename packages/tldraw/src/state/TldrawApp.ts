278860a784e6dbcae4be92c52596238383443022
Jon Noronha <jon@gamma.app>
2022-06-20 20:25:39 -0700

    export vec
    version bump
    
    remove curve

--- packages/tldraw/src/state/TldrawApp.ts
+++ packages/tldraw/src/state/TldrawApp.ts
@@ -2578,7 +2578,7 @@ export class TldrawApp extends StateManager<TDSnapshot> {
   /**
    * Zoom to the selected shapes.
    */
-  zoomToSelection = (paddingPx: number = FIT_TO_SCREEN_PADDING): this => {
+  zoomToSelection = (): this => {
     if (this.selectedIds.length === 0) return this
 
     const { rendererBounds } = this
@@ -2586,8 +2586,8 @@ export class TldrawApp extends StateManager<TDSnapshot> {
 
     let zoom = TLDR.getCameraZoom(
       Math.min(
-        (rendererBounds.width - paddingPx) / selectedBounds.width,
-        (rendererBounds.height - paddingPx) / selectedBounds.height
+        (rendererBounds.width - FIT_TO_SCREEN_PADDING) / selectedBounds.width,
+        (rendererBounds.height - FIT_TO_SCREEN_PADDING) / selectedBounds.height
       )
     )
 
