cc19f5a63a5cb214af6e66c9b1963f3e4d336363
Jon Noronha <jon@gamma.app>
2022-06-22 11:47:43 -0700

    shapesShared

--- packages/tldraw/src/index.ts
+++ packages/tldraw/src/index.ts
@@ -1,7 +1,6 @@
 export * from './Tldraw'
 export * from './types'
 export * from './state/shapes'
-export * as shapeStyleHelpers from './state/shapes/shared/shape-styles'
 export * from './state/TLDR'
 export { TldrawApp } from './state'
 export { useFileSystem } from './hooks'
