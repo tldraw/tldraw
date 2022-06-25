c1f94a0928540a9abaa34a310582986c40949557
Ali Alhaidary <75235623+ali-alhaidary@users.noreply.github.com>
2022-06-25 12:47:32 +0300

    Adding Arabic Language (#732)
    * Create ar.json
    
    * Update Tldraw.tsx
    
    * Update types.ts
    
    * Update LanguageMenu.tsx
    
    * Update Tldraw.tsx
    
    * Update LanguageMenu.tsx

--- packages/tldraw/src/components/TopPanel/LanguageMenu/LanguageMenu.tsx
+++ packages/tldraw/src/components/TopPanel/LanguageMenu/LanguageMenu.tsx
@@ -17,6 +17,7 @@ export function LanguageMenu() {
   const intl = useIntl()
 
   const languages: ILang[] = [
+    { label: 'عربي', code: 'ar' },
     { label: 'English', code: 'en' },
     { label: 'Français', code: 'fr' },
     { label: 'Italiano', code: 'it' },
