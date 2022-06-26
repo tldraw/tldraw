1b7cea41a04f640c07f5c33327bdd7e50ab63c21
cog25 <74242561+cog25@users.noreply.github.com>
2022-06-26 15:34:18 +0900

    feat: add korean translation (#752)
    * feat: add korean translation
    
    * chore: fix typo

--- packages/tldraw/src/translations/translations.ts
+++ packages/tldraw/src/translations/translations.ts
@@ -2,6 +2,7 @@ import ar from './ar.json'
 import en from './en.json'
 import fr from './fr.json'
 import it from './it.json'
+import ko_kr from './ko-kr.json'
 import zh_cn from './zh-cn.json'
 
 export const TRANSLATIONS = [
@@ -9,6 +10,7 @@ export const TRANSLATIONS = [
   { code: 'ar', label: 'عربي', messages: ar },
   { code: 'fr', label: 'Français', messages: fr },
   { code: 'it', label: 'Italiano', messages: it },
+  { code: 'ko-kr', label: '한국어', message: ko_kr },
   { code: 'zh-cn', label: 'Chinese - Simplified', messages: zh_cn },
 ] as const
 
