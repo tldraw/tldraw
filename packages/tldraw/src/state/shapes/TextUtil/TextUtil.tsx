0d0e4313a427e68b20a142aedbf43d1b65b3a06e
Jon Noronha <jon@gamma.app>
2022-07-03 15:08:26 -0700

    Add attrs for shape styles

--- packages/tldraw/src/state/shapes/TextUtil/TextUtil.tsx
+++ packages/tldraw/src/state/shapes/TextUtil/TextUtil.tsx
@@ -230,6 +230,7 @@ export class TextUtil extends TDShapeUtil<T, E> {
                 color: styles.stroke,
                 textAlign: getTextAlign(style.textAlign),
               }}
+              data-color={shape.style.color}
             >
               {isBinding && (
                 <div
@@ -391,7 +392,8 @@ export class TextUtil extends TDShapeUtil<T, E> {
     const bounds = this.getBounds(shape)
     const style = getShapeStyle(shape.style, isDarkMode)
     const elm = getTextSvgElement(shape.text, shape.style, bounds)
-    elm.setAttribute('fill', style.stroke)
+    elm.setAttribute('data-fill', style.stroke)
+    elm.setAttribute('data-color', shape.style.color)
 
     return elm
   }
