3a866569abcbd96d6969da4c01225330da5c97ea
Henrique Doro <opensource@hdoro.dev>
2022-07-09 13:53:28 -0300

    example(ui-options): starting to render custom UI

--- examples/tldraw-example/src/ui-options.tsx
+++ examples/tldraw-example/src/ui-options.tsx
@@ -1,16 +1,71 @@
+import { TDShapeType, TDToolType, Tldraw, TldrawApp } from '@tldraw/tldraw'
 import * as React from 'react'
-import { Tldraw } from '@tldraw/tldraw'
+
+function SelectButton({
+  type,
+  tldrawApp,
+  children,
+}: React.PropsWithChildren<{
+  type: TDToolType
+  tldrawApp?: TldrawApp
+}>) {
+  return (
+    <button
+      onClick={() => {
+        tldrawApp?.selectTool(type)
+      }}
+      style={{
+        border: '1px solid #333',
+        // @TODO: have the button re-render when tldrawApp.currentTool.type changes, else active states won't work
+        background: tldrawApp?.currentTool?.type === type ? 'papayawhip' : 'transparent',
+        fontSize: '1.5rem',
+        padding: '0.3em 0.8em',
+        borderRadius: '0.15em',
+      }}
+    >
+      {children}
+    </button>
+  )
+}
 
 export default function UIOptions() {
+  const [tldrawApp, setApp] = React.useState<TldrawApp>()
+
+  const handleMount = React.useCallback((app: TldrawApp) => {
+    setApp(app)
+  }, [])
+
   return (
     <div className="tldraw">
+      <div
+        style={{
+          position: 'absolute',
+          display: 'flex',
+          gap: '1em',
+          zIndex: 100,
+          bottom: '1em',
+          left: '1em',
+        }}
+      >
+        <SelectButton type="select" tldrawApp={tldrawApp}>
+          Select
+        </SelectButton>
+        <SelectButton type="erase" tldrawApp={tldrawApp}>
+          Erase
+        </SelectButton>
+        <SelectButton type={TDShapeType.Sticky} tldrawApp={tldrawApp}>
+          Card
+        </SelectButton>
+      </div>
+
       <Tldraw
+        onMount={handleMount}
         showUI={true}
-        showMenu={true}
-        showPages={true}
-        showStyles={true}
-        showTools={true}
+        showStyles={false}
+        showTools={false}
         showZoom={true}
+        showPages={false}
+        showMenu={false}
       />
     </div>
   )
