67e137c7eefda6457141158ce1426b925b56fdd7
judicaelandria <judiandriamahandry@gmail.com>
2022-07-14 16:45:54 +0300

    add grid

--- packages/tldraw/src/components/ToolsPanel/keyboardShortcutDialog.tsx
+++ packages/tldraw/src/components/ToolsPanel/keyboardShortcutDialog.tsx
@@ -2,78 +2,77 @@ import * as React from 'react'
 import * as Dialog from '@radix-ui/react-dialog'
 import { Cross2Icon } from '@radix-ui/react-icons'
 import { FormattedMessage, useIntl } from 'react-intl'
-import { DMDivider } from '~components/Primitives/DropdownMenu'
 import { IconButton } from '~components/Primitives/IconButton'
 import { RowButton } from '~components/Primitives/RowButton'
 import { styled } from '~styles'
-import { TextField } from '~components/Primitives/TextField'
 
 export function KeyboardShortcutDialog() {
   const intl = useIntl()
-  const [searchValue, setSearchValue] = React.useState('')
 
-  const shortcuts = [
-    { label: intl.formatMessage({ id: 'new.project' }), kbd: '#N' },
-    { label: intl.formatMessage({ id: 'open' }), kbd: '#O' },
-    { label: intl.formatMessage({ id: 'save' }), kbd: '#S' },
-    { label: intl.formatMessage({ id: 'save.as' }), kbd: '#⇧S' },
-    { label: intl.formatMessage({ id: 'upload.media' }), kbd: '#U' },
-    '---',
-    { label: intl.formatMessage({ id: 'undo' }), kbd: '#Z' },
-    { label: intl.formatMessage({ id: 'redo' }), kbd: '#⇧Z' },
-    { label: intl.formatMessage({ id: 'cut' }), kbd: '#X' },
-    { label: intl.formatMessage({ id: 'copy' }), kbd: '#C' },
-    { label: intl.formatMessage({ id: 'paste' }), kbd: '#V' },
-    { label: intl.formatMessage({ id: 'select.all' }), kbd: '#A' },
-    { label: intl.formatMessage({ id: 'delete' }), kbd: '⌫' },
-    '---',
-    { label: intl.formatMessage({ id: 'zoom.in' }), kbd: '#+' },
-    { label: intl.formatMessage({ id: 'zoom.out' }), kbd: '#-' },
-    { label: `${intl.formatMessage({ id: 'zoom.to' })} 100%`, kbd: '⇧+0' },
-    { label: intl.formatMessage({ id: 'zoom.to.fit' }), kbd: '⇧+1' },
-    { label: intl.formatMessage({ id: 'zoom.to.selection' }), kbd: '⇧+2' },
-    '---',
-    { label: intl.formatMessage({ id: 'preferences.dark.mode' }), kbd: '#⇧D' },
-    { label: intl.formatMessage({ id: 'preferences.focus.mode' }), kbd: '#.' },
-    { label: intl.formatMessage({ id: 'preferences.show.grid' }), kbd: '#⇧G' },
-    '---',
-    { label: intl.formatMessage({ id: 'duplicate' }), kbd: '#D' },
-    { label: intl.formatMessage({ id: 'flip.horizontal' }), kbd: '⇧H' },
-    { label: intl.formatMessage({ id: 'flip.vertical' }), kbd: '⇧V' },
-    {
-      label: `${intl.formatMessage({ id: 'lock' })} / ${intl.formatMessage({ id: 'unlock' })}`,
-      kbd: '#⇧L',
-    },
-    '---',
-    {
-      label: `${intl.formatMessage({ id: 'move' })} ${intl.formatMessage({ id: 'to.front' })}`,
-      kbd: '⇧]',
-    },
-    {
-      label: `${intl.formatMessage({ id: 'move' })} ${intl.formatMessage({ id: 'forward' })}`,
-      kbd: ']',
-    },
-    {
-      label: `${intl.formatMessage({ id: 'move' })} ${intl.formatMessage({ id: 'backward' })}`,
-      kbd: '[',
-    },
-    {
-      label: `${intl.formatMessage({ id: 'move' })} ${intl.formatMessage({ id: 'back' })}`,
-      kbd: '⇧[',
-    },
-  ]
-
-  const filteredShortcuts = React.useMemo(() => {
-    if (searchValue.length)
-      return shortcuts.filter(
-        shortcut =>
-          typeof shortcut === 'object' && shortcut.label.toLocaleLowerCase().includes(searchValue)
-      )
-    return shortcuts
-  }, [searchValue])
-
-  const handleChangeValue = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchValue(event.target.value)
+  const shortcuts = {
+    View: [
+      { label: intl.formatMessage({ id: 'zoom.in' }), kbd: '#+' },
+      { label: intl.formatMessage({ id: 'zoom.out' }), kbd: '#-' },
+      { label: `${intl.formatMessage({ id: 'zoom.to' })} 100%`, kbd: '⇧+0' },
+      { label: intl.formatMessage({ id: 'zoom.to.fit' }), kbd: '⇧+1' },
+      { label: intl.formatMessage({ id: 'zoom.to.selection' }), kbd: '⇧+2' },
+      { label: intl.formatMessage({ id: 'preferences.dark.mode' }), kbd: '#⇧D' },
+      { label: intl.formatMessage({ id: 'preferences.focus.mode' }), kbd: '#.' },
+      { label: intl.formatMessage({ id: 'preferences.show.grid' }), kbd: '#⇧G' },
+    ],
+    Edit: [
+      { label: intl.formatMessage({ id: 'undo' }), kbd: '#Z' },
+      { label: intl.formatMessage({ id: 'redo' }), kbd: '#⇧Z' },
+      { label: intl.formatMessage({ id: 'cut' }), kbd: '#X' },
+      { label: intl.formatMessage({ id: 'copy' }), kbd: '#C' },
+      { label: intl.formatMessage({ id: 'paste' }), kbd: '#V' },
+      { label: intl.formatMessage({ id: 'select.all' }), kbd: '#A' },
+      { label: intl.formatMessage({ id: 'delete' }), kbd: '⌫' },
+      { label: intl.formatMessage({ id: 'duplicate' }), kbd: '#D' },
+    ],
+    Tools: [
+      { label: intl.formatMessage({ id: 'select' }), kbd: '1' },
+      { label: intl.formatMessage({ id: 'draw' }), kbd: '2' },
+      { label: intl.formatMessage({ id: 'eraser' }), kbd: '3' },
+      { label: intl.formatMessage({ id: 'rectangle' }), kbd: '4' },
+      { label: intl.formatMessage({ id: 'ellipse' }), kbd: '5' },
+      { label: intl.formatMessage({ id: 'triangle' }), kbd: '6' },
+      { label: intl.formatMessage({ id: 'line' }), kbd: '7' },
+      { label: intl.formatMessage({ id: 'arrow' }), kbd: '8' },
+      { label: intl.formatMessage({ id: 'text' }), kbd: '9' },
+      { label: intl.formatMessage({ id: 'sticky' }), kbd: '0' },
+    ],
+    Transform: [
+      { label: intl.formatMessage({ id: 'flip.horizontal' }), kbd: '⇧H' },
+      { label: intl.formatMessage({ id: 'flip.vertical' }), kbd: '⇧V' },
+      {
+        label: `${intl.formatMessage({ id: 'lock' })} / ${intl.formatMessage({ id: 'unlock' })}`,
+        kbd: '#⇧L',
+      },
+      {
+        label: `${intl.formatMessage({ id: 'move' })} ${intl.formatMessage({ id: 'to.front' })}`,
+        kbd: '⇧]',
+      },
+      {
+        label: `${intl.formatMessage({ id: 'move' })} ${intl.formatMessage({ id: 'forward' })}`,
+        kbd: ']',
+      },
+      {
+        label: `${intl.formatMessage({ id: 'move' })} ${intl.formatMessage({ id: 'backward' })}`,
+        kbd: '[',
+      },
+      {
+        label: `${intl.formatMessage({ id: 'move' })} ${intl.formatMessage({ id: 'back' })}`,
+        kbd: '⇧[',
+      },
+    ],
+    File: [
+      { label: intl.formatMessage({ id: 'new.project' }), kbd: '#N' },
+      { label: intl.formatMessage({ id: 'open' }), kbd: '#O' },
+      { label: intl.formatMessage({ id: 'save' }), kbd: '#S' },
+      { label: intl.formatMessage({ id: 'save.as' }), kbd: '#⇧S' },
+      { label: intl.formatMessage({ id: 'upload.media' }), kbd: '#U' },
+    ],
   }
 
   return (
@@ -83,28 +82,25 @@ export function KeyboardShortcutDialog() {
           <FormattedMessage id="keyboard.shortcuts" />
         </RowButton>
       </Dialog.Trigger>
-      <DialogPortal>
+      <Dialog.Portal>
         <DialogOverlay />
         <DialogContent>
           <DialogTitle>
             <FormattedMessage id="keyboard.shortcuts" />
           </DialogTitle>
-          <DialogSearch
-            width="100%"
-            placeholder={`${intl.formatMessage({ id: 'search' })}...`}
-            value={searchValue}
-            onChange={handleChangeValue}
-          />
           <Content>
-            {filteredShortcuts.map((shortcut, i) =>
-              typeof shortcut === 'string' ? (
-                <DMDivider css={{ width: '100%' }} key={i} />
-              ) : (
-                <RowButton key={shortcut.label} kbd={shortcut.kbd} variant="wide">
-                  {shortcut.label}
-                </RowButton>
-              )
-            )}
+            {Object.entries(shortcuts).map(([key, value]) => (
+              <div>
+                <Label>{key}</Label>
+                <ContentItem>
+                  {value.map((shortcut) => (
+                    <RowButton key={shortcut.label} kbd={shortcut.kbd} variant="wide">
+                      {shortcut.label}
+                    </RowButton>
+                  ))}
+                </ContentItem>
+              </div>
+            ))}
           </Content>
           <Dialog.Close asChild>
             <DialogIconButton>
@@ -112,24 +108,35 @@ export function KeyboardShortcutDialog() {
             </DialogIconButton>
           </Dialog.Close>
         </DialogContent>
-      </DialogPortal>
+      </Dialog.Portal>
     </Dialog.Root>
   )
 }
 
-const DialogSearch = styled(TextField, {
-  marginBottom: 14,
-  background: '$hover',
-})
-
 const Content = styled('div', {
   width: '100%',
-  maxHeight: '68vh',
+  maxHeight: '74vh',
   overflowY: 'auto',
+  display: 'grid',
+  gridTemplateColumns: 'repeat(2, 1fr)',
+  gap: 20,
 })
 
-const DialogPortal = styled(Dialog.Portal, {
-  zIndex: 999999,
+const Label = styled('h3', {
+  fontSize: '$2',
+  color: '$text',
+  fontFamily: '$ui',
+})
+
+const ContentItem = styled('div', {
+  width: '100%',
+  display: 'flex',
+  flexDirection: 'column',
+  height: 'min-content',
+  gap: 4,
+  '&> button > div': {
+    paddingLeft: 0,
+  },
 })
 
 const DialogContent = styled(Dialog.Content, {
@@ -141,7 +148,7 @@ const DialogContent = styled(Dialog.Content, {
   left: '50%',
   transform: 'translate(-50%, -50%)',
   width: '90vw',
-  maxWidth: '700px',
+  maxWidth: '900px',
   minHeight: '85vh',
   maxHeight: '85vh',
   padding: 25,
