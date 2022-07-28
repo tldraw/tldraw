428623c91c7a0a65be3e10cb209ccea4ac467c0e
Steve Ruiz <steveruizok@gmail.com>
2022-07-28 09:49:22 +0100

    remove gtag

--- apps/www/pages/v/[id].tsx
+++ apps/www/pages/v/[id].tsx
@@ -1,7 +1,6 @@
 import * as React from 'react'
 import type { GetServerSideProps } from 'next'
 import dynamic from 'next/dynamic'
-import * as gtag from 'utils/gtag'
 import { Utils } from '@tldraw/core'
 
 const IFrameWarning = dynamic(() => import('components/IFrameWarning'), {
@@ -18,25 +17,8 @@ interface RoomProps {
 
 export default function Room({ id }: RoomProps) {
   if (typeof window !== 'undefined' && window.self !== window.top) {
-    gtag.event({
-      action: 'connect_to_readonly_room_in_iframe',
-      category: 'v1',
-      label: id,
-      value: 0,
-    })
-
     return <IFrameWarning url={`https://tldraw.com/v/${id}`} />
   }
-
-  gtag.event({
-    action:
-      process.env.NODE_ENV === 'production'
-        ? 'connect_to_readonly_room'
-        : 'connect_to_readonly_room_dev',
-    category: 'v1',
-    label: id,
-    value: 0,
-  })
 
   return <ReadOnlyMultiplayerEditor roomId={id} />
 }
