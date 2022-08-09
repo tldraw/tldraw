938acfd4bac181f581cfb9402dfbb903c0b79287
Steve Ruiz <steveruizok@gmail.com>
2022-08-09 23:37:34 +0100

    switch examples to vite

--- examples/core-example/README.md
+++ examples/core-example/README.md
@@ -1,3 +1,12 @@
-# @tldraw/core Simple Example
+<div style="text-align: center; transform: scale(.5);">
+  <img src="https://github.com/tldraw/tldraw/raw/main/assets/card-repo.png"/>
+</div>
 
-A (relatively) simple example project for `@tldraw/core`.
+# @tldraw/core-example
+
+An simple example project for `@tldraw/core`.
+
+To start this project:
+
+- run `yarn` from the repository's root directory
+- run `yarn dev:core` from the `examples/core-example` directory
--- examples/tldraw-example/src/main.tsx
+++ examples/tldraw-example/src/main.tsx
@@ -1,13 +1,15 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './app'
-import './styles.css'
+import { HashRouter } from 'react-router-dom'
+import App from './App'
 
 const container = document.getElementById('root')!
 const root = createRoot(container)
 root.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>
 )
