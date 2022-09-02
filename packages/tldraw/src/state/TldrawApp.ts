6c7a6bcebe2d4a8b58c10c8bbbdc2b2eba28d9cf
Anders Rex <anders@mightyapp.com>
2022-09-02 11:58:41 +0300

    Use id as assetId

--- packages/tldraw/src/state/TldrawApp.ts
+++ packages/tldraw/src/state/TldrawApp.ts
@@ -3421,7 +3421,7 @@ export class TldrawApp extends StateManager<TDSnapshot> {
           let assetId: string
 
           if (!match) {
-            assetId = Utils.uniqueId()
+            assetId = id
 
             const asset = {
               id: assetId,
