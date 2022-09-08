00e17b65f3a14577b28d1ec77e45628c36f712fa
Steve Ruiz <steveruizok@gmail.com>
2022-09-08 11:13:29 +0100

    hot fix

--- packages/core/src/hooks/useZoomEvents.ts
+++ packages/core/src/hooks/useZoomEvents.ts
@@ -37,7 +37,7 @@ export function useZoomEvents<T extends HTMLElement>(
       const [x, y, z] = normalizeWheel(e)
 
       // alt+scroll or ctrl+scroll = zoom
-      if ((e.altKey || e.ctrlKey || e.metaKey) && e.buttons === 1) {
+      if ((e.altKey || e.ctrlKey || e.metaKey) && e.buttons === 0) {
         const point = inputs.pointer?.point ?? [bounds.width / 2, bounds.height / 2]
         const delta = [...point, z * 0.618]
         const info = inputs.pan(delta, e)
