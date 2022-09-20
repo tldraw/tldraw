6a24507826dea0195ecfc09779873f6d0a6dfa5a
Brian <brianfoody@gmail.com>
2022-09-20 17:55:40 +0800

    Move the pasted point a little

--- examples/core-example-advanced/src/state/actions/selection/paste.ts
+++ examples/core-example-advanced/src/state/actions/selection/paste.ts
@@ -4,7 +4,11 @@ import type { Action } from 'state/constants'
 export const paste: Action = (data) => {
   const selectedShapes = Object.values(data.page.shapes).filter((shape) => shape.isCopied)
 
-  const newShapes = selectedShapes.map((s) => ({ ...s, id: nanoid() }))
+  const newShapes = selectedShapes.map((s) => ({
+    ...s,
+    id: nanoid(),
+    point: [s.point[0] + 50, s.point[1] + 50],
+  }))
 
   for (const shape of newShapes) {
     data.page.shapes[shape.id] = shape
