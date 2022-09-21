bbf75f3fbc87b1cc399e6943e8b9780489334b7f
James Vaughan <james@jamesbvaughan.com>
2022-09-20 21:54:25 -0700

    Add tests for the custom cursor props

--- packages/core/src/components/Canvas/Canvas.test.tsx
+++ packages/core/src/components/Canvas/Canvas.test.tsx
@@ -2,6 +2,10 @@ import * as React from 'react'
 import { mockDocument, renderWithContext } from '~test'
 import { Canvas } from './Canvas'
 
+function TestCustomCursor() {
+  return <div>Custom cursor</div>
+}
+
 describe('page', () => {
   test('mounts component without crashing', () => {
     expect(() =>
@@ -22,6 +26,31 @@ describe('page', () => {
             // noop
           }}
           assets={{}}
+        />
+      )
+    ).not.toThrowError()
+  })
+
+  test('mounts component with custom cursors without crashing', () => {
+    expect(() =>
+      renderWithContext(
+        <Canvas
+          page={mockDocument.page}
+          pageState={mockDocument.pageState}
+          hideBounds={false}
+          hideGrid={false}
+          hideIndicators={false}
+          hideHandles={false}
+          hideBindingHandles={false}
+          hideResizeHandles={false}
+          hideCloneHandles={false}
+          hideRotateHandle={false}
+          showDashedBrush={false}
+          onBoundsChange={() => {
+            // noop
+          }}
+          assets={{}}
+          components={{ Cursor: TestCustomCursor }}
         />
       )
     ).not.toThrowError()
