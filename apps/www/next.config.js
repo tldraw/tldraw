e2abe89457b4f613f24ade9417ff9762ebed3a61
Amila Welihinda <amilajack@gmail.com>
2022-11-04 19:27:14 -0700

    feat: setup palette

--- apps/www/next.config.js
+++ apps/www/next.config.js
@@ -1,8 +1,15 @@
 const withPWA = require('next-pwa')
 const withTM = require('next-transpile-modules')
+const PalettePlugin = require('@palette.dev/webpack-plugin')
 
-const { GITHUB_ID, GITHUB_API_SECRET, NODE_ENV, VERCEL_GIT_COMMIT_SHA, GA_MEASUREMENT_ID } =
-  process.env
+const {
+  GITHUB_ID,
+  GITHUB_API_SECRET,
+  NODE_ENV,
+  VERCEL_GIT_COMMIT_SHA,
+  GA_MEASUREMENT_ID,
+  PALETTE_ASSET_KEY,
+} = process.env
 
 const isProduction = NODE_ENV === 'production'
 
@@ -19,6 +26,29 @@ module.exports = withTM(['@tldraw/tldraw', '@tldraw/core'])(
       GA_MEASUREMENT_ID,
       GITHUB_ID,
       GITHUB_API_SECRET,
+    },
+    webpack(config) {
+      config.plugins.push(
+        new PalettePlugin({
+          key: process.env.PALETTE_ASSET_KEY,
+          include: ['.next/static'],
+          version: VERCEL_GIT_COMMIT_SHA,
+        })
+      )
+      return config
+    },
+    async headers() {
+      return [
+        {
+          source: '/(.*)',
+          headers: [
+            {
+              key: 'Document-Policy',
+              value: 'js-profiling',
+            },
+          ],
+        },
+      ]
     },
   })
 )
