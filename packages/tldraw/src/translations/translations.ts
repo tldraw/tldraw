93d6db870fb6dc7f46489d6d1487725767332a6c
Ahmad Saman Abdulhadi <ahmad.00079821@gmail.com>
2022-12-12 20:26:21 +0300

    feat: add kurdish language

--- packages/tldraw/src/translations/translations.ts
+++ packages/tldraw/src/translations/translations.ts
@@ -9,6 +9,7 @@ import he from './he.json'
 import it from './it.json'
 import ja from './ja.json'
 import ko_kr from './ko-kr.json'
+import ku from './ku.json'
 import main from './main.json'
 import ne from './ne.json'
 import no from './no.json'
@@ -17,11 +18,11 @@ import pt_br from './pt-br.json'
 import pt_pt from './pt-pt.json'
 import ru from './ru.json'
 import sv from './sv.json'
+import th from './th.json'
 import tr from './tr.json'
 import uk from './uk.json'
 import zh_cn from './zh-cn.json'
 import zh_tw from './zh-tw.json'
-import th from './th.json'
 
 // The default language (english) must have a value for every message.
 // Other languages may have missing messages. If the application finds
@@ -52,6 +53,7 @@ export const TRANSLATIONS: TDTranslations = [
   { locale: 'zh-ch', label: '简体中文', messages: zh_cn },
   { locale: 'zh-tw', label: '繁體中文 (台灣)', messages: zh_tw },
   { locale: 'th', label: 'ภาษาไทย', messages: th },
+  { locale: 'ku', label: 'کوردی', messages: ku },
 ]
 
 /* ----------------- (do not change) ---------------- */
