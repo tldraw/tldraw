39b784aa60fdbfe133180182b363b8f33e8b3950
alex <alex@dytry.ch>
2023-03-14 16:08:12 +0000

    alex/maintenance-mode: Add maintenance mode (#1164)

--- apps/www/pages/_app.tsx
+++ apps/www/pages/_app.tsx
@@ -1,6 +1,7 @@
 import '@fontsource/recursive'
 import Head from 'next/head'
 import type React from 'react'
+import { MaintenanceMode } from '~components/MaintenanceMode'
 import '~styles/globals.css'
 import useGtag from '~utils/useGtag'
 
@@ -50,7 +51,9 @@ function MyApp({ Component, pageProps }: any) {
 
         <title>tldraw</title>
       </Head>
-      <Component {...pageProps} />
+      <MaintenanceMode>
+        <Component {...pageProps} />
+      </MaintenanceMode>
     </>
   )
 }
