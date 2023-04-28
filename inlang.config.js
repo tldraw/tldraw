be6a6b3f60bd2d9824eac45742543d9e78ea43fd
Nils Jacobsen <58360188+NilsJacobsen@users.noreply.github.com>
2023-04-28 15:00:24 +0200

    Create inlang.config.js

---
+++ inlang.config.js
@@ -0,0 +1,28 @@
+// @ts-check
+
+/**
+ * @type { import("@inlang/core/config").DefineConfig }
+ */
+export async function defineConfig(env) {
+  const plugin = await env.$import(
+    "https://cdn.jsdelivr.net/gh/samuelstroschein/inlang-plugin-json@1/dist/index.js"
+  );
+
+  const { standardLintRules } = await env.$import(
+    "https://cdn.jsdelivr.net/gh/inlang/standard-lint-rules@1/dist/index.js"
+  );
+
+  const pluginConfig = {
+    pathPattern: "./package/tldraw/src/translation/{language}.json",
+  };
+
+  return {
+    referenceLanguage: "en",
+    languages: await plugin.getLanguages({ ...env, pluginConfig }),
+    readResources: (args) => plugin.readResources({ ...args, ...env, pluginConfig }),
+    writeResources: (args) => plugin.writeResources({ ...args, ...env, pluginConfig }),
+    lint: {
+      rules: [standardLintRules()],
+    },
+  };
+}
