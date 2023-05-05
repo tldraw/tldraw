d3b3acabb0925eae42abb35dff8b15ad8b65bd6a
alex <alex@dytry.ch>
2023-05-05 14:04:16 +0100

    alex/new-examples-app: address feedback

--- apps/examples/vite.config.ts
+++ apps/examples/vite.config.ts
@@ -4,14 +4,6 @@ import { defineConfig } from 'vite'
 export default defineConfig({
 	root: path.join(__dirname, 'src'),
 	publicDir: path.join(__dirname, 'public'),
-	resolve: {
-		alias: [
-			{
-				find: '@tldraw/',
-				replacement: path.resolve(__dirname, '../packages/'),
-			},
-		],
-	},
 	build: {
 		outDir: path.join(__dirname, 'dist'),
 		assetsInlineLimit: 0,
