11923fe1c30927f86c1f913292bc0c6059826cce
Steve Ruiz <steveruizok@gmail.com>
2023-05-07 17:44:10 +0100

    Update vite.config.ts

--- apps/examples/vite.config.ts
+++ apps/examples/vite.config.ts
@@ -2,17 +2,15 @@
 import path from 'path'
 import { defineConfig } from 'vite'
 
-export default defineConfig(() => {
-	return {
-		root: path.join(__dirname, 'src'),
-		publicDir: path.join(__dirname, 'public'),
-		build: {
-			outDir: path.join(__dirname, 'dist'),
-			assetsInlineLimit: 0,
-		},
-		server: {
-			port: 5420,
-		},
-		clearScreen: false,
-	}
+export default defineConfig({
+	root: path.join(__dirname, 'src'),
+	publicDir: path.join(__dirname, 'public'),
+	build: {
+		outDir: path.join(__dirname, 'dist'),
+		assetsInlineLimit: 0,
+	},
+	server: {
+		port: 5420,
+	},
+	clearScreen: false,
 })
