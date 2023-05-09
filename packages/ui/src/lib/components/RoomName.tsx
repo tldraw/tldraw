f72739aa59a4865634ff0d2be7bc577454e09668
Lu[ke] Wilson <l2wilson94@gmail.com>
2023-05-09 14:11:34 +0100

    add skeleton for room name

--- packages/ui/src/lib/components/RoomName.tsx
+++ packages/ui/src/lib/components/RoomName.tsx
@@ -14,12 +14,27 @@ export const RoomName = track(function RoomName() {
 	}, [])
 
 	return (
-		<div className="tlui-room-zone">
+		<div
+			style={{
+				pointerEvents: 'all',
+				//backgroundColor: 'var(--color-low)',
+				width: '150px',
+				marginLeft: 'var(--space-2)',
+			}}
+		>
 			{isEditing ? (
-				<>
+				<div
+					style={{
+						display: 'flex',
+						alignItems: 'center',
+						justifyContent: 'center',
+						gap: 'var(--space-4)',
+						paddingLeft: 'calc(var(--space-4) + 1px)',
+					}}
+				>
 					<Icon icon="file" small={true} />
 					<Input
-						className="tlui-people-menu__user__input"
+						className="tlui-room-name__input"
 						defaultValue={'Home Project'}
 						onValueChange={(value) => {
 							//TODO
@@ -30,9 +45,19 @@ export const RoomName = track(function RoomName() {
 						autofocus
 						autoselect
 					/>
-				</>
+				</div>
 			) : (
-				<Button iconLeft="file" onClick={toggleEditing} style={{ pointerEvents: 'all' }}>
+				<Button
+					className="tlui-room-name__button"
+					iconLeft="file"
+					onClick={toggleEditing}
+					style={{
+						textOverflow: 'ellipsis',
+						whiteSpace: 'nowrap',
+						overflow: 'hidden',
+						//backgroundColor: 'var(--color-low)',
+					}}
+				>
 					Home Project
 				</Button>
 			)}
