0664d6cb48985fbd35060ac64fb29287b2f9667b
Steve Ruiz <steveruizok@gmail.com>
2023-05-12 10:33:48 +0100

    Update index.ts

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -79,7 +79,7 @@ export {
 	type DialogsProviderProps,
 	type TLDialog,
 } from './lib/hooks/useDialogsProvider'
-export { type TLUiEventHandler } from './lib/hooks/useEventsProvider'
+export { type TLUiEventHandler, type TLUiEventSource } from './lib/hooks/useEventsProvider'
 export { useExportAs } from './lib/hooks/useExportAs'
 export {
 	HelpMenuSchemaContext,
