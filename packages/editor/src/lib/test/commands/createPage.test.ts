8c0eaacc3a62caf028dd1dcd1f3540ce85d1cf81
David Sheldrick <d.j.sheldrick@gmail.com>
2023-05-12 10:35:32 +0100

    add regression test

--- packages/editor/src/lib/test/commands/createPage.test.ts
+++ packages/editor/src/lib/test/commands/createPage.test.ts
@@ -1,3 +1,4 @@
+import { TLPage } from '@tldraw/tlschema'
 import { MAX_PAGES } from '../../constants'
 import { TestApp } from '../TestApp'
 
@@ -27,4 +28,31 @@ it("Doesn't create a page if max pages is reached", () => {
 		app.createPage(`Test Page ${i}`)
 	}
 	expect(app.pages.length).toBe(MAX_PAGES)
+})
+
+it('[regression] does not die if every page has the same index', () => {
+	expect(app.pages.length).toBe(1)
+	const page = app.pages[0]
+	app.store.put([
+		{
+			...page,
+			id: TLPage.createCustomId('2'),
+			name: 'a',
+		},
+		{
+			...page,
+			id: TLPage.createCustomId('3'),
+			name: 'b',
+		},
+		{
+			...page,
+			id: TLPage.createCustomId('4'),
+			name: 'c',
+		},
+	])
+
+	expect(app.pages.every((p) => p.index === page.index)).toBe(true)
+
+	app.createPage('My Special Test Page')
+	expect(app.pages.some((p) => p.name === 'My Special Test Page')).toBe(true)
 })
