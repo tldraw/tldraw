a4c7936b1834f1871327a94b48059939d4d3bcb3
Lu[ke] Wilson <l2wilson94@gmail.com>
2023-05-19 11:00:25 +0100

    prevent crash when cursor debugging

--- packages/editor/src/lib/app/statechart/TLSelectTool/children/Idle.ts
+++ packages/editor/src/lib/app/statechart/TLSelectTool/children/Idle.ts
@@ -1,5 +1,5 @@
 import { Vec2d } from '@tldraw/primitives'
-import { createShapeId, TLShape } from '@tldraw/tlschema'
+import { createShapeId, TLGeoShapeProps, TLShape } from '@tldraw/tlschema'
 import { debugFlags } from '../../../../utils/debug-flags'
 import {
 	TLClickEventInfo,
@@ -31,7 +31,14 @@ export class Idle extends StateNode {
 				// Custom cursor debugging!
 				// Change the cursor to the type specified by the shape's text label
 				if (debugFlags.debugCursors.value) {
-					this.app.setCursor({ type: (hoveringShape.props as any).text })
+					if (hoveringShape.type !== 'geo') break
+					const cursorType = (hoveringShape.props as TLGeoShapeProps).text
+					try {
+						this.app.setCursor({ type: cursorType })
+					} catch (e) {
+						console.error(`Cursor type not recognized: '${cursorType}'`)
+						this.app.setCursor({ type: 'default' })
+					}
 				}
 
 				break
