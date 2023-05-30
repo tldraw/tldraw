9ce650a85fc7d1e9211fba62acfe0aa2bfafe6ec
Steve Ruiz <steveruizok@gmail.com>
2023-05-30 15:59:58 +0100

    remove webdriver leftovers

--- scripts/e2e/commands/serve.ts
+++
@@ -1,8 +0,0 @@
-import { promiseSpawn } from './util'
-
-export default async function serve() {
-	return promiseSpawn('yarn', ['dev-webdriver'], {
-		stdio: [0, 0, 0],
-		env: { ...process.env },
-	})
-}
