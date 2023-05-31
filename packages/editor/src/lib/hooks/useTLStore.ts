9db001aebdf399c684f8a945e3a2a7ce9c888128
Steve Ruiz <steveruizok@gmail.com>
2023-05-31 15:19:24 +0100

    ok

--- packages/editor/src/lib/hooks/useTLStore.ts
+++ packages/editor/src/lib/hooks/useTLStore.ts
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { StoreOptions, createTLStore } from '../config/createTLStore'
 
+/** @public */
 export function useTLStore(opts: StoreOptions) {
 	const [store] = useState(() => createTLStore(opts))
 	return store
