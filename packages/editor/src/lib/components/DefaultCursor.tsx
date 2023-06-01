4056c59cfa8007f60d0b061c5607a4824c471c31
Lu[ke] Wilson <l2wilson94@gmail.com>
2023-06-01 15:30:45 +0100

    fix cursor chat overflowing

--- packages/editor/src/lib/components/DefaultCursor.tsx
+++ packages/editor/src/lib/components/DefaultCursor.tsx
@@ -19,22 +19,23 @@ const _Cursor: TLCursorComponent = ({ className, zoom, point, color, name, chatM
 
 	if (!point) return null
 
-	const tagText = chatMessage || name
-	const tagTitle = chatMessage ? name : null
-
 	return (
 		<div ref={rDiv} className={classNames('tl-overlays__item', className)}>
 			<svg className="tl-cursor">
 				<use href="#cursor" color={color} />
 			</svg>
-			{tagTitle && (
-				<div className="tl-nametag-title" style={{ color }}>
-					{tagTitle}
-				</div>
-			)}
-			{tagText && (
+			{chatMessage ? (
+				<>
+					<div className="tl-nametag-title" style={{ color }}>
+						{name}
+					</div>
+					<div className="tl-nametag-chat" style={{ backgroundColor: color }}>
+						{chatMessage}
+					</div>
+				</>
+			) : (
 				<div className="tl-nametag" style={{ backgroundColor: color }}>
-					{tagText}
+					{name}
 				</div>
 			)}
 		</div>
