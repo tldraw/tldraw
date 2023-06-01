befeb3b2702a1bf8f24abd46fe5063307e471f63
Steve Ruiz <steveruizok@gmail.com>
2023-06-01 16:19:46 +0100

    ok

--- packages/editor/src/lib/hooks/useTLStore.ts
+++ packages/editor/src/lib/hooks/useTLStore.ts
@@ -1,8 +1,15 @@
 import { useState } from 'react'
 import { StoreOptions, createTLStore } from '../config/createTLStore'
+import { usePrevious } from './usePrevious'
 
 /** @public */
 export function useTLStore(opts: StoreOptions) {
-	const [store] = useState(() => createTLStore(opts))
+	const [store, setStore] = useState(() => createTLStore(opts))
+	const previousOpts = usePrevious(opts)
+	if (previousOpts !== opts) {
+		const newStore = createTLStore(opts)
+		setStore(newStore)
+		return newStore
+	}
 	return store
 }
