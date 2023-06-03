5a3b0c12cc8edc2e43e17a0a9ca1d0ec8733166c
Steve Ruiz <steveruizok@gmail.com>
2023-06-03 17:39:21 +0100

    ok

--- packages/tlschema/src/shapes/TLTextShape.ts
+++ packages/tlschema/src/shapes/TLTextShape.ts
@@ -7,7 +7,7 @@ import { TLOpacityType, opacityValidator } from '../styles/TLOpacityStyle'
 import { TLSizeType, sizeValidator } from '../styles/TLSizeStyle'
 import { TLBaseShape, createShapeValidator } from './TLBaseShape'
 
-/** @public */
+/** @internal */
 export type TLTextShapeProps = {
 	color: TLColorType
 	size: TLSizeType
