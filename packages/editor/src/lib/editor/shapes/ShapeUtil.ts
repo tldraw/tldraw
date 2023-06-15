c34bd9fc5fdfc66ab7e56908095bc41e37fb23e4
Steve Ruiz <steveruizok@gmail.com>
2023-06-15 12:37:48 +0100

    ok

--- packages/editor/src/lib/editor/shapes/ShapeUtil.ts
+++ packages/editor/src/lib/editor/shapes/ShapeUtil.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { Box2d, linesIntersect, Matrix2d, VecLike } from '@tldraw/primitives'
+import { Box2d, linesIntersect, VecLike } from '@tldraw/primitives'
 import { ComputedCache } from '@tldraw/store'
 import { TLHandle, TLShape, TLShapePartial, TLUnknownShape, Vec2dModel } from '@tldraw/tlschema'
 import { computed, EMPTY_ARRAY } from 'signia'
@@ -9,7 +9,6 @@ import { TLResizeHandle } from '../types/selection-types'
 import { TLExportColors } from './shared/TLExportColors'
 
 const points = new WeakMapCache<TLShape, Vec2dModel>()
-const transforms = new WeakMapCache<TLShape, Matrix2d>()
 
 /** @public */
 export interface TLShapeUtilConstructor<
@@ -220,18 +219,6 @@ export abstract class ShapeUtil<T extends TLUnknownShape = TLUnknownShape> {
 			return new Box2d(result.x, result.y, Math.max(result.width, 1), Math.max(result.height, 1))
 		}
 		return result
-	}
-
-	/**
-	 * Get the cached transform. Do not override this method!
-	 *
-	 * @param shape - The shape.
-	 * @public
-	 */
-	transform(shape: T): Matrix2d {
-		return transforms.get<T>(shape, (shape) =>
-			Matrix2d.Compose(Matrix2d.Translate(shape.x, shape.y), Matrix2d.Rotate(shape.rotation))
-		)
 	}
 
 	/**
