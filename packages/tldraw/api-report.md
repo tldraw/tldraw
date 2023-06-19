bbe6c94ce85a665a6fc86efeaa23dea4e2928de9
Steve Ruiz <steveruizok@gmail.com>
2023-06-19 11:48:35 +0100

    Update index.ts

--- packages/tldraw/api-report.md
+++ packages/tldraw/api-report.md
@@ -4,11 +4,8 @@
 
 ```ts
 
-import { defineMigrations } from '@tldraw/store';
 import { TldrawEditorProps } from '@tldraw/editor';
 import { TldrawUiProps } from '@tldraw/ui';
-
-export { defineMigrations }
 
 // @public (undocumented)
 export function Tldraw(props: TldrawEditorProps & TldrawUiProps): JSX.Element;
--- packages/tldraw/src/index.ts
+++ packages/tldraw/src/index.ts
@@ -4,7 +4,6 @@ import '@tldraw/polyfills'
 export * from '@tldraw/editor'
 // eslint-disable-next-line local/no-export-star
 export * from '@tldraw/primitives'
-export { defineMigrations } from '@tldraw/store'
 // eslint-disable-next-line local/no-export-star
 export * from '@tldraw/ui'
 export { Tldraw } from './lib/Tldraw'
