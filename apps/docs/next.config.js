8329b1da89bb498446cebad02b429c5c2b1c49e7
Lu Wilson <l2wilson94@gmail.com>
2023-06-19 19:48:55 +0100

    [docs] Simplify paths for uncategorised pages (#1619)
    This PR changes the paths that we use for uncategorised pages on the
    docs site.
    It also redirects the old paths to the new ones.
    
    ```
    // BEFORE
    tldraw.dev/docs/ucg/shapes
    
    // AFTER
    tldraw.dev/docs/shapes
    ```
    
    I think that this is more intuitive, and will make the links that we
    share a bit clearer.
    eg: As a by-product, it fixes some broken links from the introduction
    page.
    
    As a trade-off, this PR makes our pathing system a bit more complicated.
    But I don't think it's tooooo bad? WDYT?
    
    ### Change Type
    
    - [x] `documentation` — Changes to the documentation only[^2]
    
    [^1]: publishes a `patch` release, for devDependencies use `internal`
    [^2]: will not publish a new version
    
    ### Test Plan
    
    1. Navigate to `/docs/shapes` on the docs site.
    2. Make sure that it goes to the Shapes page.
    3. Do the same for `/docs/ucg/shapes`
    
    
    ### Release Notes
    
    - Documentation: Cleaned up some paths.

--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -5,6 +5,15 @@ const nextConfig = {
 		scrollRestoration: true,
 	},
 	transpilePackages: ['@tldraw/utils'],
+	async redirects() {
+		return [
+			{
+				source: '/:sectionId/ucg/:articleId',
+				destination: '/:sectionId/:articleId',
+				permanent: true,
+			},
+		]
+	},
 }
 
 module.exports = nextConfig
