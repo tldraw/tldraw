caa464883c4fd0103a625d0110086ed099f805c7
David Sheldrick <d.j.sheldrick@gmail.com>
2023-07-04 16:34:09 +0100

    finish off svg support

--- packages/editor/src/lib/editor/shapes/geo/components/DashStyleCloud.tsx
+++ packages/editor/src/lib/editor/shapes/geo/components/DashStyleCloud.tsx
@@ -1,4 +1,4 @@
-import { Vec2d, VecLike, canonicalizeRotation } from '@tldraw/primitives'
+import { Vec2d, canonicalizeRotation } from '@tldraw/primitives'
 import { TLDefaultColorTheme, TLGeoShape, TLShapeId } from '@tldraw/tlschema'
 import * as React from 'react'
 import {
@@ -62,70 +62,55 @@ export const DashStyleCloud = React.memo(function DashStylePolygon({
 	)
 })
 
-export function DashStylePolygonSvg({
+export function DashStyleCloudSvg({
 	dash,
 	fill,
 	color,
 	theme,
 	strokeWidth,
-	outline,
-	lines,
-}: Pick<TLGeoShape['props'], 'dash' | 'fill' | 'color'> & {
-	outline: VecLike[]
+	w,
+	h,
+	id,
+	size,
+}: Pick<TLGeoShape['props'], 'dash' | 'fill' | 'color' | 'w' | 'h' | 'size'> & {
+	id: TLShapeId
 	strokeWidth: number
 	theme: TLDefaultColorTheme
-	lines?: VecLike[][]
 }) {
+	const innerPath = cloudSvgPath(w, h, id, size)
+	const arcs = getCloudArcs(w, h, id, size)
+
 	const strokeElement = document.createElementNS('http://www.w3.org/2000/svg', 'g')
 	strokeElement.setAttribute('stroke-width', strokeWidth.toString())
 	strokeElement.setAttribute('stroke', theme[color].solid)
 	strokeElement.setAttribute('fill', 'none')
 
-	Array.from(Array(outline.length)).forEach((_, i) => {
-		const A = outline[i]
-		const B = outline[(i + 1) % outline.length]
+	for (const { leftPoint, rightPoint, center, radius } of arcs) {
+		const angle = canonicalizeRotation(
+			canonicalizeRotation(Vec2d.Angle(center, rightPoint)) -
+				canonicalizeRotation(Vec2d.Angle(center, leftPoint))
+		)
+		const arcLength = radius * angle
 
-		const dist = Vec2d.Dist(A, B)
-		const { strokeDasharray, strokeDashoffset } = getPerfectDashProps(dist, strokeWidth, {
+		const { strokeDasharray, strokeDashoffset } = getPerfectDashProps(arcLength, strokeWidth, {
 			style: dash,
+			start: 'outset',
+			end: 'outset',
 		})
 
-		const line = document.createElementNS('http://www.w3.org/2000/svg', 'line')
-		line.setAttribute('x1', A.x.toString())
-		line.setAttribute('y1', A.y.toString())
-		line.setAttribute('x2', B.x.toString())
-		line.setAttribute('y2', B.y.toString())
-		line.setAttribute('stroke-dasharray', strokeDasharray.toString())
-		line.setAttribute('stroke-dashoffset', strokeDashoffset.toString())
-
-		strokeElement.appendChild(line)
-	})
-
-	if (lines) {
-		for (const [A, B] of lines) {
-			const dist = Vec2d.Dist(A, B)
-			const { strokeDasharray, strokeDashoffset } = getPerfectDashProps(dist, strokeWidth, {
-				style: dash,
-				start: 'skip',
-				end: 'skip',
-				snap: dash === 'dotted' ? 4 : 2,
-			})
-
-			const line = document.createElementNS('http://www.w3.org/2000/svg', 'line')
-			line.setAttribute('x1', A.x.toString())
-			line.setAttribute('y1', A.y.toString())
-			line.setAttribute('x2', B.x.toString())
-			line.setAttribute('y2', B.y.toString())
-			line.setAttribute('stroke-dasharray', strokeDasharray.toString())
-			line.setAttribute('stroke-dashoffset', strokeDashoffset.toString())
-
-			strokeElement.appendChild(line)
-		}
+		const path = document.createElementNS('http://www.w3.org/2000/svg', 'path')
+		path.setAttribute(
+			'd',
+			`M${leftPoint.x},${leftPoint.y}A${radius},${radius},0,0,1,${rightPoint.x},${rightPoint.y}`
+		)
+		path.setAttribute('stroke-dasharray', strokeDasharray.toString())
+		path.setAttribute('stroke-dashoffset', strokeDashoffset.toString())
+		strokeElement.appendChild(path)
 	}
 
 	// Get the fill element, if any
 	const fillElement = getShapeFillSvg({
-		d: 'M' + outline[0] + 'L' + outline.slice(1) + 'Z',
+		d: innerPath,
 		fill,
 		color,
 		theme,
