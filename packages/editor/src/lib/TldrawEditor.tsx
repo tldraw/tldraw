bd95e7c4df6a35c1fa44185938d461ff09168ccd
Lu[ke] Wilson <l2wilson94@gmail.com>
2023-07-04 14:17:52 +0100

    also link Tldraw component

--- packages/editor/src/lib/TldrawEditor.tsx
+++ packages/editor/src/lib/TldrawEditor.tsx
@@ -34,7 +34,7 @@ import { useZoomCss } from './hooks/useZoomCss'
 import { TLStoreWithStatus } from './utils/sync/StoreWithStatus'
 
 /**
- * Attributes for the {@link TldrawEditor} component.
+ * Attributes for the {@link @tldraw/tldraw#Tldraw} and {@link TldrawEditor} components.
  *
  * @param children - The editor's children.
  * @param shapes - An array of shape utils to use in the editor.
