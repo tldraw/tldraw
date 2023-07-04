b57a0dc79f1b5698ed232c2d0924a42d2f3951f3
Lu[ke] Wilson <l2wilson94@gmail.com>
2023-07-04 11:25:43 +0100

    fix firefox coarse pointer detection

--- packages/editor/src/lib/hooks/useCoarsePointer.ts
+++ packages/editor/src/lib/hooks/useCoarsePointer.ts
@@ -4,6 +4,13 @@ import { useEditor } from './useEditor'
 export function useCoarsePointer() {
 	const editor = useEditor()
 	useEffect(() => {
+		// This is a workaround for a Firefox bug where we don't correctly
+		// detect coarse VS fine pointer. For now, let's assume that you have a fine
+		// pointer if you're on Firefox on desktop.
+		if (editor.isFirefox && !editor.isAndroid && !editor.isIos) {
+			editor.isCoarsePointer = false
+			return
+		}
 		if (window.matchMedia) {
 			const mql = window.matchMedia('(pointer: coarse)')
 			const handler = () => {
