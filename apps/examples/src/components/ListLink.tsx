9400db386f5688543eaf6c7ae94f23e0afb5422c
Lu[ke] Wilson <l2wilson94@gmail.com>
2023-07-10 11:04:47 +0100

    add description to examples

--- apps/examples/src/components/ListLink.tsx
+++ apps/examples/src/components/ListLink.tsx
@@ -3,12 +3,17 @@ import { Link } from 'react-router-dom'
 export interface ListLinkProps {
 	title: string
 	route: string
+	description?: string
 }
 
-export function ListLink({ title, route }: ListLinkProps) {
+export function ListLink({ title, route, description }: ListLinkProps) {
 	return (
 		<li className="examples__list__item">
-			<Link to={route}>{title}</Link>
+			<h3>{title}</h3>
+			<Link className="examples__list__item__button" to={route}>
+				Try it out
+			</Link>
+			<p className="examples__list__item__description">{description}</p>
 		</li>
 	)
 }
--- apps/examples/src/index.tsx
+++ apps/examples/src/index.tsx
@@ -41,6 +41,7 @@ setDefaultUiAssetUrls(assetUrls)
 type Example = {
 	path: string
 	title?: string
+	description?: string
 	element: JSX.Element
 }
 
@@ -49,91 +50,109 @@ export const allExamples: Example[] = [
 		title: 'Basic (development)',
 		path: '/develop',
 		element: <ExampleBasic />,
+		description: 'A basic example of how to use the library.',
 	},
 	{
 		title: 'Editor API',
 		path: '/api',
 		element: <ExampleApi />,
+		description: 'Using the Editor API.',
 	},
 	{
 		title: 'Multiple editors',
 		path: '/multiple',
 		element: <ExampleMultiple />,
+		description: 'Using multiple editors on the same page.',
 	},
 	{
 		title: 'Scroll example',
 		path: '/scroll',
 		element: <ExampleScroll />,
+		description: 'Using tldraw on a scrollable page.',
 	},
 	{
 		title: 'Custom config',
 		path: '/custom-config',
 		element: <CustomConfigExample />,
+		description: 'Using your own custom shapes and tools.',
 	},
 	{
 		title: 'Sublibraries',
 		path: '/exploded',
 		element: <ExplodedExample />,
+		description: 'Using the tldraw components with more fine-grained control.',
 	},
 	{
 		title: 'Error boundary',
 		path: '/error-boundary',
 		element: <ErrorBoundaryExample />,
+		description: 'Using a custom error fallback for shapes.',
 	},
 	{
 		title: 'Custom UI',
 		path: '/custom-ui',
 		element: <CustomUiExample />,
+		description: 'Using your own custom UI.',
 	},
 	{
 		title: 'Hide UI',
 		path: '/hide-ui',
 		element: <HideUiExample />,
+		description: 'Using tldraw without any UI.',
 	},
 	{
 		title: 'UI components',
 		path: '/custom-components',
 		element: <CustomComponentsExample />,
+		description: 'Using your own custom components for brushes, scribbles, and snaplines.',
 	},
 	{
 		title: 'UI events',
 		path: '/ui-events',
 		element: <UiEventsExample />,
+		description: 'Listening to UI events.',
 	},
 	{
 		title: 'Store events',
 		path: '/store-events',
 		element: <StoreEventsExample />,
+		description: 'Listening to store events.',
 	},
 	{
 		title: 'User presence',
 		path: '/user-presence',
 		element: <UserPresenceExample />,
+		description: 'Testing the user presence API.',
 	},
 	{
 		title: 'UI zones',
 		path: '/zones',
 		element: <ZonesExample />,
+		description: 'Placing custom UI elements at the top of the default UI.',
 	},
 	{
 		title: 'Persistence',
 		path: '/persistence',
 		element: <PersistenceExample />,
+		description: 'Using local persistence.',
 	},
 	{
-		title: 'Custom styles',
+		title: 'Yjs',
 		path: '/yjs',
 		element: <YjsExample />,
+		description: 'Live collaboration using Yjs.',
 	},
 	{
 		title: 'Custom styles',
 		path: '/custom-styles',
 		element: <CustomStylesExample />,
+		description: 'Using custom style properties on a custom shape.',
 	},
 	{
 		title: 'Shape meta property',
 		path: '/shape-meta',
 		element: <ShapeMetaExample />,
+		description: 'Storing extra data on shapes with the meta property.',
 	},
 	// not listed
 	{
@@ -155,7 +174,12 @@ function App() {
 				{allExamples
 					.filter((example) => example.title)
 					.map((example) => (
-						<ListLink key={example.path} title={example.title!} route={example.path} />
+						<ListLink
+							key={example.path}
+							title={example.title!}
+							description={example.description}
+							route={example.path}
+						/>
 					))}
 			</ul>
 		</div>
