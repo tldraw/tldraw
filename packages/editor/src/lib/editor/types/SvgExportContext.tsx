1f4ba25111e6ed92269af04ec05c8e6f61070312
Steve Ruiz <steveruizok@gmail.com>
2023-07-18 10:53:13 +0100

    bump

--- packages/editor/src/lib/editor/types/SvgExportContext.tsx
+++ packages/editor/src/lib/editor/types/SvgExportContext.tsx
@@ -7,7 +7,7 @@ export interface SvgExportDef {
 /** @public */
 export interface SvgExportContext {
 	/**
-	 * Add contents to the <defs> section of the export SVG. Each export def should have a unique
+	 * Add contents to the `<defs>` section of the export SVG. Each export def should have a unique
 	 * key. If multiple defs come with the same key, only one will be added.
 	 */
 	addExportDef(def: SvgExportDef): void
