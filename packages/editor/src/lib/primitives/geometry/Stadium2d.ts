8dff12d7797e0755b253160f00dfea09df77630c
Steve Ruiz <steveruizok@gmail.com>
2023-07-19 17:04:04 +0100

    Update Stadium2d.ts

--- packages/editor/src/lib/primitives/geometry/Stadium2d.ts
+++ packages/editor/src/lib/primitives/geometry/Stadium2d.ts
@@ -16,21 +16,21 @@ export class Stadium2d extends Ellipse2d {
 		const cy = h / 2
 
 		const len = 10
-		const points: Vec2d[] = Array(len * 2)
+		const points: Vec2d[] = Array(len * 2 - 2)
 
 		if (h > w) {
-			for (let i = 0; i < len; i++) {
+			for (let i = 0; i < len - 1; i++) {
 				const t1 = -PI + (PI * i) / (len - 2)
 				const t2 = (PI * i) / (len - 2)
 				points[i] = new Vec2d(cx + cx * Math.cos(t1), cx + cx * Math.sin(t1))
-				points[i + len] = new Vec2d(cx + cx * Math.cos(t2), h - cx + cx * Math.sin(t2))
+				points[i + (len - 1)] = new Vec2d(cx + cx * Math.cos(t2), h - cx + cx * Math.sin(t2))
 			}
 		} else {
-			for (let i = 0; i < len; i++) {
+			for (let i = 0; i < len - 1; i++) {
 				const t1 = -TAU + (PI * i) / (len - 2)
 				const t2 = TAU + (PI * -i) / (len - 2)
 				points[i] = new Vec2d(w - cy + cy * Math.cos(t1), h - cy + cy * Math.sin(t1))
-				points[i + len] = new Vec2d(cy - cy * Math.cos(t2), h - cy + cy * Math.sin(t2))
+				points[i + (len - 1)] = new Vec2d(cy - cy * Math.cos(t2), h - cy + cy * Math.sin(t2))
 			}
 		}
 
