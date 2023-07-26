tldraw/tldraw

commit d750da8f40efda4b011a91962ef8f30c63d1e5da

`ShapeUtil.getGeometry`, selection rewrite (#1751)
This PR is a significant rewrite of our selection / hit testing logic.

It
- replaces our current geometric helpers (`getBounds`, `getOutline`,
`hitTestPoint`, and `hitTestLineSegment`) with a new geometry API
- moves our hit testing entirely to JS using geometry
- improves selection logic, especially around editing shapes, groups and
frames
- fixes many minor selection bugs (e.g. shapes behind frames)
- removes hit-testing DOM elements from ShapeFill etc.
- adds many new tests around selection
- adds new tests around selection
- makes several superficial changes to surface editor APIs

This PR is hard to evaluate. The `selection-omnibus` test suite is
intended to describe all of the selection behavior, however all existing
tests are also either here preserved and passing or (in a few cases
around editing shapes) are modified to reflect the new behavior.

## Geometry

All `ShapeUtils` implement `getGeometry`, which returns a single
geometry primitive (`Geometry2d`). For example:

```ts
class BoxyShapeUtil {
  getGeometry(shape: BoxyShape) {
    return new Rectangle2d({
        width: shape.props.width, 
        height: shape.props.height, 
        isFilled: true,
        margin: shape.props.strokeWidth
      })
    }
}
```

This geometric primitive is used for all bounds calculation, hit
testing, intersection with arrows, etc.

There are several geometric primitives that extend `Geometry2d`:
- `Arc2d`
- `Circle2d`
- `CubicBezier2d`
- `CubicSpline2d`
- `Edge2d`
- `Ellipse2d`
- `Group2d`
- `Polygon2d`
- `Rectangle2d`
- `Stadium2d`

For shapes that have more complicated geometric representations, such as
an arrow with a label, the `Group2d` can accept other primitives as its
children.

## Hit testing

Previously, we did all hit testing via events set on shapes and other
elements. In this PR, I've replaced those hit tests with our own
calculation for hit tests in JavaScript. This removed the need for many

packages/editor/src/lib/editor/shapes/ShapeUtil.ts
@@ -2,8 +2,8 @@
 import { Migrations } from '@tldraw/store'
 import { ShapeProps, TLHandle, TLShape, TLShapePartial, TLUnknownShape } from '@tldraw/tlschema'
 import { Box2d } from '../../primitives/Box2d'
-import { Vec2d, VecLike } from '../../primitives/Vec2d'
-import { linesIntersect } from '../../primitives/intersect'
+import { Vec2d } from '../../primitives/Vec2d'
+import { Geometry2d } from '../../primitives/geometry/Geometry2d'
 import type { Editor } from '../Editor'
 import { SvgExportContext } from '../types/SvgExportContext'
 import { TLResizeHandle } from '../types/selection-types'
@@ -47,6 +47,14 @@ export abstract class ShapeUtil<Shape extends TLUnknownShape = TLUnknownShape> {
 	 * @public
 	 */
 	abstract getDefaultProps(): Shape['props']
+
+	/**
+	 * Get the shape's geometry.
+	 *
+	 * @param shape - The shape.
+	 * @public
+	 */
+	abstract getGeometry(shape: Shape): Geometry2d
 
 	/**
 	 * Get a JSX element for the shape (as an HTML element).
@@ -128,13 +136,6 @@ export abstract class ShapeUtil<Shape extends TLUnknownShape = TLUnknownShape> {
 	}
 
 	/**
-	 * Whether the shape's outline is closed.
-	 *
-	 * @public
-	 */
-	isClosed: TLShapeUtilFlag<Shape> = () => true
-
-	/**
 	 * Whether the shape should hide its resize handles when selected.
 	 *
 	 * @public
@@ -147,13 +148,6 @@ export abstract class ShapeUtil<Shape extends TLUnknownShape = TLUnknownShape> {
 	 * @public
 	 */
 	hideRotateHandle: TLShapeUtilFlag<Shape> = () => false
-
-	/**
-	 * Whether the shape should hide its selection bounds background when selected.
-	 *
-	 * @public
-	 */
-	hideSelectionBoundsBg: TLShapeUtilFlag<Shape> = () => false
 
 	/**
 	 * Whether the shape should hide its selection bounds foreground when selected.
@@ -207,55 +201,7 @@ export abstract class ShapeUtil<Shape extends TLUnknownShape = TLUnknownShape> {
 	 * @public
 	 */
 	getOutlineSegments(shape: Shape): Vec2d[][] {
-		return [this.editor.getOutline(shape)]
-	}
-
-	/**
-	 * Get the (not cached) bounds for the shape.
-	 *
-	 * @param shape - The shape.
-	 * @public
-	 */
-	abstract getBounds(shape: Shape): Box2d
-
-	/**
-	 * Get the shape's (not cached) outline.
-	 *
-	 * @param shape - The shape.
-	 * @public
-	 */
-	getOutline(shape: Shape): Vec2d[] {
-		return this.editor.getBounds(shape).corners
-	}
-
-	/**
-	 * Get the shape's snap points.
-	 *
-	 * @param shape - The shape.
-	 * @public
-	 */
-	snapPoints(shape: Shape) {
-		return this.editor.getBounds(shape).snapPoints
-	}
-
-	/**
-	 * Get the shape's cached center.
-	 *
-	 * @param shape - The shape.
-	 * @public
-	 */
-	center(shape: Shape): Vec2d {
-		return this.getCenter(shape)
-	}
-
-	/**
-	 * Get the shape's (not cached) center.
-	 *
-	 * @param shape - The shape.
-	 * @public
-	 */
-	getCenter(shape: Shape) {
-		return this.editor.getBounds(shape).center
+		return [this.editor.getGeometry(shape).vertices]
 	}
 
 	/**
@@ -302,39 +248,6 @@ export abstract class ShapeUtil<Shape extends TLUnknownShape = TLUnknownShape> {
 	/** @internal */
 	expandSelectionOutlinePx(shape: Shape): number {
 		return 0
-	}
-
-	/**
-	 * Get whether a point intersects the shape.
-	 *
-	 * @param shape - The shape.
-	 * @param point - The point to test.
-	 * @returns Whether the point intersects the shape.
-	 * @public
-	 */
-	hitTestPoint(shape: Shape, point: VecLike): boolean {
-		return this.editor.getBounds(shape).containsPoint(point)
-	}
-
-	/**
-	 * Get whether a point intersects the shape.
-	 *
-	 * @param shape - The shape.
-	 * @param A - The line segment's first point.
-	 * @param B - The line segment's second point.
-	 * @returns Whether the line segment intersects the shape.
-	 * @public
-	 */
-	hitTestLineSegment(shape: Shape, A: VecLike, B: VecLike): boolean {
-		const outline = this.editor.getOutline(shape)
-
-		for (let i = 0; i < outline.length; i++) {
-			const C = outline[i]
-			const D = outline[(i + 1) % outline.length]
-			if (linesIntersect(A, B, C, D)) return true
-		}
-
-		return false
 	}
 
 	/**

packages/tldraw/src/lib/canvas/TldrawSelectionForeground.tsx
@@ -21,7 +21,7 @@ const IS_FIREFOX =
 	navigator.userAgent.toLowerCase().indexOf('firefox') > -1
 
 export const TldrawSelectionForeground: TLSelectionForegroundComponent = track(
-	function SelectionFg() {
+	function TldrawSelectionForeground({ bounds, rotation }) {
 		const editor = useEditor()
 		const rSvg = useRef<SVGSVGElement>(null)
 
@@ -38,7 +38,6 @@ export const TldrawSelectionForeground: TLSelectionForegroundComponent = track(
 		const isDefaultCursor = !editor.isMenuOpen && editor.instanceState.cursor.type === 'default'
 		const isCoarsePointer = editor.instanceState.isCoarsePointer
 
-		let bounds = editor.selectionBounds
 		const shapes = editor.selectedShapes
 		const onlyShape = editor.onlySelectedShape
 		const isLockedShape = onlyShape && editor.isShapeOrAncestorLocked(onlyShape)
@@ -57,7 +56,6 @@ export const TldrawSelectionForeground: TLSelectionForegroundComponent = track(
 		bounds = bounds.clone().expandBy(expandOutlineBy)
 
 		const zoom = editor.zoomLevel
-		const rotation = editor.selectionRotation
 		const isChangingStyle = editor.instanceState.isChangingStyle
 
 		const width = Math.max(1, bounds.width)
@@ -94,8 +92,7 @@ export const TldrawSelectionForeground: TLSelectionForegroundComponent = track(
 					'select.crop.idle',
 					'select.crop.pointing_crop',
 					'select.pointing_resize_handle',
-					'select.pointing_crop_handle',
-					'select.editing_shape'
+					'select.pointing_crop_handle'
 				)) ||
 			(showSelectionBounds &&
 				editor.isIn('select.resizing') &&

packages/tldraw/src/lib/tools/SelectTool/children/Idle.ts
@@ -1,68 +1,36 @@
 import {
+	HIT_TEST_MARGIN,
 	StateNode,
 	TLClickEventInfo,
 	TLEventHandlers,
-	TLGeoShape,
 	TLGroupShape,
 	TLKeyboardEventInfo,
-	TLPointerEventInfo,
 	TLShape,
 	TLTextShape,
 	Vec2d,
 	createShapeId,
-	debugFlags,
 } from '@tldraw/editor'
+import { getHitShapeOnCanvasPointerDown } from '../../selection-logic/getHitShapeOnCanvasPointerDown'
+import { getShouldEnterCropMode } from '../../selection-logic/getShouldEnterCropModeOnPointerDown'
+import { selectOnCanvasPointerUp } from '../../selection-logic/selectOnCanvasPointerUp'
+import { updateHoveredId } from '../../selection-logic/updateHoveredId'
 
 export class Idle extends StateNode {
 	static override id = 'idle'
 
-	override onPointerEnter: TLEventHandlers['onPointerEnter'] = (info) => {
-		switch (info.target) {
-			case 'canvas': {
-				// noop
-				break
-			}
-			case 'shape': {
-				const { selectedIds, focusLayerId } = this.editor
-				const hoveringShape = this.editor.getOutermostSelectableShape(
-					info.shape,
-					(parent) => !selectedIds.includes(parent.id)
-				)
-				if (hoveringShape.id !== focusLayerId) {
-					this.editor.hoveredId = hoveringShape.id
-				}
-
-				// Custom cursor debugging!
-				// Change the cursor to the type specified by the shape's text label
-				if (debugFlags.debugCursors.value) {
-					if (hoveringShape.type !== 'geo') break
-					const cursorType = (hoveringShape as TLGeoShape).props.text
-					try {
-						this.editor.updateInstanceState({ cursor: { type: cursorType, rotation: 0 } }, true)
-					} catch (e) {
-						console.error(`Cursor type not recognized: '${cursorType}'`)
-						this.editor.updateInstanceState({ cursor: { type: 'default', rotation: 0 } }, true)
-					}
-				}
-
-				break
-			}
-		}
+	override onEnter = () => {
+		this.parent.currentToolIdMask = undefined
+		this.editor.updateInstanceState({ cursor: { type: 'default', rotation: 0 } }, true)
 	}
 
-	override onPointerLeave: TLEventHandlers['onPointerLeave'] = (info) => {
-		switch (info.target) {
-			case 'shape': {
-				this.editor.hoveredId = null
-				break
-			}
-		}
+	override onPointerMove: TLEventHandlers['onPointerMove'] = () => {
+		updateHoveredId(this.editor)
 	}
 
 	override onPointerDown: TLEventHandlers['onPointerDown'] = (info) => {
 		if (this.editor.isMenuOpen) return
 
-		const shouldEnterCropMode = this.shouldEnterCropMode(info, true)
+		const shouldEnterCropMode = info.ctrlKey && getShouldEnterCropMode(this.editor)
 
 		if (info.ctrlKey && !shouldEnterCropMode) {
 			// On Mac, you can right click using the Control keys + Click.
@@ -72,12 +40,47 @@ export class Idle extends StateNode {
 					return
 				}
 			}
+
 			this.parent.transition('brushing', info)
 			return
 		}
 
 		switch (info.target) {
 			case 'canvas': {
+				// Check to see if we hit any shape under the pointer; if so,
+				// handle this as a pointer down on the shape instead of the canvas
+				const hitShape = getHitShapeOnCanvasPointerDown(this.editor)
+				if (hitShape) {
+					this.onPointerDown({
+						...info,
+						shape: hitShape,
+						target: 'shape',
+					})
+					return
+				}
+
+				const {
+					selectedShapeIds,
+					inputs: { currentPagePoint },
+				} = this.editor
+
+				if (selectedShapeIds.length > 0) {
+					// If there's only one shape selected, and if that shape's
+					// geometry is open, then don't test the selection background
+					if (
+						selectedShapeIds.length > 1 ||
+						this.editor.getGeometry(selectedShapeIds[0]).isClosed
+					) {
+						if (this.editor.selectionBounds?.containsPoint(currentPagePoint)) {
+							this.onPointerDown({
+								...info,
+								target: 'selection',
+							})
+							return
+						}
+					}
+				}
+
 				this.parent.transition('pointing_canvas', info)
 				break
 			}
@@ -128,6 +131,16 @@ export class Idle extends StateNode {
 						break
 					}
 					default: {
+						const { hoveredShape } = this.editor
+						if (hoveredShape && !this.editor.selectedShapeIds.includes(hoveredShape.id)) {
+							this.onPointerDown({
+								...info,
+								shape: hoveredShape,
+								target: 'shape',
+							})
+							return
+						}
+
 						this.parent.transition('pointing_selection', info)
 					}
 				}
@@ -137,13 +150,61 @@ export class Idle extends StateNode {
 	}
 
 	override onDoubleClick: TLEventHandlers['onDoubleClick'] = (info) => {
-		if (info.phase !== 'up') return
+		if (this.editor.inputs.shiftKey || info.phase !== 'up') return
 
 		switch (info.target) {
 			case 'canvas': {
-				// Create text shape and transition to editing_shape
-				if (this.editor.instanceState.isReadonly) break
-				this.handleDoubleClickOnCanvas(info)
+				const { hoveredShape } = this.editor
+				const hitShape =
+					hoveredShape && !this.editor.isShapeOfType<TLGroupShape>(hoveredShape, 'group')
+						? hoveredShape
+						: this.editor.getSelectedShapeAtPoint(this.editor.inputs.currentPagePoint) ??
+						  this.editor.getShapeAtPoint(this.editor.inputs.currentPagePoint, {
+								margin: HIT_TEST_MARGIN / this.editor.zoomLevel,
+								hitInside: true,
+						  })
+
+				const { focusedGroupId } = this.editor
+
+				if (hitShape) {
+					if (this.editor.isShapeOfType<TLGroupShape>(hitShape, 'group')) {
+						// Probably select the shape
+						selectOnCanvasPointerUp(this.editor)
+						return
+					} else {
+						const parent = this.editor.getShape(hitShape.parentId)
+						if (parent && this.editor.isShapeOfType<TLGroupShape>(parent, 'group')) {
+							// The shape is the direct child of a group. If the group is
+							// selected, then we can select the shape. If the group is the
+							// focus layer id, then we can double click into it as usual.
+							if (focusedGroupId && parent.id === focusedGroupId) {
+								// noop, double click on the shape as normal below
+							} else {
+								// The shape is the child of some group other than our current
+								// focus layer. We should probably select the group instead.
+								selectOnCanvasPointerUp(this.editor)
+								return
+							}
+						}
+					}
+
+					// double click on the shape. We'll start editing the
+					// shape if it's editable or else do a double click on
+					// the canvas.
+					this.onDoubleClick({
+						...info,
+						shape: hitShape,
+						target: 'shape',
+					})
+
+					return
+				}
+
+				if (!this.editor.inputs.shiftKey) {
+					// Create text shape and transition to editing_shape
+					if (this.editor.instanceState.isReadonly) break
+					this.handleDoubleClickOnCanvas(info)
+				}
 				break
 			}
 			case 'selection': {
@@ -209,6 +270,7 @@ export class Idle extends StateNode {
 						return
 					}
 				}
+
 				// If the shape can edit, then begin editing
 				if (this.shouldStartEditingShape(shape)) {
 					this.startEditingShape(shape, info)
@@ -243,37 +305,45 @@ export class Idle extends StateNode {
 	override onRightClick: TLEventHandlers['onRightClick'] = (info) => {
 		switch (info.target) {
 			case 'canvas': {
+				const { hoveredShape } = this.editor
+				const hitShape =
+					hoveredShape && !this.editor.isShapeOfType<TLGroupShape>(hoveredShape, 'group')
+						? hoveredShape
+						: this.editor.getShapeAtPoint(this.editor.inputs.currentPagePoint)
+				if (hitShape) {
+					this.onRightClick({
+						...info,
+						shape: hitShape,
+						target: 'shape',
+					})
+					return
+				}
+
 				this.editor.selectNone()
 				break
 			}
 			case 'shape': {
-				const { selectedIds } = this.editor.currentPageState
+				const { selectedShapeIds } = this.editor.currentPageState
 				const { shape } = info
 
 				const targetShape = this.editor.getOutermostSelectableShape(
 					shape,
-					(parent) => !selectedIds.includes(parent.id)
+					(parent) => !selectedShapeIds.includes(parent.id)
 				)
 
-				if (!selectedIds.includes(targetShape.id)) {
+				if (!selectedShapeIds.includes(targetShape.id)) {
 					this.editor.mark('selecting shape')
-					this.editor.setSelectedIds([targetShape.id])
+					this.editor.setSelectedShapeIds([targetShape.id])
 				}
 				break
 			}
 		}
 	}
 
-	override onEnter = () => {
-		this.editor.hoveredId = null
-		this.editor.updateInstanceState({ cursor: { type: 'default', rotation: 0 } }, true)
-		this.parent.currentToolIdMask = undefined
-	}
-
 	override onCancel: TLEventHandlers['onCancel'] = () => {
 		if (
-			this.editor.focusLayerId !== this.editor.currentPageId &&
-			this.editor.selectedIds.length > 0
+			this.editor.focusedGroupId !== this.editor.currentPageId &&
+			this.editor.selectedShapeIds.length > 0
 		) {
 			this.editor.popFocusLayer()
 		} else {
@@ -310,11 +380,12 @@ export class Idle extends StateNode {
 		if (this.editor.instanceState.isReadonly) {
 			switch (info.code) {
 				case 'Enter': {
-					if (this.shouldStartEditingShape() && this.editor.onlySelectedShape) {
-						this.startEditingShape(this.editor.onlySelectedShape, {
+					const { onlySelectedShape } = this.editor
+					if (onlySelectedShape && this.shouldStartEditingShape()) {
+						this.startEditingShape(onlySelectedShape, {
 							...info,
 							target: 'shape',
-							shape: this.editor.onlySelectedShape,
+							shape: onlySelectedShape,
 						})
 						return
 					}
@@ -326,25 +397,29 @@ export class Idle extends StateNode {
 				case 'Enter': {
 					const { selectedShapes } = this.editor
 
+					// On enter, if every selected shape is a group, then select all of the children of the groups
 					if (
 						selectedShapes.every((shape) => this.editor.isShapeOfType<TLGroupShape>(shape, 'group'))
 					) {
-						this.editor.setSelectedIds(
-							selectedShapes.flatMap((shape) => this.editor.getSortedChildIds(shape.id))
+						this.editor.setSelectedShapeIds(
+							selectedShapes.flatMap((shape) => this.editor.getSortedChildIdsForParent(shape.id))
 						)
 						return
 					}
 
-					if (this.shouldStartEditingShape() && this.editor.onlySelectedShape) {
-						this.startEditingShape(this.editor.onlySelectedShape, {
+					// If the only selected shape is editable, then begin editing it
+					const { onlySelectedShape } = this.editor
+					if (onlySelectedShape && this.shouldStartEditingShape(onlySelectedShape)) {
+						this.startEditingShape(onlySelectedShape, {
 							...info,
 							target: 'shape',
-							shape: this.editor.onlySelectedShape,
+							shape: onlySelectedShape,
 						})
 						return
 					}
 
-					if (this.shouldEnterCropMode(info, false)) {
+					// If the only selected shape is croppable, then begin cropping it
+					if (getShouldEnterCropMode(this.editor)) {
 						this.parent.transition('crop', info)
 					}
 					break
@@ -356,26 +431,7 @@ export class Idle extends StateNode {
 	private shouldStartEditingShape(shape: TLShape | null = this.editor.onlySelectedShape): boolean {
 		if (!shape) return false
 		if (this.editor.isShapeOrAncestorLocked(shape) && shape.type !== 'embed') return false
-
-		const util = this.editor.getShapeUtil(shape)
-		return util.canEdit(shape)
-	}
-
-	private shouldEnterCropMode(
-		info: TLPointerEventInfo | TLKeyboardEventInfo,
-		withCtrlKey: boolean
-	): boolean {
-		const singleShape = this.editor.onlySelectedShape
-		if (!singleShape) return false
-		if (this.editor.isShapeOrAncestorLocked(singleShape)) return false
-
-		const shapeUtil = this.editor.getShapeUtil(singleShape)
-		// Should the Ctrl key be pressed to enter crop mode
-		if (withCtrlKey) {
-			return shapeUtil.canCrop(singleShape) && info.ctrlKey
-		} else {
-			return shapeUtil.canCrop(singleShape)
-		}
+		return this.editor.getShapeUtil(shape).canEdit(shape)
 	}
 
 	private startEditingShape(shape: TLShape, info: TLClickEventInfo | TLKeyboardEventInfo) {
@@ -407,19 +463,8 @@ export class Idle extends StateNode {
 			},
 		])
 
-		const shape = this.editor.getShapeById(id)
+		const shape = this.editor.getShape(id)
 		if (!shape) return
-
-		const bounds = this.editor.getBounds(shape)
-
-		this.editor.updateShapes([
-			{
-				id,
-				type: 'text',
-				x: shape.x - bounds.width / 2,
-				y: shape.y - bounds.height / 2,
-			},
-		])
 
 		this.editor.setEditingId(id)
 		this.editor.select(id)
@@ -449,6 +494,20 @@ export class Idle extends StateNode {
 
 		if (!ephemeral) this.editor.mark('nudge shapes')
 
-		this.editor.nudgeShapes(this.editor.selectedIds, delta, shiftKey)
+		const { gridSize } = this.editor.documentSettings
+
+		const step = this.editor.instanceState.isGridMode
+			? shiftKey
+				? gridSize * GRID_INCREMENT
+				: gridSize
+			: shiftKey
+			? MAJOR_NUDGE_FACTOR
+			: MINOR_NUDGE_FACTOR
+
+		this.editor.nudgeShapes(this.editor.selectedShapeIds, delta.mul(step))
 	}
 }
+
+export const MAJOR_NUDGE_FACTOR = 10
+export const MINOR_NUDGE_FACTOR = 1
+export const GRID_INCREMENT = 5

commit 28b92c5e764ac8ce8dc1a66cd1d6248e3ddda085

[fix] restore bg option, fix calculations (#1765)
This PR fixes a bug introduced with #1751 where pointing the bounds of
rotated selections would not correctly hit the bounds background.

### Change Type

- [x] `patch` — Bug fix

### Test Plan

1. Create a rotated selection.
2. Point into the bounds background

- [x] Unit Tests

packages/editor/src/lib/editor/shapes/ShapeUtil.ts
@@ -150,6 +150,13 @@ export abstract class ShapeUtil<Shape extends TLUnknownShape = TLUnknownShape> {
 	hideRotateHandle: TLShapeUtilFlag<Shape> = () => false
 
 	/**
+	 * Whether the shape should hide its selection bounds background when selected.
+	 *
+	 * @public
+	 */
+	hideSelectionBoundsBg: TLShapeUtilFlag<Shape> = () => false
+
+	/**
 	 * Whether the shape should hide its selection bounds foreground when selected.
 	 *
 	 * @public

packages/tldraw/src/lib/canvas/TldrawSelectionForeground.tsx
@@ -99,13 +99,10 @@ export const TldrawSelectionForeground: TLSelectionForegroundComponent = track(
 				onlyShape &&
 				editor.isShapeOfType<TLTextShape>(onlyShape, 'text'))
 
-		if (
-			onlyShape &&
-			editor.isShapeOfType<TLEmbedShape>(onlyShape, 'embed') &&
-			shouldDisplayBox &&
-			IS_FIREFOX
-		) {
-			shouldDisplayBox = false
+		if (onlyShape && shouldDisplayBox) {
+			if (IS_FIREFOX && editor.isShapeOfType<TLEmbedShape>(onlyShape, 'embed')) {
+				shouldDisplayBox = false
+			}
 		}
 
 		const showCropHandles =

packages/tldraw/src/lib/shapes/arrow/ArrowShapeUtil.tsx
@@ -64,6 +64,7 @@ export class ArrowShapeUtil extends ShapeUtil<TLArrowShape> {
 	override canSnap = () => true
 	override hideResizeHandles: TLShapeUtilFlag<TLArrowShape> = () => true
 	override hideRotateHandle: TLShapeUtilFlag<TLArrowShape> = () => true
+	override hideSelectionBoundsBg: TLShapeUtilFlag<TLArrowShape> = () => true
 	override hideSelectionBoundsFg: TLShapeUtilFlag<TLArrowShape> = () => true
 
 	override getDefaultProps(): TLArrowShape['props'] {

packages/tldraw/src/lib/tools/SelectTool/children/Idle.ts
@@ -1,4 +1,5 @@
 import {
+	Editor,
 	HIT_TEST_MARGIN,
 	StateNode,
 	TLClickEventInfo,
@@ -8,7 +9,9 @@ import {
 	TLShape,
 	TLTextShape,
 	Vec2d,
+	VecLike,
 	createShapeId,
+	pointInPolygon,
 } from '@tldraw/editor'
 import { getHitShapeOnCanvasPointerDown } from '../../selection-logic/getHitShapeOnCanvasPointerDown'
 import { getShouldEnterCropMode } from '../../selection-logic/getShouldEnterCropModeOnPointerDown'
@@ -60,24 +63,22 @@ export class Idle extends StateNode {
 				}
 
 				const {
+					onlySelectedShape,
 					selectedShapeIds,
 					inputs: { currentPagePoint },
 				} = this.editor
 
-				if (selectedShapeIds.length > 0) {
-					// If there's only one shape selected, and if that shape's
-					// geometry is open, then don't test the selection background
-					if (
-						selectedShapeIds.length > 1 ||
-						this.editor.getGeometry(selectedShapeIds[0]).isClosed
-					) {
-						if (this.editor.selectionBounds?.containsPoint(currentPagePoint)) {
-							this.onPointerDown({
-								...info,
-								target: 'selection',
-							})
-							return
-						}
+				if (
+					selectedShapeIds.length > 1 ||
+					(onlySelectedShape &&
+						!this.editor.getShapeUtil(onlySelectedShape).hideSelectionBoundsBg(onlySelectedShape))
+				) {
+					if (isPointInRotatedSelectionBounds(this.editor, currentPagePoint)) {
+						this.onPointerDown({
+							...info,
+							target: 'selection',
+						})
+						return
 					}
 				}
 
@@ -511,3 +512,16 @@ export class Idle extends StateNode {
 export const MAJOR_NUDGE_FACTOR = 10
 export const MINOR_NUDGE_FACTOR = 1
 export const GRID_INCREMENT = 5
+
+function isPointInRotatedSelectionBounds(editor: Editor, point: VecLike) {
+	const { selectionBounds } = editor
+	if (!selectionBounds) return false
+
+	const { selectionRotation } = editor
+	if (!selectionRotation) return selectionBounds.containsPoint(point)
+
+	return pointInPolygon(
+		point,
+		selectionBounds.corners.map((c) => Vec2d.RotWith(c, selectionBounds.point, selectionRotation))
+	)
+}

packages/tldraw/src/lib/tools/selection-logic/selectOnCanvasPointerUp.ts
@@ -69,7 +69,11 @@ export function selectOnCanvasPointerUp(editor: Editor) {
 		} else {
 			// Otherwise, we clear the selction because the user selected
 			// nothing instead of their current selection.
-			editor.selectNone()
+
+			if (selectedShapeIds.length > 0) {
+				editor.mark('selecting none')
+				editor.selectNone()
+			}
 
 			// If the click was inside of the current focused group, then
 			// we keep that focused group; otherwise we clear the focused

packages/tldraw/src/test/selection-omnibus.test.ts
@@ -195,6 +195,74 @@ describe('when shape is hollow', () => {
 		editor.pointerUp()
 		expect(editor.selectedShapeIds).toEqual([])
 	})
+
+	it('drags draw shape child', () => {
+		editor
+			.selectAll()
+			.deleteShapes(editor.selectedShapeIds)
+			.setCurrentTool('draw')
+			.pointerMove(500, 500)
+			.pointerDown()
+			.pointerMove(501, 501)
+			.pointerMove(550, 550)
+			.pointerMove(599, 599)
+			.pointerMove(600, 600)
+			.pointerUp()
+			.selectAll()
+			.setCurrentTool('select')
+
+		expect(editor.selectedShapeIds.length).toBe(1)
+
+		// Not inside of the shape but inside of the selection bounds
+		editor.pointerMove(510, 590)
+		expect(editor.hoveredShapeId).toBe(null)
+
+		// Draw shapes have `hideSelectionBoundsBg` set to false
+		editor.pointerDown()
+		editor.expectToBeIn('select.pointing_selection')
+		editor.pointerUp()
+
+		editor.selectAll()
+		editor.rotateSelection(Math.PI)
+		editor.setCurrentTool('select')
+		editor.pointerMove(590, 510)
+
+		editor.pointerDown()
+		editor.expectToBeIn('select.pointing_selection')
+		editor.pointerUp()
+	})
+
+	it('does not drag arrow shape', () => {
+		editor
+			.selectAll()
+			.deleteShapes(editor.selectedShapeIds)
+			.setCurrentTool('arrow')
+			.pointerMove(500, 500)
+			.pointerDown()
+			.pointerMove(600, 600)
+			.pointerUp()
+			.selectAll()
+			.setCurrentTool('select')
+
+		expect(editor.selectedShapeIds.length).toBe(1)
+
+		// Not inside of the shape but inside of the selection bounds
+		editor.pointerMove(510, 590)
+		expect(editor.hoveredShapeId).toBe(null)
+
+		// Arrow shapes have `hideSelectionBoundsBg` set to true
+		editor.pointerDown()
+		editor.expectToBeIn('select.pointing_canvas')
+
+		editor.selectAll()
+		editor.rotateSelection(Math.PI)
+		editor.setCurrentTool('select')
+		editor.pointerMove(590, 510)
+
+		editor.pointerDown()
+		editor.expectToBeIn('select.pointing_canvas')
+		editor.pointerUp()
+	})
 })
 
 describe('when shape is a frame', () => {