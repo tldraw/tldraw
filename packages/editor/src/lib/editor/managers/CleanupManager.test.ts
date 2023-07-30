b3c51d55ceec6e48c14dd4876e7e7f1a70fa67a3
Steve Ruiz <steveruizok@gmail.com>
2023-07-30 12:59:50 +0100

    Update CleanupManager.test.ts

--- packages/editor/src/lib/editor/managers/CleanupManager.test.ts
+++ packages/editor/src/lib/editor/managers/CleanupManager.test.ts
@@ -1,14 +1,11 @@
-import { createTLStore } from '../../config/createTLStore'
-import { Editor } from '../Editor'
-
-let editor: Editor
-beforeEach(() => {
-	editor = new Editor({
-		shapeUtils: [],
-		tools: [],
-		store: createTLStore({ shapeUtils: [] }),
-		getContainer: () => document.body,
-	})
-})
+// let editor: Editor
+// beforeEach(() => {
+// 	editor = new Editor({
+// 		shapeUtils: [],
+// 		tools: [],
+// 		store: createTLStore({ shapeUtils: [] }),
+// 		getContainer: () => document.body,
+// 	})
+// })
 
 it.todo('Registers a batch complete handler')
