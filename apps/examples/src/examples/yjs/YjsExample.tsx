fdddbc16cbf4d6c0dbab6e95fb4f571d88a463af
Steve Ruiz <steveruizok@gmail.com>
2023-08-04 17:15:09 +0100

    remove yjs

--- apps/examples/src/examples/yjs/YjsExample.tsx
+++ apps/examples/src/examples/yjs/YjsExample.tsx
@@ -1,47 +1,11 @@
-import { Tldraw, track, useEditor } from '@tldraw/tldraw'
-import '@tldraw/tldraw/tldraw.css'
-import { useYjsStore } from './useYjsStore'
-
-const HOST_URL =
-	process.env.NODE_ENV === 'development' ? 'ws://localhost:1234' : 'wss://demos.yjs.dev'
-
 export default function YjsExample() {
-	const store = useYjsStore({
-		roomId: 'example6',
-		hostUrl: HOST_URL,
-	})
-
 	return (
 		<div className="tldraw__editor">
-			<Tldraw autoFocus store={store} shareZone={<NameEditor />} />
+			We've moved! See{' '}
+			<a href="https://github.com/tldraw/tldraw-yjs-example">
+				https://github.com/tldraw/tldraw-yjs-example
+			</a>
+			.
 		</div>
 	)
 }
-
-const NameEditor = track(() => {
-	const editor = useEditor()
-
-	const { color, name } = editor.user
-
-	return (
-		<div style={{ pointerEvents: 'all', display: 'flex' }}>
-			<input
-				type="color"
-				value={color}
-				onChange={(e) => {
-					editor.user.updateUserPreferences({
-						color: e.currentTarget.value,
-					})
-				}}
-			/>
-			<input
-				value={name}
-				onChange={(e) => {
-					editor.user.updateUserPreferences({
-						name: e.currentTarget.value,
-					})
-				}}
-			/>
-		</div>
-	)
-})
