tldraw/tldraw

commit 227c97a057a15426bcd0b243be2a384a1de9bdcf

[fix] awful rendering issue (#1842)
This PR fixes an extremely performance-crushing bug that was happening
in Safari and Chrome when iframes were present.

### Change Type

- [x] `patch` — Bug fix

### Test Plan

1. Create one hundred shapes
2. Create a gist or maps embed
3. Select all

If the app crashes or the rendering layers list grows to lots and lots
of layers, that's the bug.

### Release Notes

- [fix] iframe rendering issue

packages/editor/editor.css
@@ -221,14 +221,13 @@ input,
 	contain: strict;
 }
 
-.tl-shapes {
-	position: relative;
-	z-index: 1;
-}
-
-.tl-overlays {
-	position: relative;
-	z-index: 2;
+.tl-fixed-layer {
+	position: absolute;
+	top: 0px;
+	left: 0px;
+	width: 100%;
+	height: 100%;
+	contain: strict;
 }
 
 .tl-overlays__item {

packages/editor/src/lib/components/Canvas.tsx
@@ -27,6 +27,7 @@ export const Canvas = track(function Canvas({ className }: { className?: string 
 
 	const rCanvas = React.useRef<HTMLDivElement>(null)
 	const rHtmlLayer = React.useRef<HTMLDivElement>(null)
+	const rHtmlLayer2 = React.useRef<HTMLDivElement>(null)
 
 	useScreenBounds()
 	useDocumentEvents()
@@ -40,6 +41,8 @@ export const Canvas = track(function Canvas({ className }: { className?: string 
 		() => {
 			const htmlElm = rHtmlLayer.current
 			if (!htmlElm) return
+			const htmlElm2 = rHtmlLayer2.current
+			if (!htmlElm2) return
 
 			const { x, y, z } = editor.camera
 
@@ -50,6 +53,12 @@ export const Canvas = track(function Canvas({ className }: { className?: string 
 				z >= 1 ? modulate(z, [1, 8], [0.125, 0.5], true) : modulate(z, [0.1, 1], [-2, 0.125], true)
 
 			htmlElm.style.setProperty(
+				'transform',
+				`scale(${toDomPrecision(z)}) translate(${toDomPrecision(x + offset)}px,${toDomPrecision(
+					y + offset
+				)}px)`
+			)
+			htmlElm2.style.setProperty(
 				'transform',
 				`scale(${toDomPrecision(z)}) translate(${toDomPrecision(x + offset)}px,${toDomPrecision(
 					y + offset
@@ -92,22 +101,22 @@ export const Canvas = track(function Canvas({ className }: { className?: string 
 			{Background && <Background />}
 			<GridWrapper />
 			<UiLogger />
+			<svg className="tl-svg-context">
+				<defs>
+					{shapeSvgDefs}
+					{Cursor && <Cursor />}
+					<CollaboratorHint />
+					<ArrowheadDot />
+					<ArrowheadCross />
+					{SvgDefs && <SvgDefs />}
+				</defs>
+			</svg>
 			<div ref={rHtmlLayer} className="tl-html-layer" draggable={false}>
-				<svg className="tl-svg-context">
-					<defs>
-						{shapeSvgDefs}
-						{Cursor && <Cursor />}
-						<CollaboratorHint />
-						<ArrowheadDot />
-						<ArrowheadCross />
-						{SvgDefs && <SvgDefs />}
-					</defs>
-				</svg>
 				<SelectionBackgroundWrapper />
-				<div className="tl-shapes">
-					<ShapesToDisplay />
-				</div>
-				<div className="tl-overlays">
+				<ShapesToDisplay />
+			</div>
+			<div className="tl-fixed-layer">
+				<div ref={rHtmlLayer2} className="tl-html-layer">
 					{/* <GeometryDebuggingView /> */}
 					<HandlesWrapper />
 					<BrushWrapper />

packages/editor/src/lib/editor/Editor.ts
@@ -3039,8 +3039,8 @@ export class Editor extends EventEmitter<TLEventMap> {
 			maskedPageBounds: Box2d | undefined
 		}[] = []
 
-		let nextIndex = MAX_SHAPES_PER_PAGE
-		let nextBackgroundIndex = 0
+		let nextIndex = MAX_SHAPES_PER_PAGE * 2
+		let nextBackgroundIndex = MAX_SHAPES_PER_PAGE
 
 		// We only really need these if we're using editor state, but that's ok
 		const editingShapeId = this.editingShapeId

commit 046718f241d1919786daff3ce7cac58d98cfa013

[fix] overlays stacking (#1849)
This PR fixes a small bug introduced by #1842 where overlays would be
stacked behind shapes.

### Change Type

- [x] `patch` — Bug fix

packages/editor/editor.css
@@ -230,6 +230,16 @@ input,
 	contain: strict;
 }
 
+.tl-shapes {
+	position: relative;
+	z-index: 2;
+}
+
+.tl-overlays {
+	position: relative;
+	z-index: 3;
+}
+
 .tl-overlays__item {
 	position: absolute;
 	top: 0px;
@@ -291,7 +301,6 @@ input,
 	width: 1px;
 	height: 1px;
 	contain: layout style size;
-	z-index: 3;
 }
 
 /* ---------------------- Brush --------------------- */

packages/editor/src/lib/components/Canvas.tsx
@@ -52,18 +52,11 @@ export const Canvas = track(function Canvas({ className }: { className?: string 
 			const offset =
 				z >= 1 ? modulate(z, [1, 8], [0.125, 0.5], true) : modulate(z, [0.1, 1], [-2, 0.125], true)
 
-			htmlElm.style.setProperty(
-				'transform',
-				`scale(${toDomPrecision(z)}) translate(${toDomPrecision(x + offset)}px,${toDomPrecision(
-					y + offset
-				)}px)`
-			)
-			htmlElm2.style.setProperty(
-				'transform',
-				`scale(${toDomPrecision(z)}) translate(${toDomPrecision(x + offset)}px,${toDomPrecision(
-					y + offset
-				)}px)`
-			)
+			const transform = `scale(${toDomPrecision(z)}) translate(${toDomPrecision(
+				x + offset
+			)}px,${toDomPrecision(y + offset)}px)`
+			htmlElm.style.setProperty('transform', transform)
+			htmlElm2.style.setProperty('transform', transform)
 		},
 		[editor]
 	)
@@ -111,11 +104,11 @@ export const Canvas = track(function Canvas({ className }: { className?: string 
 					{SvgDefs && <SvgDefs />}
 				</defs>
 			</svg>
-			<div ref={rHtmlLayer} className="tl-html-layer" draggable={false}>
+			<div ref={rHtmlLayer} className="tl-html-layer tl-shapes" draggable={false}>
 				<SelectionBackgroundWrapper />
 				<ShapesToDisplay />
 			</div>
-			<div className="tl-fixed-layer">
+			<div className="tl-fixed-layer tl-overlays">
 				<div ref={rHtmlLayer2} className="tl-html-layer">
 					{/* <GeometryDebuggingView /> */}
 					<HandlesWrapper />