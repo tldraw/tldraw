fb1722e378be617773442dce44995a82dc48ca38
David Sheldrick <d.j.sheldrick@gmail.com>
2023-09-08 09:57:49 +0100

    fix setupTests

--- packages/tldraw/setupTests.js
+++ packages/tldraw/setupTests.js
@@ -15,7 +15,19 @@ document.fonts = {
 	[Symbol.iterator]: () => [][Symbol.iterator](),
 }
 
-global.matchMedia = () => false
+Object.defineProperty(window, 'matchMedia', {
+	writable: true,
+	value: jest.fn().mockImplementation((query) => ({
+		matches: false,
+		media: query,
+		onchange: null,
+		addListener: jest.fn(), // Deprecated
+		removeListener: jest.fn(), // Deprecated
+		addEventListener: jest.fn(),
+		removeEventListener: jest.fn(),
+		dispatchEvent: jest.fn(),
+	})),
+})
 
 Object.defineProperty(global.URL, 'createObjectURL', {
 	writable: true,
