1ab6d33f8732683d9c3e445169fa1095a8577c27
David Sheldrick <d.j.sheldrick@gmail.com>
2023-09-19 13:59:41 +0100

    use smarter calculation for shape width/height

--- packages/editor/src/lib/hooks/useDPRMultiple.ts
+++ packages/editor/src/lib/hooks/useDPRMultiple.ts
@@ -8,7 +8,8 @@ function gcd(a: number, b: number): number {
 	return b === 0 ? a : gcd(b, a % b)
 }
 
-function nearestMultiple(float: number) {
+// Returns the lowest value that the given number can be multiplied by to reach an integer
+export function nearestMultiple(float: number) {
 	const decimal = float.toString().split('.')[1]
 	if (!decimal) return 1
 	const denominator = Math.pow(10, decimal.length)
