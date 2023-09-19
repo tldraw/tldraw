021675676466b157b4ab24c32133dc00fae848ed
Lu[ke] Wilson <l2wilson94@gmail.com>
2023-09-19 10:53:55 +0100

    add failing test

--- packages/tldraw/src/test/selection-omnibus.test.ts
+++ packages/tldraw/src/test/selection-omnibus.test.ts
@@ -312,6 +312,38 @@ describe('when shape is hollow', () => {
 		editor.expectToBeIn('select.pointing_canvas')
 		editor.pointerUp()
 	})
+
+	it('does not drag line shape', () => {
+		editor
+			.selectAll()
+			.deleteShapes(editor.selectedShapeIds)
+			.setCurrentTool('line')
+			.pointerMove(500, 500)
+			.pointerDown()
+			.pointerMove(600, 600)
+			.pointerUp()
+			.selectAll()
+			.setCurrentTool('select')
+
+		expect(editor.selectedShapeIds.length).toBe(1)
+
+		// Not inside of the shape but inside of the selection bounds
+		editor.pointerMove(510, 590)
+		expect(editor.hoveredShapeId).toBe(null)
+
+		// Line shapes have `hideSelectionBoundsBg` set to true
+		editor.pointerDown()
+		editor.expectToBeIn('select.pointing_canvas')
+
+		editor.selectAll()
+		editor.rotateSelection(Math.PI)
+		editor.setCurrentTool('select')
+		editor.pointerMove(590, 510)
+
+		editor.pointerDown()
+		editor.expectToBeIn('select.pointing_canvas')
+		editor.pointerUp()
+	})
 })
 
 describe('when shape is a frame', () => {
