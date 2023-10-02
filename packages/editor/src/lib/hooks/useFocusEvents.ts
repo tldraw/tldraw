56cddb59f812ca199e1894e408d54e44d2101c27
Steve Ruiz <steveruizok@gmail.com>
2023-10-02 12:25:42 +0100

    Update useFocusEvents.ts

--- packages/editor/src/lib/hooks/useFocusEvents.ts
+++ packages/editor/src/lib/hooks/useFocusEvents.ts
@@ -5,10 +5,10 @@ import { useEditor } from './useEditor'
 export function useFocusEvents(autoFocus: boolean) {
 	const editor = useEditor()
 	useLayoutEffect(() => {
-		if (autoFocus) {
+		if (autoFocus && !editor.instanceState.isFocused) {
 			editor.updateInstanceState({ isFocused: true })
 			editor.getContainer().focus()
-		} else {
+		} else if (editor.instanceState.isFocused) {
 			editor.updateInstanceState({ isFocused: false })
 		}
 	}, [editor, autoFocus])
