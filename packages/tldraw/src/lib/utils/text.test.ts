f9df1097c72a88e1a71fc978b84c0a3f1d56fccb
David Sheldrick <d.j.sheldrick@gmail.com>
2023-11-01 15:56:26 +0000

    another test

--- packages/tldraw/src/lib/utils/text.test.ts
+++ packages/tldraw/src/lib/utils/text.test.ts
@@ -4,4 +4,8 @@ describe(cleanupText, () => {
 	it('can handle the empty string', () => {
 		expect(cleanupText('')).toBe('')
 	})
+	it('can handle space-only strings', () => {
+		expect(cleanupText(' ')).toBe('')
+		expect(cleanupText('   ')).toBe('')
+	})
 })
