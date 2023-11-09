1f47a5aef4dbd88513b9416a8711bca676e02f6b
Mitja Bezenšek <mitja.bezensek@gmail.com>
2023-11-09 12:17:01 +0100

    Fix keyboard shortcuts for vscode. (#2181)
    It looks like loading the file unfocused the editor. And since it
    [wasn't focused we didn't register keyboard
    shortcuts](https://github.com/tldraw/tldraw/blob/mitja/fix-keyboardshortcuts/packages/tldraw/src/lib/ui/hooks/useKeyboardShortcuts.ts#L27).
    
    This makes sure that the focus after loading the file stays the same as
    it was before loading it.
    
    Fixes: #2028
    Fixes: #2116
    
    ### Change Type
    
    - [x] `patch` — Bug fix
    - [ ] `minor` — New feature
    - [ ] `major` — Breaking change
    - [ ] `dependencies` — Changes to package dependencies[^1]
    - [ ] `documentation` — Changes to the documentation only[^2]
    - [ ] `tests` — Changes to any test code only[^2]
    - [ ] `internal` — Any other changes that don't affect the published
    package[^2]
    - [ ] I don't know
    
    [^1]: publishes a `patch` release, for devDependencies use `internal`
    [^2]: will not publish a new version
    
    ### Test Plan
    
    1.  Run the VS Code extension.
    2. Open a new file with the `tldraw: New project` command.
    3. Make sure keyboard shortcuts are working.
    
    ### Release Notes
    
    - Fixes keyboard shortcuts for VS Code extension.

--- packages/tldraw/src/lib/utils/file.ts
+++ packages/tldraw/src/lib/utils/file.ts
@@ -279,6 +279,7 @@ export async function parseAndLoadDocument(
 	// this file before they'll get their camera etc.
 	// restored. we could change this in the future.
 	transact(() => {
+		const isFocused = editor.instanceState.isFocused
 		editor.store.clear()
 		const [shapes, nonShapes] = partition(
 			parseFileResult.value.allRecords(),
@@ -295,6 +296,7 @@ export async function parseAndLoadDocument(
 		if (bounds) {
 			editor.zoomToBounds(bounds, 1)
 		}
+		editor.updateInstanceState({ isFocused })
 	})
 
 	if (forceDarkMode) editor.user.updateUserPreferences({ isDarkMode: true })
