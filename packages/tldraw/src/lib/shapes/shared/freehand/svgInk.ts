39897c86f99166892cd6f872e8943f6158c85795
David Sheldrick <d.j.sheldrick@gmail.com>
2023-11-28 15:11:38 +0000

    distill the logic

--- packages/tldraw/src/lib/shapes/shared/freehand/svgInk.ts
+++ packages/tldraw/src/lib/shapes/shared/freehand/svgInk.ts
@@ -3,7 +3,6 @@ import {
 	VecLike,
 	assert,
 	average,
-	canonicalizeRotation,
 	precise,
 	shortAngleDist,
 	toDomPrecision,
@@ -23,8 +22,7 @@ export function svgInk(rawInputPoints: VecLike[], options: StrokeOptions = {}) {
 
 	const points = getStrokePoints(rawInputPoints, options)
 	setStrokePointRadii(points, options)
-	const [elbowIndices, superElbowIndices] = getElbowIndices(points)
-	const partitions = partitionByAngle(points, elbowIndices, superElbowIndices)
+	const partitions = partitionAtElbows(points)
 	let svg = ''
 	for (const partition of partitions) {
 		svg += renderPartition(partition, options)
@@ -33,23 +31,11 @@ export function svgInk(rawInputPoints: VecLike[], options: StrokeOptions = {}) {
 	return svg
 }
 
-function averageAngle(a: number, b: number) {
-	const diff = shortAngleDist(a, b)
-	return a + diff / 2
-}
+function partitionAtElbows(points: StrokePoint[]): StrokePoint[][] {
+	if (points.length <= 2) return [points]
 
-function getElbowIndices(points: StrokePoint[]): [number[], number[]] {
-	if (points.length === 1) return [[0], []]
-	if (points.length === 2) return [[0, 1], []]
-
-	const withAngles = [
-		{
-			point: points[0],
-			angle: Vec2d.Angle(points[0].point, points[1].point),
-			acuteness: 1,
-			angularDist: +Infinity,
-		},
-	] as { point: StrokePoint; angle: number; acuteness: number; angularDist: number }[]
+	const result: StrokePoint[][] = []
+	let currentPartition: StrokePoint[] = [points[0]]
 	for (let i = 1; i < points.length - 1; i++) {
 		const prevPoint = points[i - 1]
 		const thisPoint = points[i]
@@ -60,108 +46,85 @@ function getElbowIndices(points: StrokePoint[]): [number[], number[]] {
 		// 1 is an infinitely thin wedge
 		// 0 is a straight line
 		const acuteness = Math.abs(shortAngleDist(prevAngle, nextAngle)) / Math.PI
+		if (acuteness > 0.8) {
+			// always treat such acute angles as elbows
+			// and use the extended .input point as the elbow point for swooshiness in fast zaggy lines
+			const elbowPoint = {
+				...thisPoint,
+				point: thisPoint.input,
+			}
+			currentPartition.push(elbowPoint)
+			result.push(cleanUpPartition(currentPartition))
+			currentPartition = [elbowPoint]
+			continue
+		}
+		currentPartition.push(thisPoint)
+		if (acuteness < 0.25) {
+			// this is not an elbow, bail out
+			continue
+		}
+		// so now we have a reasonably acute angle but it might not be an elbow if it's far
+		// away from it's neighbors
 		const avgRadius = (prevPoint.radius + thisPoint.radius + nextPoint.radius) / 3
 		const incomingNormalizedDist = Vec2d.Dist(prevPoint.point, thisPoint.point) / avgRadius
 		const outgoingNormalizedDist = Vec2d.Dist(thisPoint.point, nextPoint.point) / avgRadius
 		// angular dist is a normalized representation of how far away the point is from it's neighbors
 		// (normalized by the radius)
-		// if it's far away from both neighbors and the angle is not particularly acute, it's probably
-		// best to treat it as a soft curvy elbow rather than a hard elbow
 		const angularDist = incomingNormalizedDist + outgoingNormalizedDist
-		withAngles.push({
-			point: thisPoint,
-			angle: canonicalizeRotation(averageAngle(prevAngle, nextAngle)),
-			acuteness,
-			angularDist,
-		})
-	}
-	withAngles.push({
-		point: points[points.length - 1],
-		angle: Vec2d.Angle(points[points.length - 2].point, points[points.length - 1].point),
-		acuteness: 1,
-		angularDist: +Infinity,
-	})
-	const elbowIndices = [0]
-	const superElbowIndices = []
-	for (let i = 1; i < withAngles.length - 1; i++) {
-		const point = withAngles[i]
-		if (point.acuteness > 0.8) {
-			elbowIndices.push(i)
-			superElbowIndices.push(i)
-		} else if (point.acuteness > 0.25 && point.angularDist < 1.5) {
-			elbowIndices.push(i)
+		if (angularDist < 1.5) {
+			// if this point is kinda close to its neighbors and it has a reasonably
+			// acute angle, it's probably a hard elbow
+			currentPartition.push(thisPoint)
+			result.push(cleanUpPartition(currentPartition))
+			currentPartition = [thisPoint]
+			continue
 		}
 	}
-	elbowIndices.push(points.length - 1)
-	return [elbowIndices, superElbowIndices]
+	currentPartition.push(points[points.length - 1])
+	result.push(cleanUpPartition(currentPartition))
+
+	return result
 }
 
-function partitionByAngle(
-	points: StrokePoint[],
-	elbowIndices: number[],
-	superElbowIndices: number[]
-): StrokePoint[][] {
-	if (points.length <= 2) return [points]
-
-	if (superElbowIndices.length) {
-		points = points.map((point, i) => {
-			if (!superElbowIndices.includes(i)) {
-				return point
-			}
-			return {
-				...point,
-				point: point.input,
-			}
-		})
+function cleanUpPartition(partition: StrokePoint[]) {
+	// clean up start of partition (remove points that are too close to the start)
+	const startPoint = partition[0]
+	while (partition.length > 2) {
+		const nextPoint = partition[1]
+		const dist = Vec2d.Dist(startPoint.point, nextPoint.point)
+		const avgRadius = (startPoint.radius + nextPoint.radius) / 2
+		if (dist < avgRadius * 0.5) {
+			partition.splice(1, 1)
+		} else {
+			break
+		}
 	}
-
-	const partitions: StrokePoint[][] = []
-
-	for (let i = 0; i < elbowIndices.length - 1; i++) {
-		const start = elbowIndices[i]
-		const end = elbowIndices[i + 1]
-		const partition = points.slice(start, end + 1)
-		// clean up start of partition
-		const startPoint = partition[0]
-		while (partition.length > 2) {
-			const nextPoint = partition[1]
-			// if the two are very close to each other
-			const dist = Vec2d.Dist(startPoint.point, nextPoint.point)
-			const avgRadius = (startPoint.radius + nextPoint.radius) / 2
-			if (dist < avgRadius * 0.5) {
-				partition.splice(1, 1)
-			} else {
-				break
-			}
+	// clean up end of partition in the same fashion
+	const endPoint = partition[partition.length - 1]
+	while (partition.length > 2) {
+		const prevPoint = partition[partition.length - 2]
+		const dist = Vec2d.Dist(endPoint.point, prevPoint.point)
+		const avgRadius = (endPoint.radius + prevPoint.radius) / 2
+		if (dist < avgRadius * 0.5) {
+			partition.splice(partition.length - 2, 1)
+		} else {
+			break
 		}
-		// clean up end of partition
-		const endPoint = partition[partition.length - 1]
-		while (partition.length > 2) {
-			const prevPoint = partition[partition.length - 2]
-			const dist = Vec2d.Dist(endPoint.point, prevPoint.point)
-			const avgRadius = (endPoint.radius + prevPoint.radius) / 2
-			if (dist < avgRadius * 0.5) {
-				partition.splice(partition.length - 2, 1)
-			} else {
-				break
-			}
-		}
-		if (partition.length > 1) {
-			partition[0] = {
-				...partition[0],
-				vector: Vec2d.FromAngle(Vec2d.Angle(partition[1].point, partition[0].point)),
-			}
-			partition[partition.length - 1] = {
-				...partition[partition.length - 1],
-				vector: Vec2d.FromAngle(
-					Vec2d.Angle(partition[partition.length - 1].point, partition[partition.length - 2].point)
-				),
-			}
-		}
-		partitions.push(partition)
 	}
-
-	return partitions
+	// now readjust the cap point vectors to point to their nearest neighbors
+	if (partition.length > 1) {
+		partition[0] = {
+			...partition[0],
+			vector: Vec2d.FromAngle(Vec2d.Angle(partition[1].point, partition[0].point)),
+		}
+		partition[partition.length - 1] = {
+			...partition[partition.length - 1],
+			vector: Vec2d.FromAngle(
+				Vec2d.Angle(partition[partition.length - 1].point, partition[partition.length - 2].point)
+			),
+		}
+	}
+	return partition
 }
 
 function circlePath(cx: number, cy: number, r: number) {
