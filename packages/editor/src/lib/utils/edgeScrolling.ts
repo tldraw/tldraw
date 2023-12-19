8c0f6eb8a7bc05d1221ca66e5450a843375ed3ac
Steve Ruiz <steveruizok@gmail.com>
2023-12-19 11:25:40 +0000

    ok

--- packages/editor/src/lib/utils/edgeScrolling.ts
+++ packages/editor/src/lib/utils/edgeScrolling.ts
@@ -1,24 +1,21 @@
-import { EDGE_SCROLL_SPEED } from '../constants'
+import { EDGE_SCROLL_DISTANCE, EDGE_SCROLL_SPEED } from '../constants'
 import { Editor } from '../editor/Editor'
 
 /**
- * Helper function to get the scroll offset for a given position.
- * The closer the mouse is to the edge of the screen the faster we scroll.
- * We also adjust the speed and the start offset based on the screen size and zoom level.
- *
- * @param editor - The mouse position on the screen in pixels
- * @returns How much we should scroll in pixels
+ * Helper function to get the scroll proximity factor for a given position.
+ * @param position - The mouse position on the axis.
+ * @param dimension - The component dimension on the axis.
  * @internal
  */
-export function getEdgeProximityFactor(position: number, scrollOffset: number, extreme: number) {
+export function getEdgeProximityFactor(position: number, dimension: number) {
 	if (position < 0) {
 		return 1
-	} else if (position > extreme) {
+	} else if (position > dimension) {
 		return -1
-	} else if (position < scrollOffset) {
-		return (scrollOffset - position) / scrollOffset
-	} else if (position > extreme - scrollOffset) {
-		return -(scrollOffset - extreme + position) / scrollOffset
+	} else if (position < EDGE_SCROLL_DISTANCE) {
+		return (EDGE_SCROLL_DISTANCE - position) / EDGE_SCROLL_DISTANCE
+	} else if (position > dimension - EDGE_SCROLL_DISTANCE) {
+		return -(EDGE_SCROLL_DISTANCE - dimension + position) / EDGE_SCROLL_DISTANCE
 	}
 	return 0
 }
@@ -38,22 +35,17 @@ export function moveCameraWhenCloseToEdge(editor: Editor) {
 	const zoomLevel = editor.getZoomLevel()
 	const screenBounds = editor.getViewportScreenBounds()
 
-	// Determines how far from the edges we start the scroll behaviour
-	const insetX = screenBounds.w < 1000 ? 40 : 32
-	const insetY = screenBounds.h < 1000 ? 40 : 32
-
 	// Determines how much the speed is affected by the screen size
 	const screenSizeFactorX = screenBounds.w < 1000 ? 0.612 : 1
 	const screenSizeFactorY = screenBounds.h < 1000 ? 0.612 : 1
 
-	// Determines the base speed of the scroll
-	const pxSpeed = editor.user.getEdgeScrollSpeed() * EDGE_SCROLL_SPEED
-
-	const proximityFactorX = getEdgeProximityFactor(x, insetX, screenBounds.w)
-	const proximityFactorY = getEdgeProximityFactor(y, insetY, screenBounds.h)
+	const proximityFactorX = getEdgeProximityFactor(x, screenBounds.w)
+	const proximityFactorY = getEdgeProximityFactor(y, screenBounds.h)
 
 	if (proximityFactorX === 0 && proximityFactorY === 0) return
 
+	// Determines the base speed of the scroll
+	const pxSpeed = editor.user.getEdgeScrollSpeed() * EDGE_SCROLL_SPEED
 	const scrollDeltaX = (pxSpeed * proximityFactorX * screenSizeFactorX) / zoomLevel
 	const scrollDeltaY = (pxSpeed * proximityFactorY * screenSizeFactorY) / zoomLevel
 
