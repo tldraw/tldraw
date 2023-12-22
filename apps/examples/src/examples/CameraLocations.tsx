e8f0d4eb6e33e68bbc2147007ed92fbd3cf21fe9
Mitja Bezenšek <mitja.bezensek@gmail.com>
2023-12-22 16:02:39 +0100

    Add wrap around. Show the slide list in front of the canvas.

--- apps/examples/src/examples/CameraLocations.tsx
+++ apps/examples/src/examples/CameraLocations.tsx
@@ -1,4 +1,17 @@
-import { EASINGS, Editor, Tldraw, Vec2d } from '@tldraw/tldraw'
+import {
+	Atom,
+	Button,
+	EASINGS,
+	Editor,
+	Icon,
+	TLEditorComponents,
+	Tldraw,
+	Vec2d,
+	atom,
+	stopEventPropagation,
+	track,
+	useEditor,
+} from '@tldraw/tldraw'
 import '@tldraw/tldraw/tldraw.css'
 import { useEffect, useState } from 'react'
 
@@ -20,23 +33,91 @@ const getCurrentPosition = (editor: Editor) => {
 	return new Vec2d(camera.x, camera.y, camera.z)
 }
 
-export default function CameraLoctionsExample() {
+const SlideList = track(() => {
+	const editor = useEditor()
+	const locations = cameraLocations.get()
+	if (locations.length === 0) return null
+	const index = getLocationIndex(locations, getCurrentPosition(editor))
+	return (
+		<div
+			style={{
+				position: 'absolute',
+				display: 'flex',
+				flexDirection: 'column',
+				gap: 4,
+				top: 100,
+				left: 8,
+				padding: 4,
+				backgroundColor: 'var(--color-low)',
+				pointerEvents: 'all',
+				borderRadius: 8,
+			}}
+			onPointerDown={(e) => stopEventPropagation(e)}
+		>
+			{locations.map((location, i) => {
+				return (
+					<div
+						key={location.key + location.position.x + location.position.y}
+						style={{
+							display: 'flex',
+							gap: '4px',
+							alignItems: 'center',
+							borderRadius: 6,
+						}}
+					>
+						<Button
+							type="normal"
+							style={{
+								background: index === i ? '#f9fafb' : 'transparent',
+								borderRadius: 6,
+							}}
+							key={location.key}
+							onClick={() => {
+								editor.setCamera(location.position, {
+									duration: 200,
+									easing: EASINGS.easeInOutQuad,
+								})
+							}}
+						>
+							{`Slide ${i + 1}`}
+						</Button>
+						<Button type="normal">
+							<Icon
+								icon="trash"
+								onClick={() => cameraLocations.set(locations.filter((_l, index) => i !== index))}
+							/>
+						</Button>
+					</div>
+				)
+			})}
+		</div>
+	)
+})
+
+const components: TLEditorComponents = {
+	InFrontOfTheCanvas: SlideList,
+}
+
+const cameraLocations: Atom<CameraLocation[]> = atom('cameraLocations', [] as CameraLocation[])
+
+const CameraLoctionsExample = track(() => {
 	const [editor, setEditor] = useState<Editor | null>(null)
-	const [cameraLocations, setCameraLocations] = useState<CameraLocation[]>([])
 
 	useEffect(() => {
 		function handleKeyDown(e: KeyboardEvent) {
 			if (!editor) return
 
+			const locations = cameraLocations.get()
+
 			if (cameraMoveKeys.includes(e.key)) {
 				if (e.shiftKey) {
 					const position = getCurrentPosition(editor)
-					const index = getLocationIndex(cameraLocations, position)
+					const index = getLocationIndex(locations, position)
 					if (index === -1) {
-						setCameraLocations([...cameraLocations, { key: e.key, position: position }])
+						cameraLocations.set([...locations, { key: e.key, position: position }])
 					} else {
-						setCameraLocations(
-							cameraLocations.map((location, i) => {
+						cameraLocations.set(
+							locations.map((location, i) => {
 								if (i === index) return { ...location, position: position }
 
 								return location
@@ -44,7 +125,7 @@ export default function CameraLoctionsExample() {
 						)
 					}
 				} else {
-					const location = getLocationByKey(cameraLocations, e.key)
+					const location = getLocationByKey(locations, e.key)
 					if (!location) return
 
 					editor.setCamera(location.position, { duration: 200, easing: EASINGS.easeInOutQuad })
@@ -52,28 +133,28 @@ export default function CameraLoctionsExample() {
 			}
 			if (e.key === 'ArrowRight' && e.shiftKey) {
 				const position = getCurrentPosition(editor)
-				const locationIndex = getLocationIndex(cameraLocations, position)
+				const locationIndex = getLocationIndex(locations, position)
 				if (locationIndex === -1) return
 
-				if (locationIndex < cameraLocations.length - 1) {
-					editor.setCamera(cameraLocations[locationIndex + 1].position, {
-						duration: 200,
-						easing: EASINGS.easeInOutQuad,
-					})
-				}
-			}
-
-			if (e.key === 'ArrowLeft' && e.shiftKey) {
-				const position = getCurrentPosition(editor)
-				const locationIndex = getLocationIndex(cameraLocations, position)
-				if (locationIndex < 1) return
-
-				editor.setCamera(cameraLocations[locationIndex - 1].position, {
+				editor.setCamera(locations[(locationIndex + 1) % locations.length].position, {
 					duration: 200,
 					easing: EASINGS.easeInOutQuad,
 				})
 			}
-			console.log(cameraLocations)
+
+			if (e.key === 'ArrowLeft' && e.shiftKey) {
+				const position = getCurrentPosition(editor)
+				const locationIndex = getLocationIndex(locations, position)
+				if (locationIndex < 1) return
+
+				editor.setCamera(
+					locations[(locationIndex - 1 + locations.length) % locations.length].position,
+					{
+						duration: 200,
+						easing: EASINGS.easeInOutQuad,
+					}
+				)
+			}
 		}
 
 		document.addEventListener('keydown', handleKeyDown)
@@ -81,11 +162,13 @@ export default function CameraLoctionsExample() {
 		return () => {
 			document.removeEventListener('keydown', handleKeyDown)
 		}
-	}, [cameraLocations, editor])
+	}, [editor])
 
 	return (
 		<div className="tldraw__editor">
-			<Tldraw persistenceKey="tldraw_example" onMount={setEditor} />
+			<Tldraw persistenceKey="tldraw_example" onMount={setEditor} components={components} />
 		</div>
 	)
-}
+})
+
+export default CameraLoctionsExample
