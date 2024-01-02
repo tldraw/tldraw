e57a9117eb103b2d3fd319b20f0f2a06368e7796
Steve Ruiz <steveruizok@gmail.com>
2024-01-02 23:37:10 +0000

    undoing this change, but it should be done

--- packages/editor/src/lib/editor/shapes/shared/arrow/shared.ts
+++ packages/editor/src/lib/editor/shapes/shared/arrow/shared.ts
@@ -121,10 +121,7 @@ export const BOUND_ARROW_OFFSET = 10
 /** @internal */
 export const WAY_TOO_BIG_ARROW_BEND_FACTOR = 10
 
-/**
- * Duplicated in tldraw library
- * todo: all this should move out when we're able to abstract away arrows
- * @internal */
+/** @public */
 export const STROKE_SIZES: Record<string, number> = {
 	s: 2,
 	m: 3.5,
