ed7eb2e136c23f74e29e78a55af2e1423fdc6342
Steve Ruiz <steveruizok@gmail.com>
2024-01-03 12:13:51 +0000

    Update edgeScrolling.ts

--- packages/editor/src/lib/utils/edgeScrolling.ts
+++ packages/editor/src/lib/utils/edgeScrolling.ts
@@ -40,7 +40,7 @@ export function moveCameraWhenCloseToEdge(editor: Editor) {
 	const screenSizeFactorY = screenBounds.h < 1000 ? 0.612 : 1
 
 	const proximityFactorX = getEdgeProximityFactor(x - screenBounds.x, screenBounds.w)
-	const proximityFactorY = getEdgeProximityFactor(y - screenBounds.x, screenBounds.h)
+	const proximityFactorY = getEdgeProximityFactor(y - screenBounds.y, screenBounds.h)
 
 	if (proximityFactorX === 0 && proximityFactorY === 0) return
 
