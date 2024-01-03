f7f2e935a64be6b939f9eaada0cb11bde87c0deb
Taha <98838967+Taha-Hassan-Git@users.noreply.github.com>
2024-01-03 16:28:49 +0000

    fix speech bubble typo

--- apps/examples/src/examples/speech-bubble/SpeechBubble/SpeechBubbleTool.tsx
+++ apps/examples/src/examples/speech-bubble/SpeechBubble/SpeechBubbleTool.tsx
@@ -7,7 +7,7 @@ export class SpeechBubbleTool extends BaseBoxShapeTool {
 }
 
 /*
-This file contains our speech bubble tool. The tool is a StateNode with the `id` "screenshot".
+This file contains our speech bubble tool. The tool is a StateNode with the `id` "speech-bubble".
 
 We get a lot of functionality for free by extending the BaseBoxShapeTool. For an example of a tool
 with more custom functionality, check out the screenshot-tool example. 
