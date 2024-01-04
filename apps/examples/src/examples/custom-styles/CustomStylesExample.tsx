334bed75fd23df89c460e4a6d1cd060c304504e1
Taha <98838967+Taha-Hassan-Git@users.noreply.github.com>
2024-01-04 12:51:41 +0000

    annotate util

--- apps/examples/src/examples/custom-styles/CustomStylesExample.tsx
+++ apps/examples/src/examples/custom-styles/CustomStylesExample.tsx
@@ -28,21 +28,27 @@ export default function CustomStylesExample() {
 /*
 Introduction:
 
-This example shows how to create your own custom styles to use with your shapes. It also shows
-how to create a very simple ui for your styles. In this example, we create a custom style for
-a card shape that lets the user apply a filter.
+This example shows how to create your own custom styles to use with your shapes. 
+It also shows how to create a very simple ui for your styles. In this example, we 
+create a custom style for a card shape that lets the user apply a filter to blur,
+invert or grayscale the card.
 
 [1]
-We define an array to hold the custom shape util and cusom tool. It's important to do this outside of
-any React component so that this array doesn't get redefined on every render. We'll pass this into the 
-Tldraw component's `shapeUtils` and `tools` props.
+We define an array to hold the custom shape util and cusom tool. It's important to 
+do this outside of any React component so that this array doesn't get redefined on 
+every render. We'll pass this into the Tldraw component's `shapeUtils` and `tools` 
+props.
 
-Check out SpeechBubbleUtil.tsx and SpeechBubbleTool.tsx to see how we define the shape util and tool.
+Check out CardShape.tsx to see how we define the shape util, tool and the custom 
+style.
 
 [2]
-We pass the custom shape util and tool into the Tldraw component's `shapeUtils` and `tools` props.
-We also pass in the custom ui overrides. And render our FilterStyleUi component inside the Tldraw 
-component. Check out FilterStyleUi.tsx to see how we render this Ui only when the user has selected
-a shape that uses the custom style.
+We pass the custom shape util and tool into the Tldraw component's `shapeUtils` and 
+`tools` props. We also pass in the custom ui overrides, this will make an icon for 
+our shape/tool appear on the toolbar (see ui-overrides.ts). And render our 
+FilterStyleUi component inside the Tldraw component.
+
+Check out FilterStyleUi.tsx to see how we render this Ui only when the user has 
+selected a shape that uses the custom style.
 
 */
