dfeeaffcff3d9cdc05dc27353fdaff3734759eed
Taha <98838967+Taha-Hassan-Git@users.noreply.github.com>
2024-01-11 15:13:21 +0000

    Annotate shape meta data example

--- apps/examples/src/examples/shape-meta/ShapeMetaExample.tsx
+++ apps/examples/src/examples/shape-meta/ShapeMetaExample.tsx
@@ -1,6 +1,9 @@
 import { TLShape, Tldraw, track, useEditor } from '@tldraw/tldraw'
 import '@tldraw/tldraw/tldraw.css'
 
+// There's a guide at the bottom of this file!
+
+// [1]
 export default function ShapeMetaExample() {
 	return (
 		<div className="tldraw__editor">
@@ -18,10 +21,10 @@ export default function ShapeMetaExample() {
 	)
 }
 
-// By default, the TLShape type's meta property is { [key: string]: any }, but we can type it
-// by unioning the type with a new type that has a meta property of our choosing.
+// [2]
 type ShapeWithMyMeta = TLShape & { meta: { label: string } }
 
+// [3]
 export const ShapeLabelUiWithHelper = track(function ShapeLabelUiWithHelper() {
 	const editor = useEditor()
 	const onlySelectedShape = editor.getOnlySelectedShape() as ShapeWithMyMeta | null
@@ -47,3 +50,29 @@ export const ShapeLabelUiWithHelper = track(function ShapeLabelUiWithHelper() {
 		</div>
 	)
 })
+
+/* 
+This example shows how to use the `getInitialMetaForShape` function to set initial
+meta data for a shape and update it.
+
+[1]
+We pass a function to the `onMount` prop of the Tldraw component. This function will
+override the default `getInitialMetaForShape` function. This function is called when
+a shape is created and when a shape is duplicated. 
+
+[2]
+By default, the TLShape type's meta property is { [key: string]: any }, but we can type
+it using a union like this.
+
+[3]
+This is our minimal ui for editing the meta data of our shapes. We use the `track` 
+function to make sure this component is reactive, it will re-render when the signals
+it is tracking change. Check out the signia docs for more: 
+https://signia.tldraw.dev/docs/API/signia_react/functions/track
+
+Because our component is a child of the Tldraw component, it has access to the `useEditor` 
+hook via React context. We use this to get the only selected shape. If there is a single 
+shape selected, we render the input and update the shape's meta data when the input changes 
+via the `onChange` function.
+
+*/
