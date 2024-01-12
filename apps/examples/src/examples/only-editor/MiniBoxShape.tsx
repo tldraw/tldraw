6c5dd85feb84dd10cda45b7aa2f78907c9393767
Taha <98838967+Taha-Hassan-Git@users.noreply.github.com>
2024-01-12 15:52:28 +0000

    Fix and annotate minimal example (#2448)
    The minimal example code made reference to a delete functionality that
    wasn't working. I managed to get it working again and updated the
    background component accordingly. I'm not sure of the reason to check
    for a hovered shape before checking if you hit a shape, so I took that
    part out.
    
    There was also a check to see if the target was a 'selection' that
    didn't seem to ever fire, so I removed that as the selection behaviour
    worked fine.
    
    The main thing is the annotations though.
    
    ### Change Type
    
    - [ ] `patch` — Bug fix
    - [ ] `minor` — New feature
    - [ ] `major` — Breaking change
    - [ ] `dependencies` — Changes to package dependencies[^1]
    - [x] `documentation` — Changes to the documentation only[^2]
    - [ ] `tests` — Changes to any test code only[^2]
    - [ ] `internal` — Any other changes that don't affect the published
    package[^2]
    - [ ] I don't know
    
    [^1]: publishes a `patch` release, for devDependencies use `internal`
    [^2]: will not publish a new version
    
    ### Test Plan
    
    1. Add a step-by-step description of how to test your PR here.
    2.
    
    - [ ] Unit Tests
    - [ ] End to end tests
    
    ### Release Notes
    
    - Fix and annotate minimal example

--- apps/examples/src/examples/only-editor/MiniBoxShape.tsx
+++ apps/examples/src/examples/only-editor/MiniBoxShape.tsx
@@ -1,14 +1,19 @@
 import { BaseBoxShapeUtil, HTMLContainer, TLBaseShape } from '@tldraw/tldraw'
 
+// There's a guide at the bottom of this page!
+
+// [1]
 export type MiniBoxShape = TLBaseShape<'box', { w: number; h: number; color: string }>
 
+// [2]
 export class MiniBoxShapeUtil extends BaseBoxShapeUtil<MiniBoxShape> {
+	//[a]
 	static override type = 'box'
-
+	//[b]
 	override getDefaultProps(): MiniBoxShape['props'] {
 		return { w: 100, h: 100, color: '#efefef' }
 	}
-
+	//[c]
 	component(shape: MiniBoxShape) {
 		return (
 			<HTMLContainer>
@@ -24,8 +29,27 @@ export class MiniBoxShapeUtil extends BaseBoxShapeUtil<MiniBoxShape> {
 			</HTMLContainer>
 		)
 	}
-
+	//[d]
 	indicator(shape: MiniBoxShape) {
 		return <rect width={shape.props.w} height={shape.props.h} />
 	}
 }
+
+/* 
+This is our shape util, in tldraw all shapes extend the shape util class. In this
+example we're extending the built-in BaseBoxShapeUtil class. This class provides
+the functionality for our shape. 
+
+[1]
+The type for our shape, we can extend the built-in TLBaseShape generic to create ours.
+
+[2]
+The shape util itself. 
+	[a] The type of shape this util is for, this should be the same as the first argument
+		to the TLBaseShape generic.
+	[b] The default props for our shape. These will be used when creating a new shape.
+	[c] The component for our shape. This returns JSX and is what will be rendered on the 
+		canvas. The HtmlContainer component is a div that provides some useful styles.
+	[d] The indicator for our shape, this also returns JSX. This is what will be rendered 
+		on the canvas when the shape is selected.
+*/
