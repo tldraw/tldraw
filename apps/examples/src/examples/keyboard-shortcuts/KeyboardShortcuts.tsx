fbbe69f5bd1033c799ba1a4562990b670d8798cf
Taha <98838967+Taha-Hassan-Git@users.noreply.github.com>
2024-01-16 10:48:26 +0000

    add explainer for modifier keys and menu schema

--- apps/examples/src/examples/keyboard-shortcuts/KeyboardShortcuts.tsx
+++ apps/examples/src/examples/keyboard-shortcuts/KeyboardShortcuts.tsx
@@ -46,31 +46,43 @@ export default function KeyboardShortcuts() {
 
 /* 
 This example shows how you can replace tldraw's default keyboard shortcuts with your own,
-or add a shortcut for an action that doesn't have one. In this case we are overriding the 
-toggle grid and draw tool shortcuts, and creating a shortcut for copy as png. An override
-of an existing shortcut will automatically update the keyboard shortcuts dialog. However,
-adding a new shortcut won't, we need to add it to the keyboard shortcuts dialog ourselves.
+or add a shortcut for an action that doesn't have one. An example of how to add shortcuts
+for custom tools can be found in the custom-config example.
+
+In this case we are overriding the toggle grid and draw tool shortcuts, and creating a 
+shortcut for copy-as-png. An override of an existing shortcut will automatically update 
+the keyboard shortcuts dialog. However, adding a new shortcut won't, we'll need to add it 
+ourselves.
+
+You can describe modifier keys with the following syntax:
+	
+	shift: !
+ ctrl/cmd: $
+	  alt: ?
 
 [1]
 We start by defining our overrides, this is an object with functions that we can use to 
-change the ui. We'll be using the actions [a], tools [b] and keyboardShortcutsMenu [c] 
-overrides.
+change the ui. Keyboard shortcuts are associated with either tools (draw, eraser, etc) or 
+actions (group, undo/redo etc). We're going to override two actions [a], one tool [b], and 
+add a new shortcut to the keyboard shortcuts dialog [c].
 
 [a] actions
 	There are two actions we're modifying here, copy-as-png and toggle-grid. copy-as-png
-	doesn't have an existing shortcut, so we'll add it to the shortcuts menu later [c].
+	doesn't have an existing shortcut, but we can still add the property and later add it
+	to the keyboard shortcuts dialog [c].
 
 [b] tools
-	We're overriding the draw tool's shortcut to 'p', because we want to rename it to the
-	pen tool.
+	We're overriding the draw tool's shortcut to 'p', maybe we want to rename it to the pen
+	tool or something.
 
 [c] keyboardShortcutsMenu
 	This function takes 3 arguments, the editor instance (which we don't need), the menu
 	schema, and the ui context. The shortcutsMenu is an array, so we'll need to use the 
-	find method to return the edit section and add our new menu item to it. menuItem() is
-	a helper function that creates a menu item for us, we just need to pass it an action. 
-	We'll use the copy-as-png action that we modified in [a], we can grab it from the ui
-	context's actions object.
+	find method to return the edit group and add our new menu item to it. Check out the 
+	useKeyboardShortcutsSchema.tsx file in the tldraw repo to see the full list of groups
+	and the menu items they contain. menuItem() is a helper function that creates a new menu 
+	item for us, we just need to pass it an action or tool. We'll use the copy-as-png action 
+	that we modified in [a], we can grab it from the ui context's actions object.
 
 [2]
 Finally, we pass our overrides object into the Tldraw component's overrides prop. Now when
