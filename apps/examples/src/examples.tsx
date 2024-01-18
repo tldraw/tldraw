c36075bcf568f1c32e555d3dae09e5a3ec6e06f2
Taha <98838967+Taha-Hassan-Git@users.noreply.github.com>
2024-01-18 16:43:25 +0000

    filter and sort

--- apps/examples/src/examples.tsx
+++ apps/examples/src/examples.tsx
@@ -7,22 +7,98 @@ export type Example = {
 	path: string
 	codeUrl: string
 	hide: boolean
+	category: 'basic' | 'editor' | 'ui' | 'collaboration' | 'data' | 'shapes/tools' | null
 	order: number | null
 	componentFile: string
 	loadComponent: () => Promise<ComponentType>
 }
 
-export const examples = (
+const basicExamples = (
 	Object.values(import.meta.glob('./examples/*/README.md', { eager: true })) as Example[]
-).sort((a, b) => {
-	// sort by order then title:
-	if (a.order === b.order) {
-		return a.title.localeCompare(b.title)
-	} else if (a.order === null) {
-		return 1
-	} else if (b.order === null) {
-		return -1
-	} else {
-		return a.order - b.order
-	}
-})
+)
+	.filter((e) => e.category === 'basic')
+	.sort((a, b) => {
+		if (a.order === null) {
+			return 1
+		} else if (b.order === null) {
+			return -1
+		} else {
+			return a.order - b.order
+		}
+	})
+
+const uiExamples = (
+	Object.values(import.meta.glob('./examples/*/README.md', { eager: true })) as Example[]
+)
+	.filter((e) => e.category === 'ui')
+	.sort((a, b) => {
+		if (a.order === null) {
+			return 1
+		} else if (b.order === null) {
+			return -1
+		} else {
+			return a.order - b.order
+		}
+	})
+const shapeExamples = (
+	Object.values(import.meta.glob('./examples/*/README.md', { eager: true })) as Example[]
+)
+	.filter((e) => e.category === 'shapes/tools')
+	.sort((a, b) => {
+		if (a.order === null) {
+			return 1
+		} else if (b.order === null) {
+			return -1
+		} else {
+			return a.order - b.order
+		}
+	})
+
+const editorExamples = (
+	Object.values(import.meta.glob('./examples/*/README.md', { eager: true })) as Example[]
+)
+	.filter((e) => e.category === 'editor')
+	.sort((a, b) => {
+		if (a.order === null) {
+			return 1
+		} else if (b.order === null) {
+			return -1
+		} else {
+			return a.order - b.order
+		}
+	})
+const dataExamples = (
+	Object.values(import.meta.glob('./examples/*/README.md', { eager: true })) as Example[]
+)
+	.filter((e) => e.category === 'data')
+	.sort((a, b) => {
+		if (a.order === null) {
+			return 1
+		} else if (b.order === null) {
+			return -1
+		} else {
+			return a.order - b.order
+		}
+	})
+
+const collaborationExamples = (
+	Object.values(import.meta.glob('./examples/*/README.md', { eager: true })) as Example[]
+)
+	.filter((e) => e.category === 'collaboration')
+	.sort((a, b) => {
+		if (a.order === null) {
+			return 1
+		} else if (b.order === null) {
+			return -1
+		} else {
+			return a.order - b.order
+		}
+	})
+export const examples = [
+	...basicExamples,
+	...uiExamples,
+	...shapeExamples,
+	...editorExamples,
+	...dataExamples,
+	...collaborationExamples,
+]
