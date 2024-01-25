53f268b1e90d8e2e403e550ab83a012e9e8d9f19
Taha <98838967+Taha-Hassan-Git@users.noreply.github.com>
2024-01-25 11:42:12 +0000

    remove repetition from examples.tx

--- apps/examples/src/examples.tsx
+++ apps/examples/src/examples.tsx
@@ -13,93 +13,31 @@ export type Example = {
 	loadComponent: () => Promise<ComponentType>
 }
 
-const basicExamples = (
-	Object.values(import.meta.glob('./examples/*/README.md', { eager: true })) as Example[]
-)
-	.filter((e) => e.category === 'basic')
-	.sort((a, b) => {
-		if (a.order === null) {
-			return 1
-		} else if (b.order === null) {
-			return -1
-		} else {
-			return a.order - b.order
-		}
-	})
+type Category = 'basic' | 'editor' | 'ui' | 'collaboration' | 'data/assets' | 'shapes/tools'
 
-const uiExamples = (
-	Object.values(import.meta.glob('./examples/*/README.md', { eager: true })) as Example[]
-)
-	.filter((e) => e.category === 'ui')
-	.sort((a, b) => {
-		if (a.order === null) {
-			return 1
-		} else if (b.order === null) {
-			return -1
-		} else {
-			return a.order - b.order
-		}
-	})
-const shapeExamples = (
-	Object.values(import.meta.glob('./examples/*/README.md', { eager: true })) as Example[]
-)
-	.filter((e) => e.category === 'shapes/tools')
-	.sort((a, b) => {
-		if (a.order === null) {
-			return 1
-		} else if (b.order === null) {
-			return -1
-		} else {
-			return a.order - b.order
-		}
-	})
+const getExamplesForCategory = (category: Category) =>
+	(Object.values(import.meta.glob('./examples/*/README.md', { eager: true })) as Example[])
+		.filter((e) => e.category === category)
+		.sort((a, b) => {
+			if (a.order === null) {
+				return 1
+			} else if (b.order === null) {
+				return -1
+			} else {
+				return a.order - b.order
+			}
+		})
 
-const editorExamples = (
-	Object.values(import.meta.glob('./examples/*/README.md', { eager: true })) as Example[]
-)
-	.filter((e) => e.category === 'editor')
-	.sort((a, b) => {
-		if (a.order === null) {
-			return 1
-		} else if (b.order === null) {
-			return -1
-		} else {
-			return a.order - b.order
-		}
-	})
-const dataExamples = (
-	Object.values(import.meta.glob('./examples/*/README.md', { eager: true })) as Example[]
-)
-	.filter((e) => e.category === 'data/assets')
-	.sort((a, b) => {
-		if (a.order === null) {
-			return 1
-		} else if (b.order === null) {
-			return -1
-		} else {
-			return a.order - b.order
-		}
-	})
+const categories: Record<Category, string> = {
+	basic: 'Getting Started',
+	ui: 'UI/Theming',
+	'shapes/tools': 'Shapes & Tools',
+	'data/assets': 'Data & Assets',
+	editor: 'Editor API',
+	collaboration: 'Collaboration',
+}
 
-const collaborationExamples = (
-	Object.values(import.meta.glob('./examples/*/README.md', { eager: true })) as Example[]
-)
-	.filter((e) => e.category === 'collaboration')
-	.sort((a, b) => {
-		if (a.order === null) {
-			return 1
-		} else if (b.order === null) {
-			return -1
-		} else {
-			return a.order - b.order
-		}
-	})
-
-export const examples = [
-	{ id: 'Getting Started', value: basicExamples },
-	{ id: 'UI/Theming', value: uiExamples },
-	{ id: 'Shapes & Tools', value: shapeExamples },
-	{ id: 'Data & Assets', value: dataExamples },
-	{ id: 'Editor API', value: editorExamples },
-	{ id: 'Collaboration', value: collaborationExamples },
-]
+export const examples = Object.entries(categories).map(([category, title]) => ({
+	id: title,
+	value: getExamplesForCategory(category as Category),
+}))
