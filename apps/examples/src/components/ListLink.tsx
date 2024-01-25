d4f2e2e0637fed7fcb9a4e6ef12558d12226f4b3
Steve Ruiz <steveruizok@gmail.com>
2024-01-25 16:13:35 +0000

    ok?

--- apps/examples/src/components/ListLink.tsx
+++ apps/examples/src/components/ListLink.tsx
@@ -1,10 +1,9 @@
 import classNames from 'classnames'
-import { ForwardedRef, forwardRef, useEffect, useId, useLayoutEffect, useRef } from 'react'
+import { ForwardedRef, forwardRef, useId } from 'react'
 import { Link } from 'react-router-dom'
 import { Example } from '../examples'
-import { useMergedRefs } from '../hooks/useMergedRefs'
-import { StandaloneIcon } from './Icons'
 import { Markdown } from './Markdown'
+import { StandaloneIcon } from './StandaloneIcon'
 
 export const ListLink = forwardRef(function ListLink(
 	{
@@ -15,28 +14,6 @@ export const ListLink = forwardRef(function ListLink(
 	ref: ForwardedRef<HTMLLIElement>
 ) {
 	const id = useId()
-	const containerRef = useRef<HTMLLIElement | null>(null)
-	const wasActiveRef = useRef(isActive)
-	useEffect(() => {
-		wasActiveRef.current = isActive
-	}, [isActive])
-
-	const heightBefore =
-		wasActiveRef.current !== isActive ? containerRef.current?.offsetHeight : undefined
-
-	useLayoutEffect(() => {
-		if (heightBefore !== undefined && containerRef.current) {
-			containerRef.current.animate(
-				[{ height: heightBefore + 'px' }, { height: containerRef.current.offsetHeight + 'px' }],
-				{
-					duration: 120,
-					easing: 'ease-out',
-					fill: 'backwards',
-					delay: 100,
-				}
-			)
-		}
-	}, [heightBefore])
 
 	const mainDetails = (
 		<>
@@ -57,10 +34,6 @@ export const ListLink = forwardRef(function ListLink(
 		</>
 	)
 
-	// TODO: re-enable code sandbox links
-	// const codeSandboxPath = encodeURIComponent(
-	// 	`/src/examples${example.path}${example.componentFile.replace(/^\./, '')}`
-	// )
 	const extraDetails = (
 		<div className="examples__list__item__details" aria-hidden={!isActive}>
 			{!showDescriptionWhenInactive && <Markdown sanitizedHtml={example.description} />}
@@ -68,21 +41,15 @@ export const ListLink = forwardRef(function ListLink(
 			<div className="examples__list__item__code">
 				<a href={example.codeUrl} target="_blank" rel="noreferrer">
 					View code
+					{/* <ExternalLinkIcon /> */}
 				</a>
-				{/* <a
-					href={`https://codesandbox.io/p/devbox/github/tldraw/tldraw/tree/examples/?file=${codeSandboxPath}`}
-					target="_blank"
-					rel="noreferrer"
-				>
-					Edit in CodeSandbox
-				</a> */}
 			</div>
 		</div>
 	)
 
 	return (
 		<li
-			ref={useMergedRefs(ref, containerRef)}
+			ref={ref}
 			className={classNames('examples__list__item', isActive && 'examples__list__item__active')}
 		>
 			{!isActive && (
