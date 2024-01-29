4af6bc858fd30263dc13df2ea9a228f1a88f75ee
Steve Ruiz <steveruizok@gmail.com>
2024-01-29 15:00:05 +0000

    Update Icons.tsx

--- apps/examples/src/components/Icons.tsx
+++ apps/examples/src/components/Icons.tsx
@@ -15,8 +15,8 @@ export function StandaloneIcon(props: React.SVGProps<SVGSVGElement>) {
 export function Chevron() {
 	return (
 		<svg
-			width="16"
-			height="16"
+			width="14"
+			height="14"
 			viewBox="0 0 16 16"
 			fill="none"
 			xmlns="http://www.w3.org/2000/svg"
