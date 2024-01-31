2d600ef275facbde6ca5a84ec547f166bb7f7ae7
Lu[ke] Wilson <l2wilson94@gmail.com>
2024-01-31 12:02:01 +0000

    public api

--- packages/tldraw/src/lib/TldrawWrapper.tsx
+++ packages/tldraw/src/lib/TldrawWrapper.tsx
@@ -1,11 +1,13 @@
 import { memo } from 'react'
 
+/** @public */
 export type TldrawWrapperProps = {
 	style?: React.CSSProperties
 	className?: string
 	children?: React.ReactNode
 }
 
+/** @public */
 export const TldrawWrapper = memo(({ style, className, children }: TldrawWrapperProps) => {
 	return (
 		<div style={style} className={`tl-wrapper ${className ?? ''}`}>
