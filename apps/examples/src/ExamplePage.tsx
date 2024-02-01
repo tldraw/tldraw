7497c4fe40f7e530ea5868f8c738b7b4d469d283
Steve Ruiz <steveruizok@gmail.com>
2024-02-01 17:45:51 +0000

    logo swap

--- apps/examples/src/ExamplePage.tsx
+++ apps/examples/src/ExamplePage.tsx
@@ -1,7 +1,4 @@
-import { assert, assertExists } from '@tldraw/tldraw'
-import { ForwardedRef, forwardRef, useEffect, useRef } from 'react'
 import { Link } from 'react-router-dom'
-import ExamplesTldrawLogo from './components/ExamplesTldrawLogo'
 import { Example, examples } from './examples'
 
 export function ExamplePage({
@@ -11,33 +8,6 @@ export function ExamplePage({
 	example: Example
 	children: React.ReactNode
 }) {
-	const scrollElRef = useRef<HTMLUListElement>(null)
-	const activeElRef = useRef<HTMLLIElement>(null)
-	const isFirstScroll = useRef(true)
-
-	useEffect(() => {
-		const frame = requestAnimationFrame(() => {
-			if (activeElRef.current) {
-				const scrollEl = assertExists(scrollElRef.current)
-				const activeEl = activeElRef.current
-				assert(activeEl.offsetParent === scrollEl)
-
-				const isScrolledIntoView =
-					activeEl.offsetTop >= scrollEl.scrollTop &&
-					activeEl.offsetTop + activeEl.offsetHeight <= scrollEl.scrollTop + scrollEl.offsetHeight
-
-				if (!isScrolledIntoView) {
-					activeEl.scrollIntoView({
-						behavior: isFirstScroll.current ? 'auto' : 'smooth',
-						block: isFirstScroll.current ? 'start' : 'center',
-					})
-				}
-				isFirstScroll.current = false
-			}
-		})
-		return () => cancelAnimationFrame(frame)
-	}, [example])
-
 	const categories = examples.map((e) => e.id)
 
 	return (
@@ -45,7 +15,7 @@ export function ExamplePage({
 			<div className="example__sidebar scroll-light">
 				<div className="example__sidebar__header">
 					<Link className="example__sidebar__header__logo" to="/">
-						<ExamplesTldrawLogo />
+						<TldrawLogo />
 					</Link>
 					<div className="example__sidebar__header__socials">
 						<a href="https://twitter.com/tldraw" title="twitter" className="hoverable">
@@ -59,7 +29,7 @@ export function ExamplePage({
 						</a>
 					</div>
 				</div>
-				<ul className="example__sidebar__categories scroll-light" ref={scrollElRef}>
+				<ul className="example__sidebar__categories scroll-light">
 					{categories.map((currentCategory) => (
 						<li className="example__sidebar__category">
 							<h3 className="example__sidebar__category__header">{currentCategory}</h3>
@@ -71,7 +41,6 @@ export function ExamplePage({
 											key={sidebarExample.path}
 											example={sidebarExample}
 											isActive={sidebarExample.path === example.path}
-											ref={sidebarExample.path === example.path ? activeElRef : undefined}
 										/>
 									))}
 							</ul>
@@ -96,48 +65,60 @@ export function ExamplePage({
 	)
 }
 
-export const ExampleSidebarListItem = forwardRef(function ExampleSidebarListItem(
-	{
-		example,
-		isActive,
-	}: { example: Example; isActive?: boolean; showDescriptionWhenInactive?: boolean },
-	ref: ForwardedRef<HTMLLIElement>
-) {
+function ExampleSidebarListItem({
+	example,
+	isActive,
+}: {
+	example: Example
+	isActive?: boolean
+	showDescriptionWhenInactive?: boolean
+}) {
 	return (
-		<li ref={ref} className="examples__sidebar__item" data-active={isActive}>
+		<li className="examples__sidebar__item" data-active={isActive}>
 			<Link to={example.path} className="examples__sidebar__item__link hoverable" />
 			<h3 className="examples__sidebar__item__title">
 				<span>{example.title}</span>
 			</h3>
 			{isActive && (
-				<Link
-					to={`${example.path}/full`}
-					className="examples__list__item__standalone hoverable"
-					aria-label="Standalone"
-					title="View standalone example"
-				>
-					<StandaloneIcon />
-				</Link>
+				<div className="examples__list__item__buttons">
+					<Link
+						to={`${example.path}/full`}
+						className="examples__list__item__button hoverable"
+						aria-label="Standalone"
+						title="View standalone example"
+					>
+						<InfoIcon />
+					</Link>
+					<Link
+						to={`${example.path}/full`}
+						className="examples__list__item__button hoverable"
+						aria-label="Standalone"
+						title="View standalone example"
+					>
+						<StandaloneIcon />
+					</Link>
+				</div>
 			)}
 		</li>
 	)
-})
-
-export function StandaloneIcon(props: React.SVGProps<SVGSVGElement>) {
-	return (
-		<svg width="16" height="16" viewBox="0 0 30 30" fill="none" {...props}>
-			<path
-				d="M13 5H7C5.89543 5 5 5.89543 5 7V23C5 24.1046 5.89543 25 7 25H23C24.1046 25 25 24.1046 25 23V17M19 5H25M25 5V11M25 5L13 17"
-				stroke="currentColor"
-				strokeWidth="2"
-				strokeLinecap="round"
-				strokeLinejoin="round"
-			/>
-		</svg>
-	)
 }
 
-export function SocialIcon({ icon }: { icon: string }) {
+// function Markdown({
+// 	sanitizedHtml,
+// 	className = '',
+// }: {
+// 	sanitizedHtml: string
+// 	className?: string
+// }) {
+// 	return (
+// 		<div
+// 			className={`examples__markdown ${className}`}
+// 			dangerouslySetInnerHTML={{ __html: sanitizedHtml }}
+// 		/>
+// 	)
+// }
+
+function SocialIcon({ icon }: { icon: string }) {
 	return (
 		<img
 			className="example__sidebar__icon"
@@ -148,4 +129,34 @@ export function SocialIcon({ icon }: { icon: string }) {
 			}}
 		/>
 	)
+}
+
+function StandaloneIcon() {
+	return (
+		<svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<path
+				d="M2 2.5C2 2.22386 2.22386 2 2.5 2H5.5C5.77614 2 6 2.22386 6 2.5C6 2.77614 5.77614 3 5.5 3H3V5.5C3 5.77614 2.77614 6 2.5 6C2.22386 6 2 5.77614 2 5.5V2.5ZM9 2.5C9 2.22386 9.22386 2 9.5 2H12.5C12.7761 2 13 2.22386 13 2.5V5.5C13 5.77614 12.7761 6 12.5 6C12.2239 6 12 5.77614 12 5.5V3H9.5C9.22386 3 9 2.77614 9 2.5ZM2.5 9C2.77614 9 3 9.22386 3 9.5V12H5.5C5.77614 12 6 12.2239 6 12.5C6 12.7761 5.77614 13 5.5 13H2.5C2.22386 13 2 12.7761 2 12.5V9.5C2 9.22386 2.22386 9 2.5 9ZM12.5 9C12.7761 9 13 9.22386 13 9.5V12.5C13 12.7761 12.7761 13 12.5 13H9.5C9.22386 13 9 12.7761 9 12.5C9 12.2239 9.22386 12 9.5 12H12V9.5C12 9.22386 12.2239 9 12.5 9Z"
+				fill="currentColor"
+				fillRule="evenodd"
+				clipRule="evenodd"
+			/>
+		</svg>
+	)
+}
+
+function InfoIcon() {
+	return (
+		<svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<path
+				d="M7.49991 0.876892C3.84222 0.876892 0.877075 3.84204 0.877075 7.49972C0.877075 11.1574 3.84222 14.1226 7.49991 14.1226C11.1576 14.1226 14.1227 11.1574 14.1227 7.49972C14.1227 3.84204 11.1576 0.876892 7.49991 0.876892ZM1.82707 7.49972C1.82707 4.36671 4.36689 1.82689 7.49991 1.82689C10.6329 1.82689 13.1727 4.36671 13.1727 7.49972C13.1727 10.6327 10.6329 13.1726 7.49991 13.1726C4.36689 13.1726 1.82707 10.6327 1.82707 7.49972ZM8.24992 4.49999C8.24992 4.9142 7.91413 5.24999 7.49992 5.24999C7.08571 5.24999 6.74992 4.9142 6.74992 4.49999C6.74992 4.08577 7.08571 3.74999 7.49992 3.74999C7.91413 3.74999 8.24992 4.08577 8.24992 4.49999ZM6.00003 5.99999H6.50003H7.50003C7.77618 5.99999 8.00003 6.22384 8.00003 6.49999V9.99999H8.50003H9.00003V11H8.50003H7.50003H6.50003H6.00003V9.99999H6.50003H7.00003V6.99999H6.50003H6.00003V5.99999Z"
+				fill="currentColor"
+				fillRule="evenodd"
+				clipRule="evenodd"
+			/>
+		</svg>
+	)
+}
+
+function TldrawLogo() {
+	return <img className="examples__tldraw__logo" src="tldraw_dev_light.png" />
 }
