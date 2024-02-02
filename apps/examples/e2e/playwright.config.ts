c692391de16a6720d2814c159f980270454155fa
Taha <98838967+Taha-Hassan-Git@users.noreply.github.com>
2024-02-02 14:21:06 +0000

    bump timeout

--- apps/examples/e2e/playwright.config.ts
+++ apps/examples/e2e/playwright.config.ts
@@ -22,7 +22,7 @@ const config: PlaywrightTestConfig = {
 		 * Maximum time expect() should wait for the condition to be met.
 		 * For example in `await expect(locator).toHaveText();`
 		 */
-		timeout: 2000,
+		timeout: 4000,
 		toHaveScreenshot: {
 			maxDiffPixelRatio: 0.001,
 			threshold: 0.01,
