a686852506b42525e8bb7a6d7a8468b48a23fb34
Steve Ruiz <steveruizok@gmail.com>
2024-02-02 14:12:15 +0000

    examples site

--- apps/examples/src/ExamplePage.tsx
+++ apps/examples/src/ExamplePage.tsx
@@ -1,5 +1,24 @@
+import * as Dialog from '@radix-ui/react-alert-dialog'
+import { Dispatch, createContext, useContext, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { Example, examples } from './examples'
+
+const dialogContext = createContext<{
+	example: Example | null
+	setExampleDialog: Dispatch<Example | null>
+}>({
+	example: null,
+	setExampleDialog: () => void null,
+})
+
+export function DialogContextProvider({ children }: { children: React.ReactNode }) {
+	const [example, setExampleDialog] = useState<Example | null>(null)
+	return (
+		<dialogContext.Provider value={{ example, setExampleDialog }}>
+			{children}
+		</dialogContext.Provider>
+	)
+}
 
 export function ExamplePage({
 	example,
@@ -11,57 +30,66 @@ export function ExamplePage({
 	const categories = examples.map((e) => e.id)
 
 	return (
-		<div className="example">
-			<div className="example__sidebar scroll-light">
-				<div className="example__sidebar__header">
-					<Link className="example__sidebar__header__logo" to="/">
-						<TldrawLogo />
-					</Link>
-					<div className="example__sidebar__header__socials">
-						<a href="https://twitter.com/tldraw" title="twitter" className="hoverable">
-							<SocialIcon icon="twitter" />
+		<DialogContextProvider>
+			<div className="example">
+				<div className="example__sidebar scroll-light">
+					<div className="example__sidebar__header">
+						<Link className="example__sidebar__header__logo" to="/">
+							<TldrawLogo />
+						</Link>
+						<div className="example__sidebar__header__socials">
+							<a href="https://twitter.com/tldraw" title="twitter" className="hoverable">
+								<SocialIcon icon="twitter" />
+							</a>
+							<a href="https://github.com/tldraw/tldraw" title="github" className="hoverable">
+								<SocialIcon icon="github" />
+							</a>
+							<a href="https://discord.com/invite/SBBEVCA4PG" title="discord" className="hoverable">
+								<SocialIcon icon="discord" />
+							</a>
+						</div>
+					</div>
+					<ul className="example__sidebar__categories scroll-light">
+						{categories.map((currentCategory) => (
+							<li className="example__sidebar__category">
+								<h3 className="example__sidebar__category__header">{currentCategory}</h3>
+								<ul className="example__sidebar__category__items">
+									{examples
+										.find((category) => category.id === currentCategory)
+										?.value.map((sidebarExample) => (
+											<ExampleSidebarListItem
+												key={sidebarExample.path}
+												example={sidebarExample}
+												isActive={sidebarExample.path === example.path}
+											/>
+										))}
+								</ul>
+							</li>
+						))}
+					</ul>
+					<div className="example__sidebar__footer-links">
+						<a
+							className="example__sidebar__footer-link example__sidebar__footer-link--grey"
+							target="_blank"
+							href="https://github.com/tldraw/tldraw/issues/new?assignees=&labels=Example%20Request&projects=&template=example_request.yml&title=%5BExample Request%5D%3A+"
+						>
+							Request an example
 						</a>
-						<a href="https://github.com/tldraw/tldraw" title="github" className="hoverable">
-							<SocialIcon icon="github" />
-						</a>
-						<a href="https://discord.com/invite/SBBEVCA4PG" title="discord" className="hoverable">
-							<SocialIcon icon="discord" />
+						<a
+							className="example__sidebar__footer-link example__sidebar__footer-link--grey"
+							target="_blank"
+							href="https://tldraw.dev"
+						>
+							Visit the docs
 						</a>
 					</div>
 				</div>
-				<ul className="example__sidebar__categories scroll-light">
-					{categories.map((currentCategory) => (
-						<li className="example__sidebar__category">
-							<h3 className="example__sidebar__category__header">{currentCategory}</h3>
-							<ul className="example__sidebar__category__items">
-								{examples
-									.find((category) => category.id === currentCategory)
-									?.value.map((sidebarExample) => (
-										<ExampleSidebarListItem
-											key={sidebarExample.path}
-											example={sidebarExample}
-											isActive={sidebarExample.path === example.path}
-										/>
-									))}
-							</ul>
-						</li>
-					))}
-				</ul>
-				<div className="example__sidebar__footer-links">
-					<a
-						className="link__button link__button--grey"
-						target="_blank"
-						href="https://github.com/tldraw/tldraw/issues/new?assignees=&labels=Example%20Request&projects=&template=example_request.yml&title=%5BExample Request%5D%3A+"
-					>
-						Request an example
-					</a>
-					<a className="link__button link__button--grey" target="_blank" href="https://tldraw.dev">
-						Visit the docs
-					</a>
+				<div className="example__content">
+					{children}
+					<Dialogs />
 				</div>
 			</div>
-			<div className="example__content">{children}</div>
-		</div>
+		</DialogContextProvider>
 	)
 }
 
@@ -73,20 +101,25 @@ function ExampleSidebarListItem({
 	isActive?: boolean
 	showDescriptionWhenInactive?: boolean
 }) {
+	const { setExampleDialog } = useContext(dialogContext)
+
 	return (
 		<li className="examples__sidebar__item" data-active={isActive}>
 			<Link to={example.path} className="examples__sidebar__item__link hoverable" />
-			<h3 className="examples__sidebar__item__title">
+			<div className="examples__sidebar__item__title">
 				<span>{example.title}</span>
-			</h3>
+			</div>
 			{isActive && (
-				<div className="examples__list__item__buttons">
-					<div className="examples__list__item__button hoverable">
+				<div className="example__sidebar__item__buttons">
+					<button
+						className="example__sidebar__item__button hoverable"
+						onClick={() => setExampleDialog(example)}
+					>
 						<InfoIcon />
-					</div>
+					</button>
 					<Link
 						to={`${example.path}/full`}
-						className="examples__list__item__button hoverable"
+						className="example__sidebar__item__button hoverable"
 						aria-label="Standalone"
 						title="View standalone example"
 					>
@@ -98,20 +131,50 @@ function ExampleSidebarListItem({
 	)
 }
 
-// function Markdown({
-// 	sanitizedHtml,
-// 	className = '',
-// }: {
-// 	sanitizedHtml: string
-// 	className?: string
-// }) {
-// 	return (
-// 		<div
-// 			className={`examples__markdown ${className}`}
-// 			dangerouslySetInnerHTML={{ __html: sanitizedHtml }}
-// 		/>
-// 	)
-// }
+function Markdown({
+	sanitizedHtml,
+	className = '',
+}: {
+	sanitizedHtml: string
+	className?: string
+}) {
+	return (
+		<div
+			className={`examples__markdown ${className}`}
+			dangerouslySetInnerHTML={{ __html: sanitizedHtml }}
+		/>
+	)
+}
+
+function Dialogs() {
+	const { example, setExampleDialog } = useContext(dialogContext)
+	if (!example) return null
+
+	const handleOpenChange = (open: boolean) => {
+		if (!open) {
+			setExampleDialog(null)
+		}
+	}
+
+	return (
+		<Dialog.Root defaultOpen onOpenChange={handleOpenChange} open={!!example}>
+			<Dialog.Overlay
+				className="example__dialog__overlay"
+				onPointerDown={() => setExampleDialog(null)}
+			/>
+			<Dialog.Content className="example__dialog__content">
+				<h1>{example.title}</h1>
+				<Markdown sanitizedHtml={example.details} className="example__dialog__markdown" />
+				<div className="example__dialog__actions">
+					<a href={example.codeUrl}>
+						View Source <ExternalLinkIcon />
+					</a>
+					<Dialog.Cancel className="example__dialog__close">Close</Dialog.Cancel>
+				</div>
+			</Dialog.Content>
+		</Dialog.Root>
+	)
+}
 
 function SocialIcon({ icon }: { icon: string }) {
 	return (
@@ -147,8 +210,19 @@ function InfoIcon() {
 				fill="currentColor"
 				fillRule="evenodd"
 				clipRule="evenodd"
-				// Remove once we have the dialog
-				opacity={0.25}
+			/>
+		</svg>
+	)
+}
+
+function ExternalLinkIcon() {
+	return (
+		<svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<path
+				d="M3 2C2.44772 2 2 2.44772 2 3V12C2 12.5523 2.44772 13 3 13H12C12.5523 13 13 12.5523 13 12V8.5C13 8.22386 12.7761 8 12.5 8C12.2239 8 12 8.22386 12 8.5V12H3V3L6.5 3C6.77614 3 7 2.77614 7 2.5C7 2.22386 6.77614 2 6.5 2H3ZM12.8536 2.14645C12.9015 2.19439 12.9377 2.24964 12.9621 2.30861C12.9861 2.36669 12.9996 2.4303 13 2.497L13 2.5V2.50049V5.5C13 5.77614 12.7761 6 12.5 6C12.2239 6 12 5.77614 12 5.5V3.70711L6.85355 8.85355C6.65829 9.04882 6.34171 9.04882 6.14645 8.85355C5.95118 8.65829 5.95118 8.34171 6.14645 8.14645L11.2929 3H9.5C9.22386 3 9 2.77614 9 2.5C9 2.22386 9.22386 2 9.5 2H12.4999H12.5C12.5678 2 12.6324 2.01349 12.6914 2.03794C12.7504 2.06234 12.8056 2.09851 12.8536 2.14645Z"
+				fill="currentColor"
+				fillRule="evenodd"
+				clipRule="evenodd"
 			/>
 		</svg>
 	)
