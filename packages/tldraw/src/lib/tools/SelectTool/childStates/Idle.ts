625475c562250a2f57dba4bf04f24eb801a1756b
Mime Čuvalo <mimecuvalo@gmail.com>
2024-02-06 11:48:21 +0000

    arrows: update cursor only when in Select mode

--- packages/tldraw/src/lib/tools/SelectTool/childStates/Idle.ts
+++ packages/tldraw/src/lib/tools/SelectTool/childStates/Idle.ts
@@ -34,6 +34,28 @@ export class Idle extends StateNode {
 
 	override onPointerMove: TLEventHandlers['onPointerMove'] = () => {
 		updateHoveredId(this.editor)
+
+		const hitShape = this.editor.getShapeAtPoint(this.editor.inputs.currentPagePoint, {
+			hitInside: false,
+			hitLabels: false,
+			margin: HIT_TEST_MARGIN / this.editor.getZoomLevel(),
+			renderingOnly: true,
+		})
+		if (this.isOverArrowLabelTest(hitShape)) {
+			this.editor.updateInstanceState({
+				cursor: {
+					type: 'pointer',
+					rotation: 0,
+				},
+			})
+		} else {
+			this.editor.updateInstanceState({
+				cursor: {
+					type: 'default',
+					rotation: 0,
+				},
+			})
+		}
 	}
 
 	override onPointerDown: TLEventHandlers['onPointerDown'] = (info) => {
@@ -93,21 +115,10 @@ export class Idle extends StateNode {
 			}
 			case 'shape': {
 				const { shape } = info
-				const pointInShapeSpace = this.editor.getPointInShapeSpace(
-					shape,
-					this.editor.inputs.currentPagePoint
-				)
-				// todo: Extract into general hit test for arrows
-				if (this.editor.isShapeOfType<TLArrowShape>(shape, 'arrow')) {
-					// How should we handle multiple labels? Do shapes ever have multiple labels?
-					const labelGeometry = this.editor.getShapeGeometry<Group2d>(shape).children[1]
-					// Knowing what we know about arrows... if the shape has no text in its label,
-					// then the label geometry should not be there.
-					if (labelGeometry && pointInPolygon(pointInShapeSpace, labelGeometry.vertices)) {
-						// We're moving the label on a shape.
-						this.parent.transition('pointing_arrow_label', info)
-						break
-					}
+				if (this.isOverArrowLabelTest(shape)) {
+					// We're moving the label on a shape.
+					this.parent.transition('pointing_arrow_label', info)
+					break
 				}
 
 				if (this.editor.isShapeOrAncestorLocked(shape)) {
@@ -498,6 +509,28 @@ export class Idle extends StateNode {
 	}
 
 	isDarwin = window.navigator.userAgent.toLowerCase().indexOf('mac') > -1
+
+	isOverArrowLabelTest(shape: TLShape | undefined) {
+		if (!shape) return false
+
+		const pointInShapeSpace = this.editor.getPointInShapeSpace(
+			shape,
+			this.editor.inputs.currentPagePoint
+		)
+
+		// todo: Extract into general hit test for arrows
+		if (this.editor.isShapeOfType<TLArrowShape>(shape, 'arrow')) {
+			// How should we handle multiple labels? Do shapes ever have multiple labels?
+			const labelGeometry = this.editor.getShapeGeometry<Group2d>(shape).children[1]
+			// Knowing what we know about arrows... if the shape has no text in its label,
+			// then the label geometry should not be there.
+			if (labelGeometry && pointInPolygon(pointInShapeSpace, labelGeometry.vertices)) {
+				return true
+			}
+		}
+
+		return false
+	}
 
 	handleDoubleClickOnCanvas(info: TLClickEventInfo) {
 		// Create text shape and transition to editing_shape
