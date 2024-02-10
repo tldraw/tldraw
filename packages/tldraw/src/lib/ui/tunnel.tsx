12dd4668e34a023cc20ad18d408f76bf77cb744b
Steve Ruiz <steveruizok@gmail.com>
2024-02-10 21:58:36 +0000

    Update tunnel.tsx

--- packages/tldraw/src/lib/ui/tunnel.tsx
+++ packages/tldraw/src/lib/ui/tunnel.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useLayoutEffect } from 'react'
 import { create, StoreApi } from 'zustand'
 
 // This is basically tunnel-rat but with a default initial component.
 // Setting the `In` will replace the current `In`, so it does not
-// support lists.
+// support lists. We should consider replacing this with a context-based
+// solution so that we can support unique tunnels per component; at the moment
+// applying any custom ui override with this tunnel will result in all
+// instances having the same custom ui elements.
 // See https://github.com/pmndrs/tunnel-rat for original.
 
 type Props = { hidden?: boolean; children?: React.ReactNode }
@@ -26,13 +29,13 @@ export default function tunnel(initial: React.ReactNode) {
 			const set = useStore((state) => state.set)
 			const version = useStore((state) => state.version)
 
-			useIsomorphicLayoutEffect(() => {
+			useLayoutEffect(() => {
 				set((state) => ({
 					version: state.version + 1,
 				}))
-			}, [])
+			}, [set])
 
-			useIsomorphicLayoutEffect(() => {
+			useLayoutEffect(() => {
 				set(() => ({
 					current: hidden ? null : children,
 				}))
@@ -41,7 +44,7 @@ export default function tunnel(initial: React.ReactNode) {
 					set(() => ({
 						current: initial,
 					}))
-			}, [children, hidden, version])
+			}, [set, children, hidden, version])
 
 			return null
 		},
@@ -51,26 +54,4 @@ export default function tunnel(initial: React.ReactNode) {
 			return <>{current}</>
 		},
 	}
-}
-
-/**
- * An SSR-friendly useLayoutEffect.
- *
- * React currently throws a warning when using useLayoutEffect on the server.
- * To get around it, we can conditionally useEffect on the server (no-op) and
- * useLayoutEffect elsewhere.
- *
- * @see https://github.com/facebook/react/issues/14927
- */
-export const useIsomorphicLayoutEffect =
-	typeof window !== 'undefined' &&
-	// eslint-disable-next-line deprecation/deprecation
-	(window.document?.createElement || window.navigator?.product === 'ReactNative')
-		? React.useLayoutEffect
-		: React.useEffect
-
-export function useMutableCallback<T>(fn: T) {
-	const ref = React.useRef<T>(fn)
-	useIsomorphicLayoutEffect(() => void (ref.current = fn), [fn])
-	return ref
 }
