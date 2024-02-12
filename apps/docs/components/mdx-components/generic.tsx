a5e6ae87feea4cd6af4f7bdc4955dc1bafb0a77f
Taha <98838967+Taha-Hassan-Git@users.noreply.github.com>
2024-02-12 09:53:11 +0000

    Quick start guide (#2692)
    Following on from #2686, this PR replaces the introduction page with a
    Quick Start guide.
    
    Next Steps:
    - Better UX around the code blocks, throughout the site. A copy button
    would be great.
    - Collapsible extra info on the release version and rendering an inline
    component
    - Maybe remove the embed
    
    - [x] `documentation` — Changes to the documentation only[^2]
    
    
    ### Release Notes
    
    - Add a quick start guide
    
    ---------
    
    Co-authored-by: Mime Čuvalo <mimecuvalo@gmail.com>

--- apps/docs/components/mdx-components/generic.tsx
+++ apps/docs/components/mdx-components/generic.tsx
@@ -150,13 +150,13 @@ export const Footnotes = (props: any) => {
 /* -------------------- API docs -------------------- */
 
 export const ApiHeading = (props: any) => {
-	return <div className="article__api-heading" {...props} />
+	return <div {...props} />
 }
 
 export const Embed = (props: any) => {
 	return (
-		<div className="article__embed">
-			<iframe className="iframe" src={props.src} width="100%" height="600p" frameBorder="0" />
+		<div className={props.className || 'article__embed'}>
+			<iframe className="iframe" src={props.src} width="100%" height={600} />
 			{props.caption && <span className="article__caption">{props.caption}</span>}
 		</div>
 	)
