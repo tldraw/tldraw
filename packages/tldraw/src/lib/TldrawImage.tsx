aa7b16b45cd2a9eb77f3e230940ad67bad09aa19
Lu[ke] Wilson <l2wilson94@gmail.com>
2024-02-12 14:50:47 +0000

    tldrawimage editor and container

--- packages/tldraw/src/lib/TldrawImage.tsx
+++ packages/tldraw/src/lib/TldrawImage.tsx
@@ -1,7 +1,15 @@
+import { Editor, TLStore, TLStoreWithStatus, useLocalStore } from '@tldraw/editor'
+import { ContainerProvider, useContainer } from '@tldraw/editor/src/lib/hooks/useContainer'
+import { EditorContext } from '@tldraw/editor/src/lib/hooks/useEditor'
+import React, { useLayoutEffect, useState } from 'react'
+
 /** @public */
 export function TldrawImage() {
+	const [container, setContainer] = React.useState<HTMLDivElement | null>(null)
+
 	return (
 		<div
+			ref={setContainer}
 			style={{
 				width: '100%',
 				height: '100%',
@@ -11,7 +19,69 @@ export function TldrawImage() {
 				alignItems: 'center',
 			}}
 		>
-			Placeholder
+			{container && (
+				<ContainerProvider container={container}>
+					<TldrawImageWithOwnStore />
+				</ContainerProvider>
+			)}
 		</div>
+	)
+}
+
+function TldrawImageWithOwnStore() {
+	const store = useLocalStore({})
+
+	return <TldrawImageWithLoadingStore store={store} />
+}
+
+function TldrawImageWithLoadingStore({ store }: { store: TLStoreWithStatus }) {
+	switch (store.status) {
+		case 'error': {
+			throw store.error
+		}
+		case 'loading': {
+			// const LoadingScreen = rest.components?.LoadingScreen ?? DefaultLoadingScreen
+			return <div>Placeholder loading screen</div>
+		}
+		case 'not-synced': {
+			break
+		}
+		case 'synced-local': {
+			break
+		}
+		case 'synced-remote': {
+			break
+		}
+	}
+
+	return <TldrawImageWithReadyStore store={store.store} />
+}
+
+function TldrawImageWithReadyStore({ store }: { store: TLStore }) {
+	const container = useContainer()
+	const [editor, setEditor] = useState<Editor | null>(null)
+
+	useLayoutEffect(() => {
+		const editor = new Editor({
+			store,
+			shapeUtils: [],
+			tools: [],
+			getContainer: () => container,
+		})
+		;(window as any).app = editor
+		;(window as any).editor = editor
+		setEditor(editor)
+
+		return () => {
+			editor.dispose()
+		}
+	}, [container, store])
+
+	if (!editor) return null
+
+	return (
+		<EditorContext.Provider value={editor}>
+			<div>Placeholder</div>
+		</EditorContext.Provider>
 	)
 }
