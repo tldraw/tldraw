a0c92d108f50d6f33c2feb2e1d118ebced62d913
Lu[ke] Wilson <l2wilson94@gmail.com>
2024-02-15 16:21:52 +0000

    tidy

--- apps/examples/src/examples/external-ui/ExternalUiExample.tsx
+++ apps/examples/src/examples/external-ui/ExternalUiExample.tsx
@@ -1,47 +1,100 @@
-import { Editor, Tldraw, track } from '@tldraw/tldraw'
+import { Editor, TLComponents, Tldraw, track } from '@tldraw/tldraw'
 import '@tldraw/tldraw/tldraw.css'
-import { useState } from 'react'
+import { createContext, useContext, useEffect, useState } from 'react'
+import './external-ui.css'
 
-// There's a guide at the bottom of this file!
+const components: TLComponents = {
+	Toolbar: null,
+}
 
-// [1]
 export default function CustomUiExample() {
-	const [editor, setEditor] = useState<Editor | null>(null)
+	const [editor, setEditor] = useState<Editor | undefined>(undefined)
 
 	const handleMount = (editor: Editor) => {
 		setEditor(editor)
+		editor.updateInstanceState({ isDebugMode: false })
 	}
 
 	return (
-		<div>
-			<CustomUi editor={editor} />
-			<div style={{ width: 600, height: 400 }}>
-				<Tldraw onMount={handleMount} />
+		<div style={{ margin: 32, width: 600 }}>
+			<div style={{ height: 400 }}>
+				<Tldraw onMount={handleMount} components={components} />
 			</div>
+
+			<MyEditorContext.Provider value={editor}>
+				<ExternalToolbar />
+			</MyEditorContext.Provider>
 		</div>
 	)
 }
 
-// [2]
-const CustomUi = track(({ editor }: { editor: Editor | null }) => {
+const MyEditorContext = createContext<Editor | undefined>(undefined)
+
+function useMyEditor() {
+	const editor = useContext(MyEditorContext)
+	if (editor === null) throw new Error('useMyEditor must be used within a MyEditorContext.Provider')
+	return editor
+}
+
+const ExternalToolbar = track(() => {
+	const editor = useMyEditor()
+
+	useEffect(() => {
+		const handleKeyUp = (e: KeyboardEvent) => {
+			if (!editor) return
+			switch (e.key) {
+				case 'Delete':
+				case 'Backspace': {
+					editor.deleteShapes(editor.getSelectedShapeIds())
+					break
+				}
+				case 'v': {
+					editor.setCurrentTool('select')
+					break
+				}
+				case 'e': {
+					editor.setCurrentTool('eraser')
+					break
+				}
+				case 'x':
+				case 'p':
+				case 'b':
+				case 'd': {
+					editor.setCurrentTool('draw')
+					break
+				}
+			}
+		}
+
+		window.addEventListener('keyup', handleKeyUp)
+		return () => {
+			window.removeEventListener('keyup', handleKeyUp)
+		}
+	})
+
+	if (!editor) return null
+
 	return (
 		<div>
-			<div>
+			<div className="external-toolbar">
 				<button
-					data-isactive={editor?.getCurrentToolId() === 'select'}
-					onClick={() => editor?.setCurrentTool('select')}
+					className="external-button"
+					data-isactive={editor.getCurrentToolId() === 'select'}
+					onClick={() => editor.setCurrentTool('select')}
 				>
 					Select
 				</button>
 				<button
-					data-isactive={editor?.getCurrentToolId() === 'draw'}
-					onClick={() => editor?.setCurrentTool('draw')}
+					className="external-button"
+					data-isactive={editor.getCurrentToolId() === 'draw'}
+					onClick={() => editor.setCurrentTool('draw')}
 				>
 					Pencil
 				</button>
 				<button
-					data-isactive={editor?.getCurrentToolId() === 'eraser'}
-					onClick={() => editor?.setCurrentTool('eraser')}
+					className="external-button"
+					data-isactive={editor.getCurrentToolId() === 'eraser'}
+					onClick={() => editor.setCurrentTool('eraser')}
 				>
 					Eraser
 				</button>
@@ -49,26 +102,3 @@ const CustomUi = track(({ editor }: { editor: Editor | null }) => {
 		</div>
 	)
 })
-
-/* 
-This example shows how to create your own custom ui for the editor.
-
-[1]
-We render the Tldraw component with the `hideUi` prop. This will hide the default
-toolbar, style menu and pages menu. We also render our custom ui component inside the 
-Tldraw component. This gives us access to the editor instance via React context.
-
-The context menu isn't hidden by the `hideUi` prop, if you want to hide it you can
-render the parts that make up the Tldraw component separately and omit the context
-menu. Check out the exploded example to see how to do this.
-
-[2]
-We use the `track` function to wrap our component. This makes our component reactive- it will
-re-render when the signals it is tracking change. Check out the signia docs for more: 
-https://signia.tldraw.dev/docs/API/signia_react/functions/track
-
-We gain access to the editor instance via the `useEditor` hook. We use the `useEffect` hook
-to add event listeners for keyboard shortcuts. We use editor methods to change the current
-tool and delete shapes.
-
-*/
