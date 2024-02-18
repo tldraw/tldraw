b27f15a85ee8203233becc1b5091cf88ffe1f9eb
zhuziyao <zzubillzzu@126.com>
2024-02-18 13:18:56 +0800

    Revert "fix(exec): use child_process.exec() instead of child_process.execFile() for win32 platform"
    This reverts commit 685f20fb8e934bae67568e8490d06d50655a0ab0.

--- scripts/lib/exec.ts
+++ scripts/lib/exec.ts
@@ -1,4 +1,4 @@
-import { exec as execute } from 'child_process'
+import { execFile } from 'child_process'
 import { nicelog } from './nicelog'
 
 type ExecOpts = {
@@ -27,29 +27,50 @@ export async function exec(
 ): Promise<string> {
 	nicelog(`> $ ${command} ${args.join(' ')} (in ${pwd}))`)
 	return new Promise((resolve, reject) => {
-		const childProcess = execute(
-			`${command} ${args.filter((arg): arg is string => !!arg).join(' ')}`,
+		const data: string[] = []
+
+		const childProcess = execFile(
+			command,
+			args.filter((arg): arg is string => !!arg),
 			{ cwd: pwd, env: { ...process.env, ...env } },
-			(err, stdout, stderr) => {
-				if (err) {
-					reject(err)
-				} else {
-					const combinedOutput = `${stdout}${stderr}`
-					resolve(combinedOutput.trim())
-				}
+			(err) => {
+				if (err) reject(err)
+				else resolve(data.join(''))
 			}
 		)
 
-		if (childProcess.stdout) {
-			childProcess.stdout.on('data', (data) => {
-				processStdoutLine(data)
-			})
-		}
+		let pendingStdoutLine = ''
+		childProcess.stdout!.on('data', (chunk) => {
+			const chunkString: string = chunk.toString('utf-8')
+			data.push(chunkString)
 
-		if (childProcess.stderr) {
-			childProcess.stderr.on('data', (data) => {
-				processStderrLine(data)
-			})
-		}
+			const lines = chunkString.split('\n')
+			lines[0] = pendingStdoutLine + lines[0]
+			pendingStdoutLine = lines.pop() ?? ''
+
+			for (const line of lines) {
+				processStdoutLine(line)
+			}
+		})
+		childProcess.stdout!.on('close', () => {
+			processStdoutLine(pendingStdoutLine)
+		})
+
+		let pendingStderrLine = ''
+		childProcess.stderr!.on('data', (chunk) => {
+			const chunkString: string = chunk.toString('utf-8')
+			data.push(chunkString)
+
+			const lines = chunkString.split('\n')
+			lines[0] = pendingStderrLine + lines[0]
+			pendingStderrLine = lines.pop() ?? ''
+
+			for (const line of lines) {
+				processStderrLine(line)
+			}
+		})
+		childProcess.stderr!.on('close', () => {
+			processStderrLine(pendingStderrLine)
+		})
 	})
 }
