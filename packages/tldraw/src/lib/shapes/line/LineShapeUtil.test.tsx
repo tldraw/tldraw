d21441afa191636d71989230b93142e9141ba90e
Steve Ruiz <steveruizok@gmail.com>
2024-02-19 17:24:12 +0000

    Update LineShapeUtil.test.tsx

--- packages/tldraw/src/lib/shapes/line/LineShapeUtil.test.tsx
+++ packages/tldraw/src/lib/shapes/line/LineShapeUtil.test.tsx
@@ -200,9 +200,12 @@ describe('Snapping', () => {
 		editor.expectShapeToMatch({
 			id: id,
 			props: {
-				handles: {
-					a1: { x: 0, y: 100 },
-				},
+				points: [
+					{ x: 0, y: 100 },
+					{ x: 100, y: 0 },
+					{ x: 100, y: 100 },
+					{ x: 0, y: 100 },
+				],
 			},
 		})
 	})
