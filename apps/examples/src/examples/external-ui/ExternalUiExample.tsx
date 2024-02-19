03dee4aaedb6092602cd4c435d38ef1316f1b960
Lu[ke] Wilson <l2wilson94@gmail.com>
2024-02-19 11:13:42 +0000

    simplify example

--- apps/examples/src/examples/external-ui/ExternalUiExample.tsx
+++ apps/examples/src/examples/external-ui/ExternalUiExample.tsx
@@ -1,44 +1,30 @@
-import { Editor, TLComponents, Tldraw, track } from '@tldraw/tldraw'
+import { Editor, Tldraw, track } from '@tldraw/tldraw'
 import '@tldraw/tldraw/tldraw.css'
-import { createContext, useContext, useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import './external-ui.css'
 
-const components: TLComponents = {
-	Toolbar: null,
-}
-
-export default function CustomUiExample() {
-	const [editor, setEditor] = useState<Editor | undefined>(undefined)
-
-	const handleMount = (editor: Editor) => {
-		setEditor(editor)
-		editor.updateInstanceState({ isDebugMode: false })
-	}
+export default function ExternalUiExample() {
+	const [editor, setEditor] = useState<Editor>()
 
 	return (
 		<div style={{ margin: 32, width: 600 }}>
 			<div style={{ height: 400 }}>
-				<Tldraw onMount={handleMount} components={components} />
+				<Tldraw
+					onMount={(editor) => {
+						setEditor(editor)
+					}}
+					components={{
+						Toolbar: null,
+					}}
+				/>
 			</div>
 
-			<MyEditorContext.Provider value={editor}>
-				<ExternalToolbar />
-			</MyEditorContext.Provider>
+			<ExternalToolbar editor={editor} />
 		</div>
 	)
 }
 
-const MyEditorContext = createContext<Editor | undefined>(undefined)
-
-function useMyEditor() {
-	const editor = useContext(MyEditorContext)
-	if (editor === null) throw new Error('useMyEditor must be used within a MyEditorContext.Provider')
-	return editor
-}
-
-const ExternalToolbar = track(() => {
-	const editor = useMyEditor()
-
+const ExternalToolbar = track(({ editor }: { editor?: Editor }) => {
 	useEffect(() => {
 		const handleKeyUp = (e: KeyboardEvent) => {
 			if (!editor) return
@@ -72,29 +58,27 @@ const ExternalToolbar = track(() => {
 		}
 	})
 
-	if (!editor) return null
-
 	return (
 		<div>
 			<div className="external-toolbar">
 				<button
 					className="external-button"
-					data-isactive={editor.getCurrentToolId() === 'select'}
-					onClick={() => editor.setCurrentTool('select')}
+					data-isactive={editor ? editor.getCurrentToolId() === 'select' : true}
+					onClick={() => editor?.setCurrentTool('select')}
 				>
 					Select
 				</button>
 				<button
 					className="external-button"
-					data-isactive={editor.getCurrentToolId() === 'draw'}
-					onClick={() => editor.setCurrentTool('draw')}
+					data-isactive={editor?.getCurrentToolId() === 'draw'}
+					onClick={() => editor?.setCurrentTool('draw')}
 				>
 					Pencil
 				</button>
 				<button
 					className="external-button"
-					data-isactive={editor.getCurrentToolId() === 'eraser'}
-					onClick={() => editor.setCurrentTool('eraser')}
+					data-isactive={editor?.getCurrentToolId() === 'eraser'}
+					onClick={() => editor?.setCurrentTool('eraser')}
 				>
 					Eraser
 				</button>
