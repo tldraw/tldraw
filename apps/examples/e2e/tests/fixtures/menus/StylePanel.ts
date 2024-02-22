3ecde16eca970671ce50f38072e5a595175bb734
Taha <98838967+Taha-Hassan-Git@users.noreply.github.com>
2024-02-22 09:26:58 +0000

    use getByTestId method

--- apps/examples/e2e/tests/fixtures/menus/StylePanel.ts
+++ apps/examples/e2e/tests/fixtures/menus/StylePanel.ts
@@ -21,47 +21,48 @@ export class StylePanel {
 			'style.align',
 		]
 		this.colors = {
-			black: this.page.locator('[data-testid="style.color.black"]'),
-			grey: this.page.locator('[data-testid="style.color.grey"]'),
-			lightViolet: this.page.locator('[data-testid="style.color.light-violet"]'),
-			violet: this.page.locator('[data-testid="style.color.violet"]'),
-			blue: this.page.locator('[data-testid="style.color.blue"]'),
-			lightBlue: this.page.locator('[data-testid="style.color.light-blue"]'),
-			yellow: this.page.locator('[data-testid="style.color.yellow"]'),
-			orange: this.page.locator('[data-testid="style.color.orange"]'),
-			green: this.page.locator('[data-testid="style.color.green"]'),
-			lightGreen: this.page.locator('[data-testid="style.color.light-green"]'),
-			lightRed: this.page.locator('[data-testid="style.color.light-red"]'),
-			red: this.page.locator('[data-testid="style.color.red"]'),
+			black: this.page.getByTestId('style.color.black'),
+			grey: this.page.getByTestId('style.color.grey'),
+			lightViolet: this.page.getByTestId('style.color.light-violet'),
+			violet: this.page.getByTestId('style.color.violet'),
+			blue: this.page.getByTestId('style.color.blue'),
+			lightBlue: this.page.getByTestId('style.color.light-blue'),
+			yellow: this.page.getByTestId('style.color.yellow'),
+			orange: this.page.getByTestId('style.color.orange'),
+			green: this.page.getByTestId('style.color.green'),
+			lightGreen: this.page.getByTestId('style.color.light-green'),
+			lightRed: this.page.getByTestId('style.color.light-red'),
+			red: this.page.getByTestId('style.color.red'),
 		}
 		this.fill = {
-			none: this.page.locator('[data-testid="style.fill.none"]'),
-			semi: this.page.locator('[data-testid="style.fill.semi"]'),
-			solid: this.page.locator('[data-testid="style.fill.solid"]'),
-			pattern: this.page.locator('[data-testid="style.fill.pattern"]'),
+			none: this.page.getByTestId('style.fill.none'),
+			semi: this.page.getByTestId('style.fill.semi'),
+			solid: this.page.getByTestId('style.fill.solid'),
+			pattern: this.page.getByTestId('style.fill.pattern'),
 		}
 		this.dash = {
-			draw: this.page.locator('[data-testid="style.dash.draw"]'),
-			dashed: this.page.locator('[data-testid="style.dash.dashed"]'),
-			dotted: this.page.locator('[data-testid="style.dash.dotted"]'),
-			solid: this.page.locator('[data-testid="style.dash.solid"]'),
+			draw: this.page.getByTestId('style.dash.draw'),
+			dashed: this.page.getByTestId('style.dash.dashed'),
+			dotted: this.page.getByTestId('style.dash.dotted'),
+			solid: this.page.getByTestId('style.dash.solid'),
 		}
 		this.size = {
-			s: this.page.locator('[data-testid="style.size.s"]'),
-			m: this.page.locator('[data-testid="style.size.m"]'),
-			l: this.page.locator('[data-testid="style.size.l"]'),
-			xl: this.page.locator('[data-testid="style.size.xl"]'),
+			s: this.page.getByTestId('style.size.s'),
+			m: this.page.getByTestId('style.size.m'),
+			l: this.page.getByTestId('style.size.l'),
+			xl: this.page.getByTestId('style.size.xl'),
 		}
 		this.font = {
-			draw: this.page.locator('[data-testid="style.font.draw"]'),
-			sans: this.page.locator('[data-testid="style.font.sans"]'),
-			serif: this.page.locator('[data-testid="style.font.serif"]'),
-			mono: this.page.locator('[data-testid="style.font.mono"]'),
+			draw: this.page.getByTestId('style.font.draw'),
+			sans: this.page.getByTestId('style.font.sans'),
+			serif: this.page.getByTestId('style.font.serif'),
+			mono: this.page.getByTestId('style.font.mono'),
 		}
+
 		this.align = {
-			start: this.page.locator('[data-testid="style.align.start"]'),
-			middle: this.page.locator('[data-testid="style.align.middle"]'),
-			end: this.page.locator('[data-testid="style.align.end"]'),
+			start: this.page.getByTestId('style.align.start'),
+			middle: this.page.getByTestId('style.align.middle'),
+			end: this.page.getByTestId('style.align.end'),
 		}
 	}
 	async isHinted(style: Locator) {
