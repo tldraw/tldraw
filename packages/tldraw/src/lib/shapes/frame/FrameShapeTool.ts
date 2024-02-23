761248ba60d3a341dc90064fa5f2f2d4b52629ba
Mitja Bezenšek <mitja.bezensek@gmail.com>
2024-02-23 11:55:48 +0100

    Make it work for frames within frames.

--- packages/tldraw/src/lib/shapes/frame/FrameShapeTool.ts
+++ packages/tldraw/src/lib/shapes/frame/FrameShapeTool.ts
@@ -12,22 +12,21 @@ export class FrameShapeTool extends BaseBoxShapeTool {
 		const bounds = this.editor.getShapePageBounds(shape)!
 		const shapesToAddToFrame: TLShapeId[] = []
 		const ancestorIds = this.editor.getShapeAncestors(shape).map((shape) => shape.id)
-		const pageId = this.editor.getCurrentPageId()
 
-		this.editor.getSortedChildIdsForParent(pageId).map((pageShapeId) => {
-			const pageShape = this.editor.getShape(pageShapeId)
-			if (!pageShape) return
+		this.editor.getSortedChildIdsForParent(shape.parentId).map((siblingShapeId) => {
+			const siblingShape = this.editor.getShape(siblingShapeId)
+			if (!siblingShape) return
 			// We don't want to frame the frame itself
-			if (pageShape.id === shape.id) return
-			if (pageShape.isLocked) return
+			if (siblingShape.id === shape.id) return
+			if (siblingShape.isLocked) return
 
-			const pageShapeBounds = this.editor.getShapePageBounds(pageShape)
+			const pageShapeBounds = this.editor.getShapePageBounds(siblingShape)
 			if (!pageShapeBounds) return
 
 			// Frame shape encloses page shape
 			if (bounds.contains(pageShapeBounds)) {
-				if (canEnclose(pageShape, ancestorIds, shape)) {
-					shapesToAddToFrame.push(pageShape.id)
+				if (canEnclose(siblingShape, ancestorIds, shape)) {
+					shapesToAddToFrame.push(siblingShape.id)
 				}
 			}
 		})
