ee1ebfff5485f39e2bd1fd63ddf4d898b06ac0fb
Steve Ruiz <steveruizok@gmail.com>
2024-02-23 15:48:20 +0000

    Update ExternalUiExample.tsx

--- apps/examples/src/examples/external-ui/ExternalUiExample.tsx
+++ apps/examples/src/examples/external-ui/ExternalUiExample.tsx
@@ -1,107 +1,84 @@
-import { Editor, Tldraw, track } from '@tldraw/tldraw'
+import { Editor, Tldraw, useValue } from '@tldraw/tldraw'
 import '@tldraw/tldraw/tldraw.css'
-import { useEffect, useState } from 'react'
+import { createContext, useContext, useState } from 'react'
 import './external-ui.css'
 
 // There's a guide at the bottom of this file!
 
+// [1]
+const editorContext = createContext({} as { editor: Editor })
+
 export default function ExternalUiExample() {
-	const [editor, setEditor] = useState<Editor>()
+	const [editor, setEditor] = useState<Editor | null>(null)
 
 	return (
 		<div style={{ margin: 32, width: 600 }}>
 			<div style={{ height: 400 }}>
 				<Tldraw
-					// [1]
+					// [2]
 					onMount={(editor) => setEditor(editor)}
 					components={{ Toolbar: null }}
 				/>
 			</div>
-
-			<ExternalToolbar
-				// [2]
-				editor={editor}
-			/>
+			{/* [3] */}
+			{editor && (
+				<editorContext.Provider value={{ editor }}>
+					<button onClick={() => editor.zoomToFit()}>Zoom to fit</button>
+					<ExternalToolbar />
+				</editorContext.Provider>
+			)}
 		</div>
 	)
 }
 
-const ExternalToolbar = track(({ editor }: { editor?: Editor }) => {
-	useEffect(() => {
-		const handleKeyUp = (e: KeyboardEvent) => {
-			if (!editor) return
-			switch (e.key) {
-				case 'Delete':
-				case 'Backspace': {
-					editor.deleteShapes(editor.getSelectedShapeIds())
-					break
-				}
-				case 'v': {
-					editor.setCurrentTool('select')
-					break
-				}
-				case 'e': {
-					editor.setCurrentTool('eraser')
-					break
-				}
-				case 'x':
-				case 'p':
-				case 'b':
-				case 'd': {
-					editor.setCurrentTool('draw')
-					break
-				}
-			}
-		}
+const ExternalToolbar = () => {
+	// [6]
+	const { editor } = useContext(editorContext)
 
-		window.addEventListener('keyup', handleKeyUp)
-		return () => {
-			window.removeEventListener('keyup', handleKeyUp)
-		}
-	})
+	const currentToolId = useValue('current tool id', () => editor?.getCurrentToolId(), [editor])
 
 	return (
 		<div>
 			<div className="external-toolbar">
 				<button
 					className="external-button"
-					data-isactive={editor ? editor.getCurrentToolId() === 'select' : true}
-					onClick={() => editor?.setCurrentTool('select')}
+					data-isactive={currentToolId === 'select'}
+					onClick={() => editor.setCurrentTool('select')}
 				>
 					Select
 				</button>
 				<button
 					className="external-button"
-					data-isactive={editor?.getCurrentToolId() === 'draw'}
-					onClick={() => editor?.setCurrentTool('draw')}
+					data-isactive={currentToolId === 'draw'}
+					onClick={() => editor.setCurrentTool('draw')}
 				>
 					Pencil
-				</button>
-				<button
-					className="external-button"
-					data-isactive={editor?.getCurrentToolId() === 'eraser'}
-					onClick={() => editor?.setCurrentTool('eraser')}
-				>
-					Eraser
 				</button>
 			</div>
 		</div>
 	)
-})
+}
 
 /*
 
 This example shows how to control the tldraw editor from an external UI, outside
-of the `Tldraw` component.
+of the `Tldraw` component. There are a few ways of doing this—for example, by putting
+the editor on the window object, passing it around via props, or using React context.
+In this example, we'll use React context.
 
-[1]
-Store the editor in some state when the `Tldraw` component mounts.
+[1] 
+Use React context to store the editor at a higher place in the React component tree. 
 
-[2] Pass the editor to your external UI component. In this example, we're using
-the same component as in the `custom-ui` example, but you could use any UI you
-want. Check out that example for more info on how , including how to make the UI
-reactive.
+[2] 
+Use the `onMount` prop to get the editor instance and store it in state.
 
-This example passes the editor down as a prop, but you could also use a context.
+[3]
+When we have an editor in state, we pass it down to the context provider.
 
+[4]
+You can access the editor from anywhere now.
+
+[5]
+In all descendants of the context provider, we can use React's `useContext` hook to get
+the editor instance that we have stored up in state.
 */
