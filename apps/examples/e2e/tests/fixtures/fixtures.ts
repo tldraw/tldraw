fc35ca0832e1382d87da5d6f170502586122a52f
Taha <98838967+Taha-Hassan-Git@users.noreply.github.com>
2024-02-23 15:21:09 +0000

    actions menu fixture and single test

--- apps/examples/e2e/tests/fixtures/fixtures.ts
+++ apps/examples/e2e/tests/fixtures/fixtures.ts
@@ -1,10 +1,12 @@
 import { test as base } from '@playwright/test'
+import { ActionsMenu } from './menus/ActionsMenu'
 import { StylePanel } from './menus/StylePanel'
 import { Toolbar } from './menus/Toolbar'
 
 type Fixtures = {
 	toolbar: Toolbar
 	stylePanel: StylePanel
+	actionsMenu: ActionsMenu
 }
 
 const test = base.extend<Fixtures>({
@@ -15,6 +17,10 @@ const test = base.extend<Fixtures>({
 	stylePanel: async ({ page }, use) => {
 		const stylePanel = new StylePanel(page)
 		await use(stylePanel)
+	},
+	actionsMenu: async ({ page }, use) => {
+		const actionsMenu = new ActionsMenu(page)
+		await use(actionsMenu)
 	},
 })
 
