d266bed1eabb4b38cd6ed4efd0b6f5fe30ef4acb
David Sheldrick <d.j.sheldrick@gmail.com>
2024-02-23 16:11:09 +0000

    derive vercel routes from react-router config

--- apps/dotcom/scripts/build.ts
+++ apps/dotcom/scripts/build.ts
@@ -4,8 +4,26 @@ import { exec } from '../../../scripts/lib/exec'
 import { Config } from './vercel-output-config'
 
 import { config } from 'dotenv'
+import json5 from 'json5'
 import { nicelog } from '../../../scripts/lib/nicelog'
-import { SPA_ROUTE_FILTERS } from '../spaRouteFilters'
+
+import { T } from '@tldraw/validate'
+
+function loadSpaRoutes() {
+	// eslint-disable-next-line @typescript-eslint/no-var-requires
+	const routesJson = require('../src/__snapshots__/routes.test.ts.snap')['the_routes 1']
+	const routes = T.arrayOf(
+		T.object({
+			reactRouterPattern: T.string,
+			vercelRoutingPattern: T.string,
+		})
+	).validate(json5.parse(routesJson))
+	return routes.map((route) => ({
+		check: true,
+		src: route.vercelRoutingPattern,
+		dest: '/index.html',
+	}))
+}
 
 config({
 	path: './.env.local',
@@ -14,6 +32,9 @@ config({
 nicelog('The multiplayer server is', process.env.MULTIPLAYER_SERVER)
 
 async function build() {
+	// make sure we have the latest routes
+	await exec('yarn', ['test'])
+	const spaRoutes = loadSpaRoutes()
 	await exec('vite', ['build', '--emptyOutDir'])
 	await exec('yarn', ['run', '-T', 'sentry-cli', 'sourcemaps', 'inject', 'dist/assets'])
 	// Clear output static folder (in case we are running locally and have already built the app once before)
@@ -21,12 +42,6 @@ async function build() {
 	mkdirSync('.vercel/output', { recursive: true })
 	await exec('cp', ['-r', 'dist', '.vercel/output/static'])
 	await exec('rm', ['-rf', ...glob.sync('.vercel/output/static/**/*.js.map')])
-
-	const spaRoutes = SPA_ROUTE_FILTERS.map((route) => ({
-		check: true,
-		src: route,
-		dest: '/index.html',
-	}))
 
 	writeFileSync(
 		'.vercel/output/config.json',
--- apps/dotcom/src/routes.test.ts
+++ apps/dotcom/src/routes.test.ts
@@ -1,7 +1,6 @@
 import { expect } from '@jest/globals'
 import type { MatcherFunction } from 'expect'
 import { RouteObject } from 'react-router-dom'
-import { SPA_ROUTE_FILTERS } from '../spaRouteFilters'
 import { router } from './routes'
 
 const toMatchAny: MatcherFunction<[regexes: unknown]> = function (actual, regexes) {
@@ -47,12 +46,55 @@ function extractContentPaths(routeObject: RouteObject): string[] {
 	return paths
 }
 
-test('SPA_ROUTE_FILTERS match all React routes', () => {
-	router.flatMap(extractContentPaths).forEach((path) => {
-		expect(path).toMatchAny(SPA_ROUTE_FILTERS)
-	})
+function convertReactToVercel(path: string): string {
+	// react-router supports wildcard routes https://reactrouter.com/en/main/route/route#splats
+	// but we don't use them yet so just fail for now until we need them (if ever)
+	if (path.endsWith('*')) {
+		throw new Error('Wildcard routes are not supported yet (you can add this)')
+	}
+	// react-router supports optional route segments https://reactrouter.com/en/main/route/route#optional-segments
+	// but we don't use them yet so just fail for now until we need them (if ever)
+	if (path.match(/\?\//)) {
+		throw new Error('Optional route segments are not supported yet (you can add this)')
+	}
+	// Wrap in explicit start and end of string anchors (^ and $)
+	// and replace :param with [^/]* to match any string of non-slash characters, including the empty string
+	return '^' + path.replace(/:[^/]+/g, '[^/]*') + '/?$'
+}
+
+const spaRoutes = router
+	.flatMap(extractContentPaths)
+	.sort()
+	.map((path) => ({
+		reactRouterPattern: path,
+		vercelRoutingPattern: convertReactToVercel(path),
+	}))
+
+const allVercelRoutingPatterns = spaRoutes.map((route) => route.vercelRoutingPattern)
+
+test('the_routes', () => {
+	expect(spaRoutes).toMatchSnapshot()
 })
 
-test("SPA_ROUTE_FILTERS don't match assets", () => {
-	expect('/assets/test.png').not.toMatchAny(SPA_ROUTE_FILTERS)
+test('all React routes match', () => {
+	for (const route of spaRoutes) {
+		expect(route.reactRouterPattern).toMatch(new RegExp(route.vercelRoutingPattern))
+		for (const otherRoute of spaRoutes) {
+			if (route === otherRoute) continue
+			expect(route.reactRouterPattern).not.toMatch(new RegExp(otherRoute.vercelRoutingPattern))
+		}
+	}
+})
+
+test("non-react routes don't match", () => {
+	// lil smoke test for basic patterns
+	expect('/').toMatchAny(allVercelRoutingPatterns)
+	expect('/new').toMatchAny(allVercelRoutingPatterns)
+	expect('/r/whatever').toMatchAny(allVercelRoutingPatterns)
+	expect('/r/whatever/').toMatchAny(allVercelRoutingPatterns)
+
+	expect('/assets/test.png').not.toMatchAny(allVercelRoutingPatterns)
+	expect('/twitter-social.png').not.toMatchAny(allVercelRoutingPatterns)
+	expect('/robots.txt').not.toMatchAny(allVercelRoutingPatterns)
+	expect('/static/css/index.css').not.toMatchAny(allVercelRoutingPatterns)
 })
