df3bffe8d2e35ceba07306150c73e2d9fed192a0
David Sheldrick <d.j.sheldrick@gmail.com>
2024-02-24 10:22:45 +0000

    better sanity checks, test route extraction explicitly

--- apps/dotcom/src/routes.test.tsx
+++ apps/dotcom/src/routes.test.tsx
@@ -1,7 +1,8 @@
 import { expect } from '@jest/globals'
 import type { MatcherFunction } from 'expect'
 import { join } from 'path'
-import { RouteObject } from 'react-router-dom'
+import { ReactElement } from 'react'
+import { Route, RouteObject, createRoutesFromElements } from 'react-router-dom'
 import { router } from './routes'
 
 const toMatchAny: MatcherFunction<[regexes: unknown]> = function (actual, regexes) {
@@ -32,28 +33,49 @@ const toMatchAny: MatcherFunction<[regexes: unknown]> = function (actual, regexe
 expect.extend({ toMatchAny })
 
 function extractContentPaths(routeObject: RouteObject): string[] {
-	if (routeObject.children) {
-		const parentPath = routeObject.path || ''
-		return routeObject.children.flatMap((child) =>
-			extractContentPaths(child).map((childPath) => join(parentPath, childPath))
+	const path = routeObject.path || ''
+
+	const paths: string[] = []
+	if (
+		path &&
+		(routeObject.element || routeObject.Component || routeObject.lazy || routeObject.loader)
+	) {
+		// this is a contentful route so it gets included on it's own
+		// technically not every route with a .lazy or a .loader has content, but we don't have a way to check that
+		// and it's not a huge deal if they don't 404 correctly.
+		paths.push(path)
+	}
+	if (routeObject.children && routeObject.children.length > 0) {
+		// this route has children, so we need to recurse
+		paths.push(
+			...routeObject.children.flatMap((child) =>
+				extractContentPaths(child).map((childPath) => join(path, childPath))
+			)
 		)
-	} else if (routeObject.path) {
-		return [routeObject.path]
 	}
 
-	throw new Error('Route object has no path or children')
+	return paths
 }
 
 function convertReactToVercel(path: string): string {
 	// react-router supports wildcard routes https://reactrouter.com/en/main/route/route#splats
 	// but we don't use them yet so just fail for now until we need them (if ever)
 	if (path.endsWith('*')) {
-		throw new Error('Wildcard routes are not supported yet (you can add this)')
+		throw new Error(`Wildcard routes like '${path}' are not supported yet (you can add support!)`)
 	}
 	// react-router supports optional route segments https://reactrouter.com/en/main/route/route#optional-segments
 	// but we don't use them yet so just fail for now until we need them (if ever)
 	if (path.match(/\?\//)) {
-		throw new Error('Optional route segments are not supported yet (you can add this)')
+		throw new Error(
+			`Optional route segments like in '${path}' are not supported yet (you can add this)`
+		)
+	}
+	// make sure colons are immediately preceded by a slash
+	if (path.match(/[^/]:/)) {
+		throw new Error(`Colons in route segments must be immediately preceded by a slash in '${path}'`)
+	}
+	if (!path.startsWith('/')) {
+		throw new Error(`Route paths must start with a slash, but '${path}' does not`)
 	}
 	// Wrap in explicit start and end of string anchors (^ and $)
 	// and replace :param with [^/]* to match any string of non-slash characters, including the empty string
@@ -103,9 +125,59 @@ test('convertReactToVercel', () => {
 	expect(() =>
 		convertReactToVercel('/r/:roomId/history?/:timestamp')
 	).toThrowErrorMatchingInlineSnapshot(
-		`"Optional route segments are not supported yet (you can add this)"`
+		`"Optional route segments like in '/r/:roomId/history?/:timestamp' are not supported yet (you can add this)"`
 	)
 	expect(() => convertReactToVercel('/r/:roomId/history/*')).toThrowErrorMatchingInlineSnapshot(
-		`"Wildcard routes are not supported yet (you can add this)"`
+		`"Wildcard routes like '/r/:roomId/history/*' are not supported yet (you can add support!)"`
 	)
+	expect(() => convertReactToVercel('/r/foo:roomId/history')).toThrowErrorMatchingInlineSnapshot(
+		`"Colons in route segments must be immediately preceded by a slash in '/r/foo:roomId/history'"`
+	)
+	expect(() => convertReactToVercel('r/:roomId/history')).toThrowErrorMatchingInlineSnapshot(
+		`"Route paths must start with a slash, but 'r/:roomId/history' does not"`
+	)
+})
+
+function extract(...routes: ReactElement[]) {
+	return createRoutesFromElements(<Route>{routes}</Route>)
+		.flatMap(extractContentPaths)
+		.sort()
+}
+
+describe('extractContentPaths', () => {
+	it('only includes routes with content', () => {
+		expect(extract(<Route path="/foo" element={<div></div>} />)).toEqual(['/foo'])
+		expect(extract(<Route path="/foo" Component={() => 'hi'} />)).toEqual(['/foo'])
+		expect(
+			extract(<Route path="/foo" lazy={() => Promise.resolve({ Component: () => 'foo' })} />)
+		).toEqual(['/foo'])
+		expect(extract(<Route path="/foo" loader={async () => null} />)).toEqual(['/foo'])
+		expect(extract(<Route path="/foo"></Route>)).toEqual([])
+		expect(extract(<Route path="/foo"></Route>)).toEqual([])
+		expect(
+			extract(
+				<Route path="/foo">
+					<Route path="bar" />
+				</Route>
+			)
+		).toEqual([])
+	})
+	it('does not include parent routes without content', () => {
+		expect(
+			extract(
+				<Route path="/foo">
+					<Route path="bar" element={<div></div>} />
+				</Route>
+			)
+		).toEqual(['/foo/bar'])
+	})
+	it('does include parent routes with content', () => {
+		expect(
+			extract(
+				<Route path="/foo" element={<div></div>}>
+					<Route path="bar" element={<div></div>} />
+				</Route>
+			)
+		).toEqual(['/foo', '/foo/bar'])
+	})
 })
