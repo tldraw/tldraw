8b0216317a263ed3da8e301d97f2373c9a58a3c0
Steve Ruiz <steveruizok@gmail.com>
2024-02-25 23:18:30 +0000

    header tweaks

--- apps/docs/components/Header.tsx
+++ apps/docs/components/Header.tsx
@@ -15,33 +15,37 @@ export function Header({ sectionId }: { sectionId?: string }) {
 				</Link>
 			</div>
 			<Search />
-			<div className="layout__header__sections_and_socials">
-				<SectionLinks sectionId={sectionId} />
-				<a
-					href="https://x.com/tldraw/"
-					className="sidebar__button icon-button"
-					title="twitter"
-					target="_blank"
-				>
-					<Icon icon="twitter" />
-				</a>
-				<a
-					href="https://discord.com/invite/SBBEVCA4PG"
-					className="sidebar__button icon-button"
-					title="discord"
-					target="_blank"
-				>
-					<Icon icon="discord" />
-				</a>
-				<a
-					href="https://github.com/tldraw/tldraw"
-					className="sidebar__button icon-button"
-					title="github"
-					target="_blank"
-				>
-					<Icon icon="github" />
-				</a>
-				<ThemeSwitcher />
+			<div className="layout__header__links">
+				<div className="layout__header__sections">
+					<SectionLinks sectionId={sectionId} />
+				</div>
+				<div className="layout__header__socials">
+					<a
+						href="https://x.com/tldraw/"
+						className="sidebar__button icon-button"
+						title="twitter"
+						target="_blank"
+					>
+						<Icon icon="twitter" />
+					</a>
+					<a
+						href="https://discord.com/invite/SBBEVCA4PG"
+						className="sidebar__button icon-button"
+						title="discord"
+						target="_blank"
+					>
+						<Icon icon="discord" />
+					</a>
+					<a
+						href="https://github.com/tldraw/tldraw"
+						className="sidebar__button icon-button"
+						title="github"
+						target="_blank"
+					>
+						<Icon icon="github" />
+					</a>
+					<ThemeSwitcher />
+				</div>
 			</div>
 		</div>
 	)
