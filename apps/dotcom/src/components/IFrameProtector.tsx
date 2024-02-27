2d44573732d6f8bf60e6085052756cf38d5621ff
alex <alex@dytry.ch>
2024-02-27 10:48:46 +0000

    Open iframe production links in new tab
    cuz otherwise they don't work

--- apps/dotcom/src/components/IFrameProtector.tsx
+++ apps/dotcom/src/components/IFrameProtector.tsx
@@ -112,7 +112,7 @@ export function IFrameProtector({
 		return (
 			<div className="tldraw__editor tl-container">
 				<div className="iframe-warning__container">
-					<a className="iframe-warning__link" href={url} target="_parent">
+					<a className="iframe-warning__link" href={url} target="_blank">
 						{'Visit this page on tldraw.com '}
 						<svg
 							width="15"
