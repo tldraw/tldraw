092dfb30175bd552e84e7c8d10571723381259cc
Steve Ruiz <steveruizok@gmail.com>
2024-02-27 08:21:10 +0000

    fix mailtos

--- apps/docs/app/cla/page.tsx
+++ apps/docs/app/cla/page.tsx
@@ -103,7 +103,7 @@ export default async function ClaPage() {
 				</p>
 				<hr />
 				<p>
-					Questions or concerns? Email <a href="mailto://hello@tldraw.com">hello@tldraw.com.</a>
+					Questions or concerns? Email <a href="mailto:sales@tldraw.com">sales@tldraw.com.</a>
 				</p>
 			</main>
 		</>
