d2a824bb3dc0db3250e9fb7afbda20c4ef72e189
Taha <98838967+Taha-Hassan-Git@users.noreply.github.com>
2024-02-29 11:45:06 +0000

    copy changes

--- apps/examples/src/examples/interactive-shape/InteractiveShapeExample.tsx
+++ apps/examples/src/examples/interactive-shape/InteractiveShapeExample.tsx
@@ -6,6 +6,8 @@ import { MyShapeUtil } from './my-shape/my-shape-util'
 import snapshot from './snapshot.json'
 import { components, uiOverrides } from './ui-overrides'
 
+// There's a guide at the bottom of this file!
+
 // [1]
 const customShapeUtils = [MyShapeUtil]
 const customTools = [MyShapeTool]
@@ -32,10 +34,11 @@ export default function InteractiveShapeExample() {
 /*
 Introduction:
 
-By default the editor handles click events, but sometimes you want to handle pointer 
-events on your shape in your own ways. You can do this by using the css property 
-`pointer events: all` and stopping event propagation. In this example we want our 
-to-do shape to have a checkbox so the user can mark them as done.
+By default the editor handles pointer events, but sometimes you want to handle 
+interactions on your shape in your own ways, for example via a button. You can do this 
+by using the css property `pointer events: all` and stopping event propagation. In 
+this example we want our todo shape to have a checkbox so the user can mark them as 
+done.
 
 Most of the relevant code for this is in the my-shape-util.tsx file. We also define a
 very simple tool in my-shape-tool.tsx, and make our new tool appear on the toolbar in 
--- apps/examples/src/examples/interactive-shape/README.md
+++ apps/examples/src/examples/interactive-shape/README.md
@@ -9,4 +9,4 @@ A custom shape that has its own onClick interactions.
 
 ---
 
-By default the editor handles click events, but sometimes you want to handle pointer events on your shape in your own ways. You can do this by using the css property `pointer events: all` and stopping event propagation. In this example we want our to-do shape to have a checkbox so the user can mark them as done.
+By default the editor handles pointer events, but sometimes you want to handle interactions on your shape in your own ways, for example via a button. You can do this by using the css property `pointer events: all` and stopping event propagation. In this example we want our todo shape to have a checkbox so the user can mark them as done.
