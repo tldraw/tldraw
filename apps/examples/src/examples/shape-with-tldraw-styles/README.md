e965d7c1e0f8989589be0629b6c0d86503449666
Steve Ruiz <steveruizok@gmail.com>
2024-03-02 16:11:41 +0000

    add custom styles example.

--- apps/examples/src/examples/shape-with-tldraw-styles/README.md
+++ apps/examples/src/examples/shape-with-tldraw-styles/README.md
@@ -9,4 +9,8 @@ Using the tldraw style panel with your custom shapes
 
 ---
 
-The style panel always shows the opacity slider when a shape is selected, you can expose other parts of the style panel for your custom shape if . In this example we use the size and color styles to set the style of the text in our shape.
+The default tldraw UI will displaye UI for the styles of your selection or your current tool. For example, when you have two shapes selected that both have the tldraw's "size" style, the size selector will be displayed. If all of your selected shapes have the same value for this style, that value will be shown as selected in the panel. If they have different values, the panel will show the value as "mixed".
+
+You can use tldraw's Styles API to create your own styles that behave in the same way, though you'll also need to create a custom UI for your style.
+
+Alternatively, you can use tldraw's default styles in your own shapes. This example shows how to do that.
--- apps/examples/src/examples/shape-with-tldraw-styles/ShapeWithTldrawStylesExample.tsx
+++ apps/examples/src/examples/shape-with-tldraw-styles/ShapeWithTldrawStylesExample.tsx
@@ -5,14 +5,16 @@ import {
 	HTMLContainer,
 	T,
 	TLBaseShape,
+	TLDefaultColorStyle,
+	TLDefaultSizeStyle,
 	Tldraw,
-} from '@tldraw/tldraw'
-import { useDefaultColorTheme } from '@tldraw/tldraw/src/lib/shapes/shared/ShapeFill'
-import '@tldraw/tldraw/tldraw.css'
+} from 'tldraw'
+import { useDefaultColorTheme } from 'tldraw/src/lib/shapes/shared/ShapeFill'
+import 'tldraw/tldraw.css'
 
 // There's a guide at the bottom of this file!
 
-const FONT_SIZES = {
+const FONT_SIZES: Record<TLDefaultSizeStyle, number> = {
 	s: 14,
 	m: 25,
 	l: 38,
@@ -24,26 +26,16 @@ type IMyShape = TLBaseShape<
 	{
 		w: number
 		h: number
-		size: 's' | 'm' | 'l' | 'xl'
-		color:
-			| 'black'
-			| 'grey'
-			| 'light-violet'
-			| 'violet'
-			| 'blue'
-			| 'light-blue'
-			| 'yellow'
-			| 'orange'
-			| 'green'
-			| 'light-green'
-			| 'light-red'
-			| 'red'
+		// [1]
+		size: TLDefaultSizeStyle
+		color: TLDefaultColorStyle
 	}
 >
 
 class MyShapeUtil extends BaseBoxShapeUtil<IMyShape> {
 	static override type = 'myshape' as const
-	// [1]
+
+	// [2]
 	static override props = {
 		w: T.number,
 		h: T.number,
@@ -60,18 +52,22 @@ class MyShapeUtil extends BaseBoxShapeUtil<IMyShape> {
 		}
 	}
 
-	// [2]
 	component(shape: IMyShape) {
-		const size = FONT_SIZES[shape.props.size]
 		// eslint-disable-next-line react-hooks/rules-of-hooks
 		const theme = useDefaultColorTheme()
 
 		return (
 			<HTMLContainer
 				id={shape.id}
-				style={{ backgroundColor: 'rgba(0,0,0,0.1)', overflow: 'hidden' }}
+				style={{ backgroundColor: 'var(--color-low-border)', overflow: 'hidden' }}
 			>
-				<div style={{ fontSize: size, color: theme[shape.props.color].solid }}>
+				<div
+					style={{
+						// [3]
+						fontSize: FONT_SIZES[shape.props.size],
+						color: theme[shape.props.color].solid,
+					}}
+				>
 					Select the shape and use the style panel to change the font size and color
 				</div>
 			</HTMLContainer>
@@ -83,13 +79,12 @@ class MyShapeUtil extends BaseBoxShapeUtil<IMyShape> {
 	}
 }
 
-// [3]
 const customShapeUtils = [MyShapeUtil]
+
 export default function ShapeWithTldrawStylesExample() {
 	return (
 		<div className="tldraw__editor">
 			<Tldraw
-				// Pass in the array of custom shape classes
 				shapeUtils={customShapeUtils}
 				onMount={(editor) => {
 					editor.createShape({ type: 'myshape', x: 100, y: 100 })
@@ -100,27 +95,26 @@ export default function ShapeWithTldrawStylesExample() {
 }
 
 /* 
-Introduction:
 
-By default the style panel exposes the opacity slider fot all selected shapes, including custom shapes.
-If you want to control other styles using the tldraw style panel you can do that by using default 
-StyleProps in your shape's props[1]. In this example we use the DefaultSizeStyle and DefaultColorStyle
-to control the size and color of the text in the custom shape.
+This file shows a custom shape that uses tldraw's default styles. 
+For more on custom shapes, see our Custom Shape example.
 
 [1]
-DefaultSizeStyle and DefaultColorStyle are two of the default StyleProps that are available in the tldraw
-package. They are used to control the size and color of the text in the custom shape. 
+In this example, our custom shape will use the size and color styles from the
+default styles. When typing a custom shape, you can use our types for
+these styles.
 
 [2]
-The component method is where the shape's visual representation is defined. We need to make sure that our 
-shape renders differently when different styles are selected. We're using an object to select a different
-font size based on the size prop, and the useDefaultColorTheme hook to get the color theme based on the
-current dark mode setting. We then use the color theme to set the color of the text.
+For the shape's props, we'll pass the DefaultSizeStyle and DefaultColorStyle
+styles for the two properties, size and color. There's nothing special about
+these styles except that the editor will notice when two shapes are selected
+that share the same style. (You can use the useRelevantStyles hook to get the
+styles of the user's selected shapes.)
 
 [3]
-This is where we pass our custom shape to the tldraw component and create a new shape when the editor mounts.
-Check out the custom shape example for more information on creating custom shapes, or the editor api example
-for more information on programmatically controlling the canvas.
-
-
+Here in the component, we'll use the styles to change the way that our shape
+appears. The style values themselves are just strings, like 'xl' or 'black',
+so it's up to you to decide how to use them. In this example, we're using the
+size to set the text's font-size property, and also using the default theme
+(via the useDefaultColorTheme hook) to get the color for the text.
 */
