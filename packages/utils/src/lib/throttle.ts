48348639669e556798296eee82fc53ca8ef444f2
Mitja Bezenšek <mitja.bezensek@gmail.com>
2024-03-05 11:49:54 +0000

    Rename and cleanup.

--- packages/utils/src/lib/throttle.ts
+++ packages/utils/src/lib/throttle.ts
@@ -4,49 +4,39 @@ const isTest = () =>
 	// @ts-expect-error
 	!globalThis.__FORCE_RAF_IN_TESTS__
 
-const rafQueue: Array<() => void> = []
+const fpsQueue: Array<() => void> = []
+const targetFps = 60
+const targetTimePerFrame = 1000 / targetFps
+let frame: number | undefined
+let time = 0
+let last = 0
 
 const tick = () => {
-	const queue = rafQueue.splice(0, rafQueue.length)
+	const queue = fpsQueue.splice(0, fpsQueue.length)
 	for (const fn of queue) {
 		fn()
 	}
 }
 
-const fps = 60
-const targetTimePerFrame = 1000 / fps
-let frame: number | undefined
-let time = 0
-let last = 0
-const timePerFrame = targetTimePerFrame
-
-function sixtyFps() {
+function fps() {
 	if (frame) {
 		return
 	}
 	const now = Date.now()
 	const elapsed = now - last
 
-	// Variable frame rate
-	// if (elapsed < 1000 && elapsed > timePerFrame * 1.1) {
-	// 	timePerFrame *= 2
-	// } else if (elapsed < timePerFrame / 2) {
-	// 	timePerFrame = Math.max(targetTimePerFrame, timePerFrame / 2)
-	// }
-
-	// console.log('elapsed', elapsed, 'timePerFrame', timePerFrame)
-	if (time + elapsed < timePerFrame) {
+	if (time + elapsed < targetTimePerFrame) {
 		frame = requestAnimationFrame(() => {
 			frame = undefined
-			sixtyFps()
+			fps()
 		})
 		return
 	}
 	frame = requestAnimationFrame(() => {
 		frame = undefined
 		last = now
-		// If we fall behind more than 10 frames, we'll just reset the time so we don't
-		// try to update a number of times
+		// If we fall behind more than 10 frames, we'll just reset the time so we don't try to update a number of times
+		// This can happen if we don't interact with the page for a while
 		time = Math.min(time + elapsed - targetTimePerFrame, targetTimePerFrame * 10)
 		tick()
 	})
@@ -56,48 +46,51 @@ let started = false
 
 /**
  * Returns a throttled version of the function that will only be called max once per frame.
+ * The target frame rate is 60fps.
  * @param fn - the fun to return a throttled version of
  * @returns
  * @internal
  */
-export function rafThrottle(fn: () => void) {
+export function nextFrameThrottle(fn: () => void) {
 	if (isTest()) {
 		return fn
 	}
 
 	return () => {
-		if (rafQueue.includes(fn)) {
+		if (fpsQueue.includes(fn)) {
 			return
 		}
-		rafQueue.push(fn)
+		fpsQueue.push(fn)
 		if (!started) {
 			started = true
+			// We set last to Date.now() - targetTimePerFrame - 1 so that the first run will happen immediately
 			last = Date.now() - targetTimePerFrame - 1
 		}
-		sixtyFps()
+		fps()
 	}
 }
 
 /**
- * Calls the function on the next frame.
+ * Calls the function on the next frame. The target frame rate is 60fps.
  * If the same fn is passed again before the next frame, it will still be called only once.
- * @param fn - the fun to call on the next animation frame
+ * @param fn - the fun to call on the next frame
  * @returns
  * @internal
  */
-export function throttledRaf(fn: () => void) {
+export function throttledOnNextFrame(fn: () => void) {
 	if (isTest()) {
 		return fn()
 	}
 
-	if (rafQueue.includes(fn)) {
+	if (fpsQueue.includes(fn)) {
 		return
 	}
 
-	rafQueue.push(fn)
+	fpsQueue.push(fn)
 	if (!started) {
 		started = true
+		// We set last to Date.now() - targetTimePerFrame - 1 so that the first run will happen immediately
 		last = Date.now() - targetTimePerFrame - 1
 	}
-	sixtyFps()
+	fps()
 }
