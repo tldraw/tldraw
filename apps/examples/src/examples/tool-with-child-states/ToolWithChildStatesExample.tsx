b8e5c49d8630c3ad0666bd9d60862b7fdc4e6b7f
Taha <98838967+Taha-Hassan-Git@users.noreply.github.com>
2024-03-05 15:42:56 +0000

    better intro

--- apps/examples/src/examples/tool-with-child-states/ToolWithChildStatesExample.tsx
+++ apps/examples/src/examples/tool-with-child-states/ToolWithChildStatesExample.tsx
@@ -188,9 +188,9 @@ export default function ToolWithChildStatesExample() {
 Introduction:
 
 Tools are nodes in tldraw's state machine. They are responsible for handling user input. 
-You can create custom tools by extending the `StateNode` class and overriding its methods.
-In this example we make expand on the sticker tool example to show how to create a tool
-with child states.
+You can create custom tools by extending the `StateNode` class and overriding its 
+methods. In this example we expand on the sticker tool from the custom tool example to 
+show how to create a tool that can handle more complex interactions by using child states.
 
 [1]
 This is our custom tool. It has three child states: `Idle`, `Pointing`, and `Dragging`.
