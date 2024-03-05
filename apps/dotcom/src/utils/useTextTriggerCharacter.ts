46097e78c99da8bd77c5fd9e9c4ebf3dd8fc2bd3
Mime Čuvalo <mimecuvalo@gmail.com>
2024-03-05 09:53:50 +0000

    add comment

--- apps/dotcom/src/utils/useTextTriggerCharacter.ts
+++ apps/dotcom/src/utils/useTextTriggerCharacter.ts
@@ -8,5 +8,9 @@ export function useTextTriggerCharacter(
 	const editor = useEditor()
 	const { onKeyDown } = useEmojis(editor, inputEl, onComplete)
 
+	// N.B. We could chain multiple onKeyDown handlers here if we wanted to (say, for hashtags, or @-mentions)
+	// const { onKeyDown: onKeyDown2 } = useSomeOtherHook(editor, inputEl, onComplete)
+	// return { onKeyDown: (e, coords) => onKeyDown(e, coords) && onKeyDown2(e, coords) }
+
 	return { onKeyDown }
 }
