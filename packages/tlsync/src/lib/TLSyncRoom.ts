a44773e85dc7ffc5b4109ee3158c623142e8507f
Dan Groshev <git@dgroshev.com>
2024-03-11 22:14:35 +0000

    squish data events before sending them out

--- packages/tlsync/src/lib/TLSyncRoom.ts
+++ packages/tlsync/src/lib/TLSyncRoom.ts
@@ -46,6 +46,7 @@ import {
 	TLSocketServerSentDataEvent,
 	TLSocketServerSentEvent,
 } from './protocol'
+import { squishDataEvents } from './squish'
 
 /** @public */
 export type TLRoomSocket<R extends UnknownRecord> = {
@@ -440,7 +441,10 @@ export class TLSyncRoom<R extends UnknownRecord> {
 		session.debounceTimer = null
 
 		if (session.outstandingDataMessages.length > 0) {
-			session.socket.sendMessage({ type: 'data', data: session.outstandingDataMessages })
+			session.socket.sendMessage({
+				type: 'data',
+				data: squishDataEvents(session.outstandingDataMessages),
+			})
 			session.outstandingDataMessages.length = 0
 		}
 	}
