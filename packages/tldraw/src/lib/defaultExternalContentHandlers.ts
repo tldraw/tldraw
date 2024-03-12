tldraw/tldraw

commit dba6d4c414fa571519e252d581e3489101280acc

chore: cleanup multiple uses of FileReader (#3110)
from
https://discord.com/channels/859816885297741824/1006133967642177556/1213038401465618433

### Change Type

- [x] `patch` — Bug fix

packages/tldraw/src/lib/shapes/image/ImageShapeUtil.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 import {
 	BaseBoxShapeUtil,
+	FileHelpers,
 	HTMLContainer,
 	TLImageShape,
 	TLOnDoubleClickHandler,
@@ -19,12 +20,7 @@ import { usePrefersReducedMotion } from '../shared/usePrefersReducedMotion'
 async function getDataURIFromURL(url: string): Promise<string> {
 	const response = await fetch(url)
 	const blob = await response.blob()
-	return new Promise((resolve, reject) => {
-		const reader = new FileReader()
-		reader.onloadend = () => resolve(reader.result as string)
-		reader.onerror = reject
-		reader.readAsDataURL(blob)
-	})
+	return FileHelpers.fileToBase64(blob)
 }
 
 /** @public */

packages/tldraw/src/lib/shapes/shared/defaultStyleDefs.tsx
@@ -2,6 +2,7 @@ import {
 	DefaultColorThemePalette,
 	DefaultFontFamilies,
 	DefaultFontStyle,
+	FileHelpers,
 	HASH_PATTERN_ZOOM_NAMES,
 	MAX_ZOOM,
 	SvgExportDef,
@@ -27,12 +28,7 @@ export function getFontDefForExport(fontStyle: TLDefaultFontStyle): SvgExportDef
 			if (!url || !fontFaceRule) return null
 
 			const fontFile = await (await fetch(url)).blob()
-			const base64FontFile = await new Promise<string>((resolve, reject) => {
-				const reader = new FileReader()
-				reader.onload = () => resolve(reader.result as string)
-				reader.onerror = reject
-				reader.readAsDataURL(fontFile)
-			})
+			const base64FontFile = FileHelpers.fileToBase64(fontFile)
 
 			const newFontFaceRule = fontFaceRule.replace(url, base64FontFile)
 			const style = document.createElementNS('http://www.w3.org/2000/svg', 'style')

packages/tldraw/src/lib/ui/hooks/useClipboardEvents.ts
@@ -1,5 +1,6 @@
 import {
 	Editor,
+	FileHelpers,
 	TLArrowShape,
 	TLBookmarkShape,
 	TLEmbedShape,
@@ -79,26 +80,6 @@ function disallowClipboardEvents(editor: Editor) {
 			(activeElement.getAttribute('contenteditable') ||
 				INPUTS.indexOf(activeElement.tagName.toLowerCase()) > -1))
 	)
-}
-
-/**
- * Get a blob as a string.
- *
- * @param blob - The blob to get as a string.
- * @internal
- */
-async function blobAsString(blob: Blob) {
-	return new Promise<string>((resolve, reject) => {
-		const reader = new FileReader()
-		reader.addEventListener('loadend', () => {
-			const text = reader.result
-			resolve(text as string)
-		})
-		reader.addEventListener('error', () => {
-			reject(reader.error)
-		})
-		reader.readAsText(blob)
-	})
 }
 
 /**
@@ -270,7 +251,7 @@ const handlePasteFromClipboardApi = async (
 			things.push({
 				type: 'html',
 				source: new Promise<string>((r) =>
-					item.getType('text/html').then((blob) => blobAsString(blob).then(r))
+					item.getType('text/html').then((blob) => FileHelpers.fileToBase64(blob).then(r))
 				),
 			})
 		}
@@ -279,7 +260,7 @@ const handlePasteFromClipboardApi = async (
 			things.push({
 				type: 'url',
 				source: new Promise<string>((r) =>
-					item.getType('text/uri-list').then((blob) => blobAsString(blob).then(r))
+					item.getType('text/uri-list').then((blob) => FileHelpers.fileToBase64(blob).then(r))
 				),
 			})
 		}
@@ -288,7 +269,7 @@ const handlePasteFromClipboardApi = async (
 			things.push({
 				type: 'text',
 				source: new Promise<string>((r) =>
-					item.getType('text/plain').then((blob) => blobAsString(blob).then(r))
+					item.getType('text/plain').then((blob) => FileHelpers.fileToBase64(blob).then(r))
 				),
 			})
 		}

packages/tldraw/src/lib/utils/export/export.ts
@@ -1,5 +1,6 @@
 import {
 	Editor,
+	FileHelpers,
 	PngHelpers,
 	TLShapeId,
 	TLSvgOptions,
@@ -98,7 +99,6 @@ export async function getSvgAsString(svg: SVGElement) {
 	svg.setAttribute('width', +svg.getAttribute('width')! + '')
 	svg.setAttribute('height', +svg.getAttribute('height')! + '')
 
-	const fileReader = new FileReader()
 	const imgs = Array.from(clone.querySelectorAll('image')) as SVGImageElement[]
 
 	for (const img of imgs) {
@@ -106,11 +106,7 @@ export async function getSvgAsString(svg: SVGElement) {
 		if (src) {
 			if (!src.startsWith('data:')) {
 				const blob = await (await fetch(src)).blob()
-				const base64 = await new Promise<string>((resolve, reject) => {
-					fileReader.onload = () => resolve(fileReader.result as string)
-					fileReader.onerror = () => reject(fileReader.error)
-					fileReader.readAsDataURL(blob)
-				})
+				const base64 = await FileHelpers.fileToBase64(blob)
 				img.setAttribute('xlink:href', base64)
 			}
 		}

packages/utils/src/lib/file.ts
@@ -32,10 +32,10 @@ export class FileHelpers {
 		return await new Promise((resolve, reject) => {
 			if (file) {
 				const reader = new FileReader()
-				reader.readAsDataURL(file)
 				reader.onload = () => resolve(reader.result as string)
 				reader.onerror = (error) => reject(error)
 				reader.onabort = (error) => reject(error)
+				reader.readAsDataURL(file)
 			}
 		})
 	}

packages/utils/src/lib/media.ts
@@ -1,3 +1,4 @@
+import { FileHelpers } from './file'
 import { PngHelpers } from './png'
 
 /**
@@ -44,16 +45,8 @@ export class MediaHelpers {
 	 * Read a blob into a data url
 	 * @public
 	 */
-	static blobToDataUrl(blob: Blob): Promise<string> {
-		return new Promise((resolve, reject) => {
-			const reader = new FileReader()
-			reader.onload = () => resolve(reader.result as string)
-			reader.onerror = (e) => {
-				console.error(e)
-				reject(new Error('Could not read blob'))
-			}
-			reader.readAsDataURL(blob)
-		})
+	static blobToDataUrl(blob: Blob) {
+		return FileHelpers.fileToBase64(blob)
 	}
 
 	/**

commit 0a48aea7bb042ceaebf692e04cbdd0c97074d709

fixup file helpers (#3130)
We had a couple regressions in #3110: first a missing `await` was
causing fonts not to get properly embedded in exports. second, some
`readAsText` calls were replaced with `readAsDataURL` calls.

### Change Type

<!-- ❗ Please select a 'Scope' label ❗️ -->

- [x] `sdk` — Changes the tldraw SDK
- [ ] `dotcom` — Changes the tldraw.com web app
- [ ] `docs` — Changes to the documentation, examples, or templates.
- [ ] `vs code` — Changes to the vscode plugin
- [ ] `internal` — Does not affect user-facing stuff

<!-- ❗ Please select a 'Type' label ❗️ -->

- [x] `bugfix` — Bug fix
- [ ] `feature` — New feature
- [ ] `improvement` — Improving existing features
- [ ] `chore` — Updating dependencies, other boring stuff
- [ ] `galaxy brain` — Architectural changes
- [ ] `tests` — Changes to any test code
- [ ] `tools` — Changes to infrastructure, CI, internal scripts,
debugging tools, etc.
- [ ] `dunno` — I don't know

packages/tldraw/src/lib/defaultExternalContentHandlers.ts
@@ -1,6 +1,7 @@
 import {
 	AssetRecordType,
 	Editor,
+	FileHelpers,
 	MediaHelpers,
 	TLAsset,
 	TLAssetId,
@@ -96,7 +97,7 @@ export function registerDefaultExternalContentHandlers(
 			typeName: 'asset',
 			props: {
 				name,
-				src: await MediaHelpers.blobToDataUrl(file),
+				src: await FileHelpers.blobToDataUrl(file),
 				w: size.w,
 				h: size.h,
 				mimeType: file.type,

packages/tldraw/src/lib/shapes/image/ImageShapeUtil.tsx
@@ -20,7 +20,7 @@ import { usePrefersReducedMotion } from '../shared/usePrefersReducedMotion'
 async function getDataURIFromURL(url: string): Promise<string> {
 	const response = await fetch(url)
 	const blob = await response.blob()
-	return FileHelpers.fileToBase64(blob)
+	return FileHelpers.blobToDataUrl(blob)
 }
 
 /** @public */

packages/tldraw/src/lib/shapes/shared/defaultStyleDefs.tsx
@@ -23,12 +23,12 @@ export function getFontDefForExport(fontStyle: TLDefaultFontStyle): SvgExportDef
 			const font = findFont(fontStyle)
 			if (!font) return null
 
-			const url = (font as any).$$_url
-			const fontFaceRule = (font as any).$$_fontface
+			const url: string = (font as any).$$_url
+			const fontFaceRule: string = (font as any).$$_fontface
 			if (!url || !fontFaceRule) return null
 
 			const fontFile = await (await fetch(url)).blob()
-			const base64FontFile = FileHelpers.fileToBase64(fontFile)
+			const base64FontFile = await FileHelpers.blobToDataUrl(fontFile)
 
 			const newFontFaceRule = fontFaceRule.replace(url, base64FontFile)
 			const style = document.createElementNS('http://www.w3.org/2000/svg', 'style')

packages/tldraw/src/lib/ui/hooks/useClipboardEvents.ts
@@ -250,27 +250,30 @@ const handlePasteFromClipboardApi = async (
 		if (item.types.includes('text/html')) {
 			things.push({
 				type: 'html',
-				source: new Promise<string>((r) =>
-					item.getType('text/html').then((blob) => FileHelpers.fileToBase64(blob).then(r))
-				),
+				source: (async () => {
+					const blob = await item.getType('text/html')
+					return await FileHelpers.blobToText(blob)
+				})(),
 			})
 		}
 
 		if (item.types.includes('text/uri-list')) {
 			things.push({
 				type: 'url',
-				source: new Promise<string>((r) =>
-					item.getType('text/uri-list').then((blob) => FileHelpers.fileToBase64(blob).then(r))
-				),
+				source: (async () => {
+					const blob = await item.getType('text/uri-list')
+					return await FileHelpers.blobToText(blob)
+				})(),
 			})
 		}
 
 		if (item.types.includes('text/plain')) {
 			things.push({
 				type: 'text',
-				source: new Promise<string>((r) =>
-					item.getType('text/plain').then((blob) => FileHelpers.fileToBase64(blob).then(r))
-				),
+				source: (async () => {
+					const blob = await item.getType('text/plain')
+					return await FileHelpers.blobToText(blob)
+				})(),
 			})
 		}
 	}

packages/tldraw/src/lib/utils/export/export.ts
@@ -106,7 +106,7 @@ export async function getSvgAsString(svg: SVGElement) {
 		if (src) {
 			if (!src.startsWith('data:')) {
 				const blob = await (await fetch(src)).blob()
-				const base64 = await FileHelpers.fileToBase64(blob)
+				const base64 = await FileHelpers.blobToDataUrl(blob)
 				img.setAttribute('xlink:href', base64)
 			}
 		}

packages/tldraw/src/lib/utils/tldr/file.ts
@@ -168,7 +168,7 @@ export async function serializeTldrawJson(store: TLStore): Promise<string> {
 					let assetSrcToSave
 					try {
 						// try to save the asset as a base64 string
-						assetSrcToSave = await FileHelpers.fileToBase64(
+						assetSrcToSave = await FileHelpers.blobToDataUrl(
 							await (await fetch(record.props.src)).blob()
 						)
 					} catch {

packages/utils/src/lib/file.ts
@@ -6,29 +6,27 @@
 export class FileHelpers {
 	/**
 	 * @param dataURL - The file as a string.
-	 * @internal
 	 *
 	 * from https://stackoverflow.com/a/53817185
 	 */
-	static async base64ToFile(dataURL: string) {
+	static async dataUrlToArrayBuffer(dataURL: string) {
 		return fetch(dataURL).then(function (result) {
 			return result.arrayBuffer()
 		})
 	}
 
 	/**
-	 * Convert a file to base64.
+	 * Convert a file to a base64 encoded data url.
 	 *
 	 * @example
 	 *
 	 * ```ts
-	 * const A = fileToBase64('./test.png')
+	 * const A = FileHelpers.toDataUrl(myImageFile)
 	 * ```
 	 *
 	 * @param value - The file as a blob.
-	 * @public
 	 */
-	static async fileToBase64(file: Blob): Promise<string> {
+	static async blobToDataUrl(file: Blob): Promise<string> {
 		return await new Promise((resolve, reject) => {
 			if (file) {
 				const reader = new FileReader()
@@ -36,6 +34,29 @@ export class FileHelpers {
 				reader.onerror = (error) => reject(error)
 				reader.onabort = (error) => reject(error)
 				reader.readAsDataURL(file)
+			}
+		})
+	}
+
+	/**
+	 * Convert a file to a unicode text string.
+	 *
+	 * @example
+	 *
+	 * ```ts
+	 * const A = FileHelpers.fileToDataUrl(myTextFile)
+	 * ```
+	 *
+	 * @param value - The file as a blob.
+	 */
+	static async blobToText(file: Blob): Promise<string> {
+		return await new Promise((resolve, reject) => {
+			if (file) {
+				const reader = new FileReader()
+				reader.onload = () => resolve(reader.result as string)
+				reader.onerror = (error) => reject(error)
+				reader.onabort = (error) => reject(error)
+				reader.readAsText(file)
 			}
 		})
 	}

packages/utils/src/lib/media.ts
@@ -1,4 +1,3 @@
-import { FileHelpers } from './file'
 import { PngHelpers } from './png'
 
 /**
@@ -39,14 +38,6 @@ export class MediaHelpers {
 			img.crossOrigin = 'anonymous'
 			img.src = src
 		})
-	}
-
-	/**
-	 * Read a blob into a data url
-	 * @public
-	 */
-	static blobToDataUrl(blob: Blob) {
-		return FileHelpers.fileToBase64(blob)
 	}
 
 	/**