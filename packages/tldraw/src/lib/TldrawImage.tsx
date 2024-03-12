83544a9ea8c04f8f42d025089fffdbace1f519c4
Mime Čuvalo <mimecuvalo@gmail.com>
2024-03-12 13:36:24 +0000

    docs: fix missing API entries (#3111)
    following up on
    https://discord.com/channels/859816885297741824/1162726738774720574/1211715924613275681
    
    several things here:
    - `docs/api/.*json` were out-of-date — seems like fetch-api-source
    should run automatically? shouldn't `build-api` also override this
    directory? in particular, tldraw.api.json still had a ton of references
    to the old @tldraw/tldraw package
    - the main problem was that `generateApiContent` was failing silently.
    we were relying on Promises and this broke silently because we never
    handled exceptions. i got rid of the Promise as it was unnecessary and
    made the exceptions bubble up
    - two things were broken in the docs and those are fixed, so now the
    missing entries will resurface
    
    ### Change Type
    
    - [x] `documentation` — Changes to the documentation only[^2]
    
    ---------
    
    Co-authored-by: Steve Ruiz <steveruizok@gmail.com>

--- packages/tldraw/src/lib/TldrawImage.tsx
+++ packages/tldraw/src/lib/TldrawImage.tsx
@@ -51,7 +51,7 @@ export type TldrawImageProps = Expand<
  *
  * @example
  * ```tsx
- * <TldrawImage snapshot={snapshot} />
+ * <TldrawImage
  * 	snapshot={snapshot}
  * 	pageId={pageId}
  * 	background={false}
