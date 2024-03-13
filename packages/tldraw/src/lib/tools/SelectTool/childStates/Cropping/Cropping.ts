dcc58fe28f12d1ce642b4c7e3c65e4ae859129bc
Steve Ruiz <steveruizok@gmail.com>
2024-03-13 22:38:33 +0000

    remove d&d manager, duplicated state

--- packages/tldraw/src/lib/tools/SelectTool/childStates/Cropping/Cropping.ts
+++ packages/tldraw/src/lib/tools/SelectTool/childStates/Cropping/Cropping.ts
@@ -3,10 +3,10 @@ import { Idle } from './children/Idle'
 import { NudgingCrop } from './children/NudgingCrop'
 import { PointingCrop } from './children/PointingCrop'
 import { PointingCropHandle } from './children/PointingCropHandle'
-import { ResizingCrop } from './children/ResizingCrop'
 import { TranslatingCrop } from './children/TranslatingCrop'
 // Borrow these from select tool
 import { PointingRotateHandle } from '../PointingRotateHandle'
+import { ResizingCrop } from '../ResizingCrop'
 import { Rotating } from '../Rotating'
 
 export class Cropping extends StateNode {
