c72afe31cb14489f469d9bc196e624309ee9850f
David Sheldrick <d.j.sheldrick@gmail.com>
2024-03-14 15:31:19 +0000

    fix release eliding

--- scripts/publish-patch.ts
+++ scripts/publish-patch.ts
@@ -17,9 +17,6 @@ async function main() {
 	const latestVersionOnNpm = (await exec('npm', ['show', 'tldraw', 'version'])).trim()
 
 	const isLatestVersion = latestVersionInBranch.format() === latestVersionOnNpm
-	if (process.env.GITHUB_OUTPUT) {
-		appendFileSync(process.env.GITHUB_OUTPUT, `is_latest_version=${isLatestVersion}\n`)
-	}
 
 	const nextVersion = latestVersionInBranch.inc('patch').format()
 	// check we're on the main branch on HEAD
@@ -50,6 +47,10 @@ async function main() {
 	if (!(await didAnyPackageChange())) {
 		nicelog('No packages have changed, skipping release')
 		return
+	}
+
+	if (process.env.GITHUB_OUTPUT) {
+		appendFileSync(process.env.GITHUB_OUTPUT, `is_latest_version=${isLatestVersion}\n`)
 	}
 
 	nicelog('Releasing version', nextVersion)
