1103581ee69d74f4909ab2fc3816127dba3d21a1
Mime Čuvalo <mimecuvalo@gmail.com>
2024-03-14 15:57:38 +0000

    dont allow on mobile

--- apps/dotcom/src/utils/useTextTriggerCharacter.ts
+++ apps/dotcom/src/utils/useTextTriggerCharacter.ts
@@ -1,12 +1,19 @@
-import { useEditor } from 'tldraw'
+import { useBreakpoint, useEditor } from 'tldraw'
 import { useEmojis } from '../hooks/useEmojis'
 
 export function useTextTriggerCharacter(
 	inputEl: HTMLTextAreaElement | null,
 	onComplete: (text: string) => void
 ) {
+	const breakpoint = useBreakpoint()
 	const editor = useEditor()
 	const { onKeyDown } = useEmojis(editor, inputEl, onComplete)
+
+	if (breakpoint < 5 /* PORTRAIT_BREAKPOINT.TABLET_SM */) {
+		// We don't support emojis on mobile because mobile devices
+		// have emoji keyboards built in to them.
+		return { onKeyDown: () => {/* do nothing */} }
+	}
 
 	// N.B. We could chain multiple onKeyDown handlers here if we wanted to (say, for hashtags, or @-mentions)
 	// const { onKeyDown: onKeyDown2 } = useSomeOtherHook(editor, inputEl, onComplete)
