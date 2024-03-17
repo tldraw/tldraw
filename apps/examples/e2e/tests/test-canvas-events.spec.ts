ba751dbf5021e0a2cd7976386a595f2c06b0411d
Steve Ruiz <steveruizok@gmail.com>
2024-03-17 17:17:03 +0000

    wait a frame

--- apps/examples/e2e/tests/test-canvas-events.spec.ts
+++ apps/examples/e2e/tests/test-canvas-events.spec.ts
@@ -1,6 +1,6 @@
 import test, { expect, Page } from '@playwright/test'
 import { Editor } from 'tldraw'
-import { setupPage } from '../shared-e2e'
+import { setupPage, sleep } from '../shared-e2e'
 
 declare const __tldraw_editor_events: any[]
 
@@ -21,6 +21,7 @@ test.describe('Canvas events', () => {
 			await page.mouse.move(200, 200) // to kill any double clicks
 			await page.mouse.move(100, 100)
 			await page.mouse.down()
+			await sleep(20)
 			expect(await page.evaluate(() => __tldraw_editor_events.at(-1))).toMatchObject({
 				target: 'canvas',
 				type: 'pointer',
@@ -33,6 +34,7 @@ test.describe('Canvas events', () => {
 			await page.mouse.move(100, 100)
 			await page.mouse.down()
 			await page.mouse.move(101, 101)
+			await sleep(20)
 			expect(await page.evaluate(() => __tldraw_editor_events.at(-1))).toMatchObject({
 				target: 'canvas',
 				type: 'pointer',
@@ -46,6 +48,7 @@ test.describe('Canvas events', () => {
 			await page.mouse.down()
 			await page.mouse.move(101, 101)
 			await page.mouse.up()
+			await sleep(20)
 			expect(await page.evaluate(() => __tldraw_editor_events.at(-1))).toMatchObject({
 				target: 'canvas',
 				type: 'pointer',
@@ -70,6 +73,7 @@ test.describe('Canvas events', () => {
 		await page.mouse.move(200, 200) // to kill any double clicks
 		await page.mouse.move(100, 100)
 		await page.mouse.wheel(10, 10)
+		await sleep(20)
 		expect(await page.evaluate(() => __tldraw_editor_events.at(-1))).toMatchObject({
 			type: 'wheel',
 			name: 'wheel',
@@ -82,6 +86,7 @@ test.describe('Canvas events', () => {
 
 	test.fixme('complete', async () => {
 		await page.evaluate(async () => editor.complete())
+		await sleep(20)
 		expect(await page.evaluate(() => __tldraw_editor_events.at(-1))).toMatchObject({
 			type: 'misc',
 			name: 'complete',
@@ -90,6 +95,7 @@ test.describe('Canvas events', () => {
 
 	test.fixme('cancel', async () => {
 		await page.evaluate(async () => editor.cancel())
+		await sleep(20)
 		expect(await page.evaluate(() => __tldraw_editor_events.at(-1))).toMatchObject({
 			type: 'misc',
 			name: 'complete',
@@ -98,6 +104,7 @@ test.describe('Canvas events', () => {
 
 	test.fixme('interrupt', async () => {
 		await page.evaluate(async () => editor.interrupt())
+		await sleep(20)
 		expect(await page.evaluate(() => __tldraw_editor_events.at(-1))).toMatchObject({
 			type: 'misc',
 			name: 'interrupt',
@@ -118,6 +125,7 @@ test.describe('Shape events', () => {
 	test('pointer down', async () => {
 		await page.mouse.move(51, 51)
 		await page.mouse.down()
+		await sleep(20)
 		expect(await page.evaluate(() => __tldraw_editor_events.at(-1))).toMatchObject({
 			target: 'canvas',
 			type: 'pointer',
@@ -128,6 +136,7 @@ test.describe('Shape events', () => {
 	test('pointer move', async () => {
 		await page.mouse.move(51, 51)
 		await page.mouse.move(52, 52)
+		await sleep(20)
 		expect(await page.evaluate(() => __tldraw_editor_events.at(-1))).toMatchObject({
 			target: 'canvas',
 			type: 'pointer',
@@ -139,6 +148,7 @@ test.describe('Shape events', () => {
 		await page.mouse.move(51, 51)
 		await page.mouse.down()
 		await page.mouse.up()
+		await sleep(20)
 		expect(await page.evaluate(() => __tldraw_editor_events.at(-1))).toMatchObject({
 			target: 'canvas',
 			type: 'pointer',
