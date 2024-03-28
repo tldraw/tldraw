50cc074e1dcd34192e4aa7bfb1ad7f40a2505e8d
Mitja Bezenšek <mitja.bezensek@gmail.com>
2024-03-28 15:15:42 +0100

    Fix tests.

--- apps/examples/e2e/tests/test-shapes.spec.ts
+++ apps/examples/e2e/tests/test-shapes.spec.ts
@@ -120,6 +120,7 @@ test.describe('Shape Tools', () => {
 			// Reset for next time
 			await page.mouse.click(50, 50) // to ensure we're not focused
 			await page.keyboard.press('v') // go to the select tool
+			await page.waitForTimeout(20)
 			await page.keyboard.press('Control+a')
 			await page.keyboard.press('Backspace')
 		}
@@ -157,6 +158,7 @@ test.describe('Shape Tools', () => {
 			// Reset for next time
 			await page.mouse.click(50, 50) // to ensure we're not focused
 			await page.keyboard.press('v')
+			await page.waitForTimeout(20)
 			await page.keyboard.press('Control+a')
 			await page.keyboard.press('Backspace')
 		}
