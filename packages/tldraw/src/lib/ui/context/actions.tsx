8717ba621d4e35c574be360214de6b60ac3adc99
Steve Ruiz <steveruizok@gmail.com>
2024-04-01 18:30:33 +0100

    add white

--- packages/tldraw/src/lib/ui/context/actions.tsx
+++ packages/tldraw/src/lib/ui/context/actions.tsx
@@ -1,6 +1,7 @@
 import {
 	ANIMATION_MEDIUM_MS,
 	Box,
+	DefaultColorStyle,
 	Editor,
 	HALF_PI,
 	PageRecordType,
@@ -1279,6 +1280,23 @@ export function ActionsProvider({ overrides, children }: ActionsProviderProps) {
 						editor.moveShapesToPage(ids, newPageId)
 					})
 					trackEvent('new-page', { source })
+				},
+			},
+			{
+				id: 'select-white-color',
+				label: 'color-style.white',
+				kbd: '?w',
+				onSelect(source) {
+					const style = DefaultColorStyle
+					editor.batch(() => {
+						editor.mark('change-color')
+						if (editor.isIn('select')) {
+							editor.setStyleForSelectedShapes(style, 'white', { squashing: false })
+						}
+						editor.setStyleForNextShapes(style, 'white', { squashing: false })
+						editor.updateInstanceState({ isChangingStyle: true }, { ephemeral: true })
+					})
+					trackEvent('set-style', { source: 'style-panel', id: style.id, value: 'white' })
 				},
 			},
 		]
