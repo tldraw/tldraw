9699893481a20d68bcac0f089de46d9d92a6cf4e
Lu[ke] Wilson <l2wilson94@gmail.com>
2024-04-09 15:36:10 +0100

    slides

--- apps/examples/src/examples/slides/SlidesExample.tsx
+++ apps/examples/src/examples/slides/SlidesExample.tsx
@@ -1,5 +1,10 @@
+import { useCallback } from 'react'
 import {
 	BaseBoxShapeTool,
+	DefaultKeyboardShortcutsDialog,
+	DefaultKeyboardShortcutsDialogContent,
+	DefaultToolbar,
+	DefaultToolbarContent,
 	EASINGS,
 	Editor,
 	Geometry2d,
@@ -9,15 +14,19 @@ import {
 	ShapeUtil,
 	T,
 	TLBaseShape,
+	TLComponents,
 	TLOnResizeHandler,
 	Tldraw,
 	TldrawUiButton,
-	TldrawUiButtonIcon,
+	TldrawUiMenuItem,
 	Vec,
+	getPointerInfo,
 	resizeBox,
 	stopEventPropagation,
 	track,
 	useEditor,
+	useIsToolSelected,
+	useTools,
 	useValue,
 } from 'tldraw'
 import { getPerfectDashProps } from 'tldraw/src/lib/shapes/shared/getPerfectDashProps'
@@ -47,9 +56,15 @@ export class SlideShapeUtil extends ShapeUtil<SlideShape> {
 		return {
 			w: 720,
 			h: 480,
-			name: 'Slide',
+			name: `Slide`,
 		}
 	}
+
+	// override onBeforeCreate = (next: SlideShape) => {
+	// 	const slidesOnPage = this.editor.getCurrentPageShapes().filter((s) => s.type === 'slide')
+	// 	next.props.name = `Slide ${slidesOnPage.length + 1}`
+	// 	return next
+	// }
 
 	getGeometry(shape: SlideShape): Geometry2d {
 		return new Rectangle2d({
@@ -71,50 +86,88 @@ export class SlideShapeUtil extends ShapeUtil<SlideShape> {
 		const bounds = this.editor.getShapeGeometry(shape).bounds
 		// eslint-disable-next-line react-hooks/rules-of-hooks
 		const zoomLevel = useValue('zoom level', () => this.editor.getZoomLevel(), [this.editor])
+
+		// eslint-disable-next-line react-hooks/rules-of-hooks
+		const slides = useSlides()
+		const index = slides.findIndex((s) => s.id === shape.id)
+
+		// eslint-disable-next-line react-hooks/rules-of-hooks
+		const handleLabelPointerDown = useCallback(
+			(e: React.PointerEvent) => {
+				const event = getPointerInfo(e)
+
+				// If we're editing the frame label, we shouldn't hijack the pointer event
+				if (this.editor.getEditingShapeId() === shape.id) return
+
+				this.editor.dispatch({
+					type: 'pointer',
+					name: 'pointer_down',
+					target: 'shape',
+					shape: this.editor.getShape(shape.id)!,
+					...event,
+				})
+				e.preventDefault()
+			},
+			[shape.id]
+		)
+
 		if (!bounds) return null
 
 		return (
-			<SVGContainer>
-				<g
+			<>
+				<div
+					onPointerDown={handleLabelPointerDown}
 					style={{
-						stroke: 'var(--color-text)',
-						strokeWidth: 'calc(1px * var(--tl-scale))',
-						opacity: 0.25,
+						pointerEvents: 'all',
+						position: 'absolute',
+						background: 'var(--color-low)',
+						padding: 'calc(12px * var(--tl-scale))',
+						borderBottomRightRadius: 'calc(var(--radius-4) * var(--tl-scale))',
+						fontSize: 'calc(12px * var(--tl-scale))',
+						color: 'var(--color-text)',
+						zIndex: 1,
+						whiteSpace: 'nowrap',
 					}}
-					pointerEvents="none"
-					strokeLinecap="round"
-					strokeLinejoin="round"
 				>
-					{bounds.sides.map((side, i) => {
-						const { strokeDasharray, strokeDashoffset } = getPerfectDashProps(
-							side[0].dist(side[1]),
-							1 / zoomLevel,
-							{
-								style: 'dashed',
-								lengthRatio: 2,
-							}
-						)
+					{`${shape.props.name} ${index + 1}`}
+				</div>
+				<SVGContainer>
+					<g
+						style={{
+							stroke: 'var(--color-text)',
+							strokeWidth: 'calc(1px * var(--tl-scale))',
+							opacity: 0.25,
+						}}
+						pointerEvents="none"
+						strokeLinecap="round"
+						strokeLinejoin="round"
+					>
+						{bounds.sides.map((side, i) => {
+							const { strokeDasharray, strokeDashoffset } = getPerfectDashProps(
+								side[0].dist(side[1]),
+								1 / zoomLevel,
+								{
+									style: 'dashed',
+									lengthRatio: 6,
+								}
+							)
 
-						return (
-							<line
-								key={i}
-								x1={side[0].x}
-								y1={side[0].y}
-								x2={side[1].x}
-								y2={side[1].y}
-								strokeDasharray={strokeDasharray}
-								strokeDashoffset={strokeDashoffset}
-							/>
-						)
-					})}
-				</g>
-			</SVGContainer>
+							return (
+								<line
+									key={i}
+									x1={side[0].x}
+									y1={side[0].y}
+									x2={side[1].x}
+									y2={side[1].y}
+									strokeDasharray={strokeDasharray}
+									strokeDashoffset={strokeDashoffset}
+								/>
+							)
+						})}
+					</g>
+				</SVGContainer>
+			</>
 		)
-		// return (
-		// 	<HTMLContainer
-		// 		style={{ border: 'calc(var(--tl-scale) * 2px) solid var(--color-brush-stroke)' }}
-		// 	/>
-		// )
 	}
 
 	indicator(shape: SlideShape) {
@@ -132,7 +185,12 @@ function useSlides() {
 	const editor = useEditor()
 	return useValue<SlideShape[]>(
 		'slide shapes',
-		() => editor.getCurrentPageShapes().filter((s) => s.type === 'slide') as SlideShape[],
+		() => {
+			return editor
+				.getSortedChildIdsForParent(editor.getCurrentPageId())
+				.map((id) => editor.getShape(id))
+				.filter((s) => s?.type === 'slide') as SlideShape[]
+		},
 		[editor]
 	)
 }
@@ -159,7 +217,7 @@ function useCurrentSlide() {
 			}
 			return nearest
 		},
-		[editor]
+		[editor, slides]
 	)
 
 	return nearest.slide
@@ -174,16 +232,19 @@ const SlideList = track(() => {
 	return (
 		<div
 			style={{
-				position: 'absolute',
+				position: 'fixed',
 				display: 'flex',
 				flexDirection: 'column',
 				gap: 4,
 				top: 100,
+				maxHeight: 'calc(100vh - 200px)',
 				left: 8,
 				padding: 4,
 				backgroundColor: 'var(--color-low)',
 				pointerEvents: 'all',
 				borderRadius: 8,
+				overflow: 'scroll',
+				// bottom: 100,
 			}}
 			onPointerDown={(e) => stopEventPropagation(e)}
 		>
@@ -208,10 +269,7 @@ const SlideList = track(() => {
 								moveToSlide(editor, slide)
 							}}
 						>
-							{`Slide ${i + 1}`}
-						</TldrawUiButton>
-						<TldrawUiButton type="normal" onClick={() => editor.deleteShape(slide.id)}>
-							<TldrawUiButtonIcon icon="trash" />
+							{`${slide.props.name} ${i + 1}`}
 						</TldrawUiButton>
 					</div>
 				)
@@ -223,8 +281,30 @@ const SlideList = track(() => {
 const moveToSlide = (editor: Editor, slide: SlideShape) => {
 	const bounds = editor.getShapePageBounds(slide.id)
 	if (!bounds) return
-	editor.zoomToBounds(bounds, { duration: 500, easing: EASINGS.easeInOutCubic, inset: 40 })
-	// editor.setCamera(position, { duration: 200, easing: EASINGS.easeInOutQuad })
+	editor.zoomToBounds(bounds, { duration: 500, easing: EASINGS.easeInOutCubic, inset: 0 })
+}
+
+const components: TLComponents = {
+	InFrontOfTheCanvas: SlideList,
+	Toolbar: (props) => {
+		const tools = useTools()
+		const isSlideSelected = useIsToolSelected(tools['slide'])
+		return (
+			<DefaultToolbar {...props}>
+				<TldrawUiMenuItem {...tools['slide']} isSelected={isSlideSelected} />
+				<DefaultToolbarContent />
+			</DefaultToolbar>
+		)
+	},
+	KeyboardShortcutsDialog: (props) => {
+		const tools = useTools()
+		return (
+			<DefaultKeyboardShortcutsDialog {...props}>
+				<TldrawUiMenuItem {...tools['card']} />
+				<DefaultKeyboardShortcutsDialogContent />
+			</DefaultKeyboardShortcutsDialog>
+		)
+	},
 }
 
 const SlidesExample = track(() => {
@@ -234,15 +314,13 @@ const SlidesExample = track(() => {
 				persistenceKey="slideshow_example"
 				shapeUtils={[SlideShapeUtil]}
 				tools={[SlideTool]}
-				components={{
-					InFrontOfTheCanvas: SlideList,
-				}}
+				components={components}
 				overrides={{
 					tools(editor, tools) {
 						// Create a tool item in the ui's context.
 						tools.slide = {
 							id: 'slide',
-							icon: 'color',
+							icon: 'group',
 							label: 'Slide',
 							kbd: 's',
 							onSelect: () => {
