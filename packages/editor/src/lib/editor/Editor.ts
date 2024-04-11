ed733329abac74541861cfd20191bc32c42fad37
Mitja Bezenšek <mitja.bezensek@gmail.com>
2024-04-11 12:18:14 +0200

    Also clear it when we pan.

--- packages/editor/src/lib/editor/Editor.ts
+++ packages/editor/src/lib/editor/Editor.ts
@@ -8641,6 +8641,7 @@ export class Editor extends EventEmitter<TLEventMap> {
 						}
 
 						if (this.inputs.isPanning && this.inputs.isPointing) {
+							clearTimeout(this._longPressTimeout)
 							// Handle panning
 							const { currentScreenPoint, previousScreenPoint } = this.inputs
 							this.pan(Vec.Sub(currentScreenPoint, previousScreenPoint))
