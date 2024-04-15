45dffd1af682e0b3378ba08a99b63ac82908117b
Mitja Bezenšek <mitja.bezensek@gmail.com>
2024-04-15 18:28:18 +0200

    RBush again? (#3439)
    Adds RBush to handle spatial querying. We use it for:
    - Culling. Helps a lot with panning as we don't have to compute the
    culled shapes from scratch. Instead we just query rbush again. It makes
    culling quite granular: spatial index updates when shapes change
    (additions, removals, changes to bounds), visible shapes depends on
    that, but also updates when the viewport page bound change, culled
    shapes then depend on that but also change with selections changes. The
    api stayed the same, which is great since the fuzz tests can stay as
    they are.
    - Brushing 
    - Erasing
    - Scribble brushing
    - Getting shapes at point (for example, when updating the hover id)
    
    This improves performance of all of those operations. I might have
    missed some places where this might also be useful.
    
    ### Erasing before (Test on my old ipad)
    
    
    https://github.com/tldraw/tldraw/assets/2523721/edb9c004-a44a-4779-b2d0-98617b057314
    
    ### Erasing after
    
    
    https://github.com/tldraw/tldraw/assets/2523721/8f8367fd-fa8e-4963-ba13-720c5f0c2da5
    
    ### Creating an arrow before
    
    
    https://github.com/tldraw/tldraw/assets/2523721/4068f8b7-f7b8-4826-83f2-083b1f3783bc
    
    ### After (much better, but still bad)
    
    
    https://github.com/tldraw/tldraw/assets/2523721/11af6be6-01d8-4740-bf15-896e2dd31dd6
    
    
    
    ### Change Type
    
    <!-- ❗ Please select a 'Scope' label ❗️ -->
    
    - [ ] `sdk` — Changes the tldraw SDK
    - [ ] `dotcom` — Changes the tldraw.com web app
    - [ ] `docs` — Changes to the documentation, examples, or templates.
    - [ ] `vs code` — Changes to the vscode plugin
    - [x] `internal` — Does not affect user-facing stuff
    
    <!-- ❗ Please select a 'Type' label ❗️ -->
    
    - [ ] `bugfix` — Bug fix
    - [ ] `feature` — New feature
    - [x] `improvement` — Improving existing features
    - [ ] `chore` — Updating dependencies, other boring stuff
    - [ ] `galaxy brain` — Architectural changes
    - [ ] `tests` — Changes to any test code
    - [ ] `tools` — Changes to infrastructure, CI, internal scripts,
    debugging tools, etc.
    - [ ] `dunno` — I don't know
    
    ---------
    
    Co-authored-by: Steve Ruiz <steveruizok@gmail.com>

---
+++ packages/editor/src/lib/editor/derivations/spatialIndex.ts
@@ -0,0 +1,172 @@
+import { RESET_VALUE, computed, isUninitialized } from '@tldraw/state'
+import { TLPageId, TLShapeId, isShape, isShapeId } from '@tldraw/tlschema'
+import RBush from 'rbush'
+import { Box } from '../../primitives/Box'
+import { Editor } from '../Editor'
+
+type Element = {
+	minX: number
+	minY: number
+	maxX: number
+	maxY: number
+	id: TLShapeId
+}
+
+class TldrawRBush extends RBush<Element> {}
+
+export class SpatialIndex {
+	private readonly spatialIndex: ReturnType<typeof this.createSpatialIndex>
+	private lastPageId: TLPageId | null = null
+	private shapesInTree: Map<TLShapeId, Element>
+	private rBush: TldrawRBush
+
+	constructor(private editor: Editor) {
+		this.spatialIndex = this.createSpatialIndex()
+		this.shapesInTree = new Map<TLShapeId, Element>()
+		this.rBush = new TldrawRBush()
+	}
+
+	private addElement(id: TLShapeId, a: Element[], existingBounds?: Box) {
+		const e = this.getElement(id, existingBounds)
+		if (!e) return
+		a.push(e)
+		this.shapesInTree.set(id, e)
+	}
+
+	private getElement(id: TLShapeId, existingBounds?: Box): Element | null {
+		const bounds = existingBounds ?? this.editor.getShapeMaskedPageBounds(id)
+		if (!bounds) return null
+		return {
+			minX: bounds.minX,
+			minY: bounds.minY,
+			maxX: bounds.maxX,
+			maxY: bounds.maxY,
+			id,
+		}
+	}
+
+	private fromScratch(lastComputedEpoch: number) {
+		this.lastPageId = this.editor.getCurrentPageId()
+		this.shapesInTree = new Map<TLShapeId, Element>()
+		const elementsToAdd: Element[] = []
+
+		this.editor.getCurrentPageShapeIds().forEach((id) => {
+			this.addElement(id, elementsToAdd)
+		})
+
+		this.rBush = new TldrawRBush().load(elementsToAdd)
+
+		return lastComputedEpoch
+	}
+
+	private createSpatialIndex() {
+		const shapeHistory = this.editor.store.query.filterHistory('shape')
+
+		return computed<number>('spatialIndex', (prevValue, lastComputedEpoch) => {
+			if (isUninitialized(prevValue)) {
+				return this.fromScratch(lastComputedEpoch)
+			}
+
+			const diff = shapeHistory.getDiffSince(lastComputedEpoch)
+			if (diff === RESET_VALUE) {
+				return this.fromScratch(lastComputedEpoch)
+			}
+
+			const currentPageId = this.editor.getCurrentPageId()
+			if (!this.lastPageId || this.lastPageId !== currentPageId) {
+				return this.fromScratch(lastComputedEpoch)
+			}
+
+			let isDirty = false
+			for (const changes of diff) {
+				const elementsToAdd: Element[] = []
+				for (const record of Object.values(changes.added)) {
+					if (isShape(record)) {
+						this.addElement(record.id, elementsToAdd)
+					}
+				}
+
+				for (const [_from, to] of Object.values(changes.updated)) {
+					if (isShape(to)) {
+						const currentElement = this.shapesInTree.get(to.id)
+						const newBounds = this.editor.getShapeMaskedPageBounds(to.id)
+						if (currentElement) {
+							if (
+								newBounds?.minX === currentElement.minX &&
+								newBounds.minY === currentElement.minY &&
+								newBounds.maxX === currentElement.maxX &&
+								newBounds.maxY === currentElement.maxY
+							) {
+								continue
+							}
+							this.shapesInTree.delete(to.id)
+							this.rBush.remove(currentElement)
+							isDirty = true
+						}
+						this.addElement(to.id, elementsToAdd, newBounds)
+					}
+				}
+				if (elementsToAdd.length) {
+					this.rBush.load(elementsToAdd)
+					isDirty = true
+				}
+				for (const id of Object.keys(changes.removed)) {
+					if (isShapeId(id)) {
+						const currentElement = this.shapesInTree.get(id)
+						if (currentElement) {
+							this.shapesInTree.delete(id)
+							this.rBush.remove(currentElement)
+							isDirty = true
+						}
+					}
+				}
+			}
+
+			return isDirty ? lastComputedEpoch : prevValue
+		})
+	}
+
+	@computed
+	private _getVisibleShapes() {
+		return computed<Set<TLShapeId>>('visible shapes', (prevValue) => {
+			// Make sure the spatial index is up to date
+			const _index = this.spatialIndex.get()
+			const newValue = this.rBush.search(this.editor.getViewportPageBounds()).map((s) => s.id)
+			if (isUninitialized(prevValue)) {
+				return new Set(newValue)
+			}
+			const isSame = prevValue.size === newValue.length && newValue.every((id) => prevValue.has(id))
+			return isSame ? prevValue : new Set(newValue)
+		})
+	}
+
+	@computed
+	getVisibleShapes() {
+		return this._getVisibleShapes().get()
+	}
+
+	@computed
+	_getNotVisibleShapes() {
+		return computed<Set<TLShapeId>>('not visible shapes', (prevValue) => {
+			const visibleShapes = this._getVisibleShapes().get()
+			const pageShapes = this.editor.getCurrentPageShapeIds()
+			const nonVisibleShapes = [...pageShapes].filter((id) => !visibleShapes.has(id))
+			if (isUninitialized(prevValue)) return new Set(nonVisibleShapes)
+			const isSame =
+				prevValue.size === nonVisibleShapes.length &&
+				nonVisibleShapes.every((id) => prevValue.has(id))
+			return isSame ? prevValue : new Set(nonVisibleShapes)
+		})
+	}
+
+	@computed
+	getNotVisibleShapes() {
+		return this._getNotVisibleShapes().get()
+	}
+
+	getShapeIdsInsideBounds(bounds: Box) {
+		// Make sure the spatial index is up to date
+		const _index = this.spatialIndex.get()
+		return this.rBush.search(bounds).map((s) => s.id)
+	}
+}
