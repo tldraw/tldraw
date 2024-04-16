62f64d61d2b39f9da398a80b2167afb01480a703
Lu[ke] Wilson <l2wilson94@gmail.com>
2024-04-16 13:34:40 +0100

    apply fix to all shapes

--- packages/tldraw/src/lib/shapes/note/NoteShapeUtil.tsx
+++ packages/tldraw/src/lib/shapes/note/NoteShapeUtil.tsx
@@ -138,12 +138,6 @@ export class NoteShapeUtil extends ShapeUtil<TLNoteShape> {
 		]
 	}
 
-	override onTranslateStart = () => {
-		// Re-focus, just in case the text label of the note has stolen focus
-		const container = this.editor.getContainer()
-		container.focus()
-	}
-
 	component(shape: TLNoteShape) {
 		const {
 			id,
