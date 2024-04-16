a9b91084985add818b7d765da51388e2e5346233
Mitja Bezenšek <mitja.bezensek@gmail.com>
2024-04-16 09:37:17 +0200

    Rename.

--- packages/editor/src/lib/utils/debug-flags.ts
+++ packages/editor/src/lib/utils/debug-flags.ts
@@ -54,7 +54,7 @@ export const debugFlags = {
 	debugGeometry: createDebugValue('debugGeometry', { defaults: { all: false } }),
 	hideShapes: createDebugValue('hideShapes', { defaults: { all: false } }),
 	editOnType: createDebugValue('editOnType', { defaults: { all: false } }),
-	measurePerformance: createDebugValue('measureActionDuration', { defaults: { all: false } }),
+	measurePerformance: createDebugValue('measurePerformance', { defaults: { all: false } }),
 } as const
 
 declare global {
