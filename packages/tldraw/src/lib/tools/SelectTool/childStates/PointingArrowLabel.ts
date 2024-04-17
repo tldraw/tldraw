tldraw/tldraw

commit 3ceebc82f8adeba922f2feafbd38e5eed2822445

Faster selection / erasing (#3454)
This PR makes a small improvement to the way we measure distances.
(Often we measure distances multiple times per frame per shape on the
screen). In many cases, we compare a minimum distance. This makes those
checks faster by avoiding a square root.

### Change Type

- [x] `sdk` — Changes the tldraw SDK
- [x] `improvement` — Improving existing features

### Release Notes

- Improve performance of minimum distance checks.

packages/tldraw/src/lib/tools/SelectTool/childStates/PointingArrowLabel.ts
@@ -88,8 +88,8 @@ export class PointingArrowLabel extends StateNode {
 		let nextLabelPosition
 		if (info.isStraight) {
 			// straight arrows
-			const lineLength = Vec.Dist(info.start.point, info.end.point)
-			const segmentLength = Vec.Dist(info.end.point, nearestPoint)
+			const lineLength = Vec.Dist2(info.start.point, info.end.point)
+			const segmentLength = Vec.Dist2(info.end.point, nearestPoint)
 			nextLabelPosition = 1 - segmentLength / lineLength
 		} else {
 			const { _center, measure, angleEnd, angleStart } = groupGeometry.children[0] as Arc2d

apps/examples/src/examples/exploded/ExplodedExample.tsx
@@ -3,7 +3,6 @@ import {
 	DefaultContextMenuContent,
 	TldrawEditor,
 	TldrawHandles,
-	TldrawHoveredShapeIndicator,
 	TldrawScribble,
 	TldrawSelectionBackground,
 	TldrawSelectionForeground,
@@ -23,7 +22,6 @@ const defaultComponents = {
 	SelectionForeground: TldrawSelectionForeground,
 	SelectionBackground: TldrawSelectionBackground,
 	Handles: TldrawHandles,
-	HoveredShapeIndicator: TldrawHoveredShapeIndicator,
 }
 
 //[2]

packages/editor/editor.css
@@ -465,7 +465,7 @@ input,
 	transform-origin: top left;
 	fill: none;
 	stroke-width: calc(1.5px * var(--tl-scale));
-	contain: size;
+	contain: size layout;
 }
 
 /* ------------------ SelectionBox ------------------ */

packages/editor/src/index.ts
@@ -63,10 +63,6 @@ export {
 	type TLHandlesProps,
 } from './lib/components/default-components/DefaultHandles'
 export {
-	DefaultHoveredShapeIndicator,
-	type TLHoveredShapeIndicatorProps,
-} from './lib/components/default-components/DefaultHoveredShapeIndicator'
-export {
 	DefaultScribble,
 	type TLScribbleProps,
 } from './lib/components/default-components/DefaultScribble'

packages/editor/src/lib/components/default-components/DefaultCanvas.tsx
@@ -151,8 +151,7 @@ export function DefaultCanvas({ className }: TLCanvasComponentProps) {
 					<BrushWrapper />
 					<ScribbleWrapper />
 					<ZoomBrushWrapper />
-					<SelectedIdIndicators />
-					<HoveredShapeIndicator />
+					<ShapeIndicators />
 					<HintedShapeIndicator />
 					<SnapIndicatorWrapper />
 					<SelectionForegroundWrapper />
@@ -431,16 +430,17 @@ function ShapesToDisplay() {
 	)
 }
 
-function SelectedIdIndicators() {
+function ShapeIndicators() {
 	const editor = useEditor()
-	const selectedShapeIds = useValue('selectedShapeIds', () => editor.getSelectedShapeIds(), [
-		editor,
-	])
-	const shouldDisplay = useValue(
+	const renderingShapes = useValue('rendering shapes', () => editor.getRenderingShapes(), [editor])
+	const rPreviousSelectedShapeIds = useRef<Set<TLShapeId>>(new Set())
+	const idsToDisplay = useValue(
 		'should display selected ids',
 		() => {
-			// todo: move to tldraw selected ids wrapper
-			return (
+			// todo: move to tldraw selected ids wrappe
+			const prev = rPreviousSelectedShapeIds.current
+			const next = new Set<TLShapeId>()
+			if (
 				editor.isInAny(
 					'select.idle',
 					'select.brushing',
@@ -449,50 +449,49 @@ function SelectedIdIndicators() {
 					'select.pointing_shape',
 					'select.pointing_selection',
 					'select.pointing_handle'
-				) && !editor.getInstanceState().isChangingStyle
-			)
+				) &&
+				!editor.getInstanceState().isChangingStyle
+			) {
+				const selected = editor.getSelectedShapeIds()
+				for (const id of selected) {
+					next.add(id)
+				}
+				if (editor.isInAny('select.idle', 'select.editing_shape')) {
+					const instanceState = editor.getInstanceState()
+					if (instanceState.isHoveringCanvas && !instanceState.isCoarsePointer) {
+						const hovered = editor.getHoveredShapeId()
+						if (hovered) next.add(hovered)
+					}
+				}
+			}
+
+			if (prev.size !== next.size) {
+				rPreviousSelectedShapeIds.current = next
+				return next
+			}
+
+			for (const id of next) {
+				if (!prev.has(id)) {
+					rPreviousSelectedShapeIds.current = next
+					return next
+				}
+			}
+
+			return prev
 		},
 		[editor]
 	)
 
 	const { ShapeIndicator } = useEditorComponents()
-
 	if (!ShapeIndicator) return null
-	if (!shouldDisplay) return null
 
 	return (
 		<>
-			{selectedShapeIds.map((id) => (
-				<ShapeIndicator
-					key={id + '_indicator'}
-					className="tl-user-indicator__selected"
-					shapeId={id}
-				/>
+			{renderingShapes.map(({ id }) => (
+				<ShapeIndicator key={id + '_indicator'} shapeId={id} hidden={!idsToDisplay.has(id)} />
 			))}
 		</>
 	)
-}
-
-const HoveredShapeIndicator = function HoveredShapeIndicator() {
-	const editor = useEditor()
-	const { HoveredShapeIndicator } = useEditorComponents()
-	const isCoarsePointer = useValue(
-		'coarse pointer',
-		() => editor.getInstanceState().isCoarsePointer,
-		[editor]
-	)
-	const isHoveringCanvas = useValue(
-		'hovering canvas',
-		() => editor.getInstanceState().isHoveringCanvas,
-		[editor]
-	)
-	const hoveredShapeId = useValue('hovered id', () => editor.getCurrentPageState().hoveredShapeId, [
-		editor,
-	])
-
-	if (isCoarsePointer || !isHoveringCanvas || !hoveredShapeId || !HoveredShapeIndicator) return null
-
-	return <HoveredShapeIndicator shapeId={hoveredShapeId} />
 }
 
 function HintedShapeIndicator() {

packages/editor/src/lib/components/default-components/DefaultHoveredShapeIndicator.tsx
@@ -1,14 +1,0 @@
-import { TLShapeId } from '@tldraw/tlschema'
-import { useEditorComponents } from '../../hooks/useEditorComponents'
-
-/** @public */
-export type TLHoveredShapeIndicatorProps = {
-	shapeId: TLShapeId
-}
-
-/** @public */
-export function DefaultHoveredShapeIndicator({ shapeId }: TLHoveredShapeIndicatorProps) {
-	const { ShapeIndicator } = useEditorComponents()
-	if (!ShapeIndicator) return null
-	return <ShapeIndicator className="tl-user-indicator__hovered" shapeId={shapeId} />
-}

packages/editor/src/lib/components/default-components/DefaultShapeIndicator.tsx
@@ -1,7 +1,7 @@
-import { useStateTracking, useValue } from '@tldraw/state'
+import { useQuickReactor, useStateTracking, useValue } from '@tldraw/state'
 import { TLShape, TLShapeId } from '@tldraw/tlschema'
 import classNames from 'classnames'
-import { memo } from 'react'
+import { memo, useLayoutEffect, useRef } from 'react'
 import type { Editor } from '../../editor/Editor'
 import { ShapeUtil } from '../../editor/shapes/ShapeUtil'
 import { useEditor } from '../../hooks/useEditor'
@@ -38,6 +38,7 @@ export type TLShapeIndicatorProps = {
 	color?: string | undefined
 	opacity?: number
 	className?: string
+	hidden?: boolean
 }
 
 /** @public */
@@ -45,28 +46,34 @@ export const DefaultShapeIndicator = memo(function DefaultShapeIndicator({
 	shapeId,
 	className,
 	color,
+	hidden,
 	opacity,
 }: TLShapeIndicatorProps) {
 	const editor = useEditor()
 
-	const transform = useValue(
+	const rIndicator = useRef<SVGSVGElement>(null)
+
+	useQuickReactor(
 		'indicator transform',
 		() => {
+			const elm = rIndicator.current
+			if (!elm) return
 			const pageTransform = editor.getShapePageTransform(shapeId)
-			if (!pageTransform) return ''
-			return pageTransform.toCssString()
+			if (!pageTransform) return
+			elm.style.setProperty('transform', pageTransform.toCssString())
 		},
 		[editor, shapeId]
 	)
 
+	useLayoutEffect(() => {
+		const elm = rIndicator.current
+		if (!elm) return
+		elm.style.setProperty('display', hidden ? 'none' : 'block')
+	}, [hidden])
+
 	return (
-		<svg className={classNames('tl-overlays__item', className)}>
-			<g
-				className="tl-shape-indicator"
-				transform={transform}
-				stroke={color ?? 'var(--color-selected)'}
-				opacity={opacity}
-			>
+		<svg ref={rIndicator} className={classNames('tl-overlays__item', className)}>
+			<g className="tl-shape-indicator" stroke={color ?? 'var(--color-selected)'} opacity={opacity}>
 				<InnerIndicator editor={editor} id={shapeId} />
 			</g>
 		</svg>

packages/editor/src/lib/editor/Editor.ts
@@ -4490,7 +4490,6 @@ export class Editor extends EventEmitter<TLEventMap> {
 	 *
 	 * @public
 	 */
-
 	isPointInShape(
 		shape: TLShape | TLShapeId,
 		point: VecLike,
@@ -8377,7 +8376,7 @@ export class Editor extends EventEmitter<TLEventMap> {
 
 	private _pendingEventsForNextTick: TLEventInfo[] = []
 
-	private _flushEventsForTick = (elapsed: number) => {
+	private _flushEventsForTick(elapsed: number) {
 		this.batch(() => {
 			if (this._pendingEventsForNextTick.length > 0) {
 				const events = [...this._pendingEventsForNextTick]

commit f44ea90da61f5f3982022b6570124550ac5fb2d0

arrows: still use Dist instead of Dist2 (#3511)
A little regression from https://github.com/tldraw/tldraw/pull/3454. We
still need the exact distance here.

### Change Type

<!-- ❗ Please select a 'Scope' label ❗️ -->

- [x] `sdk` — Changes the tldraw SDK
- [ ] `dotcom` — Changes the tldraw.com web app
- [ ] `docs` — Changes to the documentation, examples, or templates.
- [ ] `vs code` — Changes to the vscode plugin
- [ ] `internal` — Does not affect user-facing stuff

<!-- ❗ Please select a 'Type' label ❗️ -->

- [x] `bugfix` — Bug fix
- [ ] `feature` — New feature
- [ ] `improvement` — Improving existing features
- [ ] `chore` — Updating dependencies, other boring stuff
- [ ] `galaxy brain` — Architectural changes
- [ ] `tests` — Changes to any test code
- [ ] `tools` — Changes to infrastructure, CI, internal scripts,
debugging tools, etc.
- [ ] `dunno` — I don't know

### Release Notes

- Fix arrow label positioning

packages/tldraw/src/lib/tools/SelectTool/childStates/PointingArrowLabel.ts
@@ -92,8 +92,8 @@ export class PointingArrowLabel extends StateNode {
 		let nextLabelPosition
 		if (info.isStraight) {
 			// straight arrows
-			const lineLength = Vec.Dist2(info.start.point, info.end.point)
-			const segmentLength = Vec.Dist2(info.end.point, nearestPoint)
+			const lineLength = Vec.Dist(info.start.point, info.end.point)
+			const segmentLength = Vec.Dist(info.end.point, nearestPoint)
 			nextLabelPosition = 1 - segmentLength / lineLength
 		} else {
 			const { _center, measure, angleEnd, angleStart } = groupGeometry.children[0] as Arc2d