272390fdbd2fb5d08393470e26de45116c26a69f
Mime Čuvalo <mimecuvalo@gmail.com>
2024-04-18 15:55:12 +0100

    404->400

--- apps/dotcom-worker/src/lib/routes/getReadonlySlug.ts
+++ apps/dotcom-worker/src/lib/routes/getReadonlySlug.ts
@@ -1,12 +1,15 @@
 import { IRequest } from 'itty-router'
 import { nanoid } from 'nanoid'
 import { Environment } from '../types'
-import { fourOhFour } from '../utils/fourOhFour'
 
 // Return a URL to a readonly version of the room
 export async function getReadonlySlug(request: IRequest, env: Environment): Promise<Response> {
 	const roomId = request.params.roomId
-	if (!roomId) return fourOhFour()
+	if (!roomId) {
+		return new Response('Bad request', {
+			status: 400,
+		})
+	}
 
 	let slug = await env.SLUG_TO_READONLY_SLUG.get(roomId)
 
