77fa3b380721e465b424c55725cca006522641d3
Steve Ruiz <steveruizok@gmail.com>
2024-04-21 12:41:09 +0100

    Update useClipboardEvents.ts

--- packages/tldraw/src/lib/ui/hooks/useClipboardEvents.ts
+++ packages/tldraw/src/lib/ui/hooks/useClipboardEvents.ts
@@ -661,8 +661,6 @@ export function useNativeClipboardEvents() {
 				return
 			}
 
-			preventDefault(e)
-
 			// If we're editing a shape, or we are focusing an editable input, then
 			// we would want the user's paste interaction to go to that element or
 			// input instead; e.g. when pasting text into a text shape's content
@@ -680,6 +678,7 @@ export function useNativeClipboardEvents() {
 				})
 			}
 
+			preventDefault(e)
 			trackEvent('paste', { source: 'kbd' })
 		}
 
