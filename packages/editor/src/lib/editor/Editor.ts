4709274b2b379cb1498d1b912b951bfa07a3da7b
Mitja Bezenšek <mitja.bezensek@gmail.com>
2024-04-22 15:01:59 +0200

    Don't allow users to change the readonly via the editor api.

--- packages/editor/src/lib/editor/Editor.ts
+++ packages/editor/src/lib/editor/Editor.ts
@@ -1182,7 +1182,12 @@ export class Editor extends EventEmitter<TLEventMap> {
 		partial: Partial<Omit<TLInstance, 'currentPageId'>>,
 		historyOptions?: TLCommandHistoryOptions
 	): this {
-		this._updateInstanceState(partial, { ephemeral: true, squashing: true, ...historyOptions })
+		if (partial.isReadonly === false && this.getInstanceState().isReadonly === true) {
+			const { isReadonly: _isReadonly, ...rest } = partial
+			this._updateInstanceState(rest, { ephemeral: true, squashing: true, ...historyOptions })
+		} else {
+			this._updateInstanceState(partial, { ephemeral: true, squashing: true, ...historyOptions })
+		}
 
 		if (partial.isChangingStyle !== undefined) {
 			clearTimeout(this._isChangingStyleTimeout)
