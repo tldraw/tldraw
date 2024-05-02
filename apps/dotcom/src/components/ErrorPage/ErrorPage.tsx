e25e9c2b3380066332f23ea9fea199e987a125ca
David Sheldrick <d.j.sheldrick@gmail.com>
2024-05-02 15:37:27 +0100

    [hotfix] various fixes (#3682)
    - graceful sync rejection handling (no reconnection loop)
    - fix viewport following
    - graceful-ish webgl error handling

--- apps/dotcom/src/components/ErrorPage/ErrorPage.tsx
+++ apps/dotcom/src/components/ErrorPage/ErrorPage.tsx
@@ -1,28 +1,39 @@
+import { ReactNode } from 'react'
 import { Link } from 'react-router-dom'
 import { isInIframe } from '../../utils/iFrame'
 
-export function ErrorPage({
-	icon,
-	messages,
-}: {
-	icon?: boolean
-	messages: { header: string; para1: string; para2?: string }
-}) {
+const GoBackLink = () => {
 	const inIframe = isInIframe()
+	return (
+		<Link to={'/'} target={inIframe ? '_blank' : '_self'}>
+			{inIframe ? 'Open tldraw.' : 'Back to tldraw.'}
+		</Link>
+	)
+}
+
+const sadFaceIcon = (
+	<img width={36} height={36} src="/404-Sad-tldraw.svg" loading="lazy" role="presentation" />
+)
+
+export function ErrorPage({
+	messages,
+	icon = sadFaceIcon,
+	cta = <GoBackLink />,
+}: {
+	icon?: ReactNode
+	messages: { header: string; para1: string; para2?: string }
+	cta?: ReactNode
+}) {
 	return (
 		<div className="error-page">
 			<div className="error-page__container">
-				{icon && (
-					<img width={36} height={36} alt={'Not found'} src="/404-Sad-tldraw.svg" loading="lazy" />
-				)}
+				{icon}
 				<div className="error-page__content">
 					<h1>{messages.header}</h1>
 					<p>{messages.para1}</p>
 					{messages.para2 && <p>{messages.para2}</p>}
 				</div>
-				<Link to={'/'} target={inIframe ? '_blank' : '_self'}>
-					{inIframe ? 'Open tldraw.' : 'Back to tldraw.'}
-				</Link>
+				{cta}
 			</div>
 		</div>
 	)
