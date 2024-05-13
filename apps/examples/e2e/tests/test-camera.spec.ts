da5fe46b3bd60868ec6f491dd2c3e39d4fd08ed1
Taha <98838967+Taha-Hassan-Git@users.noreply.github.com>
2024-05-13 12:37:01 +0100

    tidy up tests

--- apps/examples/e2e/tests/test-camera.spec.ts
+++ apps/examples/e2e/tests/test-camera.spec.ts
@@ -1,4 +1,4 @@
-import { CDPSession, expect } from '@playwright/test'
+import { CDPSession, Page, expect } from '@playwright/test'
 import { Editor } from 'tldraw'
 import { setup } from '../shared-e2e'
 import test from './fixtures/fixtures'
@@ -16,33 +16,67 @@ const dispatchTouch = async (
 }
 
 const multiTouchGesture = async ({
-	type,
 	start,
 	end,
 	steps,
 }: {
-	type: 'pinch' | 'pan'
-	start: { x: number; y: number }
-	end: { x: number; y: number }
+	start: { x: number; y: number }[]
+	end: { x: number; y: number }[]
 	steps: number
 }) => {
-	const finger1 = { x: start.x, y: start.y }
-	const finger2 = { x: start.x + 100, y: start.y + 100 }
+	const finger1 = { x: start[0].x, y: start[0].y }
+	const finger2 = { x: start[1].x, y: start[1].x }
+	const finalTouch = [finger1, finger2]
+
 	await dispatchTouch(client, 'touchStart', [finger1, finger2])
-	let finalTouch: { x: number; y: number }[] = [finger1, finger2]
-	for (let i = 1; i < 100; i++) {
+	await sleep(10)
+
+	// how far should the fingers move each step to reach the end point?
+	const finger1StepDistance = {
+		x: (end[0].x - start[0].x) / steps,
+		y: (end[0].y - start[0].y) / steps,
+	}
+	const finger2StepDistance = {
+		x: (end[1].x - start[1].x) / steps,
+		y: (end[1].y - start[1].y) / steps,
+	}
+
+	for (let i = 1; i < steps; i++) {
 		await dispatchTouch(client, 'touchMove', [
-			{ x: 100 + i * 10, y: 100 + i * 10 },
-			{ x: 200 + i * 10, y: 200 + i * 10 },
+			{ x: finger1.x + i * finger1StepDistance.x, y: finger1.y + i * finger1StepDistance.y },
+			{ x: finger2.x + i * finger2StepDistance.x, y: finger2.y + i * finger2StepDistance.y },
 		])
-		finalTouch = [
-			{ x: 100 + i * 10, y: 100 + i * 10 },
-			{ x: 200 + i * 10, y: 200 + i * 10 },
-		]
+
 		await sleep(10)
 	}
+
 	await dispatchTouch(client, 'touchEnd', finalTouch)
-	return { type, start, end, steps }
+	await sleep(10)
+}
+
+const scrollZoom = async ({
+	page,
+	currentZoomLevel,
+	zoomDirection,
+	steps,
+}: {
+	page: Page
+	currentZoomLevel: number
+	zoomDirection: 'in' | 'out'
+	steps: number
+}) => {
+	page.keyboard.down('Control')
+	// zooming with the scroll wheel is capped within the editor to make sure it's consistent between browsers,
+	//scrolling further than 15px per event doesn't have any effect
+	for (let i = 0; i < steps; i++) {
+		await page.mouse.wheel(0, zoomDirection === 'out' ? -15 : 15)
+	}
+	page.keyboard.up('Control')
+	// scrolling 15px on the mouse wheel changes the zoom level by 10%
+	const expectedZoomLevel =
+		zoomDirection === 'out' ? currentZoomLevel * 1.1 ** steps : currentZoomLevel / 1.1 ** steps
+
+	return expectedZoomLevel
 }
 
 export function sleep(ms: number) {
@@ -55,65 +89,55 @@ test.describe('camera', () => {
 	test('panning', async ({ isMobile, page }) => {
 		test.skip(!isMobile)
 		client = await page.context().newCDPSession(page)
-
 		expect(await page.evaluate(() => editor.inputs.currentPagePoint)).toEqual({
 			x: 50,
 			y: 50,
 			z: 0,
 		})
 
-		await dispatchTouch(client, 'touchStart', [
-			{ x: 100, y: 100 },
-			{ x: 200, y: 200 },
-		])
-		let finalTouch: { x: number; y: number }[] = [
-			{ x: 100, y: 100 },
-			{ x: 200, y: 200 },
-		]
-		for (let i = 1; i < 100; i++) {
-			await dispatchTouch(client, 'touchMove', [
-				{ x: 100 + i * 10, y: 100 + i * 10 },
-				{ x: 200 + i * 10, y: 200 + i * 10 },
-			])
-			finalTouch = [
-				{ x: 100 + i * 10, y: 100 + i * 10 },
-				{ x: 200 + i * 10, y: 200 + i * 10 },
-			]
-			await sleep(10)
-		}
-		await dispatchTouch(client, 'touchEnd', finalTouch)
+		// the fewer steps you use, the faster the pan. So the ratio of pixels moved is not one-to-one.
+		await multiTouchGesture({
+			start: [
+				{ x: 50, y: 50 },
+				{ x: 100, y: 100 },
+			],
+			end: [
+				{ x: 100, y: 100 },
+				{ x: 150, y: 150 },
+			],
+			steps: 50,
+		})
 
-		await sleep(10)
-		// not sure how the touch coordinates translate to the editor coordinates
+		// With current panning speed, the above gesture moves the camera by 36px on each axis
 		expect(
 			await page.evaluate(() => [
 				editor.inputs.currentPagePoint.x,
 				editor.inputs.currentPagePoint.y,
 			])
-		).toStrictEqual([160, 160])
+		).toStrictEqual([86, 86])
 	})
 
 	test('pinching on trackpad', async ({ page, isMobile }) => {
 		// pinching on trackpad is the same event as ctrl+scrollwheel
 		test.skip(isMobile)
-		const { mouse, keyboard } = page
 		expect(await page.evaluate(() => editor.getZoomLevel())).toBe(1)
-		// zooming in
-		await keyboard.down('Control')
-		// scrolling 15px on the mouse wheel is a bit less that 0.1 zoom level at default zoom speed
-		await mouse.wheel(0, 15) // 0.9
-		await mouse.wheel(0, 15) // 0.81
-		await mouse.wheel(0, 15) // 0.72
-		await keyboard.up('Control')
-		expect(await page.evaluate(() => editor.getZoomLevel())).toBeCloseTo(0.72, 1)
-		// zooming out
-		// scrolling back doesn't progress in the same steps
-		await keyboard.down('Control')
-		await mouse.wheel(0, -15) // 0.8019000000000001
-		await mouse.wheel(0, -15) // 0.88209
-		await mouse.wheel(0, -15) // 0.970299
-		await keyboard.up('Control')
-		expect(await page.evaluate(() => editor.getZoomLevel())).toBeCloseTo(0.97)
+
+		let expectedZoomLevel = await scrollZoom({
+			page,
+			currentZoomLevel: 1,
+			zoomDirection: 'in',
+			steps: 3,
+		})
+		// toBeCloseTo is used to avoid floating point errors
+		expect(await page.evaluate(() => editor.getZoomLevel())).toBeCloseTo(expectedZoomLevel, 1)
+
+		expectedZoomLevel = await scrollZoom({
+			page,
+			currentZoomLevel: expectedZoomLevel,
+			zoomDirection: 'out',
+			steps: 3,
+		})
+		expect(await page.evaluate(() => editor.getZoomLevel())).toBeCloseTo(expectedZoomLevel, 1)
 	})
 
 	test('pinching on touchscreen', async ({ page, isMobile }) => {
@@ -124,77 +148,39 @@ test.describe('camera', () => {
 		expect(await page.evaluate(() => editor.getZoomLevel())).toBe(1)
 
 		// zoom out
-
-		await dispatchTouch(client, 'touchStart', [
-			{ x: 100, y: 100 },
-			{ x: 200, y: 200 },
-		])
-		let finalTouch: { x: number; y: number }[] = [
-			{ x: 100, y: 100 },
-			{ x: 200, y: 200 },
-		]
-		for (let i = 1; i < 50; i++) {
-			await dispatchTouch(client, 'touchMove', [
-				{ x: 100 + i, y: 100 + i },
-				{ x: 200 - i, y: 200 - i },
-			])
-			finalTouch = [
-				{ x: 100 + i, y: 100 + i },
-				{ x: 200 - i, y: 200 - i },
-			]
-			await sleep(10)
-		}
-		await dispatchTouch(client, 'touchEnd', finalTouch)
-
-		await sleep(10)
+		await multiTouchGesture({
+			start: [
+				{ x: 100, y: 100 },
+				{ x: 200, y: 200 },
+			],
+			end: [
+				{ x: 149, y: 149 },
+				{ x: 151, y: 151 },
+			],
+			steps: 50,
+		})
 		expect(await page.evaluate(() => editor.getZoomLevel())).toBe(0.1)
 
 		// now zoom in
-		await dispatchTouch(client, 'touchStart', [
-			{ x: 149, y: 149 },
-			{ x: 150, y: 150 },
-		])
-
-		for (let i = 1; i < 50; i++) {
-			await dispatchTouch(client, 'touchMove', [
-				{ x: 149 - i, y: 149 - i },
-				{ x: 150 + i, y: 150 + i },
-			])
-			finalTouch = [
-				{ x: 149 - i, y: 149 - i },
-				{ x: 150 + i, y: 150 + i },
-			]
-			await sleep(10)
-		}
-		await dispatchTouch(client, 'touchEnd', finalTouch)
-		await sleep(10)
+		await multiTouchGesture({
+			start: [
+				{ x: 149, y: 149 },
+				{ x: 151, y: 151 },
+			],
+			end: [
+				{ x: 0, y: 0 },
+				{ x: 300, y: 300 },
+			],
+			steps: 50,
+		})
 		expect(await page.evaluate(() => editor.getZoomLevel())).toBe(8)
 	})
 
-	test.fixme('minimap', () => {
+	test.fixme('minimap', async () => {
 		// todo
 	})
 
-	test.only('hand tool', async ({ page, toolbar }) => {
-		const handTool = toolbar.tools.hand
-		handTool.click()
-		page.mouse.move(50, 50)
-		await page.evaluate(() => editor.updateInstanceState({ isGridMode: true }))
-		expect(
-			await page.evaluate(() => [
-				editor.inputs.currentPagePoint.x,
-				editor.inputs.currentPagePoint.y,
-			])
-		).toStrictEqual([50, 50])
-		await page.mouse.down()
-		await page.mouse.move(200, 600)
-		await page.mouse.up()
-		await page.mouse.move(50, 50)
-		expect(
-			await page.evaluate(() => [
-				editor.inputs.currentPagePoint.x,
-				editor.inputs.currentPagePoint.y,
-			])
-		).toStrictEqual([100, 100])
+	test.fixme('hand tool', async () => {
+		// todo
 	})
 })
