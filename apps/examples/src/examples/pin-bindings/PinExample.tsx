04c6591a9ba2616b115f30f34c7c7573afc8a449
alex <alex@dytry.ch>
2024-05-13 11:02:03 +0100

    alex/atomic-end-event: pin example

---
+++ apps/examples/src/examples/pin-bindings/PinExample.tsx
@@ -0,0 +1,491 @@
+import {
+	BindingOnShapeChangeOptions,
+	BindingOnShapeDeleteOptions,
+	BindingUtil,
+	Box,
+	DefaultFillStyle,
+	DefaultToolbar,
+	DefaultToolbarContent,
+	RecordProps,
+	Rectangle2d,
+	ShapeUtil,
+	StateNode,
+	TLBaseBinding,
+	TLBaseShape,
+	TLEditorComponents,
+	TLEventHandlers,
+	TLOnTranslateEndHandler,
+	TLOnTranslateStartHandler,
+	TLShapeId,
+	TLUiComponents,
+	TLUiOverrides,
+	Tldraw,
+	TldrawUiMenuItem,
+	Vec,
+	VecModel,
+	createShapeId,
+	invLerp,
+	lerp,
+	track,
+	useEditor,
+	useIsToolSelected,
+	useTools,
+} from 'tldraw'
+
+// eslint-disable-next-line @typescript-eslint/ban-types
+type PinShape = TLBaseShape<'pin', {}>
+
+const offsetX = -16
+const offsetY = -26
+class PinShapeUtil extends ShapeUtil<PinShape> {
+	static override type = 'pin' as const
+	static override props: RecordProps<PinShape> = {}
+
+	override getDefaultProps() {
+		return {}
+	}
+
+	override canBind = () => false
+	override canEdit = () => false
+	override canResize = () => false
+	override hideRotateHandle = () => true
+	override isAspectRatioLocked = () => true
+
+	override getGeometry() {
+		return new Rectangle2d({
+			width: 32,
+			height: 32,
+			x: offsetX,
+			y: offsetY,
+			isFilled: true,
+		})
+	}
+
+	override component() {
+		return (
+			<div
+				style={{
+					width: '100%',
+					height: '100%',
+					marginLeft: offsetX,
+					marginTop: offsetY,
+					fontSize: '26px',
+					textAlign: 'center',
+				}}
+			>
+				📍
+			</div>
+		)
+	}
+
+	override indicator() {
+		return <rect width={32} height={32} x={offsetX} y={offsetY} />
+	}
+
+	override onTranslateStart: TLOnTranslateStartHandler<PinShape> = (shape) => {
+		const bindings = this.editor.getBindingsFromShape(shape, 'pin')
+		this.editor.deleteBindings(bindings)
+	}
+
+	override onTranslateEnd: TLOnTranslateEndHandler<PinShape> = (initial, pin) => {
+		const pageAnchor = this.editor.getShapePageTransform(pin).applyToPoint({ x: 0, y: 0 })
+
+		const targets = this.editor
+			.getShapesAtPoint(pageAnchor, { hitInside: true })
+			.filter(
+				(shape) =>
+					shape.type !== 'pin' && shape.parentId === pin.parentId && shape.index < pin.index
+			)
+
+		for (const target of targets) {
+			const targetBounds = Box.ZeroFix(this.editor.getShapeGeometry(target)!.bounds)
+			const pointInTargetSpace = this.editor.getPointInShapeSpace(target, pageAnchor)
+
+			const anchor = {
+				x: invLerp(targetBounds.minX, targetBounds.maxX, pointInTargetSpace.x),
+				y: invLerp(targetBounds.minY, targetBounds.maxY, pointInTargetSpace.y),
+			}
+
+			this.editor.createBinding({
+				type: 'pin',
+				fromId: pin.id,
+				toId: target.id,
+				props: {
+					anchor,
+				},
+			})
+		}
+	}
+}
+
+type PinBinding = TLBaseBinding<
+	'pin',
+	{
+		anchor: VecModel
+	}
+>
+class PinBindingUtil extends BindingUtil<PinBinding> {
+	static override type = 'pin' as const
+
+	override getDefaultProps() {
+		return {
+			anchor: { x: 0.5, y: 0.5 },
+		}
+	}
+
+	private changedToShapes = new Set<TLShapeId>()
+
+	override onOperationComplete(): void {
+		if (this.changedToShapes.size === 0) return
+
+		console.time('pin layout')
+		const fixedShapes = this.changedToShapes
+		const toCheck = [...this.changedToShapes]
+
+		const initialPositions = new Map<TLShapeId, VecModel>()
+		const targetDeltas = new Map<TLShapeId, Map<TLShapeId, VecModel>>()
+
+		const addTargetDelta = (fromId: TLShapeId, toId: TLShapeId, delta: VecModel) => {
+			if (!targetDeltas.has(fromId)) targetDeltas.set(fromId, new Map())
+			targetDeltas.get(fromId)!.set(toId, delta)
+
+			if (!targetDeltas.has(toId)) targetDeltas.set(toId, new Map())
+			targetDeltas.get(toId)!.set(fromId, { x: -delta.x, y: -delta.y })
+		}
+
+		const allShapes = new Set<TLShapeId>()
+		while (toCheck.length) {
+			const shapeId = toCheck.pop()!
+
+			const shape = this.editor.getShape(shapeId)
+			if (!shape) continue
+
+			if (allShapes.has(shapeId)) continue
+			allShapes.add(shapeId)
+
+			const bindings = this.editor.getBindingsToShape<PinBinding>(shape, 'pin')
+			for (const binding of bindings) {
+				if (allShapes.has(binding.fromId)) continue
+				allShapes.add(binding.fromId)
+
+				const pin = this.editor.getShape<PinShape>(binding.fromId)
+				if (!pin) continue
+
+				const pinPosition = this.editor.getShapePageTransform(pin).applyToPoint({ x: 0, y: 0 })
+				initialPositions.set(pin.id, pinPosition)
+
+				for (const binding of this.editor.getBindingsFromShape<PinBinding>(pin.id, 'pin')) {
+					const shapeBounds = this.editor.getShapeGeometry(binding.toId)!.bounds
+					const shapeAnchor = {
+						x: lerp(shapeBounds.minX, shapeBounds.maxX, binding.props.anchor.x),
+						y: lerp(shapeBounds.minY, shapeBounds.maxY, binding.props.anchor.y),
+					}
+					const currentPageAnchor = this.editor
+						.getShapePageTransform(binding.toId)
+						.applyToPoint(shapeAnchor)
+
+					const shapeOrigin = this.editor
+						.getShapePageTransform(binding.toId)
+						.applyToPoint({ x: 0, y: 0 })
+					initialPositions.set(binding.toId, shapeOrigin)
+
+					addTargetDelta(pin.id, binding.toId, {
+						x: currentPageAnchor.x - shapeOrigin.x,
+						y: currentPageAnchor.y - shapeOrigin.y,
+					})
+
+					if (!allShapes.has(binding.toId)) toCheck.push(binding.toId)
+				}
+			}
+		}
+
+		const currentPositions = new Map(initialPositions)
+
+		const iterations = 30
+		for (let i = 0; i < iterations; i++) {
+			const movements = new Map<TLShapeId, VecModel[]>()
+			for (const [aId, deltas] of targetDeltas) {
+				if (fixedShapes.has(aId)) continue
+				const aPosition = currentPositions.get(aId)!
+				for (const [bId, targetDelta] of deltas) {
+					const bPosition = currentPositions.get(bId)!
+
+					const adjustmentDelta = {
+						x: targetDelta.x - (aPosition.x - bPosition.x),
+						y: targetDelta.y - (aPosition.y - bPosition.y),
+					}
+
+					if (!movements.has(aId)) movements.set(aId, [])
+					movements.get(aId)!.push(adjustmentDelta)
+				}
+			}
+
+			for (const [shapeId, deltas] of movements) {
+				const currentPosition = currentPositions.get(shapeId)!
+				currentPositions.set(shapeId, Vec.Average(deltas).add(currentPosition))
+			}
+		}
+
+		const updates = []
+		for (const [shapeId, position] of currentPositions) {
+			const delta = Vec.Sub(position, initialPositions.get(shapeId)!)
+			if (delta.len2() <= 0.01) continue
+
+			const newPosition = this.editor.getPointInParentSpace(shapeId, position)
+			updates.push({
+				id: shapeId,
+				type: this.editor.getShape(shapeId)!.type,
+				x: newPosition.x,
+				y: newPosition.y,
+			})
+		}
+
+		console.timeEnd('pin layout')
+
+		if (updates.length === 0) {
+			this.changedToShapes.clear()
+		} else {
+			this.editor.updateShapes(updates)
+		}
+	}
+
+	// when the shape we're stuck to changes, update the pin's position
+	override onAfterChangeToShape({ binding }: BindingOnShapeChangeOptions<PinBinding>): void {
+		this.changedToShapes.add(binding.toId)
+		// const pin = this.editor.getShape<PinShape>(binding.fromId)!
+
+		// const shapeBounds = this.editor.getShapeGeometry(shapeAfter)!.bounds
+		// const shapeAnchor = {
+		// 	x: lerp(shapeBounds.minX, shapeBounds.maxX, binding.props.anchor.x),
+		// 	y: lerp(shapeBounds.minY, shapeBounds.maxY, binding.props.anchor.y),
+		// }
+		// const pageAnchor = this.editor.getShapePageTransform(shapeAfter).applyToPoint(shapeAnchor)
+
+		// const pinParentAnchor = this.editor
+		// 	.getShapeParentTransform(pin)
+		// 	.invert()
+		// 	.applyToPoint(pageAnchor)
+
+		// this.editor.updateShape({
+		// 	id: pin.id,
+		// 	type: 'pin',
+		// 	x: pinParentAnchor.x,
+		// 	y: pinParentAnchor.y,
+		// })
+	}
+
+	// when the thing we're stuck to is deleted, delete the pin too
+	override onBeforeDeleteToShape({ binding }: BindingOnShapeDeleteOptions<PinBinding>): void {
+		const pin = this.editor.getShape<PinShape>(binding.fromId)
+		if (pin) this.editor.deleteShape(pin.id)
+	}
+}
+
+type RotationLinkBinding = TLBaseBinding<'rotationLink', { offset: number }>
+class RotationLinkBindingUtil extends BindingUtil<RotationLinkBinding> {
+	static override type = 'rotationLink' as const
+
+	override getDefaultProps() {
+		return {
+			offset: 0,
+		}
+	}
+
+	override onAfterChangeFromShape({
+		binding,
+		shapeAfter,
+	}: BindingOnShapeChangeOptions<RotationLinkBinding>): void {
+		const toShape = this.editor.getShape(binding.toId)
+		if (!toShape) return
+
+		const targetRotation = shapeAfter.rotation + binding.props.offset
+		const rotationToApply = targetRotation - toShape.rotation
+		if (Math.abs(rotationToApply) > 0.01) {
+			const shapeCenter = this.editor.getShapePageBounds(toShape)!.center
+			const shapePosition = this.editor.getShapePageTransform(toShape).applyToPoint({ x: 0, y: 0 })
+			const newPagePosition = Vec.RotWith(shapePosition, shapeCenter, rotationToApply)
+			const newPosition = this.editor.getShapeParentTransform(toShape).applyToPoint(newPagePosition)
+			this.editor.updateShape({
+				id: toShape.id,
+				type: toShape.type,
+				rotation: targetRotation,
+				x: newPosition.x,
+				y: newPosition.y,
+			})
+		}
+	}
+
+	override onAfterChangeToShape({
+		binding,
+		shapeAfter,
+	}: BindingOnShapeChangeOptions<RotationLinkBinding>): void {
+		const fromShape = this.editor.getShape(binding.fromId)
+		if (!fromShape) return
+
+		const targetRotation = shapeAfter.rotation - binding.props.offset
+		const rotationToApply = targetRotation - fromShape.rotation
+		if (Math.abs(rotationToApply) > 0.01) {
+			const shapeCenter = this.editor.getShapePageBounds(fromShape)!.center
+			const shapePosition = this.editor
+				.getShapePageTransform(fromShape)
+				.applyToPoint({ x: 0, y: 0 })
+			const newPagePosition = Vec.RotWith(shapePosition, shapeCenter, rotationToApply)
+			const newPosition = this.editor
+				.getShapeParentTransform(fromShape)
+				.applyToPoint(newPagePosition)
+			this.editor.updateShape({
+				id: fromShape.id,
+				type: fromShape.type,
+				rotation: targetRotation,
+				x: newPosition.x,
+				y: newPosition.y,
+			})
+		}
+	}
+}
+
+class PinTool extends StateNode {
+	static override id = 'pin'
+
+	override onEnter = () => {
+		this.editor.setCursor({ type: 'cross', rotation: 0 })
+	}
+
+	override onPointerDown: TLEventHandlers['onPointerDown'] = (info) => {
+		const { currentPagePoint } = this.editor.inputs
+		const pinId = createShapeId()
+		this.editor.mark(`creating:${pinId}`)
+		this.editor.createShape({
+			id: pinId,
+			type: 'pin',
+			x: currentPagePoint.x,
+			y: currentPagePoint.y,
+		})
+		this.editor.setSelectedShapes([pinId])
+		this.editor.setCurrentTool('select.translating', {
+			...info,
+			target: 'shape',
+			shape: this.editor.getShape(pinId),
+			isCreating: true,
+			onInteractionEnd: 'pin',
+			onCreate: () => {
+				this.editor.setCurrentTool('pin')
+			},
+		})
+	}
+}
+
+const overrides: TLUiOverrides = {
+	tools(editor, schema) {
+		schema['pin'] = {
+			id: 'pin',
+			label: 'Pin',
+			icon: 'heart-icon',
+			kbd: 'p',
+			onSelect: () => {
+				editor.setCurrentTool('pin')
+			},
+		}
+		return schema
+	},
+}
+
+const components: TLUiComponents & TLEditorComponents = {
+	Toolbar: (...props) => {
+		const pin = useTools().pin
+		const isPinSelected = useIsToolSelected(pin)
+		return (
+			<DefaultToolbar {...props}>
+				<TldrawUiMenuItem {...pin} isSelected={isPinSelected} />
+				<DefaultToolbarContent />
+			</DefaultToolbar>
+		)
+	},
+	InFrontOfTheCanvas: track(() => {
+		const editor = useEditor()
+
+		if (!editor.isIn('select.idle')) return null
+		if (editor.getSelectedShapeIds().length < 2) return null
+
+		const pageBounds = editor.getSelectionPageBounds()
+		if (!pageBounds) return null
+
+		const screenPosition = editor.pageToViewport(pageBounds)
+
+		function clearBindings() {
+			const shapes = editor.getSelectedShapes()
+			for (const shape of shapes) {
+				editor.deleteBindings(editor.getBindingsInvolvingShape(shape, 'rotationLink'))
+			}
+		}
+
+		function makeBindings() {
+			clearBindings()
+
+			const shapes = editor.getSelectedShapes()
+			for (let i = 0; i < shapes.length - 1; i++) {
+				const a = shapes[i]
+				for (let j = i + 1; j < shapes.length; j++) {
+					const b = shapes[j]
+					editor.createBinding({
+						type: 'rotationLink',
+						fromId: a.id,
+						toId: b.id,
+						props: {
+							offset: b.rotation - a.rotation,
+						},
+					})
+				}
+			}
+		}
+
+		const hasBindings = editor
+			.getSelectedShapes()
+			.some((shape) => editor.getBindingsFromShape(shape, 'rotationLink').length)
+
+		return (
+			<button
+				onClick={hasBindings ? clearBindings : makeBindings}
+				style={{
+					position: 'absolute',
+					top: -36,
+					left: 0,
+					transform: `translate(${screenPosition.x}px, ${screenPosition.y}px)`,
+					fontSize: 16,
+					background: 'white',
+					border: 'none',
+					width: 32,
+					height: 32,
+					cursor: 'pointer',
+					display: 'flex',
+					justifyContent: 'center',
+					alignItems: 'center',
+					borderRadius: 4,
+					boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
+					pointerEvents: 'all',
+				}}
+			>
+				{hasBindings ? '❌' : '🔄'}
+			</button>
+		)
+	}),
+}
+
+export default function PinExample() {
+	return (
+		<div className="tldraw__editor">
+			<Tldraw
+				persistenceKey="pin-example"
+				onMount={(editor) => {
+					;(window as any).editor = editor
+					editor.setStyleForNextShapes(DefaultFillStyle, 'semi')
+				}}
+				shapeUtils={[PinShapeUtil]}
+				bindingUtils={[PinBindingUtil, RotationLinkBindingUtil]}
+				tools={[PinTool]}
+				overrides={overrides}
+				components={components}
+			/>
+		</div>
+	)
+}
