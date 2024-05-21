f385ecbde2e8cabc62d3e69f13c8d552de81e92c
Mime Čuvalo <mimecuvalo@gmail.com>
2024-05-21 07:35:56 +0100

    touchscreen: whoops, fix up script tag

--- apps/dotcom/src/pages/public-touchscreen-side-panel.tsx
+++ apps/dotcom/src/pages/public-touchscreen-side-panel.tsx
@@ -1,9 +1,15 @@
 import { useEffect } from 'react'
+import { Helmet } from 'react-helmet-async'
 import '../../styles/globals.css'
 
 export function Component() {
 	useEffect(() => {
 		async function createSession() {
+			if ('meet' in window === false) {
+				setTimeout(createSession, 100)
+				return
+			}
+
 			const session = await (window as any).meet.addon.createAddonSession({
 				cloudProjectNumber: `${process.env.GOOGLE_CLOUD_PROJECT_NUMBER}`,
 			})
@@ -20,8 +26,10 @@ export function Component() {
 
 	return (
 		<>
-			{/* eslint-disable @next/next/no-sync-scripts */}
-			<script src="https://www.gstatic.com/meetjs/addons/0.1.0/meet.addons.js"></script>
+			<Helmet>
+				{/* eslint-disable @next/next/no-sync-scripts */}
+				<script src="https://www.gstatic.com/meetjs/addons/0.1.0/meet.addons.js"></script>
+			</Helmet>
 			Starting a new session…
 		</>
 	)
