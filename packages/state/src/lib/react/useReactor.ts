3ce5eb845a764fb1cbdf02422f4699903192ba3b
Mime Čuvalo <mimecuvalo@gmail.com>
2024-06-04 11:39:17 +0100

    timeouts: rework effectschedule timeout tracking

--- packages/state/src/lib/react/useReactor.ts
+++ packages/state/src/lib/react/useReactor.ts
@@ -1,10 +1,18 @@
-import { useEffect, useMemo } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import { EffectScheduler } from '../core'
 
 /** @public */
 export function useReactor(name: string, reactFn: () => void, deps: undefined | any[] = []) {
+	const [raf, setRaf] = useState(-1)
 	const scheduler = useMemo(
-		() => new EffectScheduler(name, reactFn, { scheduleEffect: (cb) => requestAnimationFrame(cb) }),
+		() =>
+			new EffectScheduler(name, reactFn, {
+				scheduleEffect: (cb) => {
+					const raf = requestAnimationFrame(cb)
+					setRaf(raf)
+					return raf
+				},
+			}),
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 		deps
 	)
@@ -14,6 +22,7 @@ export function useReactor(name: string, reactFn: () => void, deps: undefined | 
 		scheduler.execute()
 		return () => {
 			scheduler.detach()
+			cancelAnimationFrame(raf)
 		}
-	}, [scheduler])
+	}, [scheduler, raf])
 }
