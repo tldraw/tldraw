e74d2470c5220c19fb17ce910d1562ce4c116812
Mime Čuvalo <mimecuvalo@gmail.com>
2024-06-04 11:55:22 +0100

    timeouts: rework effectschedule timeout tracking (#3870)
    talked to @ds300 and decided that we needed to rework this latest bit to
    cleanup outside of `EffectScheduler`
    
    ### Change Type
    
    <!-- ❗ Please select a 'Scope' label ❗️ -->
    
    - [x] `sdk` — Changes the tldraw SDK
    - [ ] `dotcom` — Changes the tldraw.com web app
    - [ ] `docs` — Changes to the documentation, examples, or templates.
    - [ ] `vs code` — Changes to the vscode plugin
    - [ ] `internal` — Does not affect user-facing stuff
    
    <!-- ❗ Please select a 'Type' label ❗️ -->
    
    - [x] `bugfix` — Bug fix
    - [ ] `feature` — New feature
    - [ ] `improvement` — Improving existing features
    - [ ] `chore` — Updating dependencies, other boring stuff
    - [ ] `galaxy brain` — Architectural changes
    - [ ] `tests` — Changes to any test code
    - [ ] `tools` — Changes to infrastructure, CI, internal scripts,
    debugging tools, etc.
    - [ ] `dunno` — I don't know

--- packages/state/src/lib/core/EffectScheduler.ts
+++ packages/state/src/lib/core/EffectScheduler.ts
@@ -35,7 +35,7 @@ interface EffectSchedulerOptions {
 	 * @param execute - A function that will execute the effect.
 	 * @returns
 	 */
-	scheduleEffect?: (execute: () => void) => number | void | (() => void)
+	scheduleEffect?: (execute: () => void) => void
 }
 
 class __EffectScheduler__<Result> {
@@ -64,14 +64,12 @@ class __EffectScheduler__<Result> {
 	}
 
 	/** @internal */
-	private maybeRaf: number | void | (() => void) = -1
-	/** @internal */
 	readonly parentSet = new ArraySet<Signal<any, any>>()
 	/** @internal */
 	readonly parentEpochs: number[] = []
 	/** @internal */
 	readonly parents: Signal<any, any>[] = []
-	private readonly _scheduleEffect?: (execute: () => void) => number | void | (() => void)
+	private readonly _scheduleEffect?: (execute: () => void) => void
 	constructor(
 		public readonly name: string,
 		private readonly runEffect: (lastReactedEpoch: number) => Result,
@@ -101,7 +99,7 @@ class __EffectScheduler__<Result> {
 		this._scheduleCount++
 		if (this._scheduleEffect) {
 			// if the effect should be deferred (e.g. until a react render), do so
-			this.maybeRaf = this._scheduleEffect(this.maybeExecute)
+			this._scheduleEffect(this.maybeExecute)
 		} else {
 			// otherwise execute right now!
 			this.execute()
@@ -137,7 +135,6 @@ class __EffectScheduler__<Result> {
 		for (let i = 0, n = this.parents.length; i < n; i++) {
 			detach(this.parents[i], this)
 		}
-		typeof this.maybeRaf === 'number' && cancelAnimationFrame(this.maybeRaf)
 	}
 
 	/**
