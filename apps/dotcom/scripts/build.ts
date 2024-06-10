5d5d5e4026fd9bd2418a822c367d1f52323e2a87
Eric Mika <eric@ericmika.com>
2024-06-10 11:34:50 -0400

    Fix Content-Security-Policy' source value.

--- apps/dotcom/scripts/build.ts
+++ apps/dotcom/scripts/build.ts
@@ -14,7 +14,7 @@ const commonSecurityHeaders = {
 	'Strict-Transport-Security': 'max-age=63072000; includeSubDomains; preload',
 	'X-Content-Type-Options': 'nosniff',
 	'Referrer-Policy': 'no-referrer-when-downgrade',
-	'Content-Security-Policy': 'default-src: *',
+	'Content-Security-Policy': 'default-src *',
 }
 
 // We load the list of routes that should be forwarded to our SPA's index.html here.
