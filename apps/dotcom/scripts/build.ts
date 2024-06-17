ffdd68855973de1624d5f68a4d0a7cdb53d961e9
Mime Čuvalo <mimecuvalo@gmail.com>
2024-06-17 11:58:22 +0100

    more

--- apps/dotcom/scripts/build.ts
+++ apps/dotcom/scripts/build.ts
@@ -12,7 +12,14 @@ import { getMultiplayerServerURL } from '../vite.config'
 
 const cspDirectives: { [key: string]: string[] } = {
 	'default-src': [`'self'`],
-	'connect-src': [`'self'`, `ws:`, `wss:`, `https://*.ingest.sentry.io`],
+	'connect-src': [
+		`'self'`,
+		`ws:`,
+		`wss:`,
+		`https://assets.tldraw.xyz`,
+		`https://*.tldraw.workers.dev`,
+		`https://*.ingest.sentry.io`,
+	],
 	'font-src': [`'self'`, `https://fonts.googleapis.com`, `https://fonts.gstatic.com`],
 	'frame-src': [`https:`],
 	'img-src': [`'self'`, `http:`, `https:`, `data:`, `blob:`],
