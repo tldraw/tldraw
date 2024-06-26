3381d9ced3140de0109634d1b0e3cac0a78cd9aa
Steve Ruiz <steveruizok@gmail.com>
2024-06-26 18:52:19 +0100

    add fog of war

--- apps/examples/src/examples/fog-of-war/FogOfWarExample.tsx
+++ apps/examples/src/examples/fog-of-war/FogOfWarExample.tsx
@@ -1,8 +1,8 @@
-import { useRef } from 'react'
-import { Box, TLComponents, Tldraw, useEditor, useReactor } from 'tldraw'
+import { useEffect, useRef } from 'react'
+import { Box, TLComponents, Tldraw, Vec, useEditor, useReactor } from 'tldraw'
 import 'tldraw/tldraw.css'
 
-const CELL_SIZE = 100
+const CELL_SIZE = 32
 const COUNT = 100
 
 const boxes: Box[][] = []
@@ -23,32 +23,51 @@ export function Fog() {
 	const rVisibility = useRef<boolean[][]>(cells)
 	const editor = useEditor()
 
+	useEffect(() => {
+		const cvs = rCanvas.current!
+		const rect = cvs.getBoundingClientRect()
+		cvs.width = rect.width
+		cvs.height = rect.height
+	}, [editor])
+
 	useReactor(
 		'update fog',
 		() => {
 			const cells = rVisibility.current
-			const bounds = editor.getCurrentPageShapes().map((s) => editor.getShapePageBounds(s)!)
-			for (let i = 0; i < boxes.length; i++) {
-				for (let j = 0; j < boxes[i].length; j++) {
-					const box = boxes[i][j]
-					for (const bound of bounds) {
-						if (bound.includes(box)) {
+			const shapes = editor.getCurrentPageShapes()
+			for (const shape of shapes) {
+				const point = editor.getShapePageBounds(shape)!.point
+				const geometry = editor.getShapeGeometry(shape)
+				for (let i = 0; i < boxes.length; i++) {
+					for (let j = 0; j < boxes[i].length; j++) {
+						const box = boxes[i][j]
+						box.translate(Vec.Neg(point))
+						if (geometry.bounds.collides(box)) {
 							cells[i][j] = true
 						}
+						box.translate(point)
 					}
 				}
 			}
 			const cvs = rCanvas.current!
 			const ctx = cvs.getContext('2d')!
 
+			ctx.resetTransform()
+			const camera = editor.getCamera()
+
 			ctx.clearRect(0, 0, cvs.width, cvs.height)
-			ctx.fillStyle = 'rgba(0,0,0,0.5)'
+			ctx.fillStyle = 'rgba(0,0,0,0.9)'
 			ctx.fillRect(0, 0, cvs.width, cvs.height)
+
+			ctx.translate(100, 100)
+			ctx.scale(camera.z, camera.z)
+			ctx.translate(camera.x, camera.y)
 
 			for (let i = 0; i < boxes.length; i++) {
 				for (let j = 0; j < boxes[i].length; j++) {
 					if (!cells[i][j]) continue
 					const box = boxes[i][j]
+					ctx.filter = 'drop-shadow(100px)'
 					ctx.clearRect(box.x, box.y, box.width, box.height)
 				}
 			}
@@ -62,10 +81,12 @@ export function Fog() {
 			style={{
 				zIndex: 999999,
 				position: 'absolute',
-				top: 0,
-				left: 0,
-				width: '100%',
-				height: '100%',
+				top: -100,
+				left: -100,
+				width: 'calc(100% + 200px)',
+				height: 'calc(100% + 200px)',
+				WebkitFilter: 'blur(15px)',
+				filter: 'blur(15px)',
 			}}
 		/>
 	)
--- apps/examples/src/examples/fog-of-war/README.md
+++ apps/examples/src/examples/fog-of-war/README.md
@@ -7,4 +7,4 @@ keywords: [ui, fog, overlay]
 
 ---
 
-Fog of war!
+This example shows how you might keep an HTML canvas in sync with canvas content. It implements a simple "fog of war" effect, where the canvas is covered by a black overlay that can be cleared by drawing shapes on the canvas.
