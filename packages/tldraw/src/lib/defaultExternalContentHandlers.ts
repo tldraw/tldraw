41c3b1e3df5d6e086a273d6df2a524a43fa22ba7
Mime Čuvalo <mimecuvalo@gmail.com>
2024-06-26 12:36:57 +0100

    bookmarks: account for relative urls more robustly (#4022)
    Fixes up url's that don't have `/` in front or `http`
    
    ### Change Type
    
    <!-- ❗ Please select a 'Type' label ❗️ -->
    
    - [ ] `feature` — New feature
    - [x] `improvement` — Product improvement
    - [ ] `api` — API change
    - [ ] `bugfix` — Bug fix
    - [ ] `other` — Changes that don't affect SDK users, e.g. internal or
    .com changes
    
    ### Release Notes
    
    - Bookmark extractor: account for relative urls more robustly

--- packages/tldraw/src/lib/defaultExternalContentHandlers.ts
+++ packages/tldraw/src/lib/defaultExternalContentHandlers.ts
@@ -148,10 +148,10 @@ export function registerDefaultExternalContentHandlers(
 				description:
 					doc.head.querySelector('meta[property="og:description"]')?.getAttribute('content') ?? '',
 			}
-			if (meta.image.startsWith('/')) {
+			if (!meta.image.startsWith('http')) {
 				meta.image = new URL(meta.image, url).href
 			}
-			if (meta.favicon.startsWith('/')) {
+			if (!meta.favicon.startsWith('http')) {
 				meta.favicon = new URL(meta.favicon, url).href
 			}
 		} catch (error) {
