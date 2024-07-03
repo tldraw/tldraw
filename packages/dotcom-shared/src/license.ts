082ff819e9f2fb92ce4fae1cd00655d4d514f225
Mime Čuvalo <mimecuvalo@gmail.com>
2024-07-03 14:37:51 +0100

    whoops

---
+++ packages/dotcom-shared/src/license.ts
@@ -0,0 +1,4 @@
+const TLDRAW_LICENSE =
+	process.env.TLDRAW_LICENSE ??
+	'tldraw-tldraw-2025-07-03/WyJkT2xBWE5VcSIsWyIqLnRsZHJhdy5jb20iXSwxLCIyLjMuMCIsIjIwMjUtMDctMDMiXQ==.Dow6kqvC3yyT6r9oy8bpPYYwhg9+RFViDroQ49P9+M5WZ1RSwIpb6xhvZ1kPVOiN7y0Xbrxvjb2o3K8TJlxUTA=='
+export default TLDRAW_LICENSE
