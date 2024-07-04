23344830b18954132db17a8b8d9e594e4195c905
Lukas Wiesehan <mail@lukaswiesehan.de>
2024-07-04 19:15:19 +0200

    Style tables

--- apps/docs/components/content/index.tsx
+++ apps/docs/components/content/index.tsx
@@ -15,7 +15,11 @@ export const Content: React.FC<{ mdx: string }> = ({ mdx }) => {
 				'prose prose-sm prose-zinc text-zinc-800 sm:prose-base w-full max-w-3xl',
 				'prose-code:before:content-none prose-code:after:content-none prose-code:bg-zinc-200 prose-code:px-1 prose-code:py-0.5 prose-code:rounded',
 				'prose-a:no-underline prose-a:text-blue-500 hover:prose-a:text-blue-600',
-				'prose-blockquote:text-zinc-800 prose-blockquote:font-normal prose-blockquote:border-none prose-blockquote:px-4 prose-blockquote:leading-normal prose-blockquote:bg-zinc-50 prose-blockquote:py-3 prose-blockquote:rounded-xl md:prose-blockquote:mx-1'
+				'prose-blockquote:text-zinc-800 prose-blockquote:font-normal prose-blockquote:border-none prose-blockquote:px-4 prose-blockquote:leading-normal prose-blockquote:bg-zinc-50 prose-blockquote:py-3 prose-blockquote:rounded-xl',
+				'prose-table:bg-zinc-50 prose-table:rounded-xl prose-table:text-sm',
+				'prose-th:text-left prose-th:font-semibold prose-th:uppercase prose-th:text-xs prose-th:border-l prose-th:border-white prose-th:py-3 prose-th:px-4',
+				'prose-tr:border-t prose-tr:border-white',
+				'prose-td:border-l first:prose-td:border-l-0 prose-td:border-white prose-td:py-3 prose-td:px-4'
 			)}
 		>
 			<MDXRemote
