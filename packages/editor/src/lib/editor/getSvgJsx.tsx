f73a47e371bc6e1dcb629b3d2afcab8b50e69f8a
Taha <98838967+Taha-Hassan-Git@users.noreply.github.com>
2024-07-04 17:17:55 +0100

    [draft] watermark on exports (#4077)
    The goal is to expand the svg box by the right amount and translate all
    the shapes up and to the left to leave a gap for the watermark to fit in
    to. At smaller sizes there should be enough of a gap for the watermark
    to fit without needing to do that.
    
    Still to do:
    
    - [ ] Add global state that tracks whether the watermark is displayed
    - [ ] Import watermark svg
    - [ ] Align the watermark nicely
    
    ### Change type
    
    - [ ] `bugfix`
    - [ ] `improvement`
    - [ ] `feature`
    - [ ] `api`
    - [ ] `other`
    
    ### Test plan
    
    1. Create a shape...
    2.
    
    - [ ] Unit tests
    - [ ] End to end tests
    
    ### Release notes
    
    - Fixed a bug with...
    
    ---------
    
    Co-authored-by: Mime Čuvalo <mimecuvalo@gmail.com>

--- packages/editor/src/lib/editor/getSvgJsx.tsx
+++ packages/editor/src/lib/editor/getSvgJsx.tsx
@@ -3,12 +3,18 @@ import {
 	TLGroupShape,
 	TLShape,
 	TLShapeId,
+	createShapeId,
 	getDefaultColorTheme,
 } from '@tldraw/tlschema'
+import { FileHelpers, fetch } from '@tldraw/utils'
 import { Fragment, ReactElement } from 'react'
+import WatermarkDesktop from '../../../assets/watermarks/watermark-desktop.svg'
+import { Mat } from '../primitives/Mat'
 import { Editor } from './Editor'
 import { SvgExportContext, SvgExportContextProvider, SvgExportDef } from './types/SvgExportContext'
 import { TLSvgOptions } from './types/misc-types'
+
+let watermarkBase64 = ''
 
 export async function getSvgJsx(
 	editor: Editor,
@@ -60,9 +66,11 @@ export async function getSvgJsx(
 		ids.length === 1 && editor.isShapeOfType<TLFrameShape>(editor.getShape(ids[0])!, 'frame')
 			? ids[0]
 			: null
+	let didExpandPadding = false
 	if (!singleFrameShapeId) {
 		// Expand by an extra 32 pixels
 		bbox.expandBy(padding)
+		didExpandPadding = true
 	}
 
 	// We want the svg image to be BIGGER THAN USUAL to account for image quality
@@ -92,94 +100,137 @@ export async function getSvgJsx(
 		},
 	}
 
+	const createWatermarkShape = async () => {
+		const watermarkWidth = 141
+		const watermarkHeight = 32
+		const watermarkPadding = 8
+		const desiredHeight = bbox.height * 0.05
+		const scaleFactor = desiredHeight / watermarkHeight
+
+		const isWatermarkBiggerThanTheBoundingBox = bbox.height < watermarkHeight + watermarkPadding
+		if (isWatermarkBiggerThanTheBoundingBox) {
+			// Meh, not worth it. Don't worry about it, you get a freebie ;)
+			return []
+		}
+
+		const id = createShapeId()
+		const offsetX =
+			(watermarkWidth + watermarkPadding) * scaleFactor + (didExpandPadding ? padding : 0)
+		const offsetY =
+			(watermarkHeight + watermarkPadding) * scaleFactor + (didExpandPadding ? padding : 0)
+		const pageTransform = Mat.Identity()
+			.translate(bbox.maxX - offsetX, bbox.maxY - offsetY)
+			.scale(scaleFactor, scaleFactor)
+			.toCssString()
+
+		if (!watermarkBase64) {
+			const watermarkResponse = await fetch(WatermarkDesktop)
+			const watermarkBlob = await watermarkResponse.blob()
+			watermarkBase64 = await FileHelpers.blobToDataUrl(watermarkBlob)
+		}
+
+		return [
+			{
+				zIndex: 2147483647,
+				element: (
+					<g key={id} transform={pageTransform} opacity={0.5}>
+						<image href={watermarkBase64} width={watermarkWidth} height={watermarkHeight} />
+					</g>
+				),
+			},
+		]
+	}
+
 	const unorderedShapeElements = (
 		await Promise.all(
-			renderingShapes.map(async ({ id, opacity, index, backgroundIndex }) => {
-				// Don't render the frame if we're only exporting a single frame
-				if (id === singleFrameShapeId) return []
+			renderingShapes
+				.map(async ({ id, opacity, index, backgroundIndex }) => {
+					// Don't render the frame if we're only exporting a single frame
+					if (id === singleFrameShapeId) return []
 
-				const shape = editor.getShape(id)!
+					const shape = editor.getShape(id)!
 
-				if (editor.isShapeOfType<TLGroupShape>(shape, 'group')) return []
+					if (editor.isShapeOfType<TLGroupShape>(shape, 'group')) return []
 
-				const util = editor.getShapeUtil(shape)
+					const util = editor.getShapeUtil(shape)
 
-				let toSvgResult = await util.toSvg?.(shape, exportContext)
-				let toBackgroundSvgResult = await util.toBackgroundSvg?.(shape, exportContext)
+					let toSvgResult = await util.toSvg?.(shape, exportContext)
+					let toBackgroundSvgResult = await util.toBackgroundSvg?.(shape, exportContext)
 
-				if (!toSvgResult && !toBackgroundSvgResult) {
-					const bounds = editor.getShapePageBounds(shape)!
-					toSvgResult = (
-						<rect
-							width={bounds.w}
-							height={bounds.h}
-							fill={theme.solid}
-							stroke={theme.grey.pattern}
-							strokeWidth={1}
-						/>
-					)
-				}
-
-				let pageTransform = editor.getShapePageTransform(shape)!.toCssString()
-				if ('scale' in shape.props) {
-					if (shape.props.scale !== 1) {
-						pageTransform = `${pageTransform} scale(${shape.props.scale}, ${shape.props.scale})`
+					if (!toSvgResult && !toBackgroundSvgResult) {
+						const bounds = editor.getShapePageBounds(shape)!
+						toSvgResult = (
+							<rect
+								width={bounds.w}
+								height={bounds.h}
+								fill={theme.solid}
+								stroke={theme.grey.pattern}
+								strokeWidth={1}
+							/>
+						)
 					}
-				}
 
-				if (toSvgResult) {
-					toSvgResult = (
-						<g key={shape.id} transform={pageTransform} opacity={opacity}>
-							{toSvgResult}
-						</g>
-					)
-				}
-				if (toBackgroundSvgResult) {
-					toBackgroundSvgResult = (
-						<g key={`bg_${shape.id}`} transform={pageTransform} opacity={opacity}>
-							{toBackgroundSvgResult}
-						</g>
-					)
-				}
-
-				// Create svg mask if shape has a frame as parent
-				const pageMask = editor.getShapeMask(shape.id)
-				if (pageMask) {
-					// Create a clip path and add it to defs
-					const pageMaskId = `mask_${shape.id.replace(':', '_')}`
-					defChildren.push(
-						<clipPath key={defChildren.length} id={pageMaskId}>
-							{/* Create a polyline mask that does the clipping */}
-							<path d={`M${pageMask.map(({ x, y }) => `${x},${y}`).join('L')}Z`} />
-						</clipPath>
-					)
+					let pageTransform = editor.getShapePageTransform(shape)!.toCssString()
+					if ('scale' in shape.props) {
+						if (shape.props.scale !== 1) {
+							pageTransform = `${pageTransform} scale(${shape.props.scale}, ${shape.props.scale})`
+						}
+					}
 
 					if (toSvgResult) {
 						toSvgResult = (
-							<g key={shape.id} clipPath={`url(#${pageMaskId})`}>
+							<g key={shape.id} transform={pageTransform} opacity={opacity}>
 								{toSvgResult}
 							</g>
 						)
 					}
 					if (toBackgroundSvgResult) {
 						toBackgroundSvgResult = (
-							<g key={`bg_${shape.id}`} clipPath={`url(#${pageMaskId})`}>
+							<g key={`bg_${shape.id}`} transform={pageTransform} opacity={opacity}>
 								{toBackgroundSvgResult}
 							</g>
 						)
 					}
-				}
 
-				const elements = []
-				if (toSvgResult) {
-					elements.push({ zIndex: index, element: toSvgResult })
-				}
-				if (toBackgroundSvgResult) {
-					elements.push({ zIndex: backgroundIndex, element: toBackgroundSvgResult })
-				}
+					// Create svg mask if shape has a frame as parent
+					const pageMask = editor.getShapeMask(shape.id)
+					if (pageMask) {
+						// Create a clip path and add it to defs
+						const pageMaskId = `mask_${shape.id.replace(':', '_')}`
+						defChildren.push(
+							<clipPath key={defChildren.length} id={pageMaskId}>
+								{/* Create a polyline mask that does the clipping */}
+								<path d={`M${pageMask.map(({ x, y }) => `${x},${y}`).join('L')}Z`} />
+							</clipPath>
+						)
 
-				return elements
-			})
+						if (toSvgResult) {
+							toSvgResult = (
+								<g key={shape.id} clipPath={`url(#${pageMaskId})`}>
+									{toSvgResult}
+								</g>
+							)
+						}
+						if (toBackgroundSvgResult) {
+							toBackgroundSvgResult = (
+								<g key={`bg_${shape.id}`} clipPath={`url(#${pageMaskId})`}>
+									{toBackgroundSvgResult}
+								</g>
+							)
+						}
+					}
+
+					const elements = []
+					if (toSvgResult) {
+						elements.push({ zIndex: index, element: toSvgResult })
+					}
+					if (toBackgroundSvgResult) {
+						elements.push({ zIndex: backgroundIndex, element: toBackgroundSvgResult })
+					}
+
+					return elements
+				})
+				.concat(editor.isWatermarkShown ? [createWatermarkShape()] : [])
 		)
 	).flat()
 
