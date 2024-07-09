4ac5ce08b95d1e59d2fd4c8d0a8605c47c4ce0bc
Taha <98838967+Taha-Hassan-Git@users.noreply.github.com>
2024-07-09 14:11:57 +0100

    add button to export canvas as image

--- apps/examples/src/examples/export-canvas-as-image/ExportCanvasImageExample.tsx
+++ apps/examples/src/examples/export-canvas-as-image/ExportCanvasImageExample.tsx
@@ -1,10 +1,38 @@
-import { Tldraw } from 'tldraw'
+import { exportToBlob, Tldraw, TLUiComponents, useEditor } from 'tldraw'
 import 'tldraw/tldraw.css'
 
+function ExportCanvasButton() {
+	const editor = useEditor()
+	return (
+		<button
+			style={{ pointerEvents: 'all', fontSize: 18, backgroundColor: 'thistle' }}
+			onClick={async () => {
+				const shapeIds = editor.getCurrentPageShapeIds()
+				if (shapeIds.size === 0) return alert('No shapes on the canvas')
+				const blob = await exportToBlob({
+					editor,
+					ids: [...shapeIds],
+					format: 'png',
+					opts: { background: false },
+				})
+
+				const link = document.createElement('a')
+				link.href = window.URL.createObjectURL(blob)
+				link.download = 'every-shape-on-the-canvas.jpg'
+				link.click()
+			}}
+		>
+			Export canvas as image
+		</button>
+	)
+}
+const components: TLUiComponents = {
+	SharePanel: ExportCanvasButton,
+}
 export default function ExportCanvasImageExample() {
 	return (
 		<div className="tldraw__editor">
-			<Tldraw />
+			<Tldraw components={components} />
 		</div>
 	)
 }
