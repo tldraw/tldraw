a1476826e69a3930b4973e16f1303c04188967df
Lukas Wiesehan <mail@lukaswiesehan.de>
2024-07-11 13:37:46 +0200

    Basic contentlayer setup

--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -1,3 +1,5 @@
+const { withContentlayer } = require('next-contentlayer')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
@@ -73,4 +75,4 @@ const nextConfig = {
 	},
 }
 
-module.exports = nextConfig
+module.exports = withContentlayer(nextConfig)
