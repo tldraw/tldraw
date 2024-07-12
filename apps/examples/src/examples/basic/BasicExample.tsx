e7b47a00a7f0b21d073e6d805c611a979b893052
David Sheldrick <d.j.sheldrick@gmail.com>
2024-07-12 11:24:59 +0100

    fix things

--- apps/examples/src/examples/basic/BasicExample.tsx
+++ apps/examples/src/examples/basic/BasicExample.tsx
@@ -1,12 +1,10 @@
-import { useMultiplayerDemo } from '@tldraw/sync'
 import { Tldraw } from 'tldraw'
 import 'tldraw/tldraw.css'
 
 export default function BasicExample() {
-	const store = useMultiplayerDemo({ roomId: 'adam' })
 	return (
 		<div className="tldraw__editor">
-			<Tldraw store={store} />
+			<Tldraw />
 		</div>
 	)
 }
