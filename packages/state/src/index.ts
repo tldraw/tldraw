9d157ef117e73a1facd696629d0ffeff3a9fae30
David Sheldrick <d.j.sheldrick@gmail.com>
2024-07-15 12:04:25 +0100

    core -> lib

--- packages/state/src/index.ts
+++ packages/state/src/index.ts
@@ -1,24 +1,24 @@
-import { singleton } from './core/helpers'
+import { singleton } from './lib/helpers'
 
-export { ArraySet } from './core/ArraySet'
-export { atom, isAtom } from './core/Atom'
-export type { Atom, AtomOptions } from './core/Atom'
+export { ArraySet } from './lib/ArraySet'
+export { atom, isAtom } from './lib/Atom'
+export type { Atom, AtomOptions } from './lib/Atom'
 export {
 	UNINITIALIZED,
 	computed,
 	getComputedInstance,
 	isUninitialized,
 	withDiff,
-} from './core/Computed'
-export type { Computed, ComputedOptions, WithDiff } from './core/Computed'
-export { EffectScheduler, react, reactor } from './core/EffectScheduler'
-export type { EffectSchedulerOptions, Reactor } from './core/EffectScheduler'
-export { unsafe__withoutCapture, whyAmIRunning } from './core/capture'
-export { EMPTY_ARRAY } from './core/helpers'
-export { isSignal } from './core/isSignal'
-export { transact, transaction } from './core/transactions'
-export { RESET_VALUE } from './core/types'
-export type { Child, ComputeDiff, Signal } from './core/types'
+} from './lib/Computed'
+export type { Computed, ComputedOptions, WithDiff } from './lib/Computed'
+export { EffectScheduler, react, reactor } from './lib/EffectScheduler'
+export type { EffectSchedulerOptions, Reactor } from './lib/EffectScheduler'
+export { unsafe__withoutCapture, whyAmIRunning } from './lib/capture'
+export { EMPTY_ARRAY } from './lib/helpers'
+export { isSignal } from './lib/isSignal'
+export { transact, transaction } from './lib/transactions'
+export { RESET_VALUE } from './lib/types'
+export type { Child, ComputeDiff, Signal } from './lib/types'
 
 // This should be incremented any time an API change is made. i.e. for additions or removals.
 // Bugfixes need not increment this.
