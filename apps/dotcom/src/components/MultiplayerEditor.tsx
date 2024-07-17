tldraw/tldraw

commit c1fe8ec99aa96a7f355c587d7ce97afdc66bab45

put sync stuff in bemo worker (#4060)
this PR puts sync stuff in the bemo worker, and sets up a temporary
dev-only page in dotcom for testing bemo stuff


### Change type

- [ ] `bugfix`
- [ ] `improvement`
- [x] `feature`
- [ ] `api`
- [ ] `other`

### Test plan

1. Create a shape...
2.

- [ ] Unit tests
- [ ] End to end tests

### Release notes

- Fixed a bug with...

apps/dotcom/src/components/MultiplayerEditor.tsx
@@ -1,4 +1,5 @@
 import { ROOM_OPEN_MODE, RoomOpenModeToPath, type RoomOpenMode } from '@tldraw/dotcom-shared'
+import { useRemoteSyncClient } from '@tldraw/sync-react'
 import { useCallback, useEffect } from 'react'
 import {
 	DefaultContextMenu,
@@ -22,7 +23,6 @@ import {
 	useActions,
 	useValue,
 } from 'tldraw'
-import { useRemoteSyncClient } from '../hooks/useRemoteSyncClient'
 import { UrlStateParams, useUrlState } from '../hooks/useUrlState'
 import { resolveAsset } from '../utils/assetHandler'
 import { assetUrls } from '../utils/assetUrls'

commit 8aac6dc59ea783f2f1f5481c086288ce0d9118ed

Fix dotcom multiplayer analytics (#4205)
we broke it in #4060 argh

### Change type

- [x] `bugfix`
- [ ] `improvement`
- [ ] `feature`
- [ ] `api`
- [ ] `other`

### Test plan

1. Create a shape...
2.

- [ ] Unit tests
- [ ] End to end tests

### Release notes

- Fixed a bug with…

apps/dotcom/src/components/MultiplayerEditor.tsx
@@ -37,6 +37,7 @@ import { MULTIPLAYER_SERVER } from '../utils/config'
 import { createAssetFromUrl } from '../utils/createAssetFromUrl'
 import { multiplayerAssetStore } from '../utils/multiplayerAssetStore'
 import { useSharing } from '../utils/sharing'
+import { trackAnalyticsEvent } from '../utils/trackAnalyticsEvent'
 import { OPEN_FILE_ACTION, SAVE_FILE_COPY_ACTION, useFileSystem } from '../utils/useFileSystem'
 import { useHandleUiEvents } from '../utils/useHandleUiEvent'
 import { DocumentTopZone } from './DocumentName/DocumentName'
@@ -126,6 +127,7 @@ export function MultiplayerEditor({
 		uri: `${MULTIPLAYER_SERVER}/${RoomOpenModeToPath[roomOpenMode]}/${roomSlug}`,
 		roomId: roomSlug,
 		assets: multiplayerAssetStore,
+		trackAnalyticsEvent,
 	})
 
 	const sharingUiOverrides = useSharing()