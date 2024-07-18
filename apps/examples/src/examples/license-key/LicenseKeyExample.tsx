c91c812b24d9941f1aba8460f5994ca064553db5
Mitja Bezenšek <mitja.bezensek@gmail.com>
2024-07-18 15:26:09 +0200

    Show how to toggle on the feature flag.

--- apps/examples/src/examples/license-key/LicenseKeyExample.tsx
+++ apps/examples/src/examples/license-key/LicenseKeyExample.tsx
@@ -1,7 +1,9 @@
-import { Tldraw } from 'tldraw'
+import { Tldraw, debugEnableLicensing } from 'tldraw'
 import 'tldraw/tldraw.css'
 
 export default function LicenseKeyExample() {
+	// Licensing is disabled for now, let use enable it.
+	debugEnableLicensing()
 	return (
 		<div className="tldraw__editor">
 			<Tldraw licenseKey="example" />
