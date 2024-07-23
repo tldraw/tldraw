1abe7af1a5544f854a11a8c7cca97a44371e28bd
Taha <98838967+Taha-Hassan-Git@users.noreply.github.com>
2024-07-23 16:05:24 +0100

    rm basic example changes omg

--- apps/examples/src/examples/basic/BasicExample.tsx
+++ apps/examples/src/examples/basic/BasicExample.tsx
@@ -4,12 +4,7 @@ import 'tldraw/tldraw.css'
 export default function BasicExample() {
 	return (
 		<div className="tldraw__editor">
-			<Tldraw
-				onMount={(editor) => {
-					;(window as any).editor = editor
-				}}
-				persistenceKey="example"
-			/>
+			<Tldraw persistenceKey="example" />
 		</div>
 	)
 }
