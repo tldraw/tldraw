tldraw/tldraw

commit f05d102cd44ec3ab3ac84b51bf8669ef3b825481

Move from function properties to methods (#4288)
Things left to do
- [x] Update docs (things like the [tools
page](https://tldraw-docs-fqnvru1os-tldraw.vercel.app/docs/tools),
possibly more)
- [x] Write a list of breaking changes and how to upgrade.
- [x] Do another pass and check if we can update any lines that have
`@typescript-eslint/method-signature-style` and
`local/prefer-class-methods` disabled
- [x] Thinks about what to do with `TLEventHandlers`. Edit: Feels like
keeping them is the best way to go.
- [x] Remove `override` keyword where it's not needed. Not sure if it's
worth the effort. Edit: decided not to spend time here.
- [ ] What about possible detached / destructured uses?

Fixes https://github.com/tldraw/tldraw/issues/2799

### Change type

- [ ] `bugfix`
- [ ] `improvement`
- [ ] `feature`
- [x] `api`
- [ ] `other`

### Test plan

1. Create a shape...
2.

- [ ] Unit tests
- [ ] End to end tests

### Release notes

- Adds eslint rules for enforcing the use of methods instead of function
properties and fixes / disables all the resulting errors.

# Breaking changes

This change affects the syntax of how the event handlers for shape tools
and utils are defined.

## Shape utils
**Before**
```ts
export class CustomShapeUtil extends ShapeUtil<CustomShape> {
   // Defining flags
   override canEdit = () => true

   // Defining event handlers
   override onResize: TLOnResizeHandler<CustomShape> = (shape, info) => {
      ...
   }
}
```


**After**
```ts
export class CustomShapeUtil extends ShapeUtil<CustomShape> {
   // Defining flags
   override canEdit() {
      return true
   }

   // Defining event handlers
   override onResize(shape: CustomShape, info: TLResizeInfo<CustomShape>) {
      ...
   }
}
```

## Tools

**Before**
```ts
export class CustomShapeTool extends StateNode {
   // Defining child states
   static override children = (): TLStateNodeConstructor[] => [Idle, Pointing]
   
   // Defining event handlers

packages/editor/src/lib/editor/tools/BaseBoxShapeTool/children/Pointing.ts
@@ -2,7 +2,7 @@ import { createShapeId } from '@tldraw/tlschema'
 import { structuredClone } from '@tldraw/utils'
 import { Vec } from '../../../../primitives/Vec'
 import { TLBaseBoxShape } from '../../../shapes/BaseBoxShapeUtil'
-import { TLEventHandlers } from '../../../types/event-types'
+import { TLPointerEventInfo } from '../../../types/event-types'
 import { StateNode } from '../../StateNode'
 import { BaseBoxShapeTool } from '../BaseBoxShapeTool'
 
@@ -11,11 +11,11 @@ export class Pointing extends StateNode {
 
 	wasFocusedOnEnter = false
 
-	override onEnter = () => {
+	override onEnter() {
 		this.wasFocusedOnEnter = !this.editor.getIsMenuOpen()
 	}
 
-	override onPointerMove: TLEventHandlers['onPointerMove'] = (info) => {
+	override onPointerMove(info: TLPointerEventInfo) {
 		if (this.editor.inputs.isDragging) {
 			const { originPagePoint } = this.editor.inputs
 
@@ -55,19 +55,19 @@ export class Pointing extends StateNode {
 		}
 	}
 
-	override onPointerUp: TLEventHandlers['onPointerUp'] = () => {
+	override onPointerUp() {
 		this.complete()
 	}
 
-	override onCancel: TLEventHandlers['onCancel'] = () => {
+	override onCancel() {
 		this.cancel()
 	}
 
-	override onComplete: TLEventHandlers['onComplete'] = () => {
+	override onComplete() {
 		this.complete()
 	}
 
-	override onInterrupt: TLEventHandlers['onInterrupt'] = () => {
+	override onInterrupt() {
 		this.cancel()
 	}
 

packages/tldraw/src/lib/shapes/frame/FrameShapeTool.ts
@@ -1,4 +1,4 @@
-import { BaseBoxShapeTool, TLShape, TLShapeId } from '@tldraw/editor'
+import { BaseBoxShapeTool, TLShape, TLShapeId, bind } from '@tldraw/editor'
 
 /** @public */
 export class FrameShapeTool extends BaseBoxShapeTool {
@@ -6,7 +6,8 @@ export class FrameShapeTool extends BaseBoxShapeTool {
 	static override initial = 'idle'
 	override shapeType = 'frame'
 
-	override onCreate = (shape: TLShape | null): void => {
+	@bind
+	override onCreate(shape: TLShape | null): void {
 		if (!shape) return
 
 		const bounds = this.editor.getShapePageBounds(shape)!

packages/tldraw/src/lib/ui/components/Minimap/MinimapManager.ts
@@ -16,7 +16,9 @@ import { pie, rectangle, roundedRectangle } from './minimap-webgl-shapes'
 
 export class MinimapManager {
 	disposables = [] as (() => void)[]
-	close = () => this.disposables.forEach((d) => d())
+	close() {
+		return this.disposables.forEach((d) => d())
+	}
 	gl: ReturnType<typeof setupWebGl>
 	shapeGeometryCache: ComputedCache<Float32Array | null, TLShape>
 	constructor(
@@ -144,7 +146,7 @@ export class MinimapManager {
 		return new Float32Array([x, y, w, h])
 	}
 
-	getMinimapPagePoint = (clientX: number, clientY: number) => {
+	getMinimapPagePoint(clientX: number, clientY: number) {
 		const canvasPageBounds = this.getCanvasPageBounds()
 		const canvasScreenBounds = this.getCanvasScreenBounds()
 
@@ -163,12 +165,7 @@ export class MinimapManager {
 		return new Vec(x, y, 1)
 	}
 
-	minimapScreenPointToPagePoint = (
-		x: number,
-		y: number,
-		shiftKey = false,
-		clampToBounds = false
-	) => {
+	minimapScreenPointToPagePoint(x: number, y: number, shiftKey = false, clampToBounds = false) {
 		const { editor } = this
 		const vpPageBounds = editor.getViewportPageBounds()
 
@@ -208,7 +205,7 @@ export class MinimapManager {
 		return new Vec(px, py)
 	}
 
-	render = () => {
+	render() {
 		// make sure we update when dark mode switches
 		const context = this.gl.context
 		const canvasSize = this.getCanvasSize()

apps/examples/src/examples/screenshot-tool/ScreenshotTool/childStates/Pointing.ts
@@ -1,4 +1,4 @@
-import { StateNode, TLEventHandlers } from 'tldraw'
+import { StateNode } from 'tldraw'
 
 // There's a guide at the bottom of this file!
 
@@ -6,18 +6,18 @@ export class ScreenshotPointing extends StateNode {
 	static override id = 'pointing'
 
 	// [1]
-	override onPointerMove: TLEventHandlers['onPointerUp'] = () => {
+	override onPointerMove() {
 		if (this.editor.inputs.isDragging) {
 			this.parent.transition('dragging')
 		}
 	}
 
 	// [2]
-	override onPointerUp: TLEventHandlers['onPointerUp'] = () => {
+	override onPointerUp() {
 		this.complete()
 	}
 
-	override onCancel: TLEventHandlers['onCancel'] = () => {
+	override onCancel() {
 		this.complete()
 	}
 

packages/tldraw/src/lib/shapes/arrow/toolStates/Pointing.ts
@@ -1,4 +1,4 @@
-import { StateNode, TLArrowShape, TLEventHandlers, createShapeId } from '@tldraw/editor'
+import { StateNode, TLArrowShape, createShapeId } from '@tldraw/editor'
 
 export class Pointing extends StateNode {
 	static override id = 'pointing'
@@ -7,7 +7,7 @@ export class Pointing extends StateNode {
 
 	markId = ''
 
-	override onEnter = () => {
+	override onEnter() {
 		this.markId = ''
 		this.didTimeout = false
 
@@ -32,13 +32,13 @@ export class Pointing extends StateNode {
 		this.startPreciseTimeout()
 	}
 
-	override onExit = () => {
+	override onExit() {
 		this.shape = undefined
 		this.editor.setHintingShapes([])
 		this.clearPreciseTimeout()
 	}
 
-	override onPointerMove: TLEventHandlers['onPointerMove'] = () => {
+	override onPointerMove() {
 		if (this.editor.inputs.isDragging) {
 			if (!this.shape) {
 				this.createArrowShape()
@@ -59,19 +59,19 @@ export class Pointing extends StateNode {
 		}
 	}
 
-	override onPointerUp: TLEventHandlers['onPointerUp'] = () => {
+	override onPointerUp() {
 		this.cancel()
 	}
 
-	override onCancel: TLEventHandlers['onCancel'] = () => {
+	override onCancel() {
 		this.cancel()
 	}
 
-	override onComplete: TLEventHandlers['onComplete'] = () => {
+	override onComplete() {
 		this.cancel()
 	}
 
-	override onInterrupt: TLEventHandlers['onInterrupt'] = () => {
+	override onInterrupt() {
 		this.cancel()
 	}
 

packages/tldraw/src/lib/shapes/geo/toolStates/Pointing.ts
@@ -1,8 +1,8 @@
 import {
 	GeoShapeGeoStyle,
 	StateNode,
-	TLEventHandlers,
 	TLGeoShape,
+	TLPointerEventInfo,
 	Vec,
 	createShapeId,
 } from '@tldraw/editor'
@@ -10,11 +10,11 @@ import {
 export class Pointing extends StateNode {
 	static override id = 'pointing'
 
-	override onPointerUp: TLEventHandlers['onPointerUp'] = () => {
+	override onPointerUp() {
 		this.complete()
 	}
 
-	override onPointerMove: TLEventHandlers['onPointerMove'] = (info) => {
+	override onPointerMove(info: TLPointerEventInfo) {
 		if (this.editor.inputs.isDragging) {
 			const { originPagePoint } = this.editor.inputs
 
@@ -50,15 +50,15 @@ export class Pointing extends StateNode {
 		}
 	}
 
-	override onCancel: TLEventHandlers['onCancel'] = () => {
+	override onCancel() {
 		this.cancel()
 	}
 
-	override onComplete: TLEventHandlers['onComplete'] = () => {
+	override onComplete() {
 		this.complete()
 	}
 
-	override onInterrupt: TLEventHandlers['onInterrupt'] = () => {
+	override onInterrupt() {
 		this.cancel()
 	}
 

packages/tldraw/src/lib/shapes/line/toolStates/Pointing.ts
@@ -1,8 +1,6 @@
 import {
 	Mat,
 	StateNode,
-	TLEventHandlers,
-	TLInterruptEvent,
 	TLLineShape,
 	TLShapeId,
 	Vec,
@@ -22,7 +20,7 @@ export class Pointing extends StateNode {
 
 	markId: string | undefined
 
-	override onEnter = (info: { shapeId?: TLShapeId }) => {
+	override onEnter(info: { shapeId?: TLShapeId }) {
 		const { inputs } = this.editor
 		const { currentPagePoint } = inputs
 
@@ -104,7 +102,7 @@ export class Pointing extends StateNode {
 		}
 	}
 
-	override onPointerMove: TLEventHandlers['onPointerMove'] = () => {
+	override onPointerMove() {
 		if (!this.shape) return
 
 		if (this.editor.inputs.isDragging) {
@@ -125,19 +123,19 @@ export class Pointing extends StateNode {
 		}
 	}
 
-	override onPointerUp: TLEventHandlers['onPointerUp'] = () => {
+	override onPointerUp() {
 		this.complete()
 	}
 
-	override onCancel: TLEventHandlers['onCancel'] = () => {
+	override onCancel() {
 		this.cancel()
 	}
 
-	override onComplete: TLEventHandlers['onComplete'] = () => {
+	override onComplete() {
 		this.complete()
 	}
 
-	override onInterrupt: TLInterruptEvent = () => {
+	override onInterrupt() {
 		this.parent.transition('idle')
 		if (this.markId) this.editor.bailToMark(this.markId)
 		this.editor.snaps.clearIndicators()

packages/tldraw/src/lib/shapes/note/toolStates/Pointing.ts
@@ -1,8 +1,6 @@
 import {
 	Editor,
 	StateNode,
-	TLEventHandlers,
-	TLInterruptEvent,
 	TLNoteShape,
 	TLPointerEventInfo,
 	TLShapeId,
@@ -27,7 +25,7 @@ export class Pointing extends StateNode {
 
 	shape = {} as TLNoteShape
 
-	override onEnter = () => {
+	override onEnter() {
 		const { editor } = this
 
 		this.wasFocusedOnEnter = !editor.getIsMenuOpen()
@@ -50,7 +48,7 @@ export class Pointing extends StateNode {
 		}
 	}
 
-	override onPointerMove: TLEventHandlers['onPointerMove'] = (info) => {
+	override onPointerMove(info: TLPointerEventInfo) {
 		if (this.editor.inputs.isDragging) {
 			if (!this.wasFocusedOnEnter) {
 				const id = createShapeId()
@@ -81,19 +79,19 @@ export class Pointing extends StateNode {
 		}
 	}
 
-	override onPointerUp: TLEventHandlers['onPointerUp'] = () => {
+	override onPointerUp() {
 		this.complete()
 	}
 
-	override onInterrupt: TLInterruptEvent = () => {
+	override onInterrupt() {
 		this.cancel()
 	}
 
-	override onComplete: TLEventHandlers['onComplete'] = () => {
+	override onComplete() {
 		this.complete()
 	}
 
-	override onCancel: TLEventHandlers['onCancel'] = () => {
+	override onCancel() {
 		this.cancel()
 	}
 

commit b006c8a942f00d99f4794dfcbbdd39c00cb7eb9f

Fix an issue with minimap (#4318)
Fixes an issue with minimap caused by #4288. Also makes the library use
safer by handling some unbound calls in the library instead of relying
of end users to bind things.

### Change type

- [x] `bugfix`
- [ ] `improvement`
- [ ] `feature`
- [ ] `api`
- [ ] `other`


### Release notes
- Fixes a bug with the MiniMap not rendering. Makes the unbound uses
safer.

packages/editor/src/lib/editor/managers/SnapManager/HandleSnaps.ts
@@ -58,6 +58,12 @@ export class HandleSnaps {
 		const { editor } = this
 		return editor.store.createComputedCache('handle snap geometry', (shape: TLShape) => {
 			const snapGeometry = editor.getShapeUtil(shape).getHandleSnapGeometry(shape)
+			const getSelfSnapOutline = snapGeometry.getSelfSnapOutline
+				? snapGeometry.getSelfSnapOutline.bind(snapGeometry)
+				: defaultGetSelfSnapOutline
+			const getSelfSnapPoints = snapGeometry.getSelfSnapPoints
+				? snapGeometry.getSelfSnapPoints.bind(snapGeometry)
+				: defaultGetSelfSnapPoints
 
 			return {
 				outline:
@@ -66,8 +72,8 @@ export class HandleSnaps {
 						: snapGeometry.outline,
 
 				points: snapGeometry.points ?? [],
-				getSelfSnapOutline: snapGeometry.getSelfSnapOutline ?? defaultGetSelfSnapOutline,
-				getSelfSnapPoints: snapGeometry.getSelfSnapPoints ?? defaultGetSelfSnapPoints,
+				getSelfSnapOutline,
+				getSelfSnapPoints,
 			}
 		})
 	}

packages/editor/src/lib/editor/tools/BaseBoxShapeTool/children/Pointing.ts
@@ -1,4 +1,4 @@
-import { createShapeId } from '@tldraw/tlschema'
+import { TLShape, createShapeId } from '@tldraw/tlschema'
 import { structuredClone } from '@tldraw/utils'
 import { Vec } from '../../../../primitives/Vec'
 import { TLBaseBoxShape } from '../../../shapes/BaseBoxShapeUtil'
@@ -49,7 +49,7 @@ export class Pointing extends StateNode {
 					creatingMarkId,
 					creationCursorOffset: { x: 1, y: 1 },
 					onInteractionEnd: this.parent.id,
-					onCreate: (this.parent as BaseBoxShapeTool).onCreate,
+					onCreate: (shape: TLShape | null) => (this.parent as BaseBoxShapeTool).onCreate?.(shape),
 				} /** satisfies ResizingInfo, defined in main tldraw package 😧 */
 			)
 		}

packages/tldraw/src/lib/shapes/frame/FrameShapeTool.ts
@@ -1,4 +1,4 @@
-import { BaseBoxShapeTool, TLShape, TLShapeId, bind } from '@tldraw/editor'
+import { BaseBoxShapeTool, TLShape, TLShapeId } from '@tldraw/editor'
 
 /** @public */
 export class FrameShapeTool extends BaseBoxShapeTool {
@@ -6,7 +6,6 @@ export class FrameShapeTool extends BaseBoxShapeTool {
 	static override initial = 'idle'
 	override shapeType = 'frame'
 
-	@bind
 	override onCreate(shape: TLShape | null): void {
 		if (!shape) return
 

packages/tldraw/src/lib/ui/components/Minimap/MinimapManager.ts
@@ -5,6 +5,7 @@ import {
 	TLShape,
 	Vec,
 	atom,
+	bind,
 	clamp,
 	computed,
 	react,
@@ -16,6 +17,8 @@ import { pie, rectangle, roundedRectangle } from './minimap-webgl-shapes'
 
 export class MinimapManager {
 	disposables = [] as (() => void)[]
+
+	@bind
 	close() {
 		return this.disposables.forEach((d) => d())
 	}
@@ -205,6 +208,7 @@ export class MinimapManager {
 		return new Vec(px, py)
 	}
 
+	@bind
 	render() {
 		// make sure we update when dark mode switches
 		const context = this.gl.context

apps/dotcom/src/utils/multiplayerAssetStore.test.ts
@@ -8,7 +8,7 @@ describe('multiplayerAssetStore.resolve', () => {
 	it('should return null if the asset has no src', async () => {
 		const asset = { type: 'image', props: { w: 100, fileSize: FILE_SIZE } }
 		expect(
-			await resolver(asset as TLAsset, {
+			resolver(asset as TLAsset, {
 				screenScale: -1,
 				steppedScreenScale: 1,
 				dpr: 1,
@@ -24,7 +24,7 @@ describe('multiplayerAssetStore.resolve', () => {
 			props: { src: 'http://assets.tldraw.dev/video.mp4', fileSize: FILE_SIZE },
 		}
 		expect(
-			await resolver(asset as TLAsset, {
+			resolver(asset as TLAsset, {
 				screenScale: -1,
 				steppedScreenScale: 1,
 				dpr: 1,
@@ -40,7 +40,7 @@ describe('multiplayerAssetStore.resolve', () => {
 			props: { src: 'http://assets.not-tldraw.dev/video.mp4', fileSize: FILE_SIZE },
 		}
 		expect(
-			await resolver(asset as TLAsset, {
+			resolver(asset as TLAsset, {
 				screenScale: -1,
 				steppedScreenScale: 1,
 				dpr: 1,
@@ -56,7 +56,7 @@ describe('multiplayerAssetStore.resolve', () => {
 			props: { src: 'http://assets.tldraw.dev/image.jpg', fileSize: 1000 },
 		}
 		expect(
-			await resolver(asset as TLAsset, {
+			resolver(asset as TLAsset, {
 				screenScale: -1,
 				steppedScreenScale: 1,
 				dpr: 1,
@@ -69,7 +69,7 @@ describe('multiplayerAssetStore.resolve', () => {
 	it('should return the original src for if original is asked for', async () => {
 		const asset = { type: 'image', props: { src: 'http://assets.tldraw.dev/image.jpg', w: 100 } }
 		expect(
-			await resolver(asset as TLAsset, {
+			resolver(asset as TLAsset, {
 				screenScale: -1,
 				steppedScreenScale: 1,
 				dpr: 1,
@@ -82,7 +82,7 @@ describe('multiplayerAssetStore.resolve', () => {
 	it('should return the original src if it does not start with http or https', async () => {
 		const asset = { type: 'image', props: { src: 'data:somedata', w: 100, fileSize: FILE_SIZE } }
 		expect(
-			await resolver(asset as TLAsset, {
+			resolver(asset as TLAsset, {
 				screenScale: -1,
 				steppedScreenScale: 1,
 				dpr: 1,
@@ -103,7 +103,7 @@ describe('multiplayerAssetStore.resolve', () => {
 			},
 		}
 		expect(
-			await resolver(asset as TLAsset, {
+			resolver(asset as TLAsset, {
 				screenScale: -1,
 				steppedScreenScale: 1,
 				dpr: 1,
@@ -124,7 +124,7 @@ describe('multiplayerAssetStore.resolve', () => {
 			},
 		}
 		expect(
-			await resolver(asset as TLAsset, {
+			resolver(asset as TLAsset, {
 				screenScale: -1,
 				steppedScreenScale: 1,
 				dpr: 1,
@@ -140,7 +140,7 @@ describe('multiplayerAssetStore.resolve', () => {
 			props: { src: 'http://assets.tldraw.dev/doc.pdf', w: 100, fileSize: FILE_SIZE },
 		}
 		expect(
-			await resolver(asset as TLAsset, {
+			resolver(asset as TLAsset, {
 				screenScale: -1,
 				steppedScreenScale: 1,
 				dpr: 1,
@@ -156,7 +156,7 @@ describe('multiplayerAssetStore.resolve', () => {
 			props: { src: 'http://assets.tldraw.dev/image.jpg', w: 100, fileSize: FILE_SIZE },
 		}
 		expect(
-			await resolver(asset as TLAsset, {
+			resolver(asset as TLAsset, {
 				screenScale: -1,
 				steppedScreenScale: 0.5,
 				dpr: 2,
@@ -172,7 +172,7 @@ describe('multiplayerAssetStore.resolve', () => {
 			props: { src: 'http://assets.tldraw.dev/image.jpg', w: 100, fileSize: FILE_SIZE },
 		}
 		expect(
-			await resolver(asset as TLAsset, {
+			resolver(asset as TLAsset, {
 				screenScale: -1,
 				steppedScreenScale: 0.5,
 				dpr: 2,
@@ -188,7 +188,7 @@ describe('multiplayerAssetStore.resolve', () => {
 			props: { src: 'https://assets.tldraw.dev/image.jpg', w: 100, fileSize: FILE_SIZE },
 		}
 		expect(
-			await resolver(asset as TLAsset, {
+			resolver(asset as TLAsset, {
 				screenScale: -1,
 				steppedScreenScale: 5,
 				dpr: 1,