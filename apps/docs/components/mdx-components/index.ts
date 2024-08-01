4252f51dfa1c7e7d354a5d915d71608c1b3017e2
Mime Čuvalo <mimecuvalo@gmail.com>
2024-08-01 17:36:44 +0100

    docs: add license check tool

--- apps/docs/components/mdx-components/index.ts
+++ apps/docs/components/mdx-components/index.ts
@@ -27,6 +27,7 @@ import {
 	UnorderedList,
 	Video,
 } from './generic'
+import { VerifyLicense } from './license'
 
 export const components = {
 	a: A,
@@ -58,6 +59,7 @@ export const components = {
 	Small: Small,
 	Video,
 	Callout,
+	VerifyLicense,
 	...customComponents,
 	...apiComponents,
 }
