4b31b8f99606d898f2f5d82ba575cbb90d87e44f
Lukas Wiesehan <mail@lukaswiesehan.de>
2024-08-02 12:27:46 +0200

    Add docs search

--- apps/docs/scripts/update-algolia-index.ts
+++ apps/docs/scripts/update-algolia-index.ts
@@ -1,27 +1,36 @@
 import { getAllArticles } from '@/utils/get-all-articles'
 import algoliasearch from 'algoliasearch'
-import { remark } from 'remark'
-import mdx from 'remark-mdx'
+
+const sectionPriority = {
+	'getting-started': 0,
+	docs: 1,
+	examples: 2,
+	reference: 3,
+	releases: 4,
+	community: 5,
+}
 
 const updateAlgoliaIndex = async () => {
 	try {
-		const client = algoliasearch('KKH5LYXYV9', process.env.ALGOLIA_API_KEY!)
-		const strip = require('remark-mdx-to-plain-text')
-
-		const { blog, docs } = await getAllArticles()
+		const { docs, blog } = await getAllArticles()
+		const client = algoliasearch(
+			process.env.NEXT_PUBLIC_ALGOLIA_APP_ID!,
+			process.env.ALGOLIA_API_KEY!
+		)
 
 		console.log(`Indexing ${docs.length} docs articles...`)
 		const docsIndex = client.initIndex('docs')
-		const res = await docsIndex.replaceAllObjects(
-			docs.map((article) => ({
-				objectID: article.path,
-				title: article.title,
-				description: article.description,
-				content: remark().use(mdx).use(strip).process(article.content),
-			}))
+		await docsIndex.replaceAllObjects(
+			// @ts-ignore
+			docs.map((article) => ({ ...article, sectionPriority: sectionPriority[article.sectionId] })),
+			{ autoGenerateObjectIDIfNotExist: true }
 		)
+		console.log('Done.')
 
-		console.log('Done.', res)
+		console.log(`Indexing ${blog.length} blog posts...`)
+		const blogIndex = client.initIndex('blog')
+		await blogIndex.replaceAllObjects(blog, { autoGenerateObjectIDIfNotExist: true })
+		console.log('Done.')
 	} catch (error) {
 		console.log(error)
 	}
--- apps/docs/utils/get-all-articles.ts
+++ apps/docs/utils/get-all-articles.ts
@@ -2,7 +2,25 @@ import { getDb } from './ContentDatabase'
 
 export const getAllArticles = async () => {
 	const db = await getDb()
-	const docs = await db.db.all(`SELECT * FROM articles WHERE articles.sectionId != 'blog'`)
-	const blog = await db.db.all(`SELECT * FROM articles WHERE articles.sectionId = 'blog'`)
-	return { blog, docs }
+
+	const articles = await db.db.all(
+		`SELECT title, description, path, id, sectionId, sectionIndex AS articleIndex,
+		(SELECT title FROM sections WHERE sections.id = articles.sectionId) AS section,
+		(SELECT title FROM categories WHERE categories.id = articles.categoryId) AS category 
+		FROM articles`
+	)
+	const articlesWithHeadings = await Promise.all(
+		articles.map(async (article) => {
+			const headings = await db.getArticleHeadings(article.id)
+			return {
+				...article,
+				joinedHeadings: headings.map(({ title }) => title).join(' | '),
+			}
+		})
+	)
+
+	return {
+		docs: articlesWithHeadings.filter((article) => article.sectionId !== 'blog'),
+		blog: articlesWithHeadings.filter((article) => article.sectionId === 'blog'),
+	}
 }
