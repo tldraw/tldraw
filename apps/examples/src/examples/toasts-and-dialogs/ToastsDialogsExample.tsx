ceb1d3d3149c776722535624255cef341be3eba6
Steve Ruiz <steveruizok@gmail.com>
2024-08-04 13:40:43 +0100

    Update ToastsDialogsExample.tsx

--- apps/examples/src/examples/toasts-and-dialogs/ToastsDialogsExample.tsx
+++ apps/examples/src/examples/toasts-and-dialogs/ToastsDialogsExample.tsx
@@ -1,14 +1,48 @@
-import { TLComponents, Tldraw, useDialogs, useToasts } from 'tldraw'
+import {
+	TLComponents,
+	Tldraw,
+	TldrawUiButton,
+	TldrawUiButtonLabel,
+	TldrawUiDialogBody,
+	TldrawUiDialogCloseButton,
+	TldrawUiDialogFooter,
+	TldrawUiDialogHeader,
+	TldrawUiDialogTitle,
+	useDialogs,
+	useToasts,
+} from 'tldraw'
 import 'tldraw/tldraw.css'
 
 // There's a guide at the bottom of this file
 
+// [1]
 function MyDialog({ onClose }: { onClose: () => void }) {
 	return (
-		<div>
-			<h1>My Dialog Title</h1>
-			<p>My dialog body</p>
-			<button onClick={onClose}>close</button>
+		<>
+			<TldrawUiDialogHeader>
+				<TldrawUiDialogTitle>Title</TldrawUiDialogTitle>
+				<TldrawUiDialogCloseButton />
+			</TldrawUiDialogHeader>
+			<TldrawUiDialogBody style={{ maxWidth: 350 }}>Description...</TldrawUiDialogBody>
+			<TldrawUiDialogFooter className="tlui-dialog__footer__actions">
+				<TldrawUiButton type="normal" onClick={onClose}>
+					<TldrawUiButtonLabel>Cancel</TldrawUiButtonLabel>
+				</TldrawUiButton>
+				<TldrawUiButton type="primary" onClick={onClose}>
+					<TldrawUiButtonLabel>Continue</TldrawUiButtonLabel>
+				</TldrawUiButton>
+			</TldrawUiDialogFooter>
+		</>
+	)
+}
+
+// [2]
+function MySimpleDialog({ onClose }: { onClose: () => void }) {
+	return (
+		<div style={{ padding: 16 }}>
+			<h2>Title</h2>
+			<p>Description...</p>
+			<button onClick={onClose}>Okay</button>
 		</div>
 	)
 }
@@ -18,26 +52,48 @@ const CustomSharePanel = () => {
 	const { addDialog } = useDialogs()
 
 	return (
-		<button
-			style={{ fontSize: 18, backgroundColor: 'thistle', pointerEvents: 'all' }}
-			onClick={() => {
-				addToast({ title: 'Hello world!', severity: 'success' })
-				addDialog({
-					component: ({ onClose }) => <MyDialog onClose={() => onClose()} />,
-					onClose: () => {
-						// You can do something after the dialog is closed
-						void null
-					},
-				})
-			}}
-		>
-			Show toast/dialog
-		</button>
+		<div style={{ padding: 16, gap: 16, display: 'flex', pointerEvents: 'all' }}>
+			<button
+				onClick={() => {
+					addToast({ title: 'Hello world!', severity: 'success' })
+				}}
+			>
+				Show toast
+			</button>
+			<button
+				onClick={() => {
+					addDialog({
+						component: MyDialog,
+						onClose: () => {
+							// You can do something after the dialog is closed
+							void null
+						},
+					})
+				}}
+			>
+				Show dialog
+			</button>
+			<button
+				onClick={() => {
+					addDialog({
+						component: MySimpleDialog,
+						onClose: () => {
+							// You can do something after the dialog is closed
+							void null
+						},
+					})
+				}}
+			>
+				Show simple dialog
+			</button>
+		</div>
 	)
 }
+
 const components: TLComponents = {
 	SharePanel: CustomSharePanel,
 }
+
 export default function ToastsDialogsExample() {
 	return (
 		<div className="tldraw__editor">
@@ -56,4 +112,11 @@ Dialogs are especially customisable, allowing you to pass in a custom component 
 as the dialog content. Alternatively, you can use the `ExampleDialog` component which is
 provided by the library.
 
+[1]
+The tldraw library provides a set of components that you can use to build your dialogs. 
+The `onClose` function passed to the dialog component runs when the dialog closes or 
+is dismissed, but you can also call it from buttons to close the dialog.
+
+[2]
+...or you can build your own dialog component!
 */
