7d4d06fd8b47ca49dd8222b520f011f7735fd82d
Steve Ruiz <steveruizok@gmail.com>
2024-08-05 18:27:06 +0100

    Update page.tsx

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -3,14 +3,13 @@ import { DetailsSection } from '@/components/marketing/details-section'
 import { FeaturesSection } from '@/components/marketing/features-section'
 import { HeroSection } from '@/components/marketing/hero-section'
 import { InstallationSection } from '@/components/marketing/installation-section'
-import { LogoSection } from '@/components/marketing/logo-section'
 import { PricingSection } from '@/components/marketing/pricing-section'
 
 export default function Page() {
 	return (
 		<>
 			<HeroSection />
-			<LogoSection />
+			{/* <LogoSection /> */}
 			<FeaturesSection />
 			<DetailsSection />
 			<InstallationSection />
