c7715c4cb29b40944f1c46f4a81f31c3ada73d0f
Mitja Bezenšek <mitja.bezensek@gmail.com>
2024-08-08 13:38:12 +0200

    Parametrize.

--- packages/tldraw/src/lib/shapes/timer/useTimer.ts
+++ packages/tldraw/src/lib/shapes/timer/useTimer.ts
@@ -1,6 +1,9 @@
 import { TLTimerState, useEditor } from '@tldraw/editor'
 import { useEffect, useRef, useState } from 'react'
 
+const UPDATES_PER_SECOND = 10
+const TIMEOUT = 1000 / UPDATES_PER_SECOND
+
 export function useTimer(state: TLTimerState) {
 	const editor = useEditor()
 	const [time, setTime] = useState(0)
@@ -8,7 +11,7 @@ export function useTimer(state: TLTimerState) {
 	if (state === 'running') {
 		timeoutRef.current = editor.timers.setTimeout(() => {
 			return setTime(time + 1)
-		}, 1000)
+		}, TIMEOUT)
 	} else if (state === 'paused' || state === 'stopped') {
 		clearTimeout(timeoutRef.current as any)
 	}
