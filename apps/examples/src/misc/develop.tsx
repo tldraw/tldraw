bcaaecbe53761afdeddacfb2688aa73e55bd3a25
Mitja Bezenšek <mitja.bezensek@gmail.com>
2024-08-08 09:19:33 +0200

    Make initial timer work.

--- apps/examples/src/misc/develop.tsx
+++ apps/examples/src/misc/develop.tsx
@@ -11,6 +11,7 @@ export default function Develop() {
 				licenseKey={getLicenseKey()}
 				overrides={[performanceOverrides]}
 				persistenceKey="example"
+				initialState="timer"
 				onMount={(editor) => {
 					;(window as any).app = editor
 					;(window as any).editor = editor
--- packages/tlschema/src/shapes/TLTimerShape.ts
+++ packages/tlschema/src/shapes/TLTimerShape.ts
@@ -1,11 +1,12 @@
+import { T } from '@tldraw/validate'
 import { createShapePropsMigrationSequence } from '../records/TLShape'
 import { RecordProps } from '../recordsWithProps'
-import { DefaultColorStyle, TLDefaultColorStyle } from '../styles/TLColorStyle'
 import { TLBaseShape } from './TLBaseShape'
 
 /** @public */
 export interface TLTimerShapeProps {
-	color: TLDefaultColorStyle
+	time: number
+	state: string
 }
 
 /** @public */
@@ -13,7 +14,8 @@ export type TLTimerShape = TLBaseShape<'timer', TLTimerShapeProps>
 
 /** @public */
 export const timerShapeProps: RecordProps<TLTimerShape> = {
-	color: DefaultColorStyle,
+	time: T.number,
+	state: T.string,
 }
 
 /** @public */
