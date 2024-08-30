ffe24d5d93258d6cbc44f54a6a4c3708232c5b6f
David Sheldrick <d.j.sheldrick@gmail.com>
2024-08-30 13:21:02 +0100

    faster dot rendering

--- packages/editor/src/lib/components/default-components/DefaultGrid.tsx
+++ packages/editor/src/lib/components/default-components/DefaultGrid.tsx
@@ -50,6 +50,11 @@ export function DefaultGrid({ size, type, ...camera }: TLGridProps) {
 		const numRows = Math.round((endPageY - startPageY) / size)
 		const numCols = Math.round((endPageX - startPageX) / size)
 
+		const startMajorX = Math.ceil(pageViewportBounds.minX / (size * 10)) * size * 10
+		const startMajorY = Math.ceil(pageViewportBounds.minY / (size * 10)) * size * 10
+		const numMajorRows = Math.round((endPageY - startMajorY) / (size * 10))
+		const numMajorCols = Math.round((endPageX - startMajorX) / (size * 10))
+
 		const props = {
 			ctx,
 			camera,
@@ -61,6 +66,10 @@ export function DefaultGrid({ size, type, ...camera }: TLGridProps) {
 			startPageY,
 			canvasW,
 			canvasH,
+			startMajorX,
+			startMajorY,
+			numMajorRows,
+			numMajorCols,
 		}
 
 		switch (type) {
@@ -95,6 +104,10 @@ interface DrawStuffProps {
 	startPageY: number
 	canvasW: number
 	canvasH: number
+	startMajorX: number
+	startMajorY: number
+	numMajorRows: number
+	numMajorCols: number
 }
 
 function drawGridStyleGrid(
@@ -122,15 +135,41 @@ function drawGridStyleGrid(
 		clamp(invLerp(0.3, 0.6, camera.z), 0, 1)
 	)
 
-	// [6]
+	const start = performance.now()
+	if (minorLineOpacity > 0) {
+		// draw minor lines first
+		ctx.beginPath()
+		ctx.globalAlpha = minorLineOpacity
+		ctx.lineWidth = 1
+		for (let row = 0; row <= numRows; row++) {
+			const pageY = startPageY + row * size
+			// convert the page-space Y offset into our canvas' coordinate space
+			const isMajorLine = approximately(pageY % (size * 10), 0)
+			if (isMajorLine) continue
+			const canvasY = (pageY + camera.y) * camera.z * devicePixelRatio
+			drawLine(ctx, 0, canvasY, canvasW, canvasY)
+		}
+		for (let col = 0; col <= numCols; col++) {
+			const pageX = startPageX + col * size
+			// convert the page-space X offset into our canvas' coordinate space
+			const isMajorLine = approximately(pageX % (size * 10), 0)
+			if (isMajorLine) continue
+			const canvasX = (pageX + camera.x) * camera.z * devicePixelRatio
+			drawLine(ctx, canvasX, 0, canvasX, canvasH)
+		}
+		ctx.stroke()
+	}
+
+	ctx.beginPath()
+	ctx.globalAlpha = 1
+	ctx.lineWidth = majorLineStrokeWidth
 	for (let row = 0; row <= numRows; row++) {
 		const pageY = startPageY + row * size
 		// convert the page-space Y offset into our canvas' coordinate space
 		const canvasY = (pageY + camera.y) * camera.z * devicePixelRatio
 		const isMajorLine = approximately(pageY % (size * 10), 0)
-		if (minorLineOpacity === 0 && !isMajorLine) continue
-		ctx.globalAlpha = isMajorLine ? 1 : minorLineOpacity
-		drawLine(ctx, 0, canvasY, canvasW, canvasY, isMajorLine ? majorLineStrokeWidth : 1)
+		if (!isMajorLine) continue
+		drawLine(ctx, 0, canvasY, canvasW, canvasY)
 	}
 	if (!drawColumns) return
 	for (let col = 0; col <= numCols; col++) {
@@ -138,25 +177,16 @@ function drawGridStyleGrid(
 		// convert the page-space X offset into our canvas' coordinate space
 		const canvasX = (pageX + camera.x) * camera.z * devicePixelRatio
 		const isMajorLine = approximately(pageX % (size * 10), 0)
-		if (minorLineOpacity === 0 && !isMajorLine) continue
-		ctx.globalAlpha = isMajorLine ? 1 : minorLineOpacity
-		drawLine(ctx, canvasX, 0, canvasX, canvasH, isMajorLine ? majorLineStrokeWidth : 1)
+		if (!isMajorLine) continue
+		drawLine(ctx, canvasX, 0, canvasX, canvasH)
 	}
+	ctx.stroke()
+	console.log('drawGridStyleGrid', performance.now() - start)
 }
 
-function drawLine(
-	ctx: CanvasRenderingContext2D,
-	x1: number,
-	y1: number,
-	x2: number,
-	y2: number,
-	width: number
-) {
-	ctx.beginPath()
+function drawLine(ctx: CanvasRenderingContext2D, x1: number, y1: number, x2: number, y2: number) {
 	ctx.moveTo(x1, y1)
 	ctx.lineTo(x2, y2)
-	ctx.lineWidth = width
-	ctx.stroke()
 }
 
 function drawDotStyleGrid({
@@ -168,33 +198,95 @@ function drawDotStyleGrid({
 	size,
 	startPageX,
 	startPageY,
+	startMajorX,
+	startMajorY,
+	numMajorCols,
+	numMajorRows,
 }: DrawStuffProps) {
 	const minorDotOpacity = clamp(invLerp(0.5, 1, camera.z), 0, 1)
 	const majorDotRadius = lerp(2, 4, clamp(invLerp(0.3, 0.6, camera.z), 0, 1))
-	ctx.fillStyle = isDarkMode ? '#555' : '#BBB'
 
 	const start = performance.now()
-	let iters = 0
-	for (let row = 0; row <= numRows; row++) {
-		const pageY = startPageY + row * size
-		const canvasY = (pageY + camera.y) * camera.z * devicePixelRatio
-		const isMajorRowDot = approximately(pageY % (size * 10), 0)
-		if (minorDotOpacity === 0 && !isMajorRowDot) continue
-		for (let col = 0; col <= numCols; col++) {
-			iters++
-			const pageX = startPageX + col * size
-			const isMajorDot = isMajorRowDot && approximately(pageX % (size * 10), 0)
-			const canvasX = (pageX + camera.x) * camera.z * devicePixelRatio
-			ctx.globalAlpha = isMajorDot ? 1 : minorDotOpacity
-			if (minorDotOpacity === 0 && !isMajorDot) continue
-			drawDot(ctx, canvasX, canvasY, isMajorDot ? majorDotRadius : 2)
-		}
-	}
+	const color = isDarkMode ? '#555' : '#BBB'
 
-	console.log('drawDotStyleGrid', performance.now() - start, iters)
+	if (minorDotOpacity > 0) {
+		const x = (startPageX + camera.x) * camera.z * devicePixelRatio
+		const y = (startPageY + camera.y) * camera.z * devicePixelRatio
+		drawDots({
+			color,
+			opacity: minorDotOpacity,
+			rows: numRows,
+			cols: numCols,
+			spacing: size * camera.z * devicePixelRatio,
+			ctx,
+			radius: 2,
+			x,
+			y,
+		})
+	}
+	const x = (startMajorX + camera.x) * camera.z * devicePixelRatio
+	const y = (startMajorY + camera.y) * camera.z * devicePixelRatio
+
+	drawDots({
+		color,
+		opacity: 1,
+		radius: majorDotRadius,
+		x,
+		y,
+		rows: numMajorRows,
+		cols: numMajorCols,
+		spacing: size * 10 * camera.z * devicePixelRatio,
+		ctx,
+	})
+	console.log('drawDotStyleGrid', performance.now() - start)
 }
 
-function drawDot(ctx: CanvasRenderingContext2D, x: number, y: number, radius: number) {
-	// draw a rect instead
-	ctx.fillRect(x - radius, y - radius, radius * 2, radius * 2)
+const transformPath = (path: Path2D, matrix: DOMMatrix) => {
+	const copy = new Path2D()
+	copy.addPath(path, matrix)
+	return copy
+}
+
+function drawDots({
+	ctx,
+	x,
+	y,
+	radius,
+	rows,
+	cols,
+	spacing,
+	color,
+	opacity,
+}: {
+	ctx: CanvasRenderingContext2D
+	x: number
+	y: number
+	radius: number
+	rows: number
+	cols: number
+	spacing: number
+	color: string
+	opacity: number
+}) {
+	const MAX_TILE_SIZE = 50
+	ctx.fillStyle = color
+	ctx.globalAlpha = opacity
+
+	const path = new Path2D()
+
+	for (let row = 0; row < Math.min(rows, MAX_TILE_SIZE); row++) {
+		for (let col = 0; col < Math.min(cols, MAX_TILE_SIZE); col++) {
+			path.moveTo(x + col * spacing + radius, y + row * spacing)
+			path.arc(x + col * spacing, y + row * spacing, radius, 0, Math.PI * 2)
+		}
+	}
+	for (let tileRow = 0; tileRow < Math.ceil(rows / MAX_TILE_SIZE); tileRow++) {
+		for (let tileCol = 0; tileCol < Math.ceil(cols / MAX_TILE_SIZE); tileCol++) {
+			// translate to the next tile
+			const matrix = new DOMMatrix()
+			matrix.translateSelf(tileCol * MAX_TILE_SIZE * spacing, tileRow * MAX_TILE_SIZE * spacing)
+
+			ctx.fill(transformPath(path, matrix))
+		}
+	}
 }
