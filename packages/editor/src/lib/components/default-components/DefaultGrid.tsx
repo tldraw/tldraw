37fadfab48c15ebc737e395cb5ea881b760669fe
David Sheldrick <d.j.sheldrick@gmail.com>
2024-09-02 08:34:25 +0100

    wip stuff i dunno

--- packages/editor/src/lib/components/default-components/DefaultGrid.tsx
+++ packages/editor/src/lib/components/default-components/DefaultGrid.tsx
@@ -4,7 +4,7 @@ import { exhaustiveSwitchError, invLerp, lerp } from '@tldraw/utils'
 import { useLayoutEffect, useMemo, useRef } from 'react'
 import { useEditor } from '../../hooks/useEditor'
 import { useIsDarkMode } from '../../hooks/useIsDarkMode'
-import { approximately, clamp } from '../../primitives/utils'
+import { clamp } from '../../primitives/utils'
 
 /** @public */
 export interface TLGridProps {
@@ -14,6 +14,8 @@ export interface TLGridProps {
 	size: number
 	type: TLGridType
 }
+
+const MAJOR_ORDER = 5
 
 /** @public @react */
 export function DefaultGrid({ size, type, ...camera }: TLGridProps) {
@@ -43,33 +45,56 @@ export function DefaultGrid({ size, type, ...camera }: TLGridProps) {
 		// [5]
 		const pageViewportBounds = editor.getViewportPageBounds()
 
-		const startPageX = Math.ceil(pageViewportBounds.minX / size) * size
-		const startPageY = Math.ceil(pageViewportBounds.minY / size) * size
-		const endPageX = Math.floor(pageViewportBounds.maxX / size) * size
-		const endPageY = Math.floor(pageViewportBounds.maxY / size) * size
-		const numRows = Math.round((endPageY - startPageY) / size)
-		const numCols = Math.round((endPageX - startPageX) / size)
+		let minorSize = size
+		let sizeInScreenSpace = minorSize * camera.z
+		while (sizeInScreenSpace > 30) {
+			minorSize = minorSize / MAJOR_ORDER
+			sizeInScreenSpace = minorSize * camera.z
+		}
+		while (sizeInScreenSpace < 3) {
+			minorSize = minorSize * MAJOR_ORDER
+			sizeInScreenSpace = minorSize * camera.z
+		}
 
-		const startMajorX = Math.ceil(pageViewportBounds.minX / (size * 10)) * size * 10
-		const startMajorY = Math.ceil(pageViewportBounds.minY / (size * 10)) * size * 10
-		const numMajorRows = Math.round((endPageY - startMajorY) / (size * 10))
-		const numMajorCols = Math.round((endPageX - startMajorX) / (size * 10))
+		const sizeInCanvasSpace = sizeInScreenSpace * devicePixelRatio
 
-		const props = {
+		const minorOpacity = clamp(invLerp(3, 6, sizeInScreenSpace), 0, 1)
+
+		const pageToCanvasX = (x: number) => (x + camera.x) * camera.z * devicePixelRatio
+		const pageToCanvasY = (y: number) => (y + camera.y) * camera.z * devicePixelRatio
+
+		const minor: GridSpec = {
+			startX: pageToCanvasX(Math.ceil(pageViewportBounds.minX / minorSize) * minorSize),
+			startY: pageToCanvasY(Math.ceil(pageViewportBounds.minY / minorSize) * minorSize),
+			endX: pageToCanvasX(Math.floor(pageViewportBounds.maxX / minorSize) * minorSize),
+			endY: pageToCanvasY(Math.floor(pageViewportBounds.maxY / minorSize) * minorSize),
+			rows: Math.round((pageViewportBounds.maxY - pageViewportBounds.minY) / minorSize),
+			cols: Math.round((pageViewportBounds.maxX - pageViewportBounds.minX) / minorSize),
+			opacity: minorOpacity,
+			spacing: minorSize * camera.z * devicePixelRatio,
+		}
+
+		const majorSize = minorSize * MAJOR_ORDER
+
+		const major: GridSpec = {
+			startX: pageToCanvasX(Math.ceil(pageViewportBounds.minX / majorSize) * majorSize),
+			startY: pageToCanvasY(Math.ceil(pageViewportBounds.minY / majorSize) * majorSize),
+			endX: pageToCanvasX(Math.floor(pageViewportBounds.maxX / majorSize) * majorSize),
+			endY: pageToCanvasY(Math.floor(pageViewportBounds.maxY / majorSize) * majorSize),
+			rows: Math.round((pageViewportBounds.maxY - pageViewportBounds.minY) / majorSize),
+			cols: Math.round((pageViewportBounds.maxX - pageViewportBounds.minX) / majorSize),
+			opacity: 1,
+			spacing: majorSize * camera.z * devicePixelRatio,
+		}
+
+		const props: DrawStuffProps = {
 			ctx,
 			camera,
-			size,
 			isDarkMode,
-			numRows,
-			numCols,
-			startPageX,
-			startPageY,
 			canvasW,
 			canvasH,
-			startMajorX,
-			startMajorY,
-			numMajorRows,
-			numMajorCols,
+			major,
+			minor,
 		}
 
 		switch (type) {
@@ -93,95 +118,86 @@ export function DefaultGrid({ size, type, ...camera }: TLGridProps) {
 	return useMemo(() => <canvas className="tl-grid" ref={canvas} />, [])
 }
 
+interface GridSpec {
+	startX: number
+	startY: number
+	endX: number
+	endY: number
+	rows: number
+	cols: number
+	opacity: number
+	spacing: number
+}
+
 interface DrawStuffProps {
 	ctx: CanvasRenderingContext2D
 	camera: { x: number; y: number; z: number }
-	size: number
 	isDarkMode: boolean
-	numRows: number
-	numCols: number
-	startPageX: number
-	startPageY: number
 	canvasW: number
 	canvasH: number
-	startMajorX: number
-	startMajorY: number
-	numMajorRows: number
-	numMajorCols: number
+	major: GridSpec
+	minor: GridSpec
 }
 
 function drawGridStyleGrid(
-	{
-		ctx,
-		camera,
-		size,
-		isDarkMode,
-		numRows,
-		numCols,
-		startPageX,
-		startPageY,
-		canvasW,
-		canvasH,
-	}: DrawStuffProps,
+	{ ctx, camera, size, isDarkMode, canvasW, canvasH, minor, major }: DrawStuffProps,
 	drawColumns: boolean,
 	majorLineStrokeWidthMax: number
 ) {
-	// todo: extract styles from dom like we do for minimap
-	ctx.strokeStyle = isDarkMode ? '#555' : '#BBB'
-	const minorLineOpacity = clamp(invLerp(0.3, 0.6, camera.z), 0, 1)
-	const majorLineStrokeWidth = lerp(
-		1,
-		majorLineStrokeWidthMax,
-		clamp(invLerp(0.3, 0.6, camera.z), 0, 1)
-	)
-
-	const start = performance.now()
-	if (minorLineOpacity > 0) {
-		// draw minor lines first
-		ctx.beginPath()
-		ctx.globalAlpha = minorLineOpacity
-		ctx.lineWidth = 1
-		for (let row = 0; row <= numRows; row++) {
-			const pageY = startPageY + row * size
-			// convert the page-space Y offset into our canvas' coordinate space
-			const isMajorLine = approximately(pageY % (size * 10), 0)
-			if (isMajorLine) continue
-			const canvasY = (pageY + camera.y) * camera.z * devicePixelRatio
-			drawLine(ctx, 0, canvasY, canvasW, canvasY)
-		}
-		for (let col = 0; col <= numCols; col++) {
-			const pageX = startPageX + col * size
-			// convert the page-space X offset into our canvas' coordinate space
-			const isMajorLine = approximately(pageX % (size * 10), 0)
-			if (isMajorLine) continue
-			const canvasX = (pageX + camera.x) * camera.z * devicePixelRatio
-			drawLine(ctx, canvasX, 0, canvasX, canvasH)
-		}
-		ctx.stroke()
-	}
-
-	ctx.beginPath()
-	ctx.globalAlpha = 1
-	ctx.lineWidth = majorLineStrokeWidth
-	for (let row = 0; row <= numRows; row++) {
-		const pageY = startPageY + row * size
-		// convert the page-space Y offset into our canvas' coordinate space
-		const canvasY = (pageY + camera.y) * camera.z * devicePixelRatio
-		const isMajorLine = approximately(pageY % (size * 10), 0)
-		if (!isMajorLine) continue
-		drawLine(ctx, 0, canvasY, canvasW, canvasY)
-	}
-	if (!drawColumns) return
-	for (let col = 0; col <= numCols; col++) {
-		const pageX = startPageX + col * size
-		// convert the page-space X offset into our canvas' coordinate space
-		const canvasX = (pageX + camera.x) * camera.z * devicePixelRatio
-		const isMajorLine = approximately(pageX % (size * 10), 0)
-		if (!isMajorLine) continue
-		drawLine(ctx, canvasX, 0, canvasX, canvasH)
-	}
-	ctx.stroke()
-	console.log('drawGridStyleGrid', performance.now() - start)
+	// // todo: extract styles from dom like we do for minimap
+	// ctx.strokeStyle = isDarkMode ? '#555' : '#BBB'
+	// const minorLineOpacity = clamp(invLerp(0.3, 0.6, camera.z), 0, 1)
+	// const majorLineStrokeWidth = lerp(
+	// 	1,
+	// 	majorLineStrokeWidthMax,
+	// 	clamp(invLerp(0.3, 0.6, camera.z), 0, 1)
+	// )
+	// const start = performance.now()
+	// if (minorLineOpacity > 0) {
+	// 	// draw minor lines first
+	// 	ctx.beginPath()
+	// 	ctx.globalAlpha = minorLineOpacity
+	// 	ctx.lineWidth = 1
+	// 	for (let row = 0; row <= numRows; row++) {
+	// 		const pageY = startPageY + row * size
+	// 		// convert the page-space Y offset into our canvas' coordinate space
+	// 		const isMajorLine = approximately(pageY % (size * 10), 0)
+	// 		if (isMajorLine) continue
+	// 		const canvasY = (pageY + camera.y) * camera.z * devicePixelRatio
+	// 		drawLine(ctx, 0, canvasY, canvasW, canvasY)
+	// 	}
+	// 	for (let col = 0; col <= numCols; col++) {
+	// 		const pageX = startPageX + col * size
+	// 		// convert the page-space X offset into our canvas' coordinate space
+	// 		const isMajorLine = approximately(pageX % (size * 10), 0)
+	// 		if (isMajorLine) continue
+	// 		const canvasX = (pageX + camera.x) * camera.z * devicePixelRatio
+	// 		drawLine(ctx, canvasX, 0, canvasX, canvasH)
+	// 	}
+	// 	ctx.stroke()
+	// }
+	// ctx.beginPath()
+	// ctx.globalAlpha = 1
+	// ctx.lineWidth = majorLineStrokeWidth
+	// for (let row = 0; row <= numRows; row++) {
+	// 	const pageY = startPageY + row * size
+	// 	// convert the page-space Y offset into our canvas' coordinate space
+	// 	const canvasY = (pageY + camera.y) * camera.z * devicePixelRatio
+	// 	const isMajorLine = approximately(pageY % (size * 10), 0)
+	// 	if (!isMajorLine) continue
+	// 	drawLine(ctx, 0, canvasY, canvasW, canvasY)
+	// }
+	// if (!drawColumns) return
+	// for (let col = 0; col <= numCols; col++) {
+	// 	const pageX = startPageX + col * size
+	// 	// convert the page-space X offset into our canvas' coordinate space
+	// 	const canvasX = (pageX + camera.x) * camera.z * devicePixelRatio
+	// 	const isMajorLine = approximately(pageX % (size * 10), 0)
+	// 	if (!isMajorLine) continue
+	// 	drawLine(ctx, canvasX, 0, canvasX, canvasH)
+	// }
+	// ctx.stroke()
+	// console.log('drawGridStyleGrid', performance.now() - start)
 }
 
 function drawLine(ctx: CanvasRenderingContext2D, x1: number, y1: number, x2: number, y2: number) {
@@ -189,53 +205,36 @@ function drawLine(ctx: CanvasRenderingContext2D, x1: number, y1: number, x2: num
 	ctx.lineTo(x2, y2)
 }
 
-function drawDotStyleGrid({
-	camera,
-	ctx,
-	isDarkMode,
-	numCols,
-	numRows,
-	size,
-	startPageX,
-	startPageY,
-	startMajorX,
-	startMajorY,
-	numMajorCols,
-	numMajorRows,
-}: DrawStuffProps) {
-	const minorDotOpacity = clamp(invLerp(0.5, 1, camera.z), 0, 1)
+function drawDotStyleGrid({ camera, ctx, isDarkMode, major, minor }: DrawStuffProps) {
+	console.log({ major, minor })
 	const majorDotRadius = lerp(2, 4, clamp(invLerp(0.3, 0.6, camera.z), 0, 1))
 
 	const start = performance.now()
 	const color = isDarkMode ? '#555' : '#BBB'
 
-	if (minorDotOpacity > 0) {
-		const x = (startPageX + camera.x) * camera.z * devicePixelRatio
-		const y = (startPageY + camera.y) * camera.z * devicePixelRatio
+	if (minor.opacity > 0) {
 		drawDots({
 			color,
-			opacity: minorDotOpacity,
-			rows: numRows,
-			cols: numCols,
-			spacing: size * camera.z * devicePixelRatio,
+			opacity: minor.opacity,
+			rows: minor.rows,
+			cols: minor.cols,
+			spacing: minor.spacing,
 			ctx,
 			radius: 2,
-			x,
-			y,
+			x: minor.startX,
+			y: minor.startY,
 		})
 	}
-	const x = (startMajorX + camera.x) * camera.z * devicePixelRatio
-	const y = (startMajorY + camera.y) * camera.z * devicePixelRatio
 
 	drawDots({
 		color,
 		opacity: 1,
 		radius: majorDotRadius,
-		x,
-		y,
-		rows: numMajorRows,
-		cols: numMajorCols,
-		spacing: size * 10 * camera.z * devicePixelRatio,
+		x: major.startX,
+		y: major.startY,
+		rows: major.rows,
+		cols: major.cols,
+		spacing: major.spacing,
 		ctx,
 	})
 	console.log('drawDotStyleGrid', performance.now() - start)
