8d4b141344cd2f7b1d271b7b302a314968545cdd
Mitja Bezenšek <mitja.bezensek@gmail.com>
2024-09-04 15:12:25 +0200

    Show notification when timer is running but is hidden.

--- apps/dotcom/src/components/QuickActions.tsx
+++ apps/dotcom/src/components/QuickActions.tsx
@@ -1,20 +1,35 @@
-import { DefaultQuickActions, DefaultQuickActionsContent, track } from 'tldraw'
+import { DefaultQuickActions, DefaultQuickActionsContent, track, useTimer } from 'tldraw'
 import { showTimer } from './Timer'
 
 export const QuickActions = track(function QuickActions() {
 	return (
 		<DefaultQuickActions>
 			<DefaultQuickActionsContent />
-			<div className={showTimer.get() ? 'tlui-timer__quick-actions-wrapper-active' : undefined}>
-				<div
-					className="tlui-timer__quick-actions-icon-wrapper"
-					onClick={() => {
-						showTimer.set(!showTimer.get())
-					}}
-				>
-					<div>🕝</div>
-				</div>
-			</div>
+			<TimerAction />
 		</DefaultQuickActions>
+	)
+})
+
+const TimerAction = track(function TimerAction() {
+	const show = showTimer.get()
+	const { timerProps } = useTimer()
+
+	return (
+		<div
+			className={`tlui-timer__quick-actions-wrapper${show ? ' tlui-timer__quick-actions-wrapper-active' : ''}`}
+		>
+			<div
+				className="tlui-timer__quick-actions-icon-wrapper"
+				onClick={() => {
+					showTimer.set(!show)
+				}}
+			>
+				<div>🕝</div>
+			</div>
+
+			{!show && timerProps && timerProps.state.state === 'running' && (
+				<div className="tlui-timer__quick-actions-icon-notification"></div>
+			)}
+		</div>
 	)
 })
