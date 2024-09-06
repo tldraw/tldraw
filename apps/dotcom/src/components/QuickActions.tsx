1cc7e8e1f3c33a277ead5c21e28c37228084a2b6
Mitja Bezenšek <mitja.bezensek@gmail.com>
2024-09-06 14:03:19 +0200

    Add toast, add some music.

--- apps/dotcom/src/components/QuickActions.tsx
+++ apps/dotcom/src/components/QuickActions.tsx
@@ -1,10 +1,13 @@
-import { useEffect } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import {
 	DefaultQuickActions,
 	DefaultQuickActionsContent,
+	TLTimerProps,
 	TldrawUiButton,
 	track,
+	useEditor,
 	useTimer,
+	useToasts,
 } from 'tldraw'
 import { showTimer } from './Timer'
 
@@ -20,11 +23,62 @@ export const QuickActions = track(function QuickActions() {
 const TimerAction = track(function TimerAction() {
 	const show = showTimer.get()
 	const { timerProps } = useTimer()
+	const [showedToast, setShowedToast] = useState(false)
+	const { addToast } = useToasts()
+	const editor = useEditor()
+	const timeoutRef = useRef<number | undefined>(undefined)
+	const audioRef = useRef<HTMLAudioElement | null>(null)
+
 	useEffect(() => {
-		if (!show && timerProps?.state.state === 'running') {
+		if (!timerProps) return
+		switch (timerProps.state.state) {
+			case 'running':
+			case 'paused':
+				if (showedToast) {
+					setShowedToast(false)
+				}
+				break
+			case 'completed': {
+				if (showedToast) return
+				addToast({
+					id: 'timer-completed',
+					severity: 'success',
+					title: 'Timer Completed',
+					description: 'The timer has completed',
+					keepOpen: true,
+				})
+				audioRef.current?.play()
+
+				timeoutRef.current = editor.timers.setTimeout(() => {
+					const meta = editor.getDocumentSettings().meta
+					const props = meta.timer as TLTimerProps | undefined
+					if (!props || props.state.state !== 'completed') return
+					editor.updateDocumentSettings({
+						meta: {
+							...meta,
+							timer: {
+								...props,
+								remainingTime: props.initialTime,
+								state: { state: 'stopped' },
+							},
+						},
+					})
+				}, 4000)
+
+				break
+			}
+		}
+		return () => {
+			clearTimeout(timeoutRef.current)
+		}
+	}, [addToast, editor, showedToast, timerProps])
+
+	useEffect(() => {
+		if (!timerProps) return
+		if (!show && timerProps.state.state === 'running') {
 			showTimer.set(true)
 		}
-	}, [show, timerProps?.state.state])
+	}, [show, timerProps])
 	return (
 		<div
 			className={`tlui-timer__quick-actions-wrapper${show ? ' tlui-timer__quick-actions-wrapper-active' : ''}`}
@@ -38,6 +92,9 @@ const TimerAction = track(function TimerAction() {
 			>
 				<div>🕝</div>
 			</TldrawUiButton>
+			<audio ref={audioRef}>
+				<source src="./Timer.mp3" type="audio/mp3" />
+			</audio>
 		</div>
 	)
 })
