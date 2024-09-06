df6e9aee2d58a545da550803a3ec364caed3e2c5
Mitja Bezenšek <mitja.bezensek@gmail.com>
2024-09-06 13:00:41 +0200

    Always show the timer when it is running.

--- apps/dotcom/src/components/QuickActions.tsx
+++ apps/dotcom/src/components/QuickActions.tsx
@@ -1,4 +1,11 @@
-import { DefaultQuickActions, DefaultQuickActionsContent, track, useTimer } from 'tldraw'
+import { useEffect } from 'react'
+import {
+	DefaultQuickActions,
+	DefaultQuickActionsContent,
+	TldrawUiButton,
+	track,
+	useTimer,
+} from 'tldraw'
 import { showTimer } from './Timer'
 
 export const QuickActions = track(function QuickActions() {
@@ -13,23 +20,24 @@ export const QuickActions = track(function QuickActions() {
 const TimerAction = track(function TimerAction() {
 	const show = showTimer.get()
 	const { timerProps } = useTimer()
-
+	useEffect(() => {
+		if (!show && timerProps?.state.state === 'running') {
+			showTimer.set(true)
+		}
+	}, [show, timerProps?.state.state])
 	return (
 		<div
 			className={`tlui-timer__quick-actions-wrapper${show ? ' tlui-timer__quick-actions-wrapper-active' : ''}`}
 		>
-			<div
-				className="tlui-timer__quick-actions-icon-wrapper"
+			<TldrawUiButton
+				type="icon"
 				onClick={() => {
 					showTimer.set(!show)
 				}}
+				disabled={timerProps?.state.state === 'running'}
 			>
 				<div>🕝</div>
-			</div>
-
-			{!show && timerProps && timerProps.state.state === 'running' && (
-				<div className="tlui-timer__quick-actions-icon-notification"></div>
-			)}
+			</TldrawUiButton>
 		</div>
 	)
 })
