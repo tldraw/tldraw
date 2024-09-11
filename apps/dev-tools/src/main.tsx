a2cc794a59251beecefcba5d26e1ac373d99a23a
Mitja Bezenšek <mitja.bezensek@gmail.com>
2024-09-11 13:36:39 +0200

    tsconfig.

--- apps/dev-tools/src/main.tsx
+++ apps/dev-tools/src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import App from './App'
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
