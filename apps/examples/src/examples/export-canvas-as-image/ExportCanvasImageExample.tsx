bedbe88fe2ea6a42a9d4b7028bd8651f52b86d39
nayounsang <nayounsang722@gmail.com>
2024-09-15 21:28:07 +0900

    chore: exmaple script

--- apps/examples/src/examples/export-canvas-as-image/ExportCanvasImageExample.tsx
+++ apps/examples/src/examples/export-canvas-as-image/ExportCanvasImageExample.tsx
@@ -1,29 +1,46 @@
+import { useState } from 'react'
 import { exportToBlob, Tldraw, TLUiComponents, useEditor } from 'tldraw'
 import 'tldraw/tldraw.css'
 
 function ExportCanvasButton() {
 	const editor = useEditor()
+	const [isOpen, setIsOpen] = useState(false)
 	return (
-		<button
-			style={{ pointerEvents: 'all', fontSize: 18, backgroundColor: 'thistle' }}
-			onClick={async () => {
-				const shapeIds = editor.getCurrentPageShapeIds()
-				if (shapeIds.size === 0) return alert('No shapes on the canvas')
-				const blob = await exportToBlob({
-					editor,
-					ids: [...shapeIds],
-					format: 'png',
-					opts: { background: false },
-				})
+		<>
+			<button
+				style={{ pointerEvents: 'all', fontSize: 18, backgroundColor: 'thistle' }}
+				onClick={async () => {
+					const shapeIds = editor.getCurrentPageShapeIds()
+					if (shapeIds.size === 0) return alert('No shapes on the canvas')
+					const blob = await exportToBlob({
+						editor,
+						ids: [...shapeIds],
+						format: 'png',
+						opts: { background: false },
+					})
 
-				const link = document.createElement('a')
-				link.href = window.URL.createObjectURL(blob)
-				link.download = 'every-shape-on-the-canvas.jpg'
-				link.click()
-			}}
-		>
-			Export canvas as image
-		</button>
+					const link = document.createElement('a')
+					link.href = window.URL.createObjectURL(blob)
+					link.download = 'every-shape-on-the-canvas.jpg'
+					link.click()
+				}}
+			>
+				Export canvas as image
+			</button>
+			<button
+				onClick={() => {
+					setIsOpen(!isOpen)
+				}}
+			>
+				{isOpen ? 'close' : 'open'} export settings
+			</button>
+			{isOpen && (
+				<div>
+					<label htmlFor="background">background</label>
+					<input type="checkbox" id="background" />
+				</div>
+			)}
+		</>
 	)
 }
 const components: TLUiComponents = {
