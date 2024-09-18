017ec131e717bac67399d2da41e005b359648d9c
Mitja Bezenšek <mitja.bezensek@gmail.com>
2024-09-18 17:04:01 +0200

    Add some additional debugging tools. (#4539)
    Adds two context menu items to improve the developer experience.
    
    The first one just logs the currently selected shapes. 
    
    The second one allows you to select some shapes you'd want to track. It
    then prints them out when they change.
    
    ### Change type
    
    - [ ] `bugfix`
    - [ ] `improvement`
    - [ ] `feature`
    - [ ] `api`
    - [x] `other`
    
    ### Release notes
    
    - Adds some additional debugging tools to the develop experience.

---
+++ apps/examples/src/hooks/useDebugging.tsx
@@ -0,0 +1,53 @@
+import { TLShapeId, TLUiActionItem, TLUiOverrides, atom } from 'tldraw'
+
+export const trackedShapes = atom('tracked shapes', [] as TLShapeId[])
+
+export function useDebugging(): TLUiOverrides {
+	return {
+		actions(editor, actions) {
+			actions['log-shapes'] = {
+				id: 'log-shapes',
+				onSelect: () => {
+					const selectedShapes = editor.getSelectedShapes()
+					switch (selectedShapes.length) {
+						case 0:
+							// eslint-disable-next-line no-console
+							console.log('No shapes selected')
+							break
+						case 1:
+							// eslint-disable-next-line no-console
+							console.log(selectedShapes[0])
+							break
+						default:
+							// eslint-disable-next-line no-console
+							console.log(selectedShapes)
+							break
+					}
+				},
+				label: 'Log shapes',
+				readonlyOk: true,
+				kbd: '?!l',
+			} as TLUiActionItem
+
+			actions['track-changes'] = {
+				id: 'track-changes',
+				onSelect: () => {
+					const oneShape = editor.getOnlySelectedShape()
+					if (!oneShape) return
+					const tracked = trackedShapes.get()
+					const alreadyTracked = tracked.includes(oneShape.id)
+					if (alreadyTracked) {
+						trackedShapes.set(tracked.filter((id) => id !== oneShape.id))
+					} else {
+						trackedShapes.set([...tracked, oneShape.id])
+					}
+				},
+				label: 'Track changes',
+				checkbox: true,
+				readonlyOk: true,
+			} as TLUiActionItem
+
+			return actions
+		},
+	}
+}
