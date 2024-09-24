2d731edf26c03a6cd4dadccf77d11fd00e4222cd
Mitja Bezenšek <mitja.bezensek@gmail.com>
2024-09-24 08:06:16 +0200

    Add a comment about promise usage for using the clipboard (#4584)
    Follow up to @mimecuvalo comment
    [here](https://github.com/tldraw/tldraw/pull/4577#discussion_r1771693944).
    
    ### Change type
    
    - [ ] `bugfix`
    - [ ] `improvement`
    - [ ] `feature`
    - [ ] `api`
    - [x] `other`
    
    ### Release notes
    
    - Add a comment explaining why we don't resolve the promise in one of
    the cases.

--- apps/dotcom/client/src/utils/clipboard.ts
+++ apps/dotcom/client/src/utils/clipboard.ts
@@ -1,6 +1,7 @@
 /** @internal */
 export async function writeToClipboard(result: Promise<Blob | ''>) {
 	if (navigator?.clipboard?.write) {
+		// In async contexts we have to pass in a Promise to make it work in Safari
 		await navigator.clipboard.write([
 			new ClipboardItem({
 				'text/plain': result,
