60c06c267f17f67351b0d7eabde56e29736f740f
David Sheldrick <d.j.sheldrick@gmail.com>
2024-09-27 16:05:45 +0100

    fix reset-db

--- apps/dotcom/sync-worker/reset-db.sh
+++ apps/dotcom/sync-worker/reset-db.sh
@@ -1,10 +1,5 @@
 set -eux
 
-# uncomment these lines to nuke the respective live databases
-
-# npx wrangler d1 execute --env=preview botcom-preview --file=./schema.sql
-# npx wrangler d1 execute --env=staging botcom-staging --file=./schema.sql
-# npx wrangler d1 execute --env=production botcom-production --file=./schema.sql
-
 # this one just resets your local database
-npx wrangler d1 execute botcom-staging --env=dev --local --file=./schema.sql
+rm -rf ./.wrangler/state/v3/d1
+yes | npx wrangler d1 migrations apply botcom-dev --env=dev --local
