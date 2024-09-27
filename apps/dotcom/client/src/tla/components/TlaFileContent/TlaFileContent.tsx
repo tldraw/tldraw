6a7f5d98809fde2c91217a6a18fedaae6f32b958
Steve Ruiz <steveruizok@gmail.com>
2024-09-27 18:59:41 -0400

    Update TlaFileContent.tsx

--- apps/dotcom/client/src/tla/components/TlaFileContent/TlaFileContent.tsx
+++ apps/dotcom/client/src/tla/components/TlaFileContent/TlaFileContent.tsx
@@ -1,3 +1,4 @@
+import { SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/clerk-react'
 import classNames from 'classnames'
 import { useEffect } from 'react'
 import { useValue } from 'tldraw'
@@ -42,6 +43,13 @@ export function TlaFileContent({ file }: { file: TldrawAppFile }) {
 					<span className={styles.headerFolder}>My files / </span>
 					<span className={styles.headerTitle}>{TldrawApp.getFileName(file)}</span>
 				</div>
+				<SignedOut>
+					{/* @ts-ignore this is fine */}
+					<SignInButton className="tla-button tla-button__secondary tla-text_ui__regular" />
+				</SignedOut>
+				<SignedIn>
+					<UserButton />
+				</SignedIn>
 				<TlaFileShareMenu fileId={file.id} />
 			</div>
 			<div
