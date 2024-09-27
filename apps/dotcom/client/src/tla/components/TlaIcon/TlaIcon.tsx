c9372935e80887441caf20d537cee99f3ef31f7a
Steve Ruiz <steveruizok@gmail.com>
2024-09-27 18:21:55 -0400

    fix css jest

--- apps/dotcom/client/src/tla/components/TlaIcon/TlaIcon.tsx
+++ apps/dotcom/client/src/tla/components/TlaIcon/TlaIcon.tsx
@@ -14,13 +14,12 @@ export function TlaIcon({
 	const ref = useRef<HTMLDivElement>(null)
 
 	useLayoutEffect(() => {
-		if (ref?.current) {
-			// HACK: Fix for <https://linear.app/tldraw/issue/TLD-1700/dragging-around-with-the-handtool-makes-lots-of-requests-for-icons>
-			// It seems that passing `WebkitMask` to react will cause a render on each call, no idea why... but this appears to be the fix.
-			// @ts-ignore
-			// eslint-disable-next-line deprecation/deprecation
-			ref.current.style.webkitMask = `url(/tla/icon=${icon}.svg) center 100% / 100% no-repeat`
-		}
+		if (!ref.current) return
+		// HACK: Fix for <https://linear.app/tldraw/issue/TLD-1700/dragging-around-with-the-handtool-makes-lots-of-requests-for-icons>
+		// It seems that passing `WebkitMask` to react will cause a render on each call, no idea why... but this appears to be the fix.
+		// @ts-ignore
+		// eslint-disable-next-line deprecation/deprecation
+		ref.current.style.webkitMask = `url(/tla/icon=${icon}.svg) center 100% / 100% no-repeat`
 	}, [ref, icon])
 
 	if (icon === 'none') {
