4ec9d1a7e6fc890ed353d08eff6106502cdde716
Mime Čuvalo <mimecuvalo@gmail.com>
2024-10-01 22:03:54 +0100

    text: followup fix to locking text shapes (#4632)
    followup to https://github.com/tldraw/tldraw/pull/4569
    Once the text shape is locked you can't click the Select tool to switch
    out. This addresses that (because the tool is masked at the time). If
    there's a better fix for this, I'm all ears, I wasn't sure how else to
    resolve this (I see you pulled it out in the other PR @steveruizok )
    
    ### Change type
    
    - [x] `bugfix`
    - [ ] `improvement`
    - [ ] `feature`
    - [ ] `api`
    - [ ] `other`
    
    ### Release notes
    
    - Fix bug with text shape locking.
    
    ---------
    
    Co-authored-by: Steve Ruiz <steveruizok@gmail.com>

--- packages/tldraw/src/lib/ui/hooks/useTools.tsx
+++ packages/tldraw/src/lib/ui/hooks/useTools.tsx
@@ -57,6 +57,13 @@ export function ToolsProvider({ overrides, children }: TLUiToolsProviderProps) {
 				kbd: 'v',
 				readonlyOk: true,
 				onSelect(source) {
+					if (editor.isIn('select')) {
+						// There's a quirk of select mode, where editing a shape is a sub-state of select.
+						// Because the text tool can be locked/sticky, we need to make sure we exit the
+						// text tool.
+						const currentNode = editor.root.getCurrent()!
+						currentNode.exit({}, currentNode.id)
+					}
 					editor.setCurrentTool('select.idle')
 					trackEvent('select-tool', { source, id: 'select' })
 				},
