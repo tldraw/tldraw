0f20126d6393c6045c1bbf26046e36d3368aa75e
Steve Ruiz <steveruizok@gmail.com>
2024-10-01 16:56:55 -0400

    ok

--- packages/tldraw/src/lib/ui/hooks/useTools.tsx
+++ packages/tldraw/src/lib/ui/hooks/useTools.tsx
@@ -57,16 +57,15 @@ export function ToolsProvider({ overrides, children }: TLUiToolsProviderProps) {
 				kbd: 'v',
 				readonlyOk: true,
 				onSelect(source) {
-					const currentNode = editor.root.getCurrent()
-					if (currentNode) {
+					if (editor.isIn('select')) {
 						// There's a quirk of select mode, where editing a shape is a sub-state of select.
 						// Because the text tool can be locked/sticky, we need to make sure we exit the
 						// text tool.
+						const currentNode = editor.root.getCurrent()!
 						currentNode.exit({}, currentNode.id)
 						currentNode.enter({}, currentNode.id)
 					}
 					editor.setCurrentTool('select.idle')
-
 					trackEvent('select-tool', { source, id: 'select' })
 				},
 			},
