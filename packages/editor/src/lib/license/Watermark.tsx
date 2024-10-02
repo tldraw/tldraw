0ef3b1c58a323f71a55b59e2f9c90107b5e2be79
Mime Čuvalo <mimecuvalo@gmail.com>
2024-10-02 10:01:16 +0100

    menus: rework the open menu logic to be in one consistent place

--- packages/editor/src/lib/license/Watermark.tsx
+++ packages/editor/src/lib/license/Watermark.tsx
@@ -48,7 +48,6 @@ export const Watermark = memo(function Watermark() {
 const WatermarkInner = memo(function WatermarkInner({ src }: { src: string }) {
 	const editor = useEditor()
 	const isDebugMode = useValue('debug mode', () => editor.getInstanceState().isDebugMode, [editor])
-	const isMenuOpen = useValue('is menu open', () => editor.menus.hasAnyOpenMenus(), [editor])
 	const isMobile = useValue('is mobile', () => editor.getViewportScreenBounds().width < 700, [
 		editor,
 	])
@@ -61,7 +60,6 @@ const WatermarkInner = memo(function WatermarkInner({ src }: { src: string }) {
 		<div
 			className={LicenseManager.className}
 			data-debug={isDebugMode}
-			data-menu={isMenuOpen}
 			data-mobile={isMobile}
 			draggable={false}
 			{...events}
@@ -122,7 +120,7 @@ To remove the watermark, please purchase a license at tldraw.dev.
 		padding: 2px;
 		box-sizing: content-box;
 	}
-	
+
 	.${className} > a {
 		position: absolute;
 		width: 96px;
@@ -134,15 +132,10 @@ To remove the watermark, please purchase a license at tldraw.dev.
 		background-color: currentColor;
 	}
 
-	
-	.${className}[data-menu='true'] {
-		pointer-events: none;
-	}
-
 	.${className}[data-debug='true'] {
 		bottom: 46px;
 	}
-	
+
 	.${className}[data-mobile='true'] {
 		border-radius: 4px 0px 0px 4px;
 		right: -2px;
@@ -154,7 +147,7 @@ To remove the watermark, please purchase a license at tldraw.dev.
 		width: 8px;
 		height: 32px;
 	}
-	
+
 	@media (hover: hover) {
 		.${className} > a {
 			pointer-events: none;
@@ -165,13 +158,12 @@ To remove the watermark, please purchase a license at tldraw.dev.
 			transition: background-color 0.2s ease-in-out;
 			transition-delay: 0.32s;
 		}
-			
+
 		.${className}:hover > a {
 			animation: delayed_link 0.2s forwards ease-in-out;
 			animation-delay: 0.32s;
 		}
 	}
-	
 
 	@keyframes delayed_link {
 		0% {
