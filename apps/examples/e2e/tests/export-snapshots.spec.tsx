7165c837a6fe7d381b8328d1b07a873bb999234d
Mitja Bezenšek <mitja.bezensek@gmail.com>
2024-10-03 14:47:34 +0200

    Update snapshot.

--- apps/examples/e2e/tests/export-snapshots.spec.tsx
+++ apps/examples/e2e/tests/export-snapshots.spec.tsx
@@ -491,38 +491,36 @@ const snapshots: Snapshots = {
 		'': {
 			Arrow1: (
 				<TL.arrow
-					start={{ x: 0, y: 0 }}
-					end={{ x: 0, y: 300 }}
+					start={{ x: 200, y: 0 }}
+					end={{ x: 200, y: 300 }}
 					size="xl"
 					arrowheadStart="arrow"
 					arrowheadEnd="triangle"
-					text="arrow 1"
 				/>
 			),
 			Arrow2: (
 				<TL.arrow
-					start={{ x: 50, y: 0 }}
-					end={{ x: 50, y: 300 }}
+					start={{ x: 250, y: 0 }}
+					end={{ x: 250, y: 300 }}
 					size="xl"
 					arrowheadStart="square"
 					arrowheadEnd="dot"
-					text="arrow 2"
 				/>
 			),
 			Arrow3: (
 				<TL.arrow
-					start={{ x: 100, y: 0 }}
-					end={{ x: 100, y: 300 }}
+					start={{ x: 300, y: 0 }}
+					end={{ x: 300, y: 300 }}
 					size="xl"
 					arrowheadStart="pipe"
 					arrowheadEnd="diamond"
-					text="arrow 3"
+					text="with text"
 				/>
 			),
 			Arrow4: (
 				<TL.arrow
-					start={{ x: 150, y: 0 }}
-					end={{ x: 150, y: 300 }}
+					start={{ x: 350, y: 0 }}
+					end={{ x: 350, y: 300 }}
 					size="xl"
 					arrowheadStart="inverted"
 					arrowheadEnd="bar"
@@ -530,8 +528,8 @@ const snapshots: Snapshots = {
 			),
 			Arrow5: (
 				<TL.arrow
-					start={{ x: 200, y: 0 }}
-					end={{ x: 200, y: 300 }}
+					start={{ x: 400, y: 0 }}
+					end={{ x: 400, y: 300 }}
 					size="xl"
 					arrowheadStart="none"
 					arrowheadEnd="none"
