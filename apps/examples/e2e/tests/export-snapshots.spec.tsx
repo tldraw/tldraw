tldraw/tldraw

commit b19fcc399eda223b76ffe17509e7aa19c5b40f9d

Improve perf for safari (#4636)
Looks like safari has some issue with using masks. This PR switches from
using masks to clipPaths.

Might be worth hotfixing.

Reported
[here](https://discord.com/channels/859816885297741824/1290288009492824065/1290288009492824065).

### Change type

- [x] `bugfix`
- [ ] `improvement`
- [ ] `feature`
- [ ] `api`
- [ ] `other`

### Test plan

1. Create some arrows.
2. Add labels to some of them.
3. Zoom in significantly (especially in Safari) and pan around. You
should not see a drop in perf. You can turn on perf measurement in the
debug panel to see the fps for panning.
4. Make sure the clip paths worth correctly (ie no arrow bodies behind
arrow labels).

### Release notes

- Fix a performance issue with panning when zoomed in on arrows in
Safari.

packages/tldraw/src/lib/shapes/arrow/ArrowShapeUtil.tsx
@@ -28,7 +28,6 @@ import {
 	mapObjectMapValues,
 	sanitizeId,
 	structuredClone,
-	tlenv,
 	toDomPrecision,
 	track,
 	useEditor,
@@ -63,8 +62,6 @@ import {
 	getArrowTerminalsInArrowSpace,
 	removeArrowBinding,
 } from './shared'
-
-let globalRenderIndex = 0
 
 enum ARROW_HANDLES {
 	START = 'start',
@@ -862,11 +859,6 @@ const ArrowSvg = track(function ArrowSvg({
 		[editor]
 	)
 
-	const changeIndex = React.useMemo<number>(() => {
-		return tlenv.isSafari ? (globalRenderIndex += 1) : 0
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [shape])
-
 	if (!info?.isValid) return null
 
 	const strokeWidth = STROKE_SIZES[shape.props.size] * shape.props.scale
@@ -935,40 +927,26 @@ const ArrowSvg = track(function ArrowSvg({
 	const maskStartArrowhead = !(info.start.arrowhead === 'none' || info.start.arrowhead === 'arrow')
 	const maskEndArrowhead = !(info.end.arrowhead === 'none' || info.end.arrowhead === 'arrow')
 
-	// NOTE: I know right setting `changeIndex` hacky-as right! But we need this because otherwise safari loses
-	// the mask, see <https://linear.app/tldraw/issue/TLD-1500/changing-arrow-color-makes-line-pass-through-text>
-	const maskId = sanitizeId(shape.id + '_clip' + (tlenv.isSafari ? `_${changeIndex}` : ''))
+	const clipPathId = sanitizeId(shape.id + '_clip')
 
 	return (
 		<>
 			{/* Yep */}
 			<defs>
-				<mask id={maskId}>
-					<rect
-						x={toDomPrecision(-100 + bounds.minX)}
-						y={toDomPrecision(-100 + bounds.minY)}
-						width={toDomPrecision(bounds.width + 200)}
-						height={toDomPrecision(bounds.height + 200)}
-						fill="white"
+				<clipPath id={clipPathId}>
+					<ArrowClipPath
+						hasText={shape.props.text.trim().length > 0}
+						bounds={bounds}
+						labelPositionBounds={labelPosition.box}
 					/>
-					{shape.props.text.trim() && (
-						<rect
-							x={labelPosition.box.x}
-							y={labelPosition.box.y}
-							width={labelPosition.box.w}
-							height={labelPosition.box.h}
-							fill="black"
-							rx={4}
-							ry={4}
-						/>
-					)}
+
 					{as && maskStartArrowhead && (
 						<path d={as} fill={info.start.arrowhead === 'arrow' ? 'none' : 'black'} stroke="none" />
 					)}
 					{ae && maskEndArrowhead && (
 						<path d={ae} fill={info.end.arrowhead === 'arrow' ? 'none' : 'black'} stroke="none" />
 					)}
-				</mask>
+				</clipPath>
 			</defs>
 			<g
 				fill="none"
@@ -980,7 +958,12 @@ const ArrowSvg = track(function ArrowSvg({
 			>
 				{handlePath}
 				{/* firefox will clip if you provide a maskURL even if there is no mask matching that URL in the DOM */}
-				<g mask={`url(#${maskId})`}>
+				<g
+					style={{
+						clipPath: `url(#${clipPathId})`,
+						WebkitClipPath: `url(#${clipPathId})`,
+					}}
+				>
 					<rect
 						x={toDomPrecision(bounds.minX - 100)}
 						y={toDomPrecision(bounds.minY - 100)}
@@ -1014,6 +997,62 @@ const ArrowSvg = track(function ArrowSvg({
 		</>
 	)
 })
+
+function ArrowClipPath({
+	hasText,
+	bounds,
+	labelPositionBounds,
+}: {
+	hasText: boolean
+	bounds: Box
+	labelPositionBounds: Box
+}) {
+	if (hasText) {
+		// There doesn't seem to be an easy way to invert a clipPath, so we instead create this complex polygon.
+		// We create the three sides of the outer rectangle in the clockwise direction, then move to the inside rectangle
+		// and create that one in the counterclockwise direction (so we don't intersect and complete the path).
+		// We then finish with the outer rectangle. Diagram shows the order of the points.
+		//
+		//    (1, 11)--------------------------(2)
+		//       |                              |
+		//       |         Outer Rect           |
+		//       |                            	|
+		//       |  (8)-------------------(7)   |
+		//       |    |                    |    |
+		//       |    |                    |    |
+		//       |    |    Inner Rect      |    |
+		//       |    |                    |    |
+		//       |    |                    |    |
+		//       |  (5,9)-----------------(6)   |
+		//    (4, 10)--------------------------(3)
+
+		return (
+			<polygon
+				points={`
+        ${toDomPrecision(bounds.minX - 100)},${toDomPrecision(bounds.minY - 100)} 
+        ${toDomPrecision(bounds.minX + bounds.width + 100)},${toDomPrecision(bounds.minY - 100)} 
+        ${toDomPrecision(bounds.minX + bounds.width + 100)},${toDomPrecision(bounds.minY + bounds.height + 100)}
+        ${toDomPrecision(bounds.minX - 100)},${toDomPrecision(bounds.minY + bounds.height + 100)}
+        ${labelPositionBounds.x},${labelPositionBounds.y + labelPositionBounds.h}
+        ${labelPositionBounds.x + labelPositionBounds.w},${labelPositionBounds.y + labelPositionBounds.h}
+        ${labelPositionBounds.x + labelPositionBounds.w},${labelPositionBounds.y}
+        ${labelPositionBounds.x},${labelPositionBounds.y}
+        ${labelPositionBounds.x},${labelPositionBounds.y + labelPositionBounds.h}
+        ${toDomPrecision(bounds.minX - 100)},${toDomPrecision(bounds.minY + bounds.height + 100)}
+        ${toDomPrecision(bounds.minX - 100)},${toDomPrecision(bounds.minY - 100)} 
+      `}
+			/>
+		)
+	}
+	return (
+		<rect
+			x={toDomPrecision(bounds.minX - 100)}
+			y={toDomPrecision(bounds.minY - 100)}
+			width={toDomPrecision(bounds.width + 200)}
+			height={toDomPrecision(bounds.height + 200)}
+		/>
+	)
+}
 
 const shapeAtTranslationStart = new WeakMap<
 	TLArrowShape,

commit 3273821b003f5f24bd80ce6d5567cae7d23248e8

Arrowhead clipping fix (#4646)
Fixes issues with arrowheads not correctly clipping the arrow's body.
The main issue was when we used the fill none option. There was also a
difference in how it worked if arrows had labels vs if they didn't.

Also transitions the arrow indicator form using masks to using clip
paths so that we also improve the performance of panning when we have
arrows selected.

[Related
discussion](https://discord.com/channels/859816885297741824/1290288009492824065/1290906937797640273).

### Before


![image](https://github.com/user-attachments/assets/1669fc2a-e3a3-4496-ab19-2a48bd6c8bee)

### After


![image](https://github.com/user-attachments/assets/c191e83c-b8c9-4974-a599-71bc49a2490b)

### Change type

- [x] `bugfix`
- [ ] `improvement`
- [ ] `feature`
- [ ] `api`
- [ ] `other`

### Release notes

- Fix an issue introduced with #4636. The arrowheads did not correctly
clip the arrow body.

---------

Co-authored-by: huppy-bot[bot] <128400622+huppy-bot[bot]@users.noreply.github.com>
Co-authored-by: Steve Ruiz <steveruizok@gmail.com>

packages/tldraw/src/lib/shapes/arrow/ArrowShapeUtil.tsx
@@ -660,12 +660,12 @@ export class ArrowShapeUtil extends ShapeUtil<TLArrowShape> {
 
 		const path = info.isStraight ? getSolidStraightArrowPath(info) : getSolidCurvedArrowPath(info)
 
-		const includeMask =
+		const includeClipPath =
 			(as && info.start.arrowhead !== 'arrow') ||
 			(ae && info.end.arrowhead !== 'arrow') ||
 			!!labelGeometry
 
-		const maskId = sanitizeId(shape.id + '_clip')
+		const clipPathId = sanitizeId(shape.id + '_clip')
 
 		if (isEditing && labelGeometry) {
 			return (
@@ -679,51 +679,32 @@ export class ArrowShapeUtil extends ShapeUtil<TLArrowShape> {
 				/>
 			)
 		}
+		const clipStartArrowhead = !(
+			info.start.arrowhead === 'none' || info.start.arrowhead === 'arrow'
+		)
+		const clipEndArrowhead = !(info.end.arrowhead === 'none' || info.end.arrowhead === 'arrow')
 
 		return (
 			<g>
-				{includeMask && (
+				{includeClipPath && (
 					<defs>
-						<mask id={maskId}>
-							<rect
-								x={bounds.minX - 100}
-								y={bounds.minY - 100}
-								width={bounds.w + 200}
-								height={bounds.h + 200}
-								fill="white"
-							/>
-							{labelGeometry && (
-								<rect
-									x={toDomPrecision(labelGeometry.x)}
-									y={toDomPrecision(labelGeometry.y)}
-									width={labelGeometry.w}
-									height={labelGeometry.h}
-									fill="black"
-									rx={3.5 * shape.props.scale}
-									ry={3.5 * shape.props.scale}
-								/>
-							)}
-							{as && (
-								<path
-									d={as}
-									fill={info.start.arrowhead === 'arrow' ? 'none' : 'black'}
-									stroke="none"
-								/>
-							)}
-							{ae && (
-								<path
-									d={ae}
-									fill={info.end.arrowhead === 'arrow' ? 'none' : 'black'}
-									stroke="none"
-								/>
-							)}
-						</mask>
+						<ArrowClipPath
+							hasText={shape.props.text.trim().length > 0}
+							bounds={bounds}
+							labelBounds={labelGeometry ? labelGeometry.getBounds() : new Box(0, 0, 0, 0)}
+							as={clipStartArrowhead && as ? as : ''}
+							ae={clipEndArrowhead && ae ? ae : ''}
+						/>
 					</defs>
 				)}
-				{/* firefox will clip if you provide a maskURL even if there is no mask matching that URL in the DOM */}
-				<g {...(includeMask ? { mask: `url(#${maskId})` } : undefined)}>
+				<g
+					style={{
+						clipPath: includeClipPath ? `url(#${clipPathId})` : undefined,
+						WebkitClipPath: includeClipPath ? `url(#${clipPathId})` : undefined,
+					}}
+				>
 					{/* This rect needs to be here if we're creating a mask due to an svg quirk on Chrome */}
-					{includeMask && (
+					{includeClipPath && (
 						<rect
 							x={bounds.minX - 100}
 							y={bounds.minY - 100}
@@ -924,8 +905,8 @@ const ArrowSvg = track(function ArrowSvg({
 
 	const labelPosition = getArrowLabelPosition(editor, shape)
 
-	const maskStartArrowhead = !(info.start.arrowhead === 'none' || info.start.arrowhead === 'arrow')
-	const maskEndArrowhead = !(info.end.arrowhead === 'none' || info.end.arrowhead === 'arrow')
+	const clipStartArrowhead = !(info.start.arrowhead === 'none' || info.start.arrowhead === 'arrow')
+	const clipEndArrowhead = !(info.end.arrowhead === 'none' || info.end.arrowhead === 'arrow')
 
 	const clipPathId = sanitizeId(shape.id + '_clip')
 
@@ -937,15 +918,10 @@ const ArrowSvg = track(function ArrowSvg({
 					<ArrowClipPath
 						hasText={shape.props.text.trim().length > 0}
 						bounds={bounds}
-						labelPositionBounds={labelPosition.box}
+						labelBounds={labelPosition.box}
+						as={clipStartArrowhead && as ? as : ''}
+						ae={clipEndArrowhead && ae ? ae : ''}
 					/>
-
-					{as && maskStartArrowhead && (
-						<path d={as} fill={info.start.arrowhead === 'arrow' ? 'none' : 'black'} stroke="none" />
-					)}
-					{ae && maskEndArrowhead && (
-						<path d={ae} fill={info.end.arrowhead === 'arrow' ? 'none' : 'black'} stroke="none" />
-					)}
 				</clipPath>
 			</defs>
 			<g
@@ -957,7 +933,6 @@ const ArrowSvg = track(function ArrowSvg({
 				pointerEvents="none"
 			>
 				{handlePath}
-				{/* firefox will clip if you provide a maskURL even if there is no mask matching that URL in the DOM */}
 				<g
 					style={{
 						clipPath: `url(#${clipPathId})`,
@@ -973,7 +948,7 @@ const ArrowSvg = track(function ArrowSvg({
 					/>
 					<path d={path} strokeDasharray={strokeDasharray} strokeDashoffset={strokeDashoffset} />
 				</g>
-				{as && maskStartArrowhead && shape.props.fill !== 'none' && (
+				{as && clipStartArrowhead && shape.props.fill !== 'none' && (
 					<ShapeFill
 						theme={theme}
 						d={as}
@@ -982,7 +957,7 @@ const ArrowSvg = track(function ArrowSvg({
 						scale={shape.props.scale}
 					/>
 				)}
-				{ae && maskEndArrowhead && shape.props.fill !== 'none' && (
+				{ae && clipEndArrowhead && shape.props.fill !== 'none' && (
 					<ShapeFill
 						theme={theme}
 						d={ae}
@@ -1001,57 +976,25 @@ const ArrowSvg = track(function ArrowSvg({
 function ArrowClipPath({
 	hasText,
 	bounds,
-	labelPositionBounds,
+	labelBounds,
+	as,
+	ae,
 }: {
 	hasText: boolean
 	bounds: Box
-	labelPositionBounds: Box
+	labelBounds: Box
+	as: string
+	ae: string
 }) {
-	if (hasText) {
-		// There doesn't seem to be an easy way to invert a clipPath, so we instead create this complex polygon.
-		// We create the three sides of the outer rectangle in the clockwise direction, then move to the inside rectangle
-		// and create that one in the counterclockwise direction (so we don't intersect and complete the path).
-		// We then finish with the outer rectangle. Diagram shows the order of the points.
-		//
-		//    (1, 11)--------------------------(2)
-		//       |                              |
-		//       |         Outer Rect           |
-		//       |                            	|
-		//       |  (8)-------------------(7)   |
-		//       |    |                    |    |
-		//       |    |                    |    |
-		//       |    |    Inner Rect      |    |
-		//       |    |                    |    |
-		//       |    |                    |    |
-		//       |  (5,9)-----------------(6)   |
-		//    (4, 10)--------------------------(3)
-
-		return (
-			<polygon
-				points={`
-        ${toDomPrecision(bounds.minX - 100)},${toDomPrecision(bounds.minY - 100)} 
-        ${toDomPrecision(bounds.minX + bounds.width + 100)},${toDomPrecision(bounds.minY - 100)} 
-        ${toDomPrecision(bounds.minX + bounds.width + 100)},${toDomPrecision(bounds.minY + bounds.height + 100)}
-        ${toDomPrecision(bounds.minX - 100)},${toDomPrecision(bounds.minY + bounds.height + 100)}
-        ${labelPositionBounds.x},${labelPositionBounds.y + labelPositionBounds.h}
-        ${labelPositionBounds.x + labelPositionBounds.w},${labelPositionBounds.y + labelPositionBounds.h}
-        ${labelPositionBounds.x + labelPositionBounds.w},${labelPositionBounds.y}
-        ${labelPositionBounds.x},${labelPositionBounds.y}
-        ${labelPositionBounds.x},${labelPositionBounds.y + labelPositionBounds.h}
-        ${toDomPrecision(bounds.minX - 100)},${toDomPrecision(bounds.minY + bounds.height + 100)}
-        ${toDomPrecision(bounds.minX - 100)},${toDomPrecision(bounds.minY - 100)} 
-      `}
-			/>
-		)
-	}
-	return (
-		<rect
-			x={toDomPrecision(bounds.minX - 100)}
-			y={toDomPrecision(bounds.minY - 100)}
-			width={toDomPrecision(bounds.width + 200)}
-			height={toDomPrecision(bounds.height + 200)}
-		/>
-	)
+	// The direction in which we create the different path parts is important, as it determines what gets clipped.
+	// See the description on the directions in the non-zero fill rule example:
+	// https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/fill-rule#nonzero
+	// We create this one in the clockwise direction
+	const boundingBoxPath = `M${toDomPrecision(bounds.minX - 100)},${toDomPrecision(bounds.minY - 100)} h${bounds.width + 200} v${bounds.height + 200} h-${bounds.width + 200} Z`
+	// We create this one in the counter-clockwise direction, which cuts out the label box
+	const labelBoxPath = `M${toDomPrecision(labelBounds.minX)},${toDomPrecision(labelBounds.minY)} v${labelBounds.height} h${labelBounds.width} v-${labelBounds.height} Z`
+	// We also append the arrowhead paths to the clip path, so that we also clip the arrowheads
+	return <path d={`${boundingBoxPath}${hasText ? labelBoxPath : ''}${as}${ae}`} />
 }
 
 const shapeAtTranslationStart = new WeakMap<

packages/tldraw/src/lib/shapes/arrow/arrowheads.ts
@@ -55,7 +55,7 @@ function getTriangleHead({ point, int }: TLArrowPointsInfo) {
 	const PL = Vec.RotWith(int, point, PI / 6)
 	const PR = Vec.RotWith(int, point, -PI / 6)
 
-	return `M ${PL.x} ${PL.y} L ${point.x} ${point.y} L ${PR.x} ${PR.y} Z`
+	return `M ${PL.x} ${PL.y} L ${PR.x} ${PR.y} L ${point.x} ${point.y} Z`
 }
 
 function getInvertedTriangleHead({ point, int }: TLArrowPointsInfo) {
@@ -83,7 +83,7 @@ function getDiamondHead({ point, int }: TLArrowPointsInfo) {
 	const PQ = Vec.Lrp(PL, PR, 0.5)
 	PQ.add(Vec.Sub(PQ, point))
 
-	return `M ${PQ.x} ${PQ.y} L ${PL.x} ${PL.y} ${point.x} ${point.y} L ${PR.x} ${PR.y} Z`
+	return `M ${PQ.x} ${PQ.y} L ${PR.x} ${PR.y} ${point.x} ${point.y} L ${PL.x} ${PL.y} Z`
 }
 
 function getSquareHead({ int, point }: TLArrowPointsInfo) {

apps/examples/e2e/tests/export-snapshots.spec.tsx
@@ -487,6 +487,56 @@ const snapshots: Snapshots = {
 			),
 		},
 	},
+	Arrows: {
+		'': {
+			Arrow1: (
+				<TL.arrow
+					start={{ x: 200, y: 0 }}
+					end={{ x: 200, y: 300 }}
+					size="xl"
+					arrowheadStart="arrow"
+					arrowheadEnd="triangle"
+				/>
+			),
+			Arrow2: (
+				<TL.arrow
+					start={{ x: 250, y: 0 }}
+					end={{ x: 250, y: 300 }}
+					size="xl"
+					arrowheadStart="square"
+					arrowheadEnd="dot"
+				/>
+			),
+			Arrow3: (
+				<TL.arrow
+					start={{ x: 300, y: 0 }}
+					end={{ x: 300, y: 300 }}
+					size="xl"
+					arrowheadStart="pipe"
+					arrowheadEnd="diamond"
+					text="with text"
+				/>
+			),
+			Arrow4: (
+				<TL.arrow
+					start={{ x: 350, y: 0 }}
+					end={{ x: 350, y: 300 }}
+					size="xl"
+					arrowheadStart="inverted"
+					arrowheadEnd="bar"
+				/>
+			),
+			Arrow5: (
+				<TL.arrow
+					start={{ x: 400, y: 0 }}
+					end={{ x: 400, y: 300 }}
+					size="xl"
+					arrowheadStart="none"
+					arrowheadEnd="none"
+				/>
+			),
+		},
+	},
 }
 
 interface SnapshotWithoutJsx {