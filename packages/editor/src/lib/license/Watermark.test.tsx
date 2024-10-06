e10ea590fae5fcbdd977765eafff8814ce7a05bf
Steve Ruiz <steveruizok@gmail.com>
2024-10-06 12:23:49 +0100

    Update Watermark.test.tsx

--- packages/editor/src/lib/license/Watermark.test.tsx
+++ packages/editor/src/lib/license/Watermark.test.tsx
@@ -8,25 +8,25 @@ jest.mock('../hooks/useLicenseManagerState', () => ({
 	useLicenseManagerState: () => mockLicenseState,
 }))
 
-async function renderWithMockedLicenseState() {
-	const result = await act(async () => render(<TldrawEditor />))
-	const elm = await waitFor(() => result.container.querySelector(`.${LicenseManager.className}`))
-	return elm
-}
-
 describe('Watermark', () => {
 	it('Displays the watermark when the editor is unlicensed', async () => {
 		mockLicenseState = 'unlicensed'
-		expect(await renderWithMockedLicenseState()).not.toBeNull()
+		const result = await act(async () => render(<TldrawEditor />))
+		const elm = await waitFor(() => result.container.querySelector(`.${LicenseManager.className}`))
+		expect(elm).not.toBeNull()
 	})
 
 	it('Displays the watermark when the editor is licensed with watermark', async () => {
 		mockLicenseState = 'licensed-with-watermark'
-		expect(await renderWithMockedLicenseState()).not.toBeNull()
+		const result = await act(async () => render(<TldrawEditor />))
+		const elm = await waitFor(() => result.container.querySelector(`.${LicenseManager.className}`))
+		expect(elm).not.toBeNull()
 	})
 
 	it('Does not display the watermark when the editor is licensed', async () => {
 		mockLicenseState = 'licensed'
-		expect(await renderWithMockedLicenseState()).toBeNull()
+		const result = await act(async () => render(<TldrawEditor />))
+		const elm = await waitFor(() => result.container.querySelector(`.${LicenseManager.className}`))
+		expect(elm).toBeNull()
 	})
 })
