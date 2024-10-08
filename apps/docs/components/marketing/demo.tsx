a09dfa1d8088e03ec688a2515a3b73d69ef26b44
Steve Ruiz <steveruizok@gmail.com>
2024-10-08 11:30:56 +0100

    Update demo.tsx

--- apps/docs/components/marketing/demo.tsx
+++ apps/docs/components/marketing/demo.tsx
@@ -6,7 +6,7 @@ export function Demo() {
 	const skeletonTldraw = <div className="absolute inset-0 bg-[#FBFCFE] cursor-pointer" />
 
 	return (
-		<div className="w-full border-t-[5px] border-b-[5px] md:border-[5px] border-blue-500 h-96 md:rounded-br-[16px] md:rounded-bl-[16px] md:rounded-tl-[16px] md:rounded-tr-[20px] sm:h-[40rem] max-h-[80vh] overflow-hidden">
+		<div className="w-full border-t-[4px] border-b-[4px] md:border-[4px] border-blue-500 h-96 md:rounded-br-[16px] md:rounded-bl-[16px] md:rounded-tl-[16px] md:rounded-tr-[20px] sm:h-[40rem] max-h-[80vh] overflow-hidden">
 			<Suspense fallback={skeletonTldraw}>
 				<DemoTldraw />
 			</Suspense>
