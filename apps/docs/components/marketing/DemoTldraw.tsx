73b15dcf1108b6204b9f8c491b19c5f3ca9c5c5e
Steve Ruiz <steveruizok@gmail.com>
2024-10-08 11:30:08 +0100

    ok

--- apps/docs/components/marketing/DemoTldraw.tsx
+++ apps/docs/components/marketing/DemoTldraw.tsx
@@ -1,18 +1,31 @@
 'use client'
 import { cn } from '@/utils/cn'
 import { getAssetUrlsByMetaUrl } from '@tldraw/assets/urls'
-import { useEffect, useRef, useState } from 'react'
+import { useTheme } from 'next-themes'
+import { useCallback, useEffect, useRef, useState } from 'react'
 import { Editor, Tldraw, TldrawOptions } from 'tldraw'
-import './demo.css'
 
 const options: Partial<TldrawOptions> = {
 	maxPages: 1,
 }
 
 const assetUrls = getAssetUrlsByMetaUrl()
-export default function DemoTldraw({ hidden }: { hidden?: boolean }) {
+
+export default function DemoTldraw() {
 	const [editor, setEditor] = useState<Editor | null>(null)
 	const wrapper = useRef<HTMLDivElement | null>(null)
+	const { theme, setTheme } = useTheme()
+
+	const handleEditorMount = useCallback((editor: Editor) => {
+		setEditor(editor)
+	}, [])
+
+	useEffect(() => {
+		if (!editor) return
+		editor.user.updateUserPreferences({
+			colorScheme: theme === 'dark' ? 'dark' : 'light',
+		})
+	}, [theme, editor])
 
 	useEffect(() => {
 		if (!editor) return
@@ -30,19 +43,18 @@ export default function DemoTldraw({ hidden }: { hidden?: boolean }) {
 		}
 	}, [editor])
 
+	const handleEditorFocus = useCallback(() => {
+		if (!editor) return
+		editor.focus({ focusContainer: false })
+	}, [editor])
+
 	return (
 		<>
-			<div
-				ref={wrapper}
-				className={cn('z-10 h-full', hidden ? 'hidden' : '')}
-				onFocus={() => {
-					editor?.focus({ focusContainer: false })
-				}}
-			>
+			<div ref={wrapper} className={cn('z-10 h-full w-full')} onFocus={handleEditorFocus}>
 				<Tldraw
 					initialState="draw"
 					assetUrls={assetUrls}
-					onMount={setEditor}
+					onMount={handleEditorMount}
 					autoFocus
 					options={options}
 				/>
--- apps/docs/components/marketing/demo.tsx
+++ apps/docs/components/marketing/demo.tsx
@@ -1,77 +1,15 @@
 'use client'
 
-import Image from 'next/image'
-import { lazy, Suspense, useState } from 'react'
-import LgBc from '../../public/images/ui-placeholder/lg-bc.jpg'
-import LgBl from '../../public/images/ui-placeholder/lg-bl.jpg'
-import LgTl from '../../public/images/ui-placeholder/lg-tl.jpg'
-import LgTr from '../../public/images/ui-placeholder/lg-tr.jpg'
-import SmBc from '../../public/images/ui-placeholder/sm-bc.jpg'
-import SmTl from '../../public/images/ui-placeholder/sm-tl.jpg'
-import { Button } from '../common/button'
+import { lazy, Suspense } from 'react'
 
 export function Demo() {
-	const [showCanvas, setShowCanvas] = useState<boolean>(false)
-	const [isLoading, setIsLoading] = useState<boolean>(false)
-
-	const onClick = () => {
-		setIsLoading(true)
-		setShowCanvas(true)
-	}
-
-	const skeletonTldraw = (
-		<div className="absolute inset-0 bg-[#FBFCFE] cursor-pointer" onClick={onClick}>
-			<Image
-				src={LgTl}
-				alt="Tldraw UI"
-				className="absolute top-0 left-0 w-[220px] h-auto hidden sm:block"
-			/>
-			<Image
-				src={LgBl}
-				alt="Tldraw UI"
-				className="absolute bottom-0 left-0 w-[100px] h-auto hidden sm:block"
-			/>
-			<Image
-				src={LgBc}
-				alt="Tldraw UI"
-				className="absolute bottom-0 left-1/2 -translate-x-1/2 w-[450px] h-auto hidden sm:block"
-			/>
-			<Image
-				src={LgTr}
-				alt="Tldraw UI"
-				className="absolute top-0 right-0 w-[164px] h-auto hidden md:block"
-			/>
-			<Image
-				src={SmTl}
-				alt="Tldraw UI"
-				className="absolute top-0 left-0 w-[164px] h-auto sm:hidden"
-			/>
-			<Image
-				src={SmBc}
-				alt="Tldraw UI"
-				className="absolute bottom-0 left-1/2 -translate-x-1/2 w-[350px] h-auto sm:hidden"
-			/>
-			<div className="absolute inset-0 bg-[#FBFCFE]/50 flex items-center justify-center">
-				<Button
-					onClick={onClick}
-					caption={isLoading ? 'Loading…' : 'Try it'}
-					icon="play"
-					className="shadow"
-				/>
-			</div>
-		</div>
-	)
+	const skeletonTldraw = <div className="absolute inset-0 bg-[#FBFCFE] cursor-pointer" />
 
 	return (
-		<div className="w-full">
-			<div className="w-full bg-blue-500 py-1 md:rounded-2xl md:px-1 mt-1 h-96 sm:h-[40rem] max-h-[80vh]">
-				<div className="relative w-full h-full overflow-hidden bg-white shadow md:rounded-xl">
-					{!showCanvas && skeletonTldraw}
-					<Suspense fallback={skeletonTldraw}>
-						<DemoTldraw hidden={!showCanvas} />
-					</Suspense>
-				</div>
-			</div>
+		<div className="w-full border-t-[5px] border-b-[5px] md:border-[5px] border-blue-500 h-96 md:rounded-br-[16px] md:rounded-bl-[16px] md:rounded-tl-[16px] md:rounded-tr-[20px] sm:h-[40rem] max-h-[80vh] overflow-hidden">
+			<Suspense fallback={skeletonTldraw}>
+				<DemoTldraw />
+			</Suspense>
 		</div>
 	)
 }
