b91b97fcade5773f89097e6ff28b1d6223e7a1ab
Steve Ruiz <steveruizok@gmail.com>
2024-10-09 11:04:23 +0100

    ok

--- apps/dotcom/client/src/utils/migration/DebugMenuItems.tsx
+++ apps/dotcom/client/src/utils/migration/DebugMenuItems.tsx
@@ -7,15 +7,6 @@ export function DebugMenuItems() {
 	return (
 		<TldrawUiMenuGroup id="release">
 			<TldrawUiMenuItem
-				id="v1"
-				label="Test v1 content"
-				onSelect={async () => {
-					const { writeV1ContentsToIdb } = await import('./writeV1ContentsToIdb')
-					await writeV1ContentsToIdb()
-					window.location.reload()
-				}}
-			/>
-			<TldrawUiMenuItem
 				id="release-info"
 				label={'Release info'}
 				onSelect={() => {
