65f8dab07e5f4aa6a3efeddb3d6f3b691d628c17
Mitja Bezenšek <mitja.bezensek@gmail.com>
2024-10-22 12:22:41 +0200

    Limit the page name length in the move to page menu.

--- packages/tldraw/src/lib/ui/components/menu-items.tsx
+++ packages/tldraw/src/lib/ui/components/menu-items.tsx
@@ -437,7 +437,7 @@ export function MoveToPageMenu() {
 						id={page.id}
 						key={page.id}
 						disabled={currentPageId === page.id}
-						label={page.name}
+						label={page.name.length > 30 ? `${page.name.slice(0, 30)}\u2026` : page.name}
 						onSelect={() => {
 							editor.markHistoryStoppingPoint('move_shapes_to_page')
 							editor.moveShapesToPage(editor.getSelectedShapeIds(), page.id as TLPageId)
