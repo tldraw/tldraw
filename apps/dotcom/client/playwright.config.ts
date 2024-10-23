f0e7d48d645d3a9e7c2572daa568a775438596af
Mitja Bezenšek <mitja.bezensek@gmail.com>
2024-10-23 18:05:48 +0200

    Try runing it in paralel.

--- apps/dotcom/client/playwright.config.ts
+++ apps/dotcom/client/playwright.config.ts
@@ -72,10 +72,10 @@ export default defineConfig({
 	],
 
 	/* Run your local dev server before starting the tests */
-	webServer: {
-		command: 'yarn dev',
-		url: 'http://localhost:3000',
-		stdout: 'pipe',
-		reuseExistingServer: !process.env.CI,
-	},
+	// webServer: {
+	// 	command: 'yarn dev',
+	// 	url: 'http://localhost:3000',
+	// 	stdout: 'pipe',
+	// 	reuseExistingServer: !process.env.CI,
+	// },
 })
