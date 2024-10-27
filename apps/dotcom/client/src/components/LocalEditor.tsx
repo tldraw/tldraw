2345d9fea532f7a5c738f610f1f9af363e46ec4a
Steve Ruiz <steveruizok@gmail.com>
2024-10-27 18:09:47 +0000

    Make default color theme light. (#4796)
    This PR:
    - makes the default user preference light, rather than system
    - updates the theme when the media theme changes only if the user has
    selected system
    
    ## Rationale
    
    Our light theme is better and presents a better initial experience of
    the app.
    Our logged out pages only support light mode and we don't want to show a
    dark editor on a light page.
    Our docs landing page has light mode as its default and we don't want to
    show a dark editor on a light page.
    
    ### Change type
    
    - [ ] `bugfix`
    - [x] `improvement`
    - [ ] `feature`
    - [ ] `api`
    - [ ] `other`
    
    ### Release notes
    
    - Sets the default color theme to light.

--- apps/dotcom/client/src/components/LocalEditor.tsx
+++ apps/dotcom/client/src/components/LocalEditor.tsx
@@ -103,7 +103,6 @@ export function LocalEditor() {
 				overrides={[sharingUiOverrides, fileSystemUiOverrides]}
 				onUiEvent={handleUiEvent}
 				components={components}
-				inferDarkMode
 			>
 				<SneakyOnDropOverride isMultiplayer={false} />
 				<ThemeUpdater />
