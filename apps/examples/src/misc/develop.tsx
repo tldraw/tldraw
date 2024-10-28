59a0b1856e3fcdc2176fbfa65b0130824e12ba77
Mitja Bezenšek <mitja.bezensek@gmail.com>
2024-10-28 14:06:06 +0100

    WIP

--- apps/examples/src/misc/develop.tsx
+++ apps/examples/src/misc/develop.tsx
@@ -60,7 +60,7 @@ export default function Develop() {
 				overrides={[performanceOverrides, debuggingOverrides]}
 				persistenceKey="example"
 				onMount={(editor) => {
-					console.log('editor mounted 2')
+					console.log('editor mounted 3')
 					;(window as any).app = editor
 					;(window as any).editor = editor
 					const dispose = editor.store.sideEffects.registerAfterChangeHandler(
