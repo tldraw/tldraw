9e4317e9f38f8bf7fbe5633f62c7b6b9ac09a9f3
David Sheldrick <d.j.sheldrick@gmail.com>
2024-10-28 12:34:20 +0000

    better structure

--- apps/dotcom/client/src/tla/components/TlaFileError/TlaFileError.tsx
+++ apps/dotcom/client/src/tla/components/TlaFileError/TlaFileError.tsx
@@ -1,47 +1,71 @@
 import { TLRemoteSyncError, TLSyncErrorCloseEventReason } from '@tldraw/sync-core'
 import { ReactElement, useEffect } from 'react'
 import { sadFaceIcon } from '../../../components/ErrorPage/ErrorPage'
-import { notFound } from '../../../pages/not-found'
 import { useSetIsReady } from '../../hooks/useIsReady'
-import { TlaNotFoundError } from '../../utils/notFoundError'
 import { TlaSignInButton } from '../TlaSignInButton/TlaSignInButton'
 import styles from './TlaFileError.module.css'
+
+function DefaultError() {
+	return (
+		<TlaFileErrorContent
+			header="Something went wrong"
+			para1="Please try refreshing the page."
+			para2="Still having trouble? Let us know at hello@tldraw.com"
+		/>
+	)
+}
 
 export function TlaFileError({ error }: { error: unknown }) {
 	const setIsReady = useSetIsReady()
 	useEffect(() => {
 		setIsReady()
 	}, [setIsReady])
-	let header = 'Something went wrong'
-	let para1 =
-		'Please try refreshing the page. Still having trouble? Let us know at hello@tldraw.com.'
-	let para2 = ''
-	let cta = null as ReactElement | null
-	if (error instanceof TLRemoteSyncError) {
-		switch (error.reason) {
-			case TLSyncErrorCloseEventReason.NOT_FOUND: {
-				header = 'Not found'
-				para1 = 'The file you are looking for does not exist.'
-				break
-			}
-			case TLSyncErrorCloseEventReason.NOT_AUTHENTICATED: {
-				header = 'Private file'
-				para1 = 'Contact the file owner to request access.'
-				para2 = 'Or sign in if you are the file owner.'
-				cta = <TlaSignInButton />
-				break
-			}
-			case TLSyncErrorCloseEventReason.FORBIDDEN: {
-				header = 'Private file'
-				para1 = 'Contact the file owner to request access.'
-				break
-			}
-		}
-	}
-	if (error instanceof TlaNotFoundError) {
-		return notFound()
-	}
 
+	if (!(error instanceof TLRemoteSyncError)) return <DefaultError />
+
+	switch (error.reason) {
+		case TLSyncErrorCloseEventReason.NOT_FOUND: {
+			return (
+				<TlaFileErrorContent
+					header="Not found"
+					para1="The file you are looking for does not exist."
+				/>
+			)
+		}
+		case TLSyncErrorCloseEventReason.NOT_AUTHENTICATED: {
+			return (
+				<TlaFileErrorContent
+					header="Private file"
+					para1="Contact the file owner to request access."
+					para2="Or sign in if you are the file owner."
+					cta={<TlaSignInButton />}
+				/>
+			)
+		}
+		case TLSyncErrorCloseEventReason.FORBIDDEN: {
+			return (
+				<TlaFileErrorContent
+					header="Private file"
+					para1={`Contact the file owner to request access.`}
+				/>
+			)
+		}
+		default:
+			return <DefaultError />
+	}
+}
+
+function TlaFileErrorContent({
+	header,
+	para1,
+	para2,
+	cta,
+}: {
+	header: string
+	para1: string
+	para2?: string
+	cta?: ReactElement
+}) {
 	return (
 		<div className={styles.container}>
 			{sadFaceIcon}
