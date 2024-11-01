5e4d07bb104f0124a1bae17c353cb94256b4811c
David Sheldrick <d.j.sheldrick@gmail.com>
2024-11-01 14:24:16 +0000

    Call ensureStoreIsUsable after mergeRemoteChanges (#4833)
    `ensureStoreIsUsable` is marked internal and should probably stay that
    way? But it makes sense to always call it after `mergeRemoteChanges`
    (which we already do)
    
    ### Change type
    
    
    - [x] `improvement`
    
    ### Release notes
    
    - Add store consistency checks during `mergeRemoteChanges`

--- packages/sync-core/src/lib/TLSyncClient.ts
+++ packages/sync-core/src/lib/TLSyncClient.ts
@@ -391,7 +391,6 @@ export class TLSyncClient<R extends UnknownRecord, S extends Store<R> = Store<R>
 			// this.isConnectedToRoom = true
 			// this.store.applyDiff(stashedChanges, false)
 
-			this.store.ensureStoreIsUsable()
 			this.onAfterConnect?.(this, { isReadonly: event.isReadonly })
 		})
 
@@ -645,7 +644,6 @@ export class TLSyncClient<R extends UnknownRecord, S extends Store<R> = Store<R>
 					this.resetConnection()
 				}
 			})
-			this.store.ensureStoreIsUsable()
 			this.lastServerClock = diffs.at(-1)?.serverClock ?? this.lastServerClock
 		} catch (e) {
 			console.error(e)
