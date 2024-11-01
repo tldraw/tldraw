b83d32ecf6b0a00c9a4daafc19d11ea94688ce7a
David Sheldrick <d.j.sheldrick@gmail.com>
2024-11-01 13:56:37 +0000

    add border to onlySelectedShape

--- apps/examples/src/examples/basic/BasicExample.tsx
+++ apps/examples/src/examples/basic/BasicExample.tsx
@@ -1,10 +1,34 @@
-import { Tldraw } from 'tldraw'
+import { useRef } from 'react'
+import { Tldraw, react } from 'tldraw'
 import 'tldraw/tldraw.css'
+import './style.css'
 
 export default function BasicExample() {
+	const ref = useRef<HTMLDivElement>(null)
 	return (
-		<div className="tldraw__editor">
-			<Tldraw persistenceKey="example" />
+		<div className="tldraw__editor" ref={ref}>
+			<Tldraw
+				persistenceKey="example"
+				onMount={(editor) => {
+					let prevShape = editor.getOnlySelectedShape()
+					return react('update onlySelectedShape border', () => {
+						const shape = editor.getOnlySelectedShape()
+						if (shape !== prevShape) {
+							if (prevShape) {
+								ref.current
+									?.querySelector(`[data-shape-id="${prevShape.id}"]`)
+									?.classList.remove('with-padlet-border')
+							}
+							if (shape) {
+								ref.current
+									?.querySelector(`[data-shape-id="${shape.id}"]`)
+									?.classList.add('with-padlet-border')
+							}
+						}
+						prevShape = shape
+					})
+				}}
+			/>
 		</div>
 	)
 }
