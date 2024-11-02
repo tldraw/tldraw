4727ece6d9a93c9b8275db34d40367eeea13804d
Steve Ruiz <steveruizok@gmail.com>
2024-11-02 11:08:44 +0000

    Update not-logged-in.spec.ts

--- apps/dotcom/client/e2e/tests/not-logged-in.spec.ts
+++ apps/dotcom/client/e2e/tests/not-logged-in.spec.ts
@@ -25,3 +25,15 @@ todo('can not visit an unshared file', async () => {
 todo('can scroll down to see landing page content', async () => {
 	// ...
 })
+
+todo('can export images', async () => {
+	// ...
+})
+
+todo('when visiting a shared file, can copy the shared file link', async () => {
+	// ...
+})
+
+todo('when visiting a published file, can copy the published file link', async () => {
+	// ...
+})
