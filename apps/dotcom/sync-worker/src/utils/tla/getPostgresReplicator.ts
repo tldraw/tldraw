5e6b5bc46602125391c6d4c1222abd72349a6301
Mitja Bezenšek <mitja.bezensek@gmail.com>
2024-11-13 15:26:45 +0100

    Add comment.

--- apps/dotcom/sync-worker/src/utils/tla/getPostgresReplicator.ts
+++ apps/dotcom/sync-worker/src/utils/tla/getPostgresReplicator.ts
@@ -3,5 +3,6 @@ import { Environment } from '../../types'
 
 export function getPostgresReplicator(env: Environment) {
 	const id = env.TL_PG_REPLICATOR.idFromName('0')
+	// add a location hint so that the replicator lives closer to our db instance
 	return env.TL_PG_REPLICATOR.get(id, { locationHint: 'weur' }) as any as TLPostgresReplicator
 }
