36b4b7f78f0b0886d456139b9e7c1d864eedd0ce
Steve Ruiz <steveruizok@gmail.com>
2024-11-13 10:09:35 +0000

    Update develop.tsx

--- apps/examples/src/misc/develop.tsx
+++ apps/examples/src/misc/develop.tsx
@@ -1,7 +1,10 @@
 import { getLicenseKey } from '@tldraw/dotcom-shared'
 import {
+	Box,
 	DefaultContextMenu,
 	DefaultContextMenuContent,
+	TLArrowBinding,
+	TLArrowShape,
 	TLComponents,
 	Tldraw,
 	TldrawUiMenuActionCheckboxItem,
@@ -9,6 +12,7 @@ import {
 	TldrawUiMenuGroup,
 	track,
 	useEditor,
+	useValue,
 } from 'tldraw'
 import 'tldraw/tldraw.css'
 import { trackedShapes, useDebugging } from '../hooks/useDebugging'
@@ -40,6 +44,7 @@ const ContextMenu = track(() => {
 
 const components: TLComponents = {
 	ContextMenu,
+	OnTheCanvas: AllArrowsDebugDisplay,
 }
 
 function afterChangeHandler(prev: any, next: any) {
@@ -73,5 +78,129 @@ export default function Develop() {
 				components={components}
 			/>
 		</div>
+	)
+}
+
+const MINIMUM_ARM_LENGTH = 20
+
+interface ArrowDebugInfo {
+	expandedBounds: Box
+	centerBounds: Box
+}
+
+function AllArrowsDebugDisplay() {
+	const editor = useEditor()
+	const arrowInfos = useValue(
+		'arrowShapes',
+		() => {
+			const results: ArrowDebugInfo[] = []
+
+			editor
+				.getCurrentPageShapesSorted()
+				.filter((s) => editor.isShapeOfType<TLArrowShape>(s, 'arrow'))
+				.forEach((arrow) => {
+					const bindings = editor.getBindingsFromShape(arrow, 'arrow') as TLArrowBinding[]
+					if (bindings.length === 0) return
+					if (bindings.length === 1) return
+
+					const startBinding = bindings.find((b) => b.props.terminal === 'start')
+					if (!startBinding) return
+					const startShape = editor.getShape(startBinding.toId)
+					if (!startShape) return
+					const startBounds = editor.getShapePageBounds(startShape)
+					if (!startBounds) return
+
+					const endBinding = bindings.find((b) => b.props.terminal === 'end')
+					if (!endBinding) return
+					const endShape = editor.getShape(endBinding.toId)
+					if (!endShape) return
+					const endBounds = editor.getShapePageBounds(endShape)
+					if (!endBounds) return
+
+					const expandedBounds = Box.Common([startBounds, endBounds]).expandBy(MINIMUM_ARM_LENGTH)
+					const centerBounds = Box.FromPoints([startBounds.center, endBounds.center])
+
+					results.push({
+						expandedBounds,
+						centerBounds,
+					})
+				})
+
+			return results
+		},
+		[editor]
+	)
+
+	if (arrowInfos.length === 0) return null
+
+	return arrowInfos.map((info) => (
+		<ArrowDebugDisplay key={info.expandedBounds.toString()} info={info} />
+	))
+}
+
+function ArrowDebugDisplay({ info }: { info: ArrowDebugInfo }) {
+	const { centerBounds: box1, expandedBounds: box2 } = info
+	const box1Corners = box1.corners
+	const box2Corners = box2.corners
+	return (
+		<>
+			{box1Corners.map((c) => (
+				<Dot key={c.toString()} x={c.x} y={c.y} color="blue" />
+			))}
+			<Rect x={box1.x} y={box1.y} w={box1.w} h={box1.h} color="red" />
+			{box2Corners.map((c) => (
+				<Dot key={c.toString()} x={c.x} y={c.y} color="red" />
+			))}
+			<Rect x={box2.x} y={box2.y} w={box2.w} h={box2.h} color="blue" />
+			<Dot x={box1.x} y={box2.y} color="orange" />
+			<Dot x={box2.x} y={box1.y} color="orange" />
+			<Dot x={box1.x} y={box2.maxY} color="orange" />
+			<Dot x={box2.maxX} y={box1.maxY} color="orange" />
+			<Dot x={box1.maxY} y={box2.maxY} color="orange" />
+			<Dot x={box1.maxX} y={box2.y} color="orange" />
+			<Dot x={box2.maxX} y={box1.y} color="orange" />
+			<Dot x={box2.x} y={box1.maxY} color="orange" />
+		</>
+	)
+}
+
+function Dot({ x, y, color }: { x: number; y: number; color: string }) {
+	return (
+		<div
+			style={{
+				width: 8,
+				height: 8,
+				background: color,
+				borderRadius: '100%',
+				position: 'absolute',
+				transform: `translate(-50%, -50%) translate(${x}px, ${y}px)`,
+			}}
+		/>
+	)
+}
+
+function Rect({
+	x,
+	y,
+	w,
+	h,
+	color,
+}: {
+	x: number
+	y: number
+	w: number
+	h: number
+	color: string
+}) {
+	return (
+		<div
+			style={{
+				width: w,
+				height: h,
+				border: `2px dashed ${color}`,
+				position: 'absolute',
+				transform: `translate(${x}px, ${y}px)`,
+			}}
+		/>
 	)
 }
