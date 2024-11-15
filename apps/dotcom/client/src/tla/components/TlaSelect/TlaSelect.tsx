caf69919be40ae988ec5a830ef576f5650cb1d74
Mime Čuvalo <mimecuvalo@gmail.com>
2024-11-15 12:07:00 +0000

    fix up select in popper clicks, annoying

--- apps/dotcom/client/src/tla/components/TlaSelect/TlaSelect.tsx
+++ apps/dotcom/client/src/tla/components/TlaSelect/TlaSelect.tsx
@@ -1,5 +1,5 @@
 import * as Select from '@radix-ui/react-select'
-import { ReactNode, useCallback } from 'react'
+import { ReactNode, useCallback, useEffect, useState } from 'react'
 import { TlaIcon } from '../TlaIcon/TlaIcon'
 import styles from './select.module.css'
 
@@ -16,6 +16,7 @@ export function TlaSelect<T extends string>({
 	onChange(value: T): void
 	options: { value: T; label: ReactNode }[]
 }) {
+	const [isOpen, setIsOpen] = useState(false)
 	const handleChange = useCallback(
 		(value: string) => {
 			onChange(value as T)
@@ -23,15 +24,41 @@ export function TlaSelect<T extends string>({
 		[onChange]
 	)
 
+	const handleOpenChange = (open: boolean) => {
+		setIsOpen(open)
+	}
+
+	useEffect(() => {
+		if (!isOpen) return
+		// Close the select menu when the user clicks outside of it
+		// This is a workaround for an issue in Radix Select when combined with a popper menu.
+		const handleMouseDown = (event: MouseEvent) => {
+			const target = event.target as HTMLElement
+			if (!target.closest(`.${styles.content}`)) {
+				setIsOpen(false)
+			}
+		}
+
+		document.body.addEventListener('mousedown', handleMouseDown)
+		return () => {
+			document.body.removeEventListener('mousedown', handleMouseDown)
+		}
+	}, [isOpen])
+
 	return (
-		<Select.Root value={value} onValueChange={handleChange}>
+		<Select.Root
+			open={isOpen}
+			value={value}
+			onOpenChange={handleOpenChange}
+			onValueChange={handleChange}
+		>
 			<Select.Trigger className={styles.trigger} disabled={disabled} aria-label={label}>
 				<span className={styles.label}>{label}</span>
 				<Select.Icon>
 					<TlaIcon icon="chevron-down" className={styles.chevron} />
 				</Select.Icon>
 			</Select.Trigger>
-			<Select.Content className={styles.content} asChild position="popper">
+			<Select.Content className={styles.content}>
 				<div>
 					{options.map((option) => (
 						<Select.Item key={option.value} className={styles.option} value={option.value}>
