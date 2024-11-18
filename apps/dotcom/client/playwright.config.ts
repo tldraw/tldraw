78cbe815162dce9ff8ccacd2c2d088aceaeccff4
Mitja Bezenšek <mitja.bezensek@gmail.com>
2024-11-18 13:13:28 +0100

    Increase timeout.

--- apps/dotcom/client/playwright.config.ts
+++ apps/dotcom/client/playwright.config.ts
@@ -28,7 +28,7 @@ export default defineConfig({
 	/* Reporter to use. See https://playwright.dev/docs/test-reporters */
 	reporter: process.env.CI ? [['list'], ['github'], ['html', { open: 'never' }]] : 'list',
 	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-	timeout: 20 * 1000,
+	timeout: 30 * 1000,
 	use: {
 		/* Base URL to use in actions like `await page.goto('/')`. */
 		// baseURL: 'http://127.0.0.1:3000',
