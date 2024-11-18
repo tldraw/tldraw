98216595b639f1f5d54c0a5595f6a33441d150a6
Mitja Bezenšek <mitja.bezensek@gmail.com>
2024-11-18 17:03:12 +0100

    botcom e2e tests (#4852)
    * Makes e2e test more stable.
    * Brings the e2e (dotcom) workflow.
    * Allows parallelization of tests by using multiple users and isolating
    the tests in the db. It seems to work fine till around 4 workers, then
    you might run into some postgres deadlocks.
    * Improves [clerk usage in
    tests](https://clerk.com/docs/testing/playwright/overview).
    * Adds missing tests.
    * Pulls in @Taha-Hassan-Git tests from
    https://github.com/tldraw/tldraw/pull/4827
    * Adds `@step('step name')` helper decorator that can be use in fixtures
    to make the test output a bit less noisy.
    * Adds `repeatTest` helper which allows you to run a single test
    multiple times. It's a 1:1 replacement of `test` (so you can also use it
    with `.only`) to make it easier to switch between the two.
    
    ### Change type
    
    - [ ] `bugfix`
    - [x] `improvement`
    - [ ] `feature`
    - [ ] `api`
    - [ ] `other`
    
    ### Release notes
    
    - Bring back dotcom e2e tests, make them more stable, and add a few more
    of them.

--- apps/dotcom/client/e2e/fixtures/tla-test.ts
+++ apps/dotcom/client/e2e/fixtures/tla-test.ts
@@ -1,9 +1,14 @@
-import { test as base } from '@playwright/test'
+import { setupClerkTestingToken } from '@clerk/testing/playwright'
+import { test as base, expect } from '@playwright/test'
+import fs from 'fs'
+import { OTHER_USERS, USERS } from '../consts'
 import { Database } from './Database'
+import { DeleteFileDialog } from './DeleteFileDialog'
 import { Editor } from './Editor'
 import { HomePage } from './HomePage'
 import { ShareMenu } from './ShareMenu'
 import { Sidebar } from './Sidebar'
+import { getStorageStateFileName } from './helpers'
 
 interface TlaFixtures {
 	homePage: HomePage
@@ -11,24 +16,123 @@ interface TlaFixtures {
 	sidebar: Sidebar
 	shareMenu: ShareMenu
 	database: Database
+	deleteFileDialog: DeleteFileDialog
+	setupAndCleanup: void
+	retry(fn: () => Promise<void>): Promise<void>
 }
 
-export const test = base.extend<TlaFixtures>({
+interface TlaWorkerFixtures {
+	workerStorageState: string
+}
+
+export const test = base.extend<TlaFixtures, TlaWorkerFixtures>({
 	sidebar: async ({ page }, testUse) => {
-		testUse(new Sidebar(page))
+		await testUse(new Sidebar(page))
 	},
 	editor: async ({ page, sidebar }, testUse) => {
-		testUse(new Editor(page, sidebar))
+		await testUse(new Editor(page, sidebar))
 	},
 	homePage: async ({ page, editor }, testUse) => {
-		testUse(new HomePage(page, editor))
+		await testUse(new HomePage(page, editor))
 	},
 	database: async ({ page }, testUse) => {
-		testUse(new Database(page))
+		await testUse(new Database(page, test.info().parallelIndex))
 	},
 	shareMenu: async ({ page }, testUse) => {
-		testUse(new ShareMenu(page))
+		await testUse(new ShareMenu(page))
 	},
+	deleteFileDialog: async ({ page }, testUse) => {
+		await testUse(new DeleteFileDialog(page))
+	},
+	// This is an auto fixture which makes sure that we are on the home page when the test starts
+	// and that we clean up when the tests completes
+	setupAndCleanup: [
+		async ({ page, homePage, database }, testUse) => {
+			// All the code before testUse is run before each test
+
+			// Make sure we don't get marked as a bot
+			// https://clerk.com/docs/testing/playwright/overview
+			await setupClerkTestingToken({ page })
+
+			await homePage.goto()
+			await homePage.isLoaded()
+
+			await testUse()
+
+			// All the code after testUse is run after each test
+			await database.reset()
+		},
+		{ auto: true },
+	],
+	storageState: ({ workerStorageState }, testUse) => testUse(workerStorageState),
+	workerStorageState: [
+		async ({ browser }, testUse) => {
+			const id = test.info().parallelIndex
+
+			const fileName = getStorageStateFileName(id, 'huppy')
+			if (fs.existsSync(fileName)) {
+				// Reuse existing authentication state if any.
+				await testUse(fileName)
+				return
+			}
+			for (const user of ['huppy' as const, 'suppy' as const]) {
+				const fileName = getStorageStateFileName(id, user)
+				// Important: make sure we authenticate in a clean environment by unsetting storage state.
+				const page = await browser.newPage({ storageState: undefined })
+				let email: string
+				if (user === 'huppy') {
+					email = USERS[id]
+				} else {
+					email = OTHER_USERS[id]
+				}
+				const sidebar = new Sidebar(page)
+				const editor = new Editor(page, sidebar)
+				const homePage = new HomePage(page, editor)
+				await homePage.loginAs(email)
+				await expect(page.getByTestId('tla-sidebar-layout')).toBeVisible()
+
+				await page.context().storageState({ path: fileName })
+				await page.close()
+			}
+			await testUse(fileName)
+		},
+		{ scope: 'worker' },
+	],
 })
 
 export { expect } from '@playwright/test'
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
+export function step(target: Function, context: any) {
+	if (context.kind === 'method') {
+		return async function (...args: any[]) {
+			// @ts-expect-error Parameter 'this' implicitly has an 'any' type.ts(7006)
+			return await test.step(`${this.constructor.name}.${context.name}`, async () => {
+				// @ts-expect-error Parameter 'this' implicitly has an 'any' type.ts(7006)
+				return target.apply(this, args)
+			})
+		}
+	} else {
+		console.error('Only supporting methods for step decorator.')
+	}
+}
+
+export function repeatTest(
+	name: string,
+	fn: (...args: any) => Promise<void>,
+	{ times = 5, only = false } = {}
+) {
+	const getName = (i: number) => `${name} (${i + 1} of ${times})`
+	for (let i = 0; i < times; i++) {
+		if (only) {
+			// eslint-disable-next-line no-only-tests/no-only-tests
+			test.only(getName(i), fn)
+		} else {
+			test(getName(i), fn)
+		}
+	}
+	return
+}
+
+repeatTest.only = (name: string, fn: (...args: any) => Promise<void>, { times = 5 } = {}) =>
+	repeatTest(name, fn, { times, only: true })
--- apps/dotcom/client/e2e/tests/database.spec.ts
+++ apps/dotcom/client/e2e/tests/database.spec.ts
@@ -1,23 +1,33 @@
+import { Editor } from '../fixtures/Editor'
+import { HomePage } from '../fixtures/HomePage'
+import { Sidebar } from '../fixtures/Sidebar'
 import { expect, test } from '../fixtures/tla-test'
 
-test.beforeEach(async ({ homePage }) => {
-	await homePage.goto()
-	await homePage.isLoaded()
-})
-
-test('correctly clears db', async ({ editor, homePage, sidebar, database }) => {
-	await editor.ensureSidebarOpen()
-	const currentCount = await sidebar.getNumberOfFiles()
+test('correctly clears db ', async ({ page, browser, editor, sidebar, database }) => {
 	const documentsToAdd = 3
-	for (let i = 0; i < documentsToAdd; i++) {
-		await sidebar.createNewDocument()
-	}
-	const newCount = await sidebar.getNumberOfFiles()
-	expect(newCount).toBe(currentCount + documentsToAdd)
+	await test.step(`create ${documentsToAdd} files file`, async () => {
+		await editor.ensureSidebarOpen()
+		const currentCount = await sidebar.getNumberOfFiles()
+		for (let i = 0; i < documentsToAdd; i++) {
+			await sidebar.createNewDocument()
+		}
+		const newCount = await sidebar.getNumberOfFiles()
+		expect(newCount).toBe(currentCount + documentsToAdd)
+	})
 
-	await database.reset()
-	await homePage.goto()
-	await homePage.isLoaded()
-	const afterReset = await sidebar.getNumberOfFiles()
-	expect(afterReset).toBe(1)
+	await test.step('close the page and reset db', async () => {
+		await page.close()
+		await database.reset()
+	})
+
+	await test.step('check that files reset', async () => {
+		const newPage = await browser.newPage()
+		const newSidebar = new Sidebar(newPage)
+		const newEditor = new Editor(newPage, newSidebar)
+		const newHomePage = new HomePage(newPage, newEditor)
+		await newHomePage.goto()
+		await newHomePage.isLoaded()
+		const afterReset = await newSidebar.getNumberOfFiles()
+		expect(afterReset).toBe(1)
+	})
 })
