a50aa129989128f63f3238d88c79f2d732ee49bc
Steve Ruiz <steveruizok@gmail.com>
2024-11-19 11:18:03 +0000

    ok

--- packages/editor/src/lib/components/default-components/DefaultErrorFallback.tsx
+++ packages/editor/src/lib/components/default-components/DefaultErrorFallback.tsx
@@ -168,18 +168,8 @@ My browser: ${navigator.userAgent}`
 						<h2>Something&apos;s gone wrong.</h2>
 						<p>
 							Sorry, we encountered an error. Please refresh the page to continue. If you keep
-							seeing this error, you can{' '}
-							<a href={url.toString()} style={{ color: 'blue', textDecoration: 'underline' }}>
-								create a GitHub issue
-							</a>{' '}
-							or{' '}
-							<a
-								href="https://discord.gg/Cq6cPsTfNy"
-								style={{ color: 'blue', textDecoration: 'underline' }}
-							>
-								ask for help on Discord
-							</a>
-							.
+							seeing this error, you can create a <a href={url.toString()}>GitHub issue</a> or ask
+							for help on <a href="https://discord.gg/Cq6cPsTfNy">Discord</a>.
 						</p>
 						{shouldShowError && (
 							<>
