e8e1ce0f2483eb1f62081ac9fb5241198a04fb05
Mitja Bezenšek <mitja.bezensek@gmail.com>
2024-12-03 16:51:35 +0100

    Log.

--- apps/dotcom/sync-worker/src/utils/analytics.ts
+++ apps/dotcom/sync-worker/src/utils/analytics.ts
@@ -12,6 +12,7 @@ export function writeDataPoint(
 	name: string,
 	{ blobs, indexes, doubles }: EventData
 ) {
+	console.log('writeDataPoint', name, blobs, indexes, doubles)
 	measure?.writeDataPoint({
 		blobs: [name, env.WORKER_NAME ?? 'development-tldraw-multiplayer', ...(blobs ?? [])],
 		doubles,
