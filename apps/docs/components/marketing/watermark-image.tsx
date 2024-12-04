401d22248c83553f2352063972238642001df75c
alex <alex@dytry.ch>
2024-12-04 14:13:50 +0000

    alex/landing: landing dark mode fixes

--- apps/docs/components/marketing/watermark-image.tsx
+++ apps/docs/components/marketing/watermark-image.tsx
@@ -1,14 +1,20 @@
 'use client'
+import WatermarkDark from '@/public/watermark-desktop-dark.svg'
+import Watermark from '@/public/watermark-desktop.svg'
 import { useTheme } from 'next-themes'
+import Image from 'next/image'
 
 export function WatermarkImage() {
 	const { theme } = useTheme()
+	console.log(theme)
 	return (
 		<div className="watermark">
-			<img
+			<Image src={WatermarkDark} alt="Watermark" className="hidden dark:block" height={80} />
+			<Image src={Watermark} alt="Watermark" className="block dark:hidden" height={80} />
+			{/* <img
 				src={theme === 'dark' ? '/watermark-desktop-dark.svg' : '/watermark-desktop.svg'}
 				className="h-20"
-			/>
+			/> */}
 		</div>
 	)
 }
