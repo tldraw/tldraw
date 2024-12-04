0c3a919177c8900bfb5462662b3b55f0187c22d5
alex <alex@dytry.ch>
2024-12-04 14:16:34 +0000

    alex/landing: cleanup

--- apps/docs/components/marketing/watermark-image.tsx
+++ apps/docs/components/marketing/watermark-image.tsx
@@ -6,15 +6,10 @@ import Image from 'next/image'
 
 export function WatermarkImage() {
 	const { theme } = useTheme()
-	console.log(theme)
 	return (
 		<div className="watermark">
 			<Image src={WatermarkDark} alt="Watermark" className="hidden dark:block" height={80} />
 			<Image src={Watermark} alt="Watermark" className="block dark:hidden" height={80} />
-			{/* <img
-				src={theme === 'dark' ? '/watermark-desktop-dark.svg' : '/watermark-desktop.svg'}
-				className="h-20"
-			/> */}
 		</div>
 	)
 }
