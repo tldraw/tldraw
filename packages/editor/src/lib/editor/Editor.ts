tldraw/tldraw

commit 760bc82c20f18c1eb28e82d6c1ec4a7680c43b09

Lod Perf (effect scheduler) (#4682)
This PR aims to improve on the LOD performance of image and video
elements. Its main goal is to reduce the number of renders caused by the
hooks in `useAsset`. It does this by moving the majority of the logic
into an effect scheduler (via our `react`), keeping the result (url and
asset) in React state, and only updating the result it has actually
changed. We also debounce subsequent renders

### Change type

- [x] `improvement`

### Release notes

- The `useAssets` hook now is now specifically for `TLImageAsset`- or
`TLVideoAsset`-type assets.

packages/editor/src/lib/editor/Editor.ts
@@ -4128,8 +4128,8 @@ export class Editor extends EventEmitter<TLEventMap> {
 	 *
 	 * @public
 	 */
-	getAsset(asset: TLAssetId | TLAsset): TLAsset | undefined {
-		return this.store.get(typeof asset === 'string' ? asset : asset.id) as TLAsset | undefined
+	getAsset<T extends TLAsset>(asset: T | T['id']): T | undefined {
+		return this.store.get(typeof asset === 'string' ? asset : asset.id) as T | undefined
 	}
 
 	async resolveAssetUrl(

packages/tldraw/src/lib/shapes/video/VideoShapeUtil.tsx
@@ -47,7 +47,6 @@ export class VideoShapeUtil extends BaseBoxShapeUtil<TLVideoShape> {
 		const { asset, url } = useAsset({
 			shapeId: shape.id,
 			assetId: shape.props.assetId,
-			width: shape.props.w,
 		})
 
 		return <VideoShape editor={this.editor} shape={shape} asset={asset} url={url} />
@@ -172,9 +171,7 @@ const VideoShape = memo(function VideoShape({
 					</div>
 				</div>
 			</HTMLContainer>
-			{'url' in shape.props && shape.props.url && (
-				<HyperlinkButton url={shape.props.url} zoomLevel={editor.getZoomLevel()} />
-			)}
+			{'url' in shape.props && shape.props.url && <HyperlinkButton url={shape.props.url} />}
 		</>
 	)
 })

packages/tldraw/src/lib/shapes/bookmark/BookmarkShapeUtil.tsx
@@ -147,9 +147,7 @@ function BookmarkShapeComponent({
 						) : (
 							<div className="tl-bookmark__placeholder" />
 						)}
-						{asset?.props.image && (
-							<HyperlinkButton url={shape.props.url} zoomLevel={util.editor.getZoomLevel()} />
-						)}
+						{asset?.props.image && <HyperlinkButton url={shape.props.url} />}
 					</div>
 				)}
 				<div className="tl-bookmark__copy_container">

packages/tldraw/src/lib/shapes/geo/GeoShapeUtil.tsx
@@ -481,9 +481,7 @@ export class GeoShapeUtil extends BaseBoxShapeUtil<TLGeoShape> {
 						/>
 					</HTMLContainer>
 				)}
-				{shape.props.url && (
-					<HyperlinkButton url={shape.props.url} zoomLevel={this.editor.getZoomLevel()} />
-				)}
+				{shape.props.url && <HyperlinkButton url={shape.props.url} />}
 			</>
 		)
 	}

packages/tldraw/src/lib/shapes/image/ImageShapeUtil.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable react-hooks/rules-of-hooks */
 import {
 	BaseBoxShapeUtil,
 	Editor,
@@ -6,7 +5,6 @@ import {
 	HTMLContainer,
 	Image,
 	MediaHelpers,
-	TLAsset,
 	TLImageShape,
 	TLImageShapeProps,
 	TLResizeInfo,
@@ -20,6 +18,8 @@ import {
 	sanitizeId,
 	structuredClone,
 	toDomPrecision,
+	useEditor,
+	useValue,
 } from '@tldraw/editor'
 import classNames from 'classnames'
 import { memo, useEffect, useState } from 'react'
@@ -102,13 +102,7 @@ export class ImageShapeUtil extends BaseBoxShapeUtil<TLImageShape> {
 	}
 
 	component(shape: TLImageShape) {
-		const { asset, url } = useAsset({
-			shapeId: shape.id,
-			assetId: shape.props.assetId,
-			width: shape.props.w,
-		})
-
-		return <ImageShape asset={asset} editor={this.editor} shape={shape} url={url} />
+		return <ImageShape shape={shape} />
 	}
 
 	indicator(shape: TLImageShape) {
@@ -256,36 +250,22 @@ export class ImageShapeUtil extends BaseBoxShapeUtil<TLImageShape> {
 	}
 }
 
-function isAnimated(editor: Editor, shape: TLImageShape) {
-	const asset = shape.props.assetId ? editor.getAsset(shape.props.assetId) : undefined
+const ImageShape = memo(function ImageShape({ shape }: { shape: TLImageShape }) {
+	const editor = useEditor()
 
-	if (!asset) return false
+	const { asset, url } = useAsset({
+		shapeId: shape.id,
+		assetId: shape.props.assetId,
+	})
 
-	return (
-		('mimeType' in asset.props && MediaHelpers.isAnimatedImageType(asset?.props.mimeType)) ||
-		('isAnimated' in asset.props && asset.props.isAnimated)
-	)
-}
-
-const ImageShape = memo(function ImageShape({
-	editor,
-	shape,
-	asset,
-	url,
-}: {
-	editor: Editor
-	shape: TLImageShape
-	asset?: TLAsset | null
-	url: string | null
-}) {
-	const isCropping = editor.getCroppingShapeId() === shape.id
 	const prefersReducedMotion = usePrefersReducedMotion()
 	const [staticFrameSrc, setStaticFrameSrc] = useState('')
 	const [loadedUrl, setLoadedUrl] = useState<null | string>(null)
-	const isSelected = shape.id === editor.getOnlySelectedShapeId()
+
+	const isAnimated = getIsAnimated(editor, shape)
 
 	useEffect(() => {
-		if (url && isAnimated(editor, shape)) {
+		if (url && isAnimated) {
 			let cancelled = false
 
 			const image = Image()
@@ -310,17 +290,20 @@ const ImageShape = memo(function ImageShape({
 				cancelled = true
 			}
 		}
-	}, [editor, prefersReducedMotion, url, shape])
+	}, [editor, isAnimated, prefersReducedMotion, url])
 
-	if (asset?.type === 'bookmark') {
-		throw Error("Bookmark assets can't be rendered as images")
-	}
-
-	const showCropPreview = isSelected && isCropping && editor.isIn('select.crop')
+	const showCropPreview = useValue(
+		'show crop preview',
+		() =>
+			shape.id === editor.getOnlySelectedShapeId() &&
+			editor.getCroppingShapeId() === shape.id &&
+			editor.isIn('select.crop'),
+		[editor, shape.id]
+	)
 
 	// We only want to reduce motion for mimeTypes that have motion
 	const reduceMotion =
-		prefersReducedMotion && (asset?.props.mimeType?.includes('video') || isAnimated(editor, shape))
+		prefersReducedMotion && (asset?.props.mimeType?.includes('video') || isAnimated)
 
 	const containerStyle = getCroppedContainerStyle(shape)
 
@@ -347,16 +330,14 @@ const ImageShape = memo(function ImageShape({
 				>
 					{asset ? null : <BrokenAssetIcon />}
 				</div>
-				{'url' in shape.props && shape.props.url && (
-					<HyperlinkButton url={shape.props.url} zoomLevel={editor.getZoomLevel()} />
-				)}
+				{'url' in shape.props && shape.props.url && <HyperlinkButton url={shape.props.url} />}
 			</HTMLContainer>
 		)
 	}
 
 	// We don't set crossOrigin for non-animated images because for Cloudflare we don't currently
 	// have that set up.
-	const crossOrigin = isAnimated(editor, shape) ? 'anonymous' : undefined
+	const crossOrigin = isAnimated ? 'anonymous' : undefined
 
 	return (
 		<>
@@ -407,13 +388,22 @@ const ImageShape = memo(function ImageShape({
 						/>
 					)}
 				</div>
-				{shape.props.url && (
-					<HyperlinkButton url={shape.props.url} zoomLevel={editor.getZoomLevel()} />
-				)}
+				{shape.props.url && <HyperlinkButton url={shape.props.url} />}
 			</HTMLContainer>
 		</>
 	)
 })
+
+function getIsAnimated(editor: Editor, shape: TLImageShape) {
+	const asset = shape.props.assetId ? editor.getAsset(shape.props.assetId) : undefined
+
+	if (!asset) return false
+
+	return (
+		('mimeType' in asset.props && MediaHelpers.isAnimatedImageType(asset?.props.mimeType)) ||
+		('isAnimated' in asset.props && asset.props.isAnimated)
+	)
+}
 
 /**
  * When an image is cropped we need to translate the image to show the portion withing the cropped

packages/tldraw/src/lib/shapes/note/NoteShapeUtil.tsx
@@ -230,9 +230,7 @@ export class NoteShapeUtil extends ShapeUtil<TLNoteShape> {
 						onKeyDown={handleKeyDown}
 					/>
 				</div>
-				{'url' in shape.props && shape.props.url && (
-					<HyperlinkButton url={shape.props.url} zoomLevel={this.editor.getZoomLevel()} />
-				)}
+				{'url' in shape.props && shape.props.url && <HyperlinkButton url={shape.props.url} />}
 			</>
 		)
 	}

packages/tldraw/src/lib/shapes/shared/HyperlinkButton.tsx
@@ -1,14 +1,16 @@
-import { stopEventPropagation } from '@tldraw/editor'
+import { stopEventPropagation, useEditor, useValue } from '@tldraw/editor'
 import classNames from 'classnames'
 
 const LINK_ICON =
 	"data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='30' height='30' fill='none'%3E%3Cpath stroke='%23000' stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M13 5H7a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2v-6M19 5h6m0 0v6m0-6L13 17'/%3E%3C/svg%3E"
 
-export function HyperlinkButton({ url, zoomLevel }: { url: string; zoomLevel: number }) {
+export function HyperlinkButton({ url }: { url: string }) {
+	const editor = useEditor()
+	const hideButton = useValue('zoomLevel', () => editor.getZoomLevel() < 0.32, [editor])
 	return (
 		<a
 			className={classNames('tl-hyperlink-button', {
-				'tl-hyperlink-button__hidden': zoomLevel < 0.32,
+				'tl-hyperlink-button__hidden': hideButton,
 			})}
 			href={url}
 			target="_blank"

packages/tldraw/src/lib/shapes/shared/useAsset.ts
@@ -1,10 +1,16 @@
 import {
+	Editor,
 	TLAssetId,
+	TLImageAsset,
+	TLImageShape,
 	TLShapeId,
+	TLVideoAsset,
+	TLVideoShape,
+	debounce,
+	react,
 	useDelaySvgExport,
 	useEditor,
 	useSvgExportContext,
-	useValue,
 } from '@tldraw/editor'
 import { useEffect, useRef, useState } from 'react'
 
@@ -19,81 +25,107 @@ import { useEffect, useRef, useState } from 'react'
  *
  * @public
  */
-export function useAsset(options: {
-	shapeId: TLShapeId
-	assetId: TLAssetId | null
-	width: number
-}) {
-	const { shapeId, assetId, width } = options
+export function useAsset({ shapeId, assetId }: { shapeId: TLShapeId; assetId: TLAssetId | null }) {
 	const editor = useEditor()
-	const [url, setUrl] = useState<string | null>(null)
-	const [isPlaceholder, setIsPlaceholder] = useState(false)
 	const isExport = !!useSvgExportContext()
-	const asset = assetId ? editor.getAsset(assetId) : null
-	const culledShapes = editor.getCulledShapes()
-	const isCulled = culledShapes.has(shapeId)
-	const didAlreadyResolve = useRef(false)
 	const isReady = useDelaySvgExport()
 
+	// We use a state to store the result of the asset resolution, and we're going to avoid updating this whenever we can
+	const [result, setResult] = useState<{
+		asset: (TLImageAsset | TLVideoAsset) | null
+		url: string | null
+	}>(() => ({
+		asset: assetId ? editor.getAsset<TLImageAsset | TLVideoAsset>(assetId) ?? null : null,
+		url: null as string | null,
+	}))
+
+	// A flag for whether we've resolved the asset URL at least once, after which we can debounce
+	const didAlreadyResolve = useRef(false)
+
+	// The last URL that we've seen for the shape
+	const previousUrl = useRef<string | null>(null)
+
 	useEffect(() => {
-		if (url) didAlreadyResolve.current = true
-	}, [url])
-
-	const shapeScale = asset && 'w' in asset.props ? width / asset.props.w : 1
-	// We debounce the zoom level to reduce the number of times we fetch a new image and,
-	// more importantly, to not cause zooming in and out to feel janky.
-	const screenScale = useValue('zoom level', () => editor.getZoomLevel() * shapeScale, [
-		editor,
-		shapeScale,
-	])
-
-	useEffect(() => {
-		if (url) didAlreadyResolve.current = true
-	}, [url])
-
-	useEffect(() => {
-		if (!isExport && isCulled) return
-
-		if (assetId && !asset?.props.src) {
-			const preview = editor.getTemporaryAssetPreview(assetId)
-
-			if (preview) {
-				setUrl(preview)
-				setIsPlaceholder(true)
-				isReady()
-				return
-			}
-		}
+		if (!assetId) return
 
 		let isCancelled = false
+		let cancelDebounceFn: (() => void) | undefined
 
-		async function resolve() {
-			const resolvedUrl = await editor.resolveAssetUrl(assetId, {
-				screenScale,
-				shouldResolveToOriginal: isExport,
-			})
+		const cleanupEffectScheduler = react('update state', () => {
+			if (!isExport && editor.getCulledShapes().has(shapeId)) return
 
-			if (!isCancelled) {
-				setUrl(resolvedUrl)
-				setIsPlaceholder(false)
-				isReady()
+			// Get the fresh asset
+			const asset = editor.getAsset<TLImageAsset | TLVideoAsset>(assetId)
+			if (!asset) return
+
+			// Get the fresh shape
+			const shape = editor.getShape<TLImageShape | TLVideoShape>(shapeId)
+			if (!shape) return
+
+			// Set initial preview for the shape if it has no source (if it was pasted into a local project as base64)
+			if (!asset.props.src) {
+				const preview = editor.getTemporaryAssetPreview(asset.id)
+				if (preview) {
+					if (previousUrl.current !== preview) {
+						previousUrl.current = preview // just for kicks, let's save the url as the previous URL
+						setResult((prev) => ({ ...prev, isPlaceholder: true, url: preview })) // set the preview as the URL
+						isReady() // let the SVG export know we're ready for export
+					}
+					return
+				}
 			}
+
+			// aside ...we could bail here if the only thing that has changed is the shape has changed from culled to not culled
+
+			const screenScale = editor.getZoomLevel() * (shape.props.w / asset.props.w)
+
+			// If we already resolved the URL, debounce fetching potentially multiple image variations.
+			if (didAlreadyResolve.current) {
+				resolveAssetUrlDebounced(editor, assetId, screenScale, isExport, (url) => {
+					if (isCancelled) return // don't update if the hook has remounted
+					if (previousUrl.current === url) return // don't update the state if the url is the same
+					previousUrl.current = url // keep the url around to compare with the next one
+					setResult(() => ({ asset, url }))
+				})
+				cancelDebounceFn = resolveAssetUrlDebounced.cancel // cancel the debounce when the hook unmounts
+			} else {
+				resolveAssetUrl(editor, assetId, screenScale, isExport, (url) => {
+					if (isCancelled) return // don't update if the hook has remounted
+					didAlreadyResolve.current = true // mark that we've resolved our first image
+					previousUrl.current = url // keep the url around to compare with the next one
+					setResult(() => ({ asset, url }))
+				})
+			}
+		})
+
+		return () => {
+			cleanupEffectScheduler()
+			cancelDebounceFn?.()
+			isCancelled = true
 		}
+	}, [editor, assetId, isExport, isReady, shapeId])
 
-		// If we already resolved the URL, debounce fetching potentially multiple image variations.
-		if (didAlreadyResolve.current) {
-			const timer = editor.timers.setTimeout(resolve, 500)
-			return () => {
-				clearTimeout(timer)
-				isCancelled = true
-			}
-		} else {
-			resolve()
-			return () => {
-				isCancelled = true
-			}
-		}
-	}, [assetId, asset?.props.src, isCulled, screenScale, editor, isExport, isReady])
-
-	return { asset, url, isPlaceholder }
+	return result
 }
+
+function resolveAssetUrl(
+	editor: Editor,
+	assetId: TLAssetId,
+	screenScale: number,
+	isExport: boolean,
+	callback: (url: string | null) => void
+) {
+	editor
+		.resolveAssetUrl(assetId, {
+			screenScale,
+			shouldResolveToOriginal: isExport,
+		})
+		// There's a weird bug with out debounce function that doesn't
+		// make it work right with async functions, so we use a callback
+		// here instead of returning a promise.
+		.then((url) => {
+			callback(url)
+		})
+}
+
+const resolveAssetUrlDebounced = debounce(resolveAssetUrl, 500)

commit e86a737fc512ccca686b016864b11faa0557580e

assets: fix up resolving when copy/pasting multiple items; also, videos (#5061)
Fixes issue https://github.com/tldraw/tldraw/issues/5052

This is a regression from https://github.com/tldraw/tldraw/pull/4682
(which was rolled up into https://github.com/tldraw/tldraw/pull/4659 )

Couple things were problematic here:
- main issue was that `debounce`'d urls all rolled up into one single
debounce call that was _shared_ for all assets. this needed to be split
to be per asset, which is now the case. (see
`getResolveAssetUrlDebounced`)
- As long as I was looking at this it turns out videos weren't working
at all. We weren't accounting for `data:video` as well as `data:image` -
that's now been corrected. Not sure how long that's been broken for...
- Noticed also that `isReady()` was not called appropriately in the
non-preview case - that's now fixed as well.
- finally, made `VideoShapeUtil`'s calling of `useImageOrVideoAsset`
consistent with `ImageShapeUtil`

### Change type

- [x] `bugfix`
- [ ] `improvement`
- [ ] `feature`
- [ ] `api`
- [ ] `other`

### Release notes

- Fixed bugs with copy/pasting multilple assets from one board to
another.
- Fixed bug with copy/pasting videos from one board to another.

packages/editor/src/lib/editor/Editor.ts
@@ -8183,6 +8183,7 @@ export class Editor extends EventEmitter<TLEventMap> {
 				if (
 					(asset.type === 'image' || asset.type === 'video') &&
 					!asset.props.src?.startsWith('data:image') &&
+					!asset.props.src?.startsWith('data:video') &&
 					!asset.props.src?.startsWith('http')
 				) {
 					const assetWithDataUrl = structuredClone(asset as TLImageAsset | TLVideoAsset)
@@ -8420,8 +8421,8 @@ export class Editor extends EventEmitter<TLEventMap> {
 			}
 
 			if (
-				(asset.type === 'image' || asset.type === 'video') &&
-				asset.props.src?.startsWith('data:image')
+				(asset.type === 'image' && asset.props.src?.startsWith('data:image')) ||
+				(asset.type === 'video' && asset.props.src?.startsWith('data:video'))
 			) {
 				// it's src is a base64 image or video; we need to create a new asset without the src,
 				// then create a new asset from the original src. So we save a copy of the original asset,

packages/tldraw/src/lib/shapes/shared/useImageOrVideoAsset.ts
@@ -12,7 +12,7 @@ import {
 	useEditor,
 	useSvgExportContext,
 } from '@tldraw/editor'
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useMemo, useRef, useState } from 'react'
 
 /**
  * This is a handy helper hook that resolves an asset to an optimized URL for a given shape, or its
@@ -35,6 +35,8 @@ export function useImageOrVideoAsset({
 	const editor = useEditor()
 	const isExport = !!useSvgExportContext()
 	const isReady = useDelaySvgExport()
+
+	const resolveAssetUrlDebounced = useMemo(() => debounce(resolveAssetUrl, 500), [])
 
 	// We use a state to store the result of the asset resolution, and we're going to avoid updating this whenever we can
 	const [result, setResult] = useState<{
@@ -85,22 +87,23 @@ export function useImageOrVideoAsset({
 
 			const screenScale = editor.getZoomLevel() * (shape.props.w / asset.props.w)
 
+			function resolve(asset: TLImageAsset | TLVideoAsset, url: string | null) {
+				if (isCancelled) return // don't update if the hook has remounted
+				if (previousUrl.current === url) return // don't update the state if the url is the same
+				didAlreadyResolve.current = true // mark that we've resolved our first image
+				previousUrl.current = url // keep the url around to compare with the next one
+				setResult({ asset, url })
+				isReady() // let the SVG export know we're ready for export
+			}
+
 			// If we already resolved the URL, debounce fetching potentially multiple image variations.
 			if (didAlreadyResolve.current) {
-				resolveAssetUrlDebounced(editor, assetId, screenScale, isExport, (url) => {
-					if (isCancelled) return // don't update if the hook has remounted
-					if (previousUrl.current === url) return // don't update the state if the url is the same
-					previousUrl.current = url // keep the url around to compare with the next one
-					setResult(() => ({ asset, url }))
-				})
+				resolveAssetUrlDebounced(editor, assetId, screenScale, isExport, (url) =>
+					resolve(asset, url)
+				)
 				cancelDebounceFn = resolveAssetUrlDebounced.cancel // cancel the debounce when the hook unmounts
 			} else {
-				resolveAssetUrl(editor, assetId, screenScale, isExport, (url) => {
-					if (isCancelled) return // don't update if the hook has remounted
-					didAlreadyResolve.current = true // mark that we've resolved our first image
-					previousUrl.current = url // keep the url around to compare with the next one
-					setResult(() => ({ asset, url }))
-				})
+				resolveAssetUrl(editor, assetId, screenScale, isExport, (url) => resolve(asset, url))
 			}
 		})
 
@@ -109,7 +112,7 @@ export function useImageOrVideoAsset({
 			cancelDebounceFn?.()
 			isCancelled = true
 		}
-	}, [editor, assetId, isExport, isReady, shapeId])
+	}, [editor, assetId, isExport, isReady, shapeId, resolveAssetUrlDebounced])
 
 	return result
 }
@@ -133,8 +136,6 @@ function resolveAssetUrl(
 			callback(url)
 		})
 }
-
-const resolveAssetUrlDebounced = debounce(resolveAssetUrl, 500)
 
 /**
  * @deprecated Use {@link useImageOrVideoAsset} instead.

packages/tldraw/src/lib/shapes/video/VideoShapeUtil.tsx
@@ -1,12 +1,11 @@
-/* eslint-disable react-hooks/rules-of-hooks */
 import {
 	BaseBoxShapeUtil,
 	Editor,
 	HTMLContainer,
 	MediaHelpers,
-	TLAsset,
 	TLVideoShape,
 	toDomPrecision,
+	useEditor,
 	useEditorComponents,
 	useIsEditing,
 	videoShapeMigrations,
@@ -44,12 +43,7 @@ export class VideoShapeUtil extends BaseBoxShapeUtil<TLVideoShape> {
 	}
 
 	component(shape: TLVideoShape) {
-		const { asset, url } = useImageOrVideoAsset({
-			shapeId: shape.id,
-			assetId: shape.props.assetId,
-		})
-
-		return <VideoShape editor={this.editor} shape={shape} asset={asset} url={url} />
+		return <VideoShape shape={shape} />
 	}
 
 	indicator(shape: TLVideoShape) {
@@ -63,21 +57,17 @@ export class VideoShapeUtil extends BaseBoxShapeUtil<TLVideoShape> {
 	}
 }
 
-const VideoShape = memo(function VideoShape({
-	editor,
-	shape,
-	asset,
-	url,
-}: {
-	editor: Editor
-	shape: TLVideoShape
-	asset?: TLAsset | null
-	url: string | null
-}) {
+const VideoShape = memo(function VideoShape({ shape }: { shape: TLVideoShape }) {
+	const editor = useEditor()
 	const showControls = editor.getShapeGeometry(shape).bounds.w * editor.getZoomLevel() >= 110
 	const isEditing = useIsEditing(shape.id)
 	const prefersReducedMotion = usePrefersReducedMotion()
 	const { Spinner } = useEditorComponents()
+
+	const { asset, url } = useImageOrVideoAsset({
+		shapeId: shape.id,
+		assetId: shape.props.assetId,
+	})
 
 	const rVideo = useRef<HTMLVideoElement>(null!)
 