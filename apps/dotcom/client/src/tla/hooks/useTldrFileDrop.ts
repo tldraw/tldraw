8c826238a6d3726f68f47f1a2a41696437bfe212
Mitja Bezenšek <mitja.bezensek@gmail.com>
2024-12-06 14:24:53 +0100

    Improve dragging ux.

--- apps/dotcom/client/src/tla/hooks/useTldrFileDrop.ts
+++ apps/dotcom/client/src/tla/hooks/useTldrFileDrop.ts
@@ -1,8 +1,10 @@
 import { useAuth } from '@clerk/clerk-react'
 import { DragEvent, useCallback, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import { Editor, TLStoreSnapshot, parseTldrawJsonFile, tlmenus, useToasts } from 'tldraw'
 import { globalEditor } from '../../utils/globalEditor'
 import { defineMessages, useIntl } from '../utils/i18n'
+import { getFilePath } from '../utils/urls'
 import { useApp } from './useAppState'
 
 const messages = defineMessages({
@@ -14,6 +16,7 @@ const messages = defineMessages({
 
 export function useTldrFileDrop() {
 	const app = useApp()
+	const navigate = useNavigate()
 
 	const [isDraggingOver, setIsDraggingOver] = useState(false)
 
@@ -54,7 +57,7 @@ export function useTldrFileDrop() {
 					title: uploadingTitle,
 				})
 
-				await app.createFilesFromTldrFiles(snapshots, token)
+				const results = await app.createFilesFromTldrFiles(snapshots, token)
 
 				removeToast(id)
 				addToast({
@@ -62,9 +65,12 @@ export function useTldrFileDrop() {
 					title: addedTitle,
 					keepOpen: true,
 				})
+				if (results.slugs.length > 0) {
+					navigate(getFilePath(results.slugs[0]), { state: { mode: 'create' } })
+				}
 			}
 		},
-		[app, addToast, removeToast, auth, intl]
+		[app, addToast, removeToast, auth, intl, navigate]
 	)
 
 	const onDragOver = useCallback((e: DragEvent) => {
