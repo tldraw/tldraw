b006097670687c557cefc11458c32cc5909792cf
Steve Ruiz <steveruizok@gmail.com>
2024-12-07 20:26:10 +0000

    tiny little state machine, positions

--- apps/dotcom/client/src/tla/layouts/TlaSidebarLayout/TlaSidebarLayout.tsx
+++ apps/dotcom/client/src/tla/layouts/TlaSidebarLayout/TlaSidebarLayout.tsx
@@ -1,5 +1,5 @@
 import React, { ReactNode, useCallback, useRef } from 'react'
-import { clamp, useQuickReactor, useValue } from 'tldraw'
+import { clamp, stopEventPropagation, useQuickReactor, useValue } from 'tldraw'
 import { TlaSidebar } from '../../components/TlaSidebar/TlaSidebar'
 import { useApp } from '../../hooks/useAppState'
 import { usePreventAccidentalDrops } from '../../hooks/usePreventAccidentalDrops'
@@ -62,45 +62,68 @@ function SidebarResizeHandle({
 	onClose(): void
 	layoutRef: React.RefObject<HTMLDivElement>
 }) {
+	const rResizeState = useRef({ name: 'idle' } as
+		| { name: 'idle' }
+		| { name: 'pointing'; startWidth: number; startX: number }
+		| { name: 'resizing'; startWidth: number; startX: number })
+
 	const handlePointerDown = useCallback(
 		(event: React.PointerEvent) => {
-			const startX = event.clientX
 			const startWidth = layoutRef.current
 				? parseInt(getComputedStyle(layoutRef.current).getPropertyValue('--tla-sidebar-width'), 10)
 				: 0
-			let didMove = false
-			const handlePointerMove = (moveEvent: PointerEvent) => {
-				const dx = moveEvent.clientX - startX
-				if (!didMove && Math.abs(dx) < 5) return
-				if (!didMove) {
-					didMove = true
-					layoutRef.current?.setAttribute('data-resizing', 'true')
-				}
+
+			event.currentTarget.setPointerCapture(event.pointerId)
+			rResizeState.current = { name: 'pointing', startX: event.clientX, startWidth }
+		},
+		[layoutRef]
+	)
+
+	const handlePointerMove = useCallback(
+		(moveEvent: React.PointerEvent) => {
+			if (rResizeState.current.name === 'idle') return
+
+			if (rResizeState.current.name === 'pointing') {
+				const dx = moveEvent.clientX - rResizeState.current.startX
+				if (Math.abs(dx) < 5) return
+				rResizeState.current = { ...rResizeState.current, name: 'resizing' }
+				layoutRef.current?.setAttribute('data-resizing', 'true')
+			}
+
+			if (rResizeState.current.name === 'resizing') {
 				const newWidth = clamp(
-					startWidth + (moveEvent.clientX - startX),
+					rResizeState.current.startWidth + (moveEvent.clientX - rResizeState.current.startX),
 					MIN_SIDEBAR_WIDTH,
 					MAX_SIDEBAR_WIDTH
 				)
 				onResize(newWidth)
 			}
-			const handlePointerUp = () => {
-				document.removeEventListener('pointermove', handlePointerMove)
-				document.removeEventListener('pointerup', handlePointerUp)
-				layoutRef.current?.removeAttribute('data-resizing')
-			}
-			document.addEventListener('pointermove', handlePointerMove)
-			document.addEventListener('pointerup', handlePointerUp)
 		},
-		[layoutRef, onResize]
+		[onResize, layoutRef]
+	)
+
+	const handlePointerUp = useCallback(
+		(event: React.PointerEvent) => {
+			layoutRef.current?.removeAttribute('data-resizing')
+			event.currentTarget.releasePointerCapture(event.pointerId)
+
+			if (rResizeState.current.name === 'resizing') {
+				stopEventPropagation(event)
+			} else {
+				onClose()
+			}
+
+			rResizeState.current = { name: 'idle' }
+		},
+		[layoutRef, onClose]
 	)
 
 	return (
 		<div
 			className={styles.resizeHandle}
 			onPointerDown={handlePointerDown}
-			onClick={() => {
-				onClose()
-			}}
+			onPointerMove={handlePointerMove}
+			onPointerUp={handlePointerUp}
 		>
 			<div className={styles.resizeHandleIndicator}></div>
 		</div>
