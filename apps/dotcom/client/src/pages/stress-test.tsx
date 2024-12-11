bf82a9c48b37af634a93337eaeaa6dcaa0cbcb18
Mitja Bezenšek <mitja.bezensek@gmail.com>
2024-12-11 23:16:10 +0100

    Add some logging

--- apps/dotcom/client/src/pages/stress-test.tsx
+++ apps/dotcom/client/src/pages/stress-test.tsx
@@ -18,11 +18,15 @@ function useLocalValue(key: string, initialValue: string) {
 	return [value, setValue] as const
 }
 
+let start = Date.now()
+
 export function Component() {
 	const [state, setState] = useState(null as null | STCoordinatorState)
 	const [coordinatorUrl] = useLocalValue('st_coordinator_url', '')
 	const [accessToken] = useLocalValue('st_access_token', '')
 	const [uri] = useLocalValue('st_uri', '')
+	const [currentTest, setCurrentTest] = useState<string | null>(null)
+	const [done, setDone] = useState<string | null>(null)
 
 	useEffect(() => {
 		if (!coordinatorUrl || !accessToken) return
@@ -33,17 +37,30 @@ export function Component() {
 				},
 			})
 				.then((res) => res.json())
-				.then(setState)
+				.then((r) => {
+					const currentCount = currentTest ? state?.tests[currentTest]?.events.length : 0
+					const newCount = currentTest ? r?.tests[currentTest]?.events.length : 0
+					if (newCount && currentTest && currentCount === newCount) {
+						const duration = Date.now() - start
+						setCurrentTest(null)
+						const logMessage = `done in ${duration} ms. processed ${currentCount} events (${Math.floor(newCount / (duration / 1000))} events/s)`
+						setDone(logMessage)
+					}
+					setState(r)
+				})
 		}, 1000)
 		return () => clearInterval(interval)
-	}, [coordinatorUrl, accessToken])
+	}, [coordinatorUrl, accessToken, state?.tests, currentTest])
 
 	if (!state) {
 		return <div>Loading...</div>
 	}
 
 	const handleStart = async () => {
+		start = Date.now()
+		setDone(null)
 		const testId = uniqueId()
+		setCurrentTest(testId)
 		const res = await fetch(coordinatorUrl + `/${testId}/start`, {
 			method: 'POST',
 			headers: {
@@ -56,6 +73,7 @@ export function Component() {
 		}
 	}
 	const handleStop = async () => {
+		setCurrentTest(null)
 		const res = await fetch(coordinatorUrl + '/first-one/stop', {
 			method: 'POST',
 			headers: {
@@ -67,17 +85,19 @@ export function Component() {
 			console.info('test stopped')
 		}
 	}
-	const events = Object.values(state.tests).flatMap((t) => t.events)
+	// const events = Object.values(state.tests).flatMap((t) => t.events)
 
-	const createEvents = events.filter((e) => e.type === 'operation' && e.operation === 'create user')
-
-	console.log(createEvents)
+	// const createEvents = events.filter((e) => e.type === 'operation' && e.operation === 'create user')
+	//
+	// console.log(createEvents)
+	// console.log('duration', Date.now() - start)
 
 	return (
 		<div style={{ height: '100%', overflow: 'scroll' }}>
-			<pre>{JSON.stringify(state, null, 2)}</pre>
+			{done && <div>{done}</div>}
 			<button onClick={handleStart}>Start test</button>
 			<button onClick={handleStop}>Stop test</button>
+			<pre>{JSON.stringify(state, null, 2)}</pre>
 		</div>
 	)
 }
