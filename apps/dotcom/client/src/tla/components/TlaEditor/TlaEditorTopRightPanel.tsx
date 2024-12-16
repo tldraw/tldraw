54a9d806b75f762cad6b04e282572db486ceda15
Steve Ruiz <steveruizok@gmail.com>
2024-12-16 11:59:45 +0000

    fix event placement

--- apps/dotcom/client/src/tla/components/TlaEditor/TlaEditorTopRightPanel.tsx
+++ apps/dotcom/client/src/tla/components/TlaEditor/TlaEditorTopRightPanel.tsx
@@ -1,8 +1,10 @@
 import { SignInButton } from '@clerk/clerk-react'
 import classNames from 'classnames'
-import { useRef } from 'react'
+import { useCallback, useRef } from 'react'
+import { useParams } from 'react-router-dom'
 import { PeopleMenu, usePassThroughWheelEvents } from 'tldraw'
 import { useCurrentFileId } from '../../hooks/useCurrentFileId'
+import { useTldrawAppUiEvents } from '../../utils/app-ui-events'
 import { F } from '../../utils/i18n'
 import { TlaCtaButton } from '../TlaCtaButton/TlaCtaButton'
 import { TlaFileShareMenu } from '../TlaFileShareMenu/TlaFileShareMenu'
@@ -19,6 +21,7 @@ export function TlaEditorTopRightPanel({
 	const ref = useRef<HTMLDivElement>(null)
 	usePassThroughWheelEvents(ref)
 	const fileId = useCurrentFileId()
+	const trackEvent = useTldrawAppUiEvents()
 
 	if (isAnonUser) {
 		return (
@@ -30,7 +33,10 @@ export function TlaEditorTopRightPanel({
 					forceRedirectUrl={location.pathname + location.search}
 					signUpForceRedirectUrl={location.pathname + location.search}
 				>
-					<TlaCtaButton data-testid="tla-sign-up">
+					<TlaCtaButton
+						data-testid="tla-sign-up"
+						onClick={() => trackEvent('open-share-menu', { source: 'anon-landing-page' })}
+					>
 						<F defaultMessage="Sign in" />
 					</TlaCtaButton>
 				</SignInButton>
@@ -41,11 +47,29 @@ export function TlaEditorTopRightPanel({
 	return (
 		<div ref={ref} className={styles.topRightPanel}>
 			<PeopleMenu displayUserWhenAlone={false} />
+			{context === 'legacy' && <LegacyImportButton />}
 			<TlaFileShareMenu fileId={fileId!} source="file-header" context={context}>
-				<TlaCtaButton data-testid="tla-share-button">
+				<TlaCtaButton
+					data-testid="tla-share-button"
+					onClick={() => trackEvent('open-share-menu', { source: 'top-bar' })}
+				>
 					<F defaultMessage="Share" />
 				</TlaCtaButton>
 			</TlaFileShareMenu>
 		</div>
+	)
+}
+
+function LegacyImportButton() {
+	const roomId = useParams<{ roomId: string }>().roomId
+	const handleClick = useCallback(() => {
+		// todo: slurp the room id and navigate to the new file
+		alert(roomId)
+	}, [roomId])
+
+	return (
+		<TlaCtaButton data-testid="tla-import-button" onClick={handleClick}>
+			<F defaultMessage="Copy to my files" />
+		</TlaCtaButton>
 	)
 }
