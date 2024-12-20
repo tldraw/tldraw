c49468971e58bdfb3fc1cc8e3ccb726f6eda3c4d
Mitja Bezenšek <mitja.bezensek@gmail.com>
2024-12-20 10:18:16 +0100

    Use a different token

--- apps/dotcom/stress-tester/deploy-and-tail.sh
+++ apps/dotcom/stress-tester/deploy-and-tail.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 source .env
-npx wrangler deploy --env $TLDRAW_ENV --var "TLDRAW_ENV:$TLDRAW_ENV" --var "TEST_AUTH_SECRET:$TEST_AUTH_SECRET" --var "WORKER_NAME:pr-5113-tldraw-stress-test-multiplayer"
+npx wrangler deploy --env $TLDRAW_ENV --var "TLDRAW_ENV:$TLDRAW_ENV" --var "TEST_AUTH_SECRET:$TEST_AUTH_SECRET" --var "WORKER_NAME:pr-5113-tldraw-stress-test-multiplayer" --var "ACCESS_TOKEN:$ACCESS_TOKEN"
 npx wrangler tail stress-tester-dev
