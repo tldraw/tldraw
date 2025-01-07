5694568e2b2de6c839c8d8bb1fcc7dfe35e7e32e
Mitja Bezenšek <mitja.bezensek@gmail.com>
2025-01-07 10:57:59 +0100

    Fix max image dimension prop not getting applied. (#5176)
    The problem was twofold:
    - we weren't awaiting the updating of the assets
    - we used old asset values when creating the shapes.
    
    fixes https://github.com/tldraw/tldraw/issues/5173 / TLD-2910
    
    ### Change type
    
    - [x] `bugfix`
    - [ ] `improvement`
    - [ ] `feature`
    - [ ] `api`
    - [ ] `other`
    
    ### Test plan
    
    1. In `develop.tsx` set `maxImageDimension` to some value.
    2. Paste an image.
    3. The image should be within the max dimension.
    
    ### Release notes
    
    - Fix a bug with `maxImageDimension` not getting applied to pasted
    images.

--- packages/tldraw/src/lib/defaultExternalContentHandlers.ts
+++ packages/tldraw/src/lib/defaultExternalContentHandlers.ts
@@ -235,7 +235,6 @@ export function registerDefaultExternalContentHandlers(
 				: editor.getViewportPageBounds().center)
 
 		const pagePoint = new Vec(position.x, position.y)
-		const assets: TLAsset[] = []
 		const assetsToUpdate: {
 			asset: TLAsset
 			file: File
@@ -288,11 +287,11 @@ export function registerDefaultExternalContentHandlers(
 			if (isImageType) {
 				temporaryAssetPreview = editor.createTemporaryAssetPreview(assetId, file)
 			}
-			assets.push(assetInfo)
 			assetsToUpdate.push({ asset: assetInfo, file, temporaryAssetPreview })
 		}
 
-		Promise.allSettled(
+		const assets: TLAsset[] = []
+		await Promise.allSettled(
 			assetsToUpdate.map(async (assetAndFile) => {
 				try {
 					const newAsset = await editor.getAssetForExternalContent({
@@ -304,8 +303,10 @@ export function registerDefaultExternalContentHandlers(
 						throw Error('Could not create an asset')
 					}
 
+					const updated = { ...newAsset, id: assetAndFile.asset.id }
+					assets.push(updated)
 					// Save the new asset under the old asset's id
-					editor.updateAssets([{ ...newAsset, id: assetAndFile.asset.id }])
+					editor.updateAssets([updated])
 				} catch (error) {
 					toasts.addToast({
 						title: msg('assets.files.upload-failed'),
