490d299be6193d25de5de4bc5233de27d8d004eb
David Sheldrick <d.j.sheldrick@gmail.com>
2025-01-30 13:46:53 +0000

    Public preview opt-in (#5324)
    Instead of a full-blown banner, I just added an item in the menu, and a
    little dot to entice people to click. this is a common pattern when apps
    add new stuff and I think it works pretty well here, especially since
    the goal is just to make sure the backend can handle the scale before we
    go full switchover.
    
    ![Kapture 2025-01-30 at 11 23
    56](https://github.com/user-attachments/assets/507ee977-8e8d-4231-8df8-bfc34fee8017)
    
    ### Change type
    
    - [x] `other`

---
+++ apps/dotcom/client/src/components/preview-stuff.tsx
@@ -0,0 +1,55 @@
+import { SignUpButton } from '@clerk/clerk-react'
+import { CSSProperties } from 'react'
+import {
+	TldrawUiButton,
+	TldrawUiButtonLabel,
+	TldrawUiMenuGroup,
+	atom,
+	getFromLocalStorage,
+	setInLocalStorage,
+	useValue,
+} from 'tldraw'
+
+const STORAGE_KEY = 'tla-preview-hide-blue-dot'
+const hideBlueDot$ = atom('hideBlueDot', !!getFromLocalStorage(STORAGE_KEY))
+function hideBlueDot() {
+	hideBlueDot$.set(true)
+	setInLocalStorage(STORAGE_KEY, 'true')
+}
+
+export function PreviewBlueDot({ style }: { style?: CSSProperties }) {
+	const hideBlueDot = useValue(hideBlueDot$)
+	if (hideBlueDot) return null
+	return (
+		<span
+			style={{
+				width: 8,
+				height: 8,
+				borderRadius: '50%',
+				backgroundColor: 'var(--color-selected)',
+				display: 'inline-block',
+				border: '1px solid',
+				borderColor: 'var(--color-background)',
+				...style,
+			}}
+		/>
+	)
+}
+
+export function PreviewMenuItem() {
+	return (
+		<TldrawUiMenuGroup id="basic">
+			<SignUpButton mode="modal" forceRedirectUrl="/" signInForceRedirectUrl="/">
+				<TldrawUiButton
+					type="normal"
+					onClick={() => {
+						setTimeout(hideBlueDot, 1000)
+					}}
+				>
+					<PreviewBlueDot style={{ marginRight: 6 }} />
+					<TldrawUiButtonLabel>Preview the new tldraw.com</TldrawUiButtonLabel>
+				</TldrawUiButton>
+			</SignUpButton>
+		</TldrawUiMenuGroup>
+	)
+}
