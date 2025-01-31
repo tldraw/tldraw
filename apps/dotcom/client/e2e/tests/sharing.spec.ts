a3aae75d2aca1612d8df7e2362341f74ed8896c4
David Sheldrick <d.j.sheldrick@gmail.com>
2025-01-31 11:18:07 +0000

    undo debug helper

--- apps/dotcom/client/e2e/tests/sharing.spec.ts
+++ apps/dotcom/client/e2e/tests/sharing.spec.ts
@@ -1,11 +1,10 @@
-import { sleep } from '@tldraw/utils'
 import fs from 'fs'
 import path from 'path'
 import { openNewTab } from '../fixtures/helpers'
 import { expect, test } from '../fixtures/tla-test'
 
 test.beforeEach(async ({ context }) => {
-	await Promise.race([context.grantPermissions(['clipboard-read', 'clipboard-write']), sleep(1000)])
+	await context.grantPermissions(['clipboard-read', 'clipboard-write'])
 })
 
 /* --------------------- Sharing -------------------- */
