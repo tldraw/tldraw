8cacef7113daf0a33e9915256f8a87e33f6b53e4
David Sheldrick <d.j.sheldrick@gmail.com>
2025-02-05 16:15:11 +0000

    offer the 'report a problem' dialog for editor errors

--- apps/dotcom/client/src/tla/components/TlaFileError/TlaFileError.tsx
+++ apps/dotcom/client/src/tla/components/TlaFileError/TlaFileError.tsx
@@ -1,25 +1,32 @@
 import { SignInButton } from '@clerk/clerk-react'
 import { TLRemoteSyncError, TLSyncErrorCloseEventReason } from '@tldraw/sync-core'
 import { ReactElement, useEffect } from 'react'
+import { TldrawUiButton, useDialogs } from 'tldraw'
 import { sadFaceIcon } from '../../../components/ErrorPage/ErrorPage'
 import { useSetIsReady } from '../../hooks/useIsReady'
 import { F } from '../../utils/i18n'
 import { TlaCtaButton } from '../TlaCtaButton/TlaCtaButton'
+import { ReportAProblemDialog } from '../dialogs/ReportAProblemDialog'
 import styles from './TlaFileError.module.css'
 
 function DefaultError() {
+	const dialogs = useDialogs()
 	return (
 		<TlaFileErrorContent
 			header={<F defaultMessage="Something went wrong" />}
 			para1={<F defaultMessage="Please try refreshing the page." />}
 			para2={
 				<F
-					defaultMessage="Still having trouble? Let us know at <a>hello@tldraw.com</a>"
+					defaultMessage="Still having trouble? <a>Report a problem</a>"
 					values={{
 						a: (chunks) => (
-							<a href="mailto:hello@tldraw.com" target="_blank" rel="noopener noreferrer">
+							<TldrawUiButton
+								onClick={() => dialogs.addDialog({ component: ReportAProblemDialog })}
+								type={'primary'}
+								style={{ display: 'inline' }}
+							>
 								{chunks}
-							</a>
+							</TldrawUiButton>
 						),
 					}}
 				/>
