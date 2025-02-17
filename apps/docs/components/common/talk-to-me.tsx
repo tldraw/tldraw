e410cb9c8c20383d68f336d512ee8a47bd4df2b9
Steve Ruiz <steveruizok@gmail.com>
2025-02-17 17:20:46 +0000

    mobile

--- apps/docs/components/common/talk-to-me.tsx
+++ apps/docs/components/common/talk-to-me.tsx
@@ -10,7 +10,7 @@ export function TalkToMe() {
 	if (didDismiss) return null
 
 	return (
-		<div className="fixed z-[1000] bottom-2 right-2 h-fit flex justify-items-end">
+		<div className="fixed z-[50] bottom-2 right-2 h-fit flex justify-items-end">
 			<div className=" right-2 bottom-2 flex flex-row w-fit items-center gap-2 pl-0 pr-4 py-2 bg-black text-white dark:bg-white dark:text-black border border-gray-200 rounded-lg shadow-lg overflow-hidden text-nowrap">
 				<button
 					className="w-auto pl-3 pr-1 h-full flex items-center opacity-[.618] cursor-pointer flex-shrink-0 hover:opacity-[1]"
@@ -19,13 +19,22 @@ export function TalkToMe() {
 					<XMarkIcon className="w-3 h-3" />
 				</button>
 				<Link
-					className="flex flex-row gap-2 items-center"
 					href="https://tldraw.typeform.com/developers?utm_source=landing-page&utm_medium=banner&utm_campaign=devrelsurvey"
 					target="_blank"
 					onClick={() => setDidDismiss(true)}
 				>
-					Take our developer survey
-					<ArrowRightCircleIcon className="w-4 h-4 flex-shrink-0" />
+					<div className="hidden md:flex flex-row gap-2 items-center">
+						Take our developer survey
+						<ArrowRightCircleIcon className="w-4 h-4 flex-shrink-0" />
+					</div>
+					<div className="hidden sm:flex md:hidden flex-row gap-2 items-center">
+						Take our survey
+						<ArrowRightCircleIcon className="w-4 h-4 flex-shrink-0" />
+					</div>
+					<div className="flex sm:hidden flex-row gap-2 items-center">
+						hi
+						<ArrowRightCircleIcon className="w-4 h-4 flex-shrink-0" />
+					</div>
 				</Link>
 			</div>
 		</div>
