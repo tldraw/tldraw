39c06751ad60df4336e92858e7e0295a3fbc85f9
Mime Čuvalo <mimecuvalo@gmail.com>
2025-02-18 15:28:49 +0000

    rework; add docs

--- packages/tldraw/src/lib/utils/text/richText.ts
+++ packages/tldraw/src/lib/utils/text/richText.ts
@@ -2,7 +2,6 @@ import {
 	Extension,
 	Extensions,
 	JSONContent,
-	Editor as TipTapEditor,
 	generateHTML,
 	generateJSON,
 	generateText,
@@ -10,9 +9,8 @@ import {
 import Code from '@tiptap/extension-code'
 import Highlight from '@tiptap/extension-highlight'
 import Link from '@tiptap/extension-link'
-import { Plugin } from '@tiptap/pm/state'
 import StarterKit from '@tiptap/starter-kit'
-import { Editor, TLRichText, stopEventPropagation } from '@tldraw/editor'
+import { Editor, TLRichText } from '@tldraw/editor'
 import TextDirection from './textDirection'
 
 const KeyboardShiftEnterTweakExtension = Extension.create({
@@ -22,52 +20,6 @@ const KeyboardShiftEnterTweakExtension = Extension.create({
 			// We don't support soft breaks, so we just use the default enter command.
 			'Shift-Enter': ({ editor }) => editor.commands.enter(),
 		}
-	},
-})
-
-// it ain't pretty but we need to know when the user is pointing.
-// this is used to prevent the toolbar from showing when the user is
-// selecting text with their mouse. TT doesn't give us a good way to
-// pipe this data out, so we're hijacking their event emitter. wahoo
-function handlePointingStart(editor: TipTapEditor, e: unknown) {
-	const _pe = e as PointerEvent
-	if (_pe.pointerType) {
-		;(_pe.currentTarget as HTMLElement).setPointerCapture(_pe.pointerId)
-	}
-	// @ts-expect-error
-	editor.emit('pointer-state-change', { isPointing: true })
-	stopEventPropagation(e)
-	return false
-}
-
-function handlePointingEnd(editor: TipTapEditor, e: unknown) {
-	const _pe = e as PointerEvent
-	if (_pe.pointerType) {
-		;(_pe.currentTarget as HTMLElement).releasePointerCapture(_pe.pointerId)
-	}
-	// @ts-expect-error
-	editor.emit('pointer-state-change', { isPointing: false })
-	stopEventPropagation(e)
-	return false
-}
-
-const PointerStateExtension = Extension.create({
-	name: 'pointerStateExtension',
-	addProseMirrorPlugins() {
-		return [
-			new Plugin({
-				props: {
-					handleDOMEvents: {
-						touchstart: (_v, e) => handlePointingStart(this.editor, e),
-						pointerdown: (_v, e) => handlePointingStart(this.editor, e),
-						mousedown: (_v, e) => handlePointingStart(this.editor, e),
-						pointerup: (_v, e) => handlePointingEnd(this.editor, e),
-						touchend: (_v, e) => handlePointingEnd(this.editor, e),
-						mouseup: (_v, e) => handlePointingEnd(this.editor, e),
-					},
-				},
-			}),
-		]
 	},
 })
 
@@ -89,11 +41,6 @@ export const tipTapDefaultExtensions: Extensions = [
 	Highlight,
 	KeyboardShiftEnterTweakExtension,
 	TextDirection,
-	// TODO: This is causing issues with list item selection
-	// enabling onPointerDownCapture in RichTextArea fixes part of the problem
-	// but selecting in some cases the toolbar selection won't work when list items
-	// are involved.
-	// PointerStateExtension,
 ]
 
 /**
