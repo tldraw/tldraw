2e46cb1ed8c0b3743eb96f1b1fed8785d284dde0
David Sheldrick <d.j.sheldrick@gmail.com>
2025-02-20 14:14:50 +0000

    support legacy unfinished slurps

--- apps/dotcom/client/src/tla/utils/slurping.tsx
+++ apps/dotcom/client/src/tla/utils/slurping.tsx
@@ -73,8 +73,9 @@ export async function maybeSlurp(opts: SlurperOpts) {
 	if (opts.abortSignal.aborted) return
 	if (!opts.app.isFileOwner(opts.fileId)) return
 	const file = opts.app.getFile(opts.fileId)
-	if (!file?.createSource?.startsWith('lf/')) return
-	const persistenceKey = file.createSource.slice('lf/'.length)
+	const persistenceKey =
+		(opts.editor.getDocumentSettings().meta.slurpPersistenceKey as string | undefined) ||
+		file?.createSource?.match(/lf\/(.+)/)?.[1]
 	if (!persistenceKey) return
 	if (opts.editor.getDocumentSettings().meta.slurpFinished) return
 	return new Slurper({ ...opts, slurpPersistenceKey: persistenceKey }).slurp()
