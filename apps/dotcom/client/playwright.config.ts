5be98a391002a2b0414248c0f35588cc98ad214f
David Sheldrick <d.j.sheldrick@gmail.com>
2025-03-04 13:21:51 +0000

    ignore stdout/stderr

--- apps/dotcom/client/playwright.config.ts
+++ apps/dotcom/client/playwright.config.ts
@@ -92,7 +92,7 @@ export default defineConfig({
 		reuseExistingServer: !process.env.CI,
 		cwd: path.join(__dirname, '../../../'),
 		// remove comment if you wish to see the output of the server
-		stdout: 'pipe',
-		stderr: 'pipe',
+		// stdout: 'pipe',
+		// stderr: 'pipe',
 	},
 })
