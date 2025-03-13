7a045db8a252fb6a12fe3dbf7a08b4f7091182e0
Mime Čuvalo <mimecuvalo@gmail.com>
2025-03-13 15:13:50 +0000

    not for this pr

--- packages/tldraw/src/lib/ui/components/primitives/TldrawUiButtonPicker.tsx
+++ packages/tldraw/src/lib/ui/components/primitives/TldrawUiButtonPicker.tsx
@@ -112,18 +112,17 @@ export const TldrawUiButtonPicker = memo(function TldrawUiButtonPicker<T extends
 	return (
 		<div data-testid={`style.${uiType}`} className={classNames('tlui-buttons__grid')}>
 			{items.map((item) => {
-				const label = title + ' — ' + msg(`${uiType}-style.${item.value}` as TLUiTranslationKey)
 				return (
 					<TldrawUiButton
 						type="icon"
 						key={item.value}
 						data-id={item.value}
 						data-testid={`style.${uiType}.${item.value}`}
-						aria-label={label}
+						aria-label={item.value}
 						data-state={
 							value.type === 'shared' && value.value === item.value ? 'hinted' : undefined
 						}
-						title={label}
+						title={title + ' — ' + msg(`${uiType}-style.${item.value}` as TLUiTranslationKey)}
 						className={classNames('tlui-button-grid__button')}
 						style={
 							style === (DefaultColorStyle as StyleProp<unknown>)
