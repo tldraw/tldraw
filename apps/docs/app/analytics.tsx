5a8b0de0650e001876a93e654679bc274a84c284
Mitja Bezenšek <mitja.bezensek@gmail.com>
2025-03-15 16:04:38 +0100

    Don't show dismissible elements until we are sure of their status (#5570)
    We used to default to "user didn't dismiss" this yet. Which meant that
    we always showed a dismissible element first, then we read the actual
    value, and we then might have dismissed the elements (if that's what the
    stored state dictated). We now have a "not initialized" state during
    which we don't show the elements.
    
    Side effect of this change is that some users that have already
    dismissed these elements might see them again. I guess we could add some
    sort of migration of the state, but meh, doesn't seem worth the hassle?
    
    - [x] `improvement`
    
    ---------
    
    Co-authored-by: Steve Ruiz <steveruizok@gmail.com>

--- apps/docs/app/analytics.tsx
+++ apps/docs/app/analytics.tsx
@@ -8,7 +8,7 @@ import Cookies from 'js-cookie'
 import Script from 'next/script'
 import { useEffect, useState } from 'react'
 
-type CookieConsent = 'unknown' | 'opted-in' | 'opted-out'
+type CookieConsent = undefined | 'unknown' | 'opted-in' | 'opted-out'
 
 export default function Analytics() {
 	const [hasConsent, setHasConsent] = useState<CookieConsent>('unknown')
@@ -63,7 +63,9 @@ function CookieConsent({
 		}
 	}
 
-	if (hasConsent !== 'unknown') return null
+	if (hasConsent === undefined || hasConsent !== 'unknown') {
+		return null
+	}
 
 	return (
 		<>
