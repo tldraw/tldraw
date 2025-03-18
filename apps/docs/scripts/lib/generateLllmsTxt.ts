ab46dacdc64d3152ddc270b10c279dc119b05245
Lu[ke] Wilson <l2wilson94@gmail.com>
2025-03-18 15:18:35 +0000

    clean

--- apps/docs/scripts/lib/generateLllmsTxt.ts
+++ apps/docs/scripts/lib/generateLllmsTxt.ts
@@ -1,4 +1,3 @@
-import { Article } from '@/types/content-types'
 import fs from 'fs'
 import path from 'path'
 import { Database } from 'sqlite'
@@ -7,36 +6,24 @@ import { CONTENT_DIR } from './utils'
 const { log: nicelog } = console
 
 export async function generateLlmsTxt(db: Database<sqlite3.Database, sqlite3.Statement>) {
-	nicelog('hello world')
-
 	const examplesDbQuery = await db.all('SELECT * FROM articles WHERE sectionId = "examples"')
 
-	nicelog('examplesDbQuery', examplesDbQuery.length)
-
 	const lines = []
-
 	lines.push(`# tldraw SDK Examples`)
 	lines.push(``)
 
 	for (const example of examplesDbQuery) {
-		addExample(lines, example)
+		lines.push(`## ${example.title}`)
+		lines.push(``)
+		lines.push(`Keywords: ${example.keywords}`)
+		lines.push(``)
+		lines.push(`${example.content}`)
+		lines.push(``)
+		lines.push(`\`\`\`tsx`)
+		lines.push(`${example.componentCode}`)
+		lines.push(`\`\`\``)
+		lines.push(``)
 	}
 
-	const content = lines.join('\n')
-
-	const OUTPUT_DIR = CONTENT_DIR
-	fs.writeFileSync(path.join(OUTPUT_DIR, 'llms.txt'), content)
-}
-
-function addExample(lines: string[], example: Article) {
-	lines.push(`## ${example.title}`)
-	lines.push(``)
-	lines.push(`Keywords: ${example.keywords}`)
-	lines.push(``)
-	lines.push(`${example.content}`)
-	lines.push(``)
-	lines.push(`\`\`\`tsx`)
-	lines.push(`${example.componentCode}`)
-	lines.push(`\`\`\``)
-	lines.push(``)
+	fs.writeFileSync(path.join(CONTENT_DIR, 'llms.txt'), lines.join('\n'))
 }
