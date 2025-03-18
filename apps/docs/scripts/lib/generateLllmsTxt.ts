a14c1f54bbc36eff568fbd8eac0c6bb1c8b9ad8b
Lu[ke] Wilson <l2wilson94@gmail.com>
2025-03-18 15:32:12 +0000

    some output improvements, and publish

--- apps/docs/scripts/lib/generateLllmsTxt.ts
+++ apps/docs/scripts/lib/generateLllmsTxt.ts
@@ -2,8 +2,7 @@ import fs from 'fs'
 import path from 'path'
 import { Database } from 'sqlite'
 import sqlite3 from 'sqlite3'
-import { CONTENT_DIR } from './utils'
-const { log: nicelog } = console
+import { PUBLIC_DIR } from './utils'
 
 export async function generateLlmsTxt(db: Database<sqlite3.Database, sqlite3.Statement>) {
 	const examplesDbQuery = await db.all('SELECT * FROM articles WHERE sectionId = "examples"')
@@ -15,15 +14,21 @@ export async function generateLlmsTxt(db: Database<sqlite3.Database, sqlite3.Sta
 	for (const example of examplesDbQuery) {
 		lines.push(`## ${example.title}`)
 		lines.push(``)
-		lines.push(`Keywords: ${example.keywords}`)
+		lines.push(`Keywords: ${example.keywords.trim()}`)
+		if (example.description?.trim()) {
+			lines.push(``)
+			lines.push(`${example.description.trim()}`)
+		}
 		lines.push(``)
-		lines.push(`${example.content}`)
+		lines.push(`${example.content.trim()}`)
+		lines.push(``)
+		lines.push(`### App.tsx`)
 		lines.push(``)
 		lines.push(`\`\`\`tsx`)
-		lines.push(`${example.componentCode}`)
+		lines.push(`${example.componentCode.trim()}`)
 		lines.push(`\`\`\``)
 		lines.push(``)
 	}
 
-	fs.writeFileSync(path.join(CONTENT_DIR, 'llms.txt'), lines.join('\n'))
+	fs.writeFileSync(path.join(PUBLIC_DIR, 'llms.txt'), lines.join('\n'))
 }
