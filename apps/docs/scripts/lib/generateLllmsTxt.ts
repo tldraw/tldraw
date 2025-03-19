08da9dca0ca08e2a75c0d10a7563d7c39ee1769c
Lu[ke] Wilson <l2wilson94@gmail.com>
2025-03-19 12:47:48 +0000

    add overview llms page

--- apps/docs/scripts/lib/generateLllmsTxt.ts
+++ apps/docs/scripts/lib/generateLllmsTxt.ts
@@ -7,25 +7,42 @@ import { PUBLIC_DIR } from './utils'
 type DbType = Database<sqlite3.Database, sqlite3.Statement>
 
 export async function generateLlmsTxt(db: DbType) {
+	const overviewMarkdown = await getMarkdownForOverview(db)
 	const examplesMarkdown = await getMarkdownForExamples(db)
 	const docsMarkdown = await getMarkdownForDocs(db)
 
+	fs.writeFileSync(path.join(PUBLIC_DIR, 'llms.txt'), overviewMarkdown)
 	fs.writeFileSync(path.join(PUBLIC_DIR, 'llms-full.txt'), `${docsMarkdown}\n${examplesMarkdown}`)
 	fs.writeFileSync(path.join(PUBLIC_DIR, 'llms-examples.txt'), examplesMarkdown)
 	fs.writeFileSync(path.join(PUBLIC_DIR, 'llms-docs.txt'), docsMarkdown)
-	fs.writeFileSync(path.join(PUBLIC_DIR, 'llms-api.txt'), 'Coming soon.')
-	fs.writeFileSync(path.join(PUBLIC_DIR, 'llms.txt'), 'Coming soon.')
 }
 
-const ALLOWED_FILE_TYPES = ['tsx', 'ts', 'js', 'jsx', 'json', 'md', 'css', 'html']
+async function getMarkdownForOverview(db: DbType) {
+	let result = `# tldraw SDK\n\n`
+
+	const guides = await db.all('SELECT * FROM articles WHERE sectionId = "docs"')
+	const examples = await db.all('SELECT * FROM articles WHERE sectionId = "examples"')
+
+	result += `## Guides\n\n`
+	for (const guide of guides) {
+		result += `- [${guide.title}](https://tldraw.dev/docs/${guide.id})\n`
+	}
+
+	result += `\n## Examples\n\n`
+	for (const example of examples) {
+		result += `- [${example.title}](https://tldraw.dev/examples/${example.id})\n`
+	}
+
+	return result
+}
 
 async function getMarkdownForDocs(db: DbType) {
 	const lines = []
 	const guides = await db.all('SELECT * FROM articles WHERE sectionId = "docs"')
 
 	lines.push(`# tldraw SDK Documentation`)
-	lines.push(``)
 	for (const guide of guides) {
+		lines.push(``)
 		lines.push(`## ${guide.title}`)
 		lines.push(``)
 		lines.push(`${guide.content.trim()}`)
@@ -70,6 +87,7 @@ async function getMarkdownForExamples(db: DbType) {
 	return lines.join('\n')
 }
 
+const ALLOWED_FILE_TYPES = ['tsx', 'ts', 'js', 'jsx', 'json', 'md', 'css', 'html']
 function getMarkdownForFile(fileName: string, fileContent: string) {
 	const lines = []
 	const type = fileName.split('.').pop()
