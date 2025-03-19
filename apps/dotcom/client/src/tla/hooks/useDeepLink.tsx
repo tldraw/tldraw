6cc0e45cb321f041e884cfea8073cb534b06276f
David Sheldrick <d.j.sheldrick@gmail.com>
2025-03-19 12:44:51 +0000

    throttle useDeepLink to avoid recomputing a qr code on every frame if following someone else's screen.

--- apps/dotcom/client/src/tla/hooks/useDeepLink.tsx
+++ apps/dotcom/client/src/tla/hooks/useDeepLink.tsx
@@ -1,14 +1,24 @@
-import { useValue } from 'tldraw'
+import { useEffect, useState } from 'react'
+import { react, throttle } from 'tldraw'
 import { globalEditor } from '../../utils/globalEditor'
 
+function getDeepLink() {
+	const editor = globalEditor.get()
+	if (!editor) return null
+	return editor.createDeepLink().toString()
+}
+
 export function useEditorDeepLink() {
-	return useValue(
-		'deepLink',
-		() => {
-			const editor = globalEditor.get()
-			if (!editor) return null
-			return editor.createDeepLink().toString()
-		},
-		[]
-	)
+	const [deepLink, setDeepLink] = useState(getDeepLink())
+	useEffect(() => {
+		const throttledSetDeepLink = throttle(setDeepLink, 300)
+		const unsub = react('setDeepLink', () => {
+			throttledSetDeepLink(getDeepLink())
+		})
+		return () => {
+			unsub()
+			throttledSetDeepLink.cancel()
+		}
+	}, [])
+	return deepLink
 }
