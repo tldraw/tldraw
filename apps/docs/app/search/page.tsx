a8eaae8efdb25a56e0490aab868b125ac24fe157
Lu[ke] Wilson <l2wilson94@gmail.com>
2025-03-25 13:47:25 +0000

    fix mobile sidebar overscrolling

--- apps/docs/app/search/page.tsx
+++ apps/docs/app/search/page.tsx
@@ -8,10 +8,10 @@ export default function Page() {
 	return (
 		<div className="w-full max-w-screen-xl mx-auto md:px-5 md:flex md:pt-8">
 			<DocsSidebar />
-			<div className="fixed z-10 flex items-center justify-between w-full h-12 px-5 bg-white border-b border-zinc-100 dark:border-zinc-800 dark:bg-zinc-950 backdrop-blur md:hidden">
+			<div className="sticky top-14 z-10 flex items-center justify-between w-full h-12 px-5 bg-white border-b border-zinc-100 dark:border-zinc-800 dark:bg-zinc-950 backdrop-blur md:hidden">
 				<DocsMobileSidebar />
 			</div>
-			<main className="relative w-full max-w-3xl px-5 pt-24 shrink md:pr-0 lg:pl-12 xl:pr-12 md:pt-0 min-w-[1px]">
+			<main className="relative w-full max-w-3xl px-5 pt-10 shrink md:pr-0 lg:pl-12 xl:pr-12 md:pt-0 min-w-[1px]">
 				<FullPageSearch />
 			</main>
 		</div>
