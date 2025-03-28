7791a5eba1b67335028b92bd000821bfb5fdd4c2
Mime Čuvalo <mimecuvalo@gmail.com>
2025-03-28 12:38:24 +0000

    add debug, stop formatting lerna.json

--- packages/tldraw/src/lib/ui/components/A11y.tsx
+++ packages/tldraw/src/lib/ui/components/A11y.tsx
@@ -1,11 +1,40 @@
-import { useValue } from '@tldraw/editor'
-import { memo } from 'react'
+import { debugFlags, useValue } from '@tldraw/editor'
+import { memo, useEffect } from 'react'
 import { useA11y } from '../context/a11y'
 
 /** @public @react */
 export const DefaultA11y = memo(function TldrawUiA11y() {
 	const currentMsg = useA11y()
 	const msg = useValue('a11y-msg', () => currentMsg.currentMsg.get(), [])
+
+	useEffect(() => {
+		if (debugFlags.a11y.get()) {
+			const log = (msg: string) => {
+				// eslint-disable-next-line no-console
+				console.debug(
+					`%ca11y%c: ${msg}`,
+					`color: white; background: #40C057; padding: 2px;border-radius: 3px;`,
+					'font-weight: normal'
+				)
+			}
+			const handleKeyDown = (e: KeyboardEvent) => {
+				const el = document.activeElement
+				if (e.key === 'Tab' && el) {
+					const label = el.getAttribute('aria-label') || el.getAttribute('title') || el.textContent
+					if (label) {
+						log(label)
+					}
+				}
+			}
+
+			if (msg.msg) {
+				log(msg.msg)
+			}
+
+			document.addEventListener('keydown', handleKeyDown)
+			return () => document.removeEventListener('keydown', handleKeyDown)
+		}
+	}, [msg])
 
 	return (
 		msg.msg && (
