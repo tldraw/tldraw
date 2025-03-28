ede867ca54f6b390220ee3e358e5be9b65325041
David Sheldrick <d.j.sheldrick@gmail.com>
2025-03-28 15:37:35 +0000

    cleanup

--- packages/editor/src/lib/editor/Editor.ts
+++ packages/editor/src/lib/editor/Editor.ts
@@ -6,7 +6,6 @@ import {
 	react,
 	transact,
 	unsafe__withoutCapture,
-	whyAmIRunning,
 } from '@tldraw/state'
 import {
 	ComputedCache,
@@ -1217,7 +1216,6 @@ export class Editor extends EventEmitter<TLEventMap> {
 	run(fn: () => void, opts?: TLEditorRunOptions): this {
 		const previousIgnoreShapeLock = this._shouldIgnoreShapeLock
 		this._shouldIgnoreShapeLock = opts?.ignoreShapeLock ?? previousIgnoreShapeLock
-
 		try {
 			this.history.batch(fn, opts)
 		} finally {
@@ -4425,7 +4423,6 @@ export class Editor extends EventEmitter<TLEventMap> {
 	/** @internal */
 	@computed private _getShapePageBoundsCache(): ComputedCache<Box, TLShape> {
 		return this.store.createComputedCache<Box, TLShape>('pageBoundsCache', (shape) => {
-			whyAmIRunning()
 			const pageTransform = this._getShapePageTransformCache().get(shape.id)
 
 			if (!pageTransform) return new Box()
