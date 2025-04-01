56f44974c7cc09f5276240c6128922a3d9df364b
Mitja Bezenšek <mitja.bezensek@gmail.com>
2025-04-01 13:20:13 +0200

    Fix jest tests.

--- apps/dotcom/client/setupTests.js
+++ apps/dotcom/client/setupTests.js
@@ -4,6 +4,7 @@ process.env.MULTIPLAYER_SERVER = 'https://localhost:8787'
 process.env.ASSET_UPLOAD = 'https://localhost:8788'
 process.env.IMAGE_WORKER = 'https://images.tldraw.xyz'
 process.env.TLDRAW_ENV = 'test'
+process.env.ZERO_SERVER = 'http://localhost:4848'
 
 global.TextEncoder = require('util').TextEncoder
 global.TextDecoder = require('util').TextDecoder
