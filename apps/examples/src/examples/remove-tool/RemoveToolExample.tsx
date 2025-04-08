c15c0ec5544a6baccfb1409a83b85def77a0a533
Steve Ruiz <steveruizok@gmail.com>
2025-04-08 16:57:09 +0100

    ok

---
+++ apps/examples/src/examples/remove-tool/RemoveToolExample.tsx
@@ -0,0 +1,21 @@
+import { Tldraw } from 'tldraw'
+import 'tldraw/tldraw.css'
+
+export default function RemoveToolExample() {
+	return (
+		<>
+			<div className="tldraw__editor">
+				<Tldraw
+					overrides={{
+						tools: (_editor, tools) => {
+							console.log(tools)
+							// Remove the text tool
+							delete tools.text
+							return tools
+						},
+					}}
+				/>
+			</div>
+		</>
+	)
+}
