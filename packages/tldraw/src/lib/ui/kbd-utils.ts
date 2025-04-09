8b5b998696f67811ed0a872524b829315aec411b
Mime Čuvalo <mimecuvalo@gmail.com>
2025-04-09 16:31:43 +0100

    pretty

--- packages/tldraw/src/lib/ui/kbd-utils.ts
+++ packages/tldraw/src/lib/ui/kbd-utils.ts
@@ -7,17 +7,19 @@ const altKey = tlenv.isDarwin ? '⌥' : 'Alt'
 export function kbd(str: string) {
 	if (str === ',') return [',']
 
-	return str
-		.split(',')[0]
-		// If the string contains [[Tab]], we don't split these up
-		// as they're meant to be atomic.
-		.split(/(\[\[[^\]]+\]\])/g)
-		.map((s) => (s.startsWith('[[') ? s.replace(/[[\]]/g, '') : s.split('')))
-		.flat()
-		.map((sub) => {
-			const subStr = sub.replace(/\$/g, cmdKey).replace(/\?/g, altKey).replace(/!/g, '⇧')
-			return subStr[0].toUpperCase() + subStr.slice(1)
-		})
+	return (
+		str
+			.split(',')[0]
+			// If the string contains [[Tab]], we don't split these up
+			// as they're meant to be atomic.
+			.split(/(\[\[[^\]]+\]\])/g)
+			.map((s) => (s.startsWith('[[') ? s.replace(/[[\]]/g, '') : s.split('')))
+			.flat()
+			.map((sub) => {
+				const subStr = sub.replace(/\$/g, cmdKey).replace(/\?/g, altKey).replace(/!/g, '⇧')
+				return subStr[0].toUpperCase() + subStr.slice(1)
+			})
+	)
 }
 
 /** @public */
