tldraw/tldraw

commit 3bf31007c5a7274f3f7926a84c96c89a4cc2c278

[feature] add rich text and contextual toolbar (#4895)
We're looking to add rich text to the editor!

We originally started with ProseMirror but it became quickly clear that
since it's more down-to-the-metal we'd have to rebuild a bunch of
functionality, effectively managing a rich text editor in addition to a
2D canvas. Examples of this include behaviors around lists where people
expect certain behaviors around combination of lists next to each other,
tabbing, etc.
On top of those product expectations, we'd need to provide a
higher-level API that provided better DX around things like
transactions, switching between lists↔headers, and more.

Given those considerations, a very natural fit was to use TipTap. Much
like tldraw, they provide a great experience around manipulating a rich
text editor. And, we want to pass on those product/DX benefits
downstream to our SDK users.

Some high-level notes:
- the data is stored as the TipTap stringified JSON, it's lightly
validated at the moment, but not stringently.
- there was originally going to be a short-circuit path for plaintext
but it ended up being error-prone with richtext/plaintext living
side-by-side. (this meant there were two separate fields)
- We could still add a way to render faster — I just want to avoid it
being two separate fields, too many footguns.
- things like arrow labels are only plain text (debatable though).

Other related efforts:
- https://github.com/tldraw/tldraw/pull/3051
- https://github.com/tldraw/tldraw/pull/2825

Todo
- [ ] figure out whether we should have a migration or not. This is what
we discussed cc @ds300 and @SomeHats - and whether older clients would
start messing up newer clients. The data becomes lossy if older clients
overwrite with plaintext.

<img width="427" alt="Screenshot 2024-12-09 at 14 43 51"
src="https://github.com/user-attachments/assets/f9dddaee-bfd3-4746-8016-d4247e417e80">
<img width="378" alt="Screenshot 2024-12-09 at 14 42 59"

packages/editor/src/lib/hooks/useCanvasEvents.ts
@@ -100,8 +100,9 @@ export function useCanvasEvents() {
 				if (
 					e.target.tagName !== 'A' &&
 					e.target.tagName !== 'TEXTAREA' &&
+					e.target.isContentEditable &&
 					// When in EditingShape state, we are actually clicking on a 'DIV'
-					// not A/TEXTAREA element yet. So, to preserve cursor position
+					// not A/TEXTAREA/contenteditable element yet. So, to preserve cursor position
 					// for edit mode on mobile we need to not preventDefault.
 					// TODO: Find out if we still need this preventDefault in general though.
 					!(editor.getEditingShape() && e.target.className.includes('tl-text-content'))

commit a880bf3ec9cb0ded7661a6a794fb1f4899891b20

events: fix wrong contentEditable check (#5888)
Fix #5877 . It turns out this is a regression from #4895 - thanks
@MitjaBezensek for the great bisecting!

### Change type

- [x] `bugfix`
- [ ] `improvement`
- [ ] `feature`
- [ ] `api`
- [ ] `other`

### Release notes

- Fix regression of iPad Pencil drawing in certain cases.

packages/editor/src/lib/hooks/useCanvasEvents.ts
@@ -107,7 +107,7 @@ export function useCanvasEvents() {
 				if (
 					e.target.tagName !== 'A' &&
 					e.target.tagName !== 'TEXTAREA' &&
-					e.target.isContentEditable &&
+					!e.target.isContentEditable &&
 					// When in EditingShape state, we are actually clicking on a 'DIV'
 					// not A/TEXTAREA/contenteditable element yet. So, to preserve cursor position
 					// for edit mode on mobile we need to not preventDefault.