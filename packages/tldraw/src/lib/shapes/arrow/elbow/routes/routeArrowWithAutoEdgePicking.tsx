2f2a59fffd6daf64f11cb20e2da8cfd854d23648
alex <alex@dytry.ch>
2025-04-24 14:40:36 +0100

    alex/elbow-arrows: fix INT-1348: Auto-bound arrow doesnt switch to other side quick enough resulting in weird shaped arrow

--- packages/tldraw/src/lib/shapes/arrow/elbow/routes/routeArrowWithAutoEdgePicking.tsx
+++ packages/tldraw/src/lib/shapes/arrow/elbow/routes/routeArrowWithAutoEdgePicking.tsx
@@ -1,3 +1,4 @@
+import { exhaustiveSwitchError } from '@tldraw/editor'
 import {
 	ElbowArrowRoute,
 	ElbowArrowSide,
@@ -79,37 +80,98 @@ export function routeArrowWithAutoEdgePicking(info: ElbowArrowWorkingInfo): Elbo
 		bNonPartialSides.map((bSide) => [aSide, bSide, 'auto', 'auto'] as const)
 	)
 
-	return pickShortest(info, nonPartialRouteCandidates)
+	return pickShortest(info, 'distance', nonPartialRouteCandidates)
 }
 
 export function routeArrowWithPartialEdgePicking(
 	info: ElbowArrowWorkingInfo,
 	aSide: ElbowArrowSide
 ) {
+	let idealRoute = null
+
+	const aRight = info.A.edges.right
+	const aLeft = info.A.edges.left
+	const bTop = info.B.edges.top
+	const bBottom = info.B.edges.bottom
+
+	switch (aSide) {
+		case 'right':
+			if (info.gapX > 0 && info.gapX > Math.abs(info.gapY) && info.midX !== null) {
+				idealRoute = tryRouteArrow(info, 'right', 'left')
+			} else if (
+				aRight &&
+				bTop &&
+				(aRight.expanded ?? aRight.value) <= bTop.crossTarget &&
+				aRight.crossTarget <= (bTop.expanded ?? bTop.value)
+			) {
+				idealRoute = tryRouteArrow(info, 'right', 'top')
+			} else if (
+				aRight &&
+				bBottom &&
+				(aRight.expanded ?? aRight.value) <= bBottom.crossTarget &&
+				aRight.crossTarget >= (bBottom.expanded ?? bBottom.value)
+			) {
+				idealRoute = tryRouteArrow(info, 'right', 'bottom')
+			}
+			break
+		case 'left':
+			if (info.gapX < 0 && Math.abs(info.gapX) > Math.abs(info.gapY) && info.midX !== null) {
+				idealRoute = tryRouteArrow(info, 'left', 'right')
+			} else if (
+				aLeft &&
+				bTop &&
+				(aLeft.expanded ?? aLeft.value) >= bTop.crossTarget &&
+				aLeft.crossTarget <= (bTop.expanded ?? bTop.value)
+			) {
+				idealRoute = tryRouteArrow(info, 'left', 'top')
+			} else if (
+				aLeft &&
+				bBottom &&
+				(aLeft.expanded ?? aLeft.value) >= bBottom.crossTarget &&
+				aLeft.crossTarget >= (bBottom.expanded ?? bBottom.value)
+			) {
+				idealRoute = tryRouteArrow(info, 'left', 'bottom')
+			}
+			break
+		case 'top':
+		case 'bottom':
+			// top and bottom are handled by the pickShortest approach below - it automatically
+			// picks the path we would pick with heuristics anyway.
+			break
+		default:
+			exhaustiveSwitchError(aSide)
+	}
+
+	if (idealRoute) {
+		idealRoute.aEdgePicking = 'manual'
+		idealRoute.bEdgePicking = 'auto'
+		return idealRoute
+	}
+
 	switch (aSide) {
 		case 'top':
-			return pickShortest(info, [
+			return pickShortest(info, 'corners', [
 				['top', 'bottom', 'manual', 'auto'],
 				['top', 'right', 'manual', 'auto'],
 				['top', 'left', 'manual', 'auto'],
 				['top', 'top', 'manual', 'auto'],
 			])
 		case 'bottom':
-			return pickShortest(info, [
+			return pickShortest(info, 'corners', [
 				['bottom', 'top', 'manual', 'auto'],
 				['bottom', 'right', 'manual', 'auto'],
 				['bottom', 'left', 'manual', 'auto'],
 				['bottom', 'bottom', 'manual', 'auto'],
 			])
 		case 'left':
-			return pickShortest(info, [
+			return pickShortest(info, 'corners', [
 				['left', 'right', 'manual', 'auto'],
 				['left', 'bottom', 'manual', 'auto'],
 				['left', 'top', 'manual', 'auto'],
 				['left', 'left', 'manual', 'auto'],
 			])
 		case 'right':
-			return pickShortest(info, [
+			return pickShortest(info, 'corners', [
 				['right', 'left', 'manual', 'auto'],
 				['right', 'top', 'manual', 'auto'],
 				['right', 'bottom', 'manual', 'auto'],
@@ -120,6 +182,7 @@ export function routeArrowWithPartialEdgePicking(
 
 function pickShortest(
 	info: ElbowArrowWorkingInfo,
+	measure: 'distance' | 'corners',
 	edges: ReadonlyArray<
 		readonly [ElbowArrowSide, ElbowArrowSide, ElbowArrowSideReason, ElbowArrowSideReason]
 	>
@@ -128,12 +191,12 @@ function pickShortest(
 	let bestLength = Infinity
 	let bias = 0
 	for (const [aSide, bSide, aEdgePicking, bEdgePicking] of edges) {
-		bias += 1
+		bias += measure === 'distance' ? 1 : 0.1
 		const route = tryRouteArrow(info, aSide, bSide)
 		if (route) {
 			route.aEdgePicking = aEdgePicking
 			route.bEdgePicking = bEdgePicking
-			const length = route.distance
+			const length = measure === 'distance' ? route.distance : route.points.length
 			if (length + bias < bestLength) {
 				bestLength = length
 				bestRoute = route
