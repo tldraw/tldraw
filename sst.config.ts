e0dbcebeb554a7878ae74c633699021145ef3b52
Mitja Bezenšek <mitja.bezensek@gmail.com>
2025-04-30 12:41:12 +0200

    Increase view syncer cpu. (#5980)
    Noticed that view syncer in prod was mostly on 100% cpu usage so decided
    to bump it.
    
    ### Change type
    
    - [x] `improvement`

--- sst.config.ts
+++ sst.config.ts
@@ -2,6 +2,7 @@
 /// <reference path="./.sst/platform/config.d.ts" />
 import { readFileSync } from 'fs'
 
+const isProduction = process.env.TLDRAW_ENV === 'production'
 const githubPrNumber = process.env.GITHUB_REF?.match(/refs\/pull\/(\d+)\/merge/)?.[1]
 
 let previewId = null as null | string
@@ -12,7 +13,7 @@ if (process.env.TLDRAW_ENV === 'preview' && githubPrNumber) {
 let domain = undefined as undefined | string
 if (process.env.TLDRAW_ENV === 'preview' && previewId) {
 	domain = `${previewId}.zero.tldraw.com`
-} else if (process.env.TLDRAW_ENV === 'production') {
+} else if (isProduction) {
 	domain = 'production.zero.tldraw.com'
 } else if (process.env.TLDRAW_ENV === 'staging') {
 	domain = 'staging.zero.tldraw.com'
@@ -117,7 +118,7 @@ export default $config({
 
 		// View Syncer Service
 		const viewSyncer = cluster.addService(`view-syncer`, {
-			cpu: '1 vCPU',
+			cpu: isProduction ? '2 vCPU' : '1 vCPU',
 			memory: '2 GB',
 			architecture: 'arm64',
 			image: commonEnv.ZERO_IMAGE_URL,
