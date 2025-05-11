2c806d901bd5dec1afb748ba2add6f1606b97de3
Steve Ruiz <steveruizok@gmail.com>
2025-05-11 14:45:50 +0100

    e2e joy

--- apps/dotcom/client/e2e/global.setup.ts
+++ apps/dotcom/client/e2e/global.setup.ts
@@ -1,7 +1,11 @@
 import { clerkSetup } from '@clerk/testing/playwright'
 import { test as setup } from '@playwright/test'
+import fs from 'fs/promises'
+
+// delete the e2e/auth directory
 
 // eslint-disable-next-line no-empty-pattern
 setup('clerk setup', async ({}) => {
+	await fs.rm('./.auth', { recursive: true, force: true })
 	await clerkSetup()
 })
