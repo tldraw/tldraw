tldraw/tldraw

commit 77b2db3d87258dfb422e8f39e22e5d4e5a0b056f

Redesign account / help menu (#6008)
This PR updates the design of a help menu in the sidebar.

<img width="421" alt="image"
src="https://github.com/user-attachments/assets/80313e28-7dfe-4c63-9522-78cb4b97f2a2"
/>

<img width="479" alt="image"
src="https://github.com/user-attachments/assets/5d4b64ea-4e05-42ce-bdf4-3093afddbeb1"
/>

<img width="454" alt="image"
src="https://github.com/user-attachments/assets/48b37424-921e-46b0-9851-71721a3ff93d"
/>

<img width="300" alt="image"
src="https://github.com/user-attachments/assets/a839b37c-52a5-4a88-9abe-9c15d3b25f6e"
/>

<img width="562" alt="image"
src="https://github.com/user-attachments/assets/559866f6-a993-48e6-815c-5859e31ae502"
/>

I've done a few things here:
- remove the avatar from the user row in the sidebar
- separate out account menu and help menu
- tighten up the cookie banner
- tighten up the cookie settings
- tighten up the feedback dialog
- remove application settings from file menu
- remove upload file from file menu #5350

### Change type

- [ ] `bugfix`
- [x] `improvement`
- [ ] `feature`
- [ ] `api`
- [ ] `other`

### Release notes

- Design improvements on the app sidebar

apps/dotcom/client/src/tla/components/dialogs/SubmitFeedbackDialog.tsx
@@ -6,6 +6,7 @@ import {
 	TldrawUiButton,
 	TldrawUiButtonLabel,
 	TldrawUiDialogBody,
+	TldrawUiDialogCloseButton,
 	TldrawUiDialogFooter,
 	TldrawUiDialogHeader,
 	TldrawUiDialogTitle,
@@ -17,7 +18,7 @@ import {
 } from 'tldraw'
 import { F, defineMessages, useIntl } from '../../utils/i18n'
 import { ExternalLink } from '../ExternalLink/ExternalLink'
-import { TlaFormCheckbox } from '../tla-form/tla-form'
+import styles from './dialogs.module.css'
 
 const messages = defineMessages({
 	submitted: { defaultMessage: 'Feedback submitted' },
@@ -26,29 +27,28 @@ const messages = defineMessages({
 
 const descriptionKey = 'tldraw-feedback-description'
 
-export function SubmitFeedbackDialog({ onClose }: { onClose(): void }) {
+export function SubmitFeedbackDialog() {
 	const isSignedIn = useAuth().isSignedIn
 	if (isSignedIn) {
-		return <SignedInSubmitFeedbackDialog onClose={onClose} />
+		return <SignedInSubmitFeedbackDialog />
 	}
-	return <SignedOutSubmitFeedbackDialog onClose={onClose} />
+	return <SignedOutSubmitFeedbackDialog />
 }
 
-function SignedOutSubmitFeedbackDialog({ onClose }: { onClose(): void }) {
+function SignedOutSubmitFeedbackDialog() {
 	return (
-		<div>
+		<div className={styles.dialogContainer}>
 			<TldrawUiDialogHeader>
-				<TldrawUiDialogTitle style={{ fontWeight: 700 }}>
-					<F defaultMessage="Give us feedback" />
+				<TldrawUiDialogTitle>
+					<F defaultMessage="Send feedback" />
 				</TldrawUiDialogTitle>
+				<TldrawUiDialogCloseButton />
 			</TldrawUiDialogHeader>
-			<TldrawUiDialogBody
-				style={{ maxWidth: 350, display: 'flex', flexDirection: 'column', gap: 8, paddingTop: 0 }}
-			>
+			<TldrawUiDialogBody className={styles.dialogBody}>
 				<p>
-					<F defaultMessage="See something wrong? Got an idea to improve tldraw? We’d love to hear it!" />
+					<F defaultMessage="Have a bug, issue, or idea for tldraw? Let us know!" />
 				</p>
-				<ul style={{ gap: 4 }}>
+				<ul>
 					<li>
 						<ExternalLink to="https://discord.tldraw.com/?utm_source=dotcom&utm_medium=organic&utm_campaign=dotcom-feedback">
 							<F defaultMessage="Chat with us on Discord" />
@@ -61,18 +61,12 @@ function SignedOutSubmitFeedbackDialog({ onClose }: { onClose(): void }) {
 					</li>
 				</ul>
 			</TldrawUiDialogBody>
-			<TldrawUiDialogFooter className="tlui-dialog__footer__actions">
-				<TldrawUiButton type="normal" onClick={onClose}>
-					<TldrawUiButtonLabel>
-						<F defaultMessage="Close" />
-					</TldrawUiButtonLabel>
-				</TldrawUiButton>
-			</TldrawUiDialogFooter>
+			<TldrawUiDialogFooter />
 		</div>
 	)
 }
 
-function SignedInSubmitFeedbackDialog({ onClose }: { onClose(): void }) {
+function SignedInSubmitFeedbackDialog() {
 	const input = useRef<HTMLTextAreaElement>(null)
 	const checkBox = useRef<HTMLInputElement>(null)
 	const toasts = useToasts()
@@ -100,29 +94,24 @@ function SignedInSubmitFeedbackDialog({ onClose }: { onClose(): void }) {
 				})
 			})
 		deleteFromLocalStorage(descriptionKey)
-		onClose()
 		toasts.addToast({
 			severity: 'success',
 			title: intl.formatMessage(messages.submitted),
 			description: intl.formatMessage(messages.thanks),
 		})
-	}, [intl, onClose, toasts])
+	}, [intl, toasts])
 	return (
-		<div>
+		<div className={styles.feedbackDialog}>
 			<TldrawUiDialogHeader>
-				<TldrawUiDialogTitle style={{ fontWeight: 700 }}>
-					<F defaultMessage="Give us feedback" />
+				<TldrawUiDialogTitle>
+					<F defaultMessage="Send feedback" />
 				</TldrawUiDialogTitle>
+				<TldrawUiDialogCloseButton />
 			</TldrawUiDialogHeader>
-			<TldrawUiDialogBody
-				style={{ maxWidth: 350, display: 'flex', flexDirection: 'column', gap: 8, paddingTop: 0 }}
-			>
-				<p>
-					<F defaultMessage="See something wrong? Got an idea to improve tldraw? We’d love to hear it!" />
-				</p>
+			<TldrawUiDialogBody className={styles.dialogBody}>
 				<p>
 					<F
-						defaultMessage="Fill out this form, <discord>chat with us on Discord</discord> or <github>submit an issue on GitHub</github>."
+						defaultMessage="Have a bug, issue, or idea for tldraw? Let us know! Fill out this form and we will follow up over email if needed. You can also <discord>chat with us on Discord</discord> or <github>submit an issue on GitHub</github>."
 						values={{
 							discord: (chunks) => {
 								return (
@@ -140,47 +129,26 @@ function SignedInSubmitFeedbackDialog({ onClose }: { onClose(): void }) {
 					/>
 				</p>
 				<textarea
-					placeholder="Please provide as much detail as possible"
+					placeholder="Please provide as much detail as possible."
 					defaultValue={getFromLocalStorage(descriptionKey) ?? undefined}
 					onInput={(e) => {
 						setInLocalStorage(descriptionKey, e.currentTarget.value)
 					}}
-					style={{
-						border: '1px solid var(--tla-color-border)',
-						borderRadius: '6px',
-						minHeight: '100px',
-						padding: '6px 8px',
-						resize: 'vertical',
-					}}
+					className={styles.feedbackDialogTextArea}
 					ref={input}
 				/>
 			</TldrawUiDialogBody>
 			<TldrawUiDialogFooter className="tlui-dialog__footer__actions">
-				<div
-					style={{
-						display: 'flex',
-						flexDirection: 'column',
-						flex: 1,
-					}}
-				>
-					<div style={{ display: 'flex', flex: 1, padding: '0px 12px' }}>
-						<TlaFormCheckbox ref={checkBox} defaultChecked={false}>
-							<F defaultMessage="Allow tldraw to email me about this" />
-						</TlaFormCheckbox>
-					</div>
-					<div style={{ display: 'flex', flex: 1, justifyContent: 'flex-end' }}>
-						<TldrawUiButton type="normal" onClick={onClose}>
-							<TldrawUiButtonLabel>
-								<F defaultMessage="Cancel" />
-							</TldrawUiButtonLabel>
-						</TldrawUiButton>
-						<TldrawUiButton type="primary" onClick={onSubmit}>
-							<TldrawUiButtonLabel>
-								<F defaultMessage="Submit" />
-							</TldrawUiButtonLabel>
-						</TldrawUiButton>
-					</div>
-				</div>
+				<TldrawUiButton type="normal">
+					<TldrawUiButtonLabel>
+						<F defaultMessage="Cancel" />
+					</TldrawUiButtonLabel>
+				</TldrawUiButton>
+				<TldrawUiButton type="primary" onClick={onSubmit}>
+					<TldrawUiButtonLabel>
+						<F defaultMessage="Submit" />
+					</TldrawUiButtonLabel>
+				</TldrawUiButton>
 			</TldrawUiDialogFooter>
 		</div>
 	)

commit be59c37b047e871db593cf896ca86df83c155f2c

fix feedback dialog (#6049)
feedback dialog is not sending. this was broken in #6008 cc @steveruizok

### Change type

- [x] `other`

apps/dotcom/client/src/tla/components/dialogs/SubmitFeedbackDialog.tsx
@@ -68,16 +68,14 @@ function SignedOutSubmitFeedbackDialog() {
 
 function SignedInSubmitFeedbackDialog() {
 	const input = useRef<HTMLTextAreaElement>(null)
-	const checkBox = useRef<HTMLInputElement>(null)
 	const toasts = useToasts()
 	const intl = useIntl()
 	const onSubmit = useCallback(async () => {
 		if (!input.current?.value?.trim()) return
-		if (!checkBox.current) return
 		fetch('/api/app/submit-feedback', {
 			method: 'POST',
 			body: JSON.stringify({
-				allowContact: checkBox.current.checked,
+				allowContact: true,
 				description: input.current.value.trim(),
 			} satisfies SubmitFeedbackRequestBody),
 		})