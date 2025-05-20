51a4f2433a9638a91a2306866a727e44c1718250
Mime Čuvalo <mimecuvalo@gmail.com>
2025-05-20 23:02:55 +0100

    analytics: once more, with feeling

--- apps/dotcom/client/src/utils/csp.ts
+++ apps/dotcom/client/src/utils/csp.ts
@@ -23,6 +23,7 @@ export const cspDirectives: { [key: string]: string[] } = {
 		'https://zero.tldraw.com',
 		'http://localhost:4848',
 		'https://analytics.tldraw.com',
+		'https://stats.g.doubleclick.net',
 	],
 	'font-src': [`'self'`, `https://fonts.googleapis.com`, `https://fonts.gstatic.com`, 'data:'],
 	'frame-src': [`https:`],
