0daf8b5b59dfeb42d8f73ea4fbf72b7603dd8f4d
Steve Ruiz <steveruizok@gmail.com>
2025-06-01 06:02:23 +0100

    Update SneakyHandToolEmptyPage.tsx

--- apps/dotcom/client/src/tla/components/TlaEditor/sneaky/SneakyHandToolEmptyPage.tsx
+++ apps/dotcom/client/src/tla/components/TlaEditor/sneaky/SneakyHandToolEmptyPage.tsx
@@ -1,12 +1,11 @@
 import { useEffect } from 'react'
 import { useEditor } from 'tldraw'
-import { getIsCoarsePointer } from '../../../utils/getIsCoarsePointer'
 
 export function SneakyHandToolEmptyPage() {
 	const editor = useEditor()
 
 	useEffect(() => {
-		if (getIsCoarsePointer() && editor.getCurrentPageShapeIds().size > 0) {
+		if (editor.getInstanceState().isCoarsePointer && editor.getCurrentPageShapeIds().size > 0) {
 			editor.setCurrentTool('hand')
 		}
 	}, [editor])
