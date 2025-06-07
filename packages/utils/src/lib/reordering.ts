67fd1c7b9cdd3924f33a14bb2fb84db0aaa686be
Steve Ruiz <steveruizok@gmail.com>
2025-06-07 09:56:54 -0700

    revert

--- packages/utils/src/lib/reordering.ts
+++ packages/utils/src/lib/reordering.ts
@@ -113,14 +113,9 @@ export function getIndices(n: number, start = 'a1' as IndexKey) {
  * @param b - An object with an index property.
  * @public */
 export function sortByIndex<T extends { index: IndexKey }>(a: T, b: T) {
-	return sortIndices(a.index, b.index)
-}
-
-/** @internal */
-export function sortIndices(a: IndexKey, b: IndexKey) {
-	if (a < b) {
+	if (a.index < b.index) {
 		return -1
-	} else if (a > b) {
+	} else if (a.index > b.index) {
 		return 1
 	}
 	return 0
