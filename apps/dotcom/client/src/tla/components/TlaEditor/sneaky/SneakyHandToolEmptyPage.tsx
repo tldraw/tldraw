181708c5d5c4e7abadf104d2a6e5aeb2eb6abdbf
Mitja Bezenšek <mitja.bezensek@gmail.com>
2025-06-09 15:19:22 +0200

    Fix comment.

--- apps/dotcom/client/src/tla/components/TlaEditor/sneaky/SneakyHandToolEmptyPage.tsx
+++ apps/dotcom/client/src/tla/components/TlaEditor/sneaky/SneakyHandToolEmptyPage.tsx
@@ -6,7 +6,7 @@ export function SneakyHandToolEmptyPage() {
 	const editor = useEditor()
 
 	useEffect(() => {
-		// If the editor is in coarse pointer mode and there are no shapes on the current page,
+		// If the editor is in coarse pointer mode and there are some shapes on the current page,
 		// start on the hand tool to avoid accidental selections / drags as the user tries to
 		// orient themselves or move around the page
 		if (getIsCoarsePointer() && editor.getCurrentPageShapeIds().size > 0) {
