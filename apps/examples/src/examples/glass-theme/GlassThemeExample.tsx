7b507698e435fc78037c61f2ecb4e514c549754e
Lu[ke] Wilson <l2wilson94@gmail.com>
2025-06-10 17:12:59 +0100

    fix it

--- apps/examples/src/examples/glass-theme/GlassThemeExample.tsx
+++ apps/examples/src/examples/glass-theme/GlassThemeExample.tsx
@@ -3,9 +3,9 @@ import 'tldraw/tldraw.css'
 import './glass-theme.css'
 
 export default function GlassThemeExample() {
-    return (
-        <div className="tldraw__editor glass-theme">
-            <Tldraw />
-        </div>
-    )
+	return (
+		<div className="tldraw__editor">
+			<Tldraw persistenceKey="glass-example" />
+		</div>
+	)
 }
