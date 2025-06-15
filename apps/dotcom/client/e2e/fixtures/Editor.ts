f9fb94f690dc29470d149c0a393e7d6848dbcaae
Steve Ruiz <steveruizok@gmail.com>
2025-06-15 20:55:16 +0100

    ok

--- apps/dotcom/client/e2e/fixtures/Editor.ts
+++ apps/dotcom/client/e2e/fixtures/Editor.ts
@@ -83,9 +83,5 @@ export class Editor {
 	async createTextShape(text: string) {
 		await this.page.locator('.tl-background').click({ clickCount: 2 })
 		await this.page.locator('div[contenteditable="true"]').fill(text)
-		await this.page.evaluate(() => {
-			// Since we're using `getByText` to find the text shape, we need to reset the HTML inside of the text measurement element or else we'll get that one, too
-			;(window as any).editor.textManager.resetElmHtml()
-		})
 	}
 }
