9a71ec88cb83e9c973af0e6d499af321db386bc3
Mime Čuvalo <mimecuvalo@gmail.com>
2025-06-16 16:34:23 +0100

    e2e

--- apps/examples/e2e/tests/test-image-toolbar.spec.ts
+++ apps/examples/e2e/tests/test-image-toolbar.spec.ts
@@ -175,7 +175,7 @@ test.describe('Image toolbar behaviour', () => {
 		if (isMobile) return
 
 		// Click the Alt Text button
-		await page.getByText('Alt').click()
+		await page.getByTitle('Alternative text').click()
 
 		// Enter some alt text
 		await page.getByTestId('image-toolbar.alt-text-input').fill('A test image for e2e testing')
