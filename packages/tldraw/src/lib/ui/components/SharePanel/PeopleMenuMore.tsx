5025678d917f31c4bd1626e55348db7164723e43
Steve Ruiz <steveruizok@gmail.com>
2025-06-30 16:54:24 +0100

    tweakz

--- packages/tldraw/src/lib/ui/components/SharePanel/PeopleMenuMore.tsx
+++ packages/tldraw/src/lib/ui/components/SharePanel/PeopleMenuMore.tsx
@@ -1,5 +1,3 @@
 export function PeopleMenuMore({ count }: { count: number }) {
-	return (
-		<div className="tlui-people-menu__avatar tlui-people-menu__more">{'+' + Math.abs(count)}</div>
-	)
+	return <div className="tlui-people-menu__avatar tlui-people-menu__more">{Math.abs(count)}</div>
 }
