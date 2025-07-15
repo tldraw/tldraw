96ce4c2f8607353ce239487fe9bac042fb897c7b
alex <alex@dytry.ch>
2025-07-15 15:53:11 +0100

    alex/spinner-animation: fix

--- packages/tldraw/api-report.api.md
+++ packages/tldraw/api-report.api.md
@@ -2534,7 +2534,7 @@ export interface SolidPathBuilderOpts extends BasePathBuilderOpts {
 }
 
 // @internal (undocumented)
-export function Spinner(props: React_3.SVGProps<SVGSVGElement>): JSX_2.Element | null;
+export function Spinner(props: React_3.SVGProps<SVGSVGElement>): JSX_2.Element;
 
 // @public (undocumented)
 export function SplineStylePickerSet({ styles }: StylePickerSetProps): JSX_2.Element | null;
--- packages/tldraw/src/lib/ui/components/Spinner.tsx
+++ packages/tldraw/src/lib/ui/components/Spinner.tsx
@@ -1,11 +1,10 @@
-import { useEditorComponents } from '@tldraw/editor'
+import { DefaultSpinner } from '@tldraw/editor'
 import React from 'react'
 import { useTranslation } from '../hooks/useTranslation/useTranslation'
 
 /** @internal */
 export function Spinner(props: React.SVGProps<SVGSVGElement>) {
-	const { Spinner } = useEditorComponents()
 	const msg = useTranslation()
 
-	return Spinner && <Spinner aria-label={msg('app.loading')} {...props} />
+	return <DefaultSpinner aria-label={msg('app.loading')} {...props} />
 }
