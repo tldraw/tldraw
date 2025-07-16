b28973bc05be4846a2079e2ee3ebcc864b2c8d93
alex <alex@dytry.ch>
2025-07-16 17:48:59 +0100

    alex/vue-template: tldraw vue template

---
+++ templates/vue/src/App.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { createElement } from 'react'
+import { createRoot } from 'react-dom/client'
+import { onMounted, useTemplateRef } from 'vue'
+import { TldrawWrapper } from './TldrawWrapper'
+
+const tldrawWrapperEl = useTemplateRef('tldraw')
+
+onMounted(() => {
+	if (!tldrawWrapperEl.value) return
+	createRoot(tldrawWrapperEl.value).render(createElement(TldrawWrapper))
+})
+</script>
+
+<template>
+	<header>
+		Here's a vue app that includes tldraw!
+	</header>
+
+	<main ref="tldraw" />
+</template>
+
+<style scoped>
+header {
+	padding: 1rem;
+	border-bottom: 1px solid #e0e0e0;
+}
+
+main {
+	flex: 1;
+}
+</style>
