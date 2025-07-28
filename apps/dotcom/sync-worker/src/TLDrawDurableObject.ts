64aec6a60a6251822f543593aefea9cb693c967b
Mitja Bezenšek <mitja.bezensek@gmail.com>
2025-07-28 15:42:51 +0200

    Bring back clock.

--- apps/dotcom/sync-worker/src/TLDrawDurableObject.ts
+++ apps/dotcom/sync-worker/src/TLDrawDurableObject.ts
@@ -720,7 +720,7 @@ export class TLDrawDurableObject extends DurableObject {
 				this.maybeAssociateFileAssets()
 
 				const key = getR2KeyForRoom({ slug: slug, isApp: this.documentInfo.isApp })
-				await this._uploadSnapshotToR2(room, snapshot, key)
+				await this._uploadSnapshotToR2(room, snapshot, clock, key)
 
 				this._lastPersistedClock = clock
 
@@ -746,6 +746,7 @@ export class TLDrawDurableObject extends DurableObject {
 	private async _uploadSnapshotToR2(
 		room: TLSocketRoom<TLRecord, SessionMeta>,
 		snapshot: RoomSnapshot,
+		clock: number,
 		key: string
 	) {
 		const out1 = await this.r2.rooms.createMultipartUpload(key)
@@ -772,7 +773,7 @@ export class TLDrawDurableObject extends DurableObject {
 				partNumber++
 			}
 
-			for (const chunk of generateSnapshotChunks(snapshot)) {
+			for (const chunk of generateSnapshotChunks(snapshot, clock)) {
 				let remainingChunk = chunk
 
 				while (remainingChunk.byteLength > 0) {
@@ -997,10 +998,10 @@ async function listAllObjectKeys(bucket: R2Bucket, prefix: string): Promise<stri
 	return keys
 }
 
-function* generateSnapshotChunks(snapshot: RoomSnapshot): Generator<Uint8Array> {
+function* generateSnapshotChunks(snapshot: RoomSnapshot, clock: number): Generator<Uint8Array> {
 	const encoder = new TextEncoder()
 
-	yield encoder.encode(`{"tombstones":`)
+	yield encoder.encode(`{"clock":${clock},"tombstones":`)
 	yield encoder.encode(JSON.stringify(snapshot.tombstones))
 	yield encoder.encode(`,"schema":`)
 	yield encoder.encode(JSON.stringify(snapshot.schema))
