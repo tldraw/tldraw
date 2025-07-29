5e2c8b536c67f7fcda270f2b8eac4632883560b7
Mitja Bezenšek <mitja.bezensek@gmail.com>
2025-07-29 10:18:43 +0200

    Add tests.

--- apps/dotcom/sync-worker/src/TLDrawDurableObject.ts
+++ apps/dotcom/sync-worker/src/TLDrawDurableObject.ts
@@ -41,6 +41,7 @@ import { PERSIST_INTERVAL_MS } from './config'
 import { createPostgresConnectionPool } from './postgres'
 import { getR2KeyForRoom } from './r2'
 import { getPublishedRoomSnapshot } from './routes/tla/getPublishedFile'
+import { generateSnapshotChunks } from './snapshotUtils'
 import { Analytics, DBLoadResult, Environment, TLServerEvent } from './types'
 import { EventData, writeDataPoint } from './utils/analytics'
 import { createSupabaseClient } from './utils/createSupabaseClient'
@@ -995,46 +996,4 @@ async function listAllObjectKeys(bucket: R2Bucket, prefix: string): Promise<stri
 	} while (cursor)
 
 	return keys
-}
-
-function toJsonString(value: unknown): string {
-	if (value === undefined) {
-		return 'null'
-	}
-	return JSON.stringify(value)
-}
-
-function* generateSnapshotChunks(snapshot: RoomSnapshot): Generator<Uint8Array> {
-	const encoder = new TextEncoder()
-
-	yield encoder.encode('{')
-
-	const keys = Object.keys(snapshot) as Array<keyof RoomSnapshot>
-	let isFirstKey = true
-
-	for (const key of keys) {
-		if (isFirstKey) {
-			isFirstKey = false
-		} else {
-			yield encoder.encode(',')
-		}
-
-		yield encoder.encode(`"${key}":`)
-
-		const value = snapshot[key]
-		if (Array.isArray(value)) {
-			yield encoder.encode('[')
-			for (let i = 0; i < value.length; i++) {
-				if (i > 0) {
-					yield encoder.encode(',')
-				}
-				yield encoder.encode(toJsonString(value[i]))
-			}
-			yield encoder.encode(']')
-		} else {
-			yield encoder.encode(toJsonString(value))
-		}
-	}
-
-	yield encoder.encode('}')
 }
