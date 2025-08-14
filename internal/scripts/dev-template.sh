cb45622449820475cc1e9be25d146eb9120d3495
alex <alex@dytry.ch>
2025-08-14 18:11:32 +0100

    Workflow builder starter kit (#6412)
    Adds a workflow builder template. See the README for more information.
    
    ### Change type
    
    - [x] `other`
    
    ### Release notes
    
    ### API Changes
    
    - `TldrawUiToolbar` now accepts an optional `orientation` prop, which
    can be either horizontal or vertical. It's horizontal by default.
    
    ---------
    
    Co-authored-by: Mime Čuvalo <mimecuvalo@gmail.com>
    Co-authored-by: SergioRiquelmeSanchez <sergioriquelmesanchez@gmail.com>

--- internal/scripts/dev-template.sh
+++ internal/scripts/dev-template.sh
@@ -9,4 +9,10 @@ if [ -z "$template_name" ]; then
     exit 1
 fi
 
-LAZYREPO_PRETTY_OUTPUT=0 lazy run dev --filter="templates/$template_name" --filter='packages/tldraw' --filter='apps/bemo-worker' 
+if [ ! -d "templates/$template_name" ]; then
+    echo "Template $template_name does not exist"
+    echo "Available templates: $(ls templates | awk 'ORS=", "' | sed 's/, $//')"
+    exit 1
+fi
+
+LAZYREPO_PRETTY_OUTPUT=0 lazy run dev --filter="templates/$template_name" --filter='packages/tldraw' --filter='apps/bemo-worker'
