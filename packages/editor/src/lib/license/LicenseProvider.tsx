894cec94fbf507c30df926994bedb1771d81be35
Mitja Bezenšek <mitja.bezensek@gmail.com>
2025-08-28 13:49:44 +0200

    Fix internal license logic. (#6665)
    Looks like we never properly errored out on a expired internal license. 
    
    We should probably hotfix this so it goes out in the 3.x branch?
    
    ### Change type
    
    - [x] `bugfix`
    
    
    ### Release notes
    
    - Fix a bug with expired internal license check.
    
    ### API changes
    
    * Add an `internal-expired` state to the license manager.

--- packages/editor/src/lib/license/LicenseProvider.tsx
+++ packages/editor/src/lib/license/LicenseProvider.tsx
@@ -1,3 +1,4 @@
+import { useValue } from '@tldraw/state-react'
 import { createContext, ReactNode, useContext, useState } from 'react'
 import { LicenseManager } from './LicenseManager'
 
@@ -16,5 +17,12 @@ export function LicenseProvider({
 	children: ReactNode
 }) {
 	const [licenseManager] = useState(() => new LicenseManager(licenseKey))
+	const licenseState = useValue(licenseManager.state)
+
+	// If internal license has expired, don't render the editor at all
+	if (licenseState === 'internal-expired') {
+		return <div data-testid="tl-license-expired" style={{ display: 'none' }} />
+	}
+
 	return <LicenseContext.Provider value={licenseManager}>{children}</LicenseContext.Provider>
 }
