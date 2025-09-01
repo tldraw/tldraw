3634280eed9cdaeb545514550356b7bdef2d7cbd
Lu[ke] Wilson <l2wilson94@gmail.com>
2025-09-01 09:43:45 +0100

    update wording of go to agent  button

--- templates/agent/client/components/GoToAgentButton.tsx
+++ templates/agent/client/components/GoToAgentButton.tsx
@@ -73,7 +73,7 @@ export function GoToAgentButton() {
 						</svg>
 					}
 				/>
-				<TldrawUiButtonLabel>Follow Agent</TldrawUiButtonLabel>
+				<TldrawUiButtonLabel>Go to agent</TldrawUiButtonLabel>
 			</div>
 		</TldrawUiButton>
 	)
