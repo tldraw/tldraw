1efac4751756172f095ebcd2c70ba8dcfeffee1f
Mime Čuvalo <mimecuvalo@gmail.com>
2025-09-16 22:59:29 +0100

    a11y: more followup work to #6750 (#6760)
    followup tweaks to https://github.com/tldraw/tldraw/pull/6750
    
    - [x] hitting Escape when a tooltip is open, just hides the tooltip and
    doesn't lose focus.
    - [x] shows focus ring on rich text toolbar when appropriate, followup
    tweak to https://github.com/tldraw/tldraw/pull/6125
    - [x] allows Confirm button for AltTextEditor to be tabbable, along with
    buttons for the LinkEditor
    
    ### Change type
    
    - [x] `bugfix`
    - [ ] `improvement`
    - [ ] `feature`
    - [ ] `api`
    - [ ] `other`
    
    ### Release notes
    
    - a11y: followup tweaks to https://github.com/tldraw/tldraw/pull/6750
    
    ### API changes
    
    - adds `aria-label` to `TLUiInput`

--- packages/tldraw/src/lib/ui/components/Toolbar/LinkEditor.tsx
+++ packages/tldraw/src/lib/ui/components/Toolbar/LinkEditor.tsx
@@ -2,9 +2,9 @@ import { preventDefault, TiptapEditor, useEditor } from '@tldraw/editor'
 import { useEffect, useRef, useState } from 'react'
 import { useUiEvents } from '../../context/events'
 import { useTranslation } from '../../hooks/useTranslation/useTranslation'
+import { TldrawUiButton } from '../primitives/Button/TldrawUiButton'
 import { TldrawUiButtonIcon } from '../primitives/Button/TldrawUiButtonIcon'
 import { TldrawUiInput } from '../primitives/TldrawUiInput'
-import { TldrawUiToolbarButton } from '../primitives/TldrawUiToolbar'
 
 /** @public */
 export interface LinkEditorProps {
@@ -77,7 +77,7 @@ export function LinkEditor({ textEditor, value: initialValue, onClose }: LinkEdi
 				placeholder="example.com"
 				aria-label="example.com"
 			/>
-			<TldrawUiToolbarButton
+			<TldrawUiButton
 				className="tlui-rich-text__toolbar-link-visit"
 				title={msg('tool.rich-text-link-visit')}
 				type="icon"
@@ -86,8 +86,8 @@ export function LinkEditor({ textEditor, value: initialValue, onClose }: LinkEdi
 				disabled={!value}
 			>
 				<TldrawUiButtonIcon small icon="external-link" />
-			</TldrawUiToolbarButton>
-			<TldrawUiToolbarButton
+			</TldrawUiButton>
+			<TldrawUiButton
 				className="tlui-rich-text__toolbar-link-remove"
 				title={msg('tool.rich-text-link-remove')}
 				data-testid="rich-text.link-remove"
@@ -96,7 +96,7 @@ export function LinkEditor({ textEditor, value: initialValue, onClose }: LinkEdi
 				onClick={handleRemoveLink}
 			>
 				<TldrawUiButtonIcon small icon="trash" />
-			</TldrawUiToolbarButton>
+			</TldrawUiButton>
 		</>
 	)
 }
