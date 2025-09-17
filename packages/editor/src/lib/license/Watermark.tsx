66c4fceed7a4ffb5da54f3db6fbb356896b83531
Steve Ruiz <steveruizok@gmail.com>
2025-09-17 15:50:14 +0100

    Update Watermark.tsx (#6776)
    This PR updates the copy for the unlicensed watermark.
    
    ### Change type
    
    - [ ] `bugfix`
    - [ ] `improvement`
    - [ ] `feature`
    - [ ] `api`
    - [x] `other`

--- packages/editor/src/lib/license/Watermark.tsx
+++ packages/editor/src/lib/license/Watermark.tsx
@@ -47,7 +47,8 @@ const UnlicensedWatermark = memo(function UnlicensedWatermark({
 	const ref = useRef<HTMLDivElement>(null)
 	usePassThroughWheelEvents(ref)
 
-	const url = 'https://tldraw.dev/?utm_source=dotcom&utm_medium=organic&utm_campaign=watermark'
+	const url =
+		'https://tldraw.dev/pricing?utm_source=dotcom&utm_medium=organic&utm_campaign=watermark'
 
 	return (
 		<div
@@ -67,23 +68,10 @@ const UnlicensedWatermark = memo(function UnlicensedWatermark({
 					markEventAsHandled(e)
 					preventDefault(e)
 				}}
-				title="Unlicensed - click to get a license"
+				title="The tldraw SDK requires a license key to work in production. You can get a free 100-day trial license at tldraw.dev/pricing."
 				onClick={() => runtime.openWindow(url, '_blank')}
-				style={{
-					position: 'absolute',
-					pointerEvents: 'all',
-					cursor: 'pointer',
-					color: 'var(--tl-color-text)',
-					opacity: 0.8,
-					border: 0,
-					padding: 0,
-					backgroundColor: 'transparent',
-					fontSize: '11px',
-					fontWeight: '600',
-					textAlign: 'center',
-				}}
 			>
-				Unlicensed
+				Get a license for production
 			</button>
 		</div>
 	)
@@ -130,7 +118,7 @@ const WatermarkInner = memo(function WatermarkInner({
 					markEventAsHandled(e)
 					preventDefault(e)
 				}}
-				title="made with tldraw"
+				title="Build infinite canvas applications with the tldraw SDK. Learn more at https://tldraw.dev."
 				onClick={() => runtime.openWindow(url, '_blank')}
 				style={{ mask: maskCss, WebkitMask: maskCss }}
 			/>
@@ -142,7 +130,8 @@ const LicenseStyles = memo(function LicenseStyles() {
 	const editor = useEditor()
 	const className = LicenseManager.className
 
-	const CSS = `/* ------------------- SEE LICENSE -------------------
+	const CSS = `
+/* ------------------- SEE LICENSE -------------------
 The tldraw watermark is part of tldraw's license. It is shown for unlicensed
 or "licensed-with-watermark" users. By using this library, you agree to
 preserve the watermark's behavior, keeping it visible, unobscured, and
@@ -151,87 +140,105 @@ available to user-interaction.
 To remove the watermark, please purchase a license at tldraw.dev.
 */
 
-	.${className} {
-		position: absolute;
-		bottom: max(var(--tl-space-2), env(safe-area-inset-bottom));
-		right: max(var(--tl-space-2), env(safe-area-inset-right));
-		width: 96px;
-		height: 32px;
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		z-index: var(--tl-layer-watermark) !important;
-		background-color: color-mix(in srgb, var(--tl-color-background) 62%, transparent);
+.${className} {
+	position: absolute;
+	bottom: max(var(--tl-space-2), env(safe-area-inset-bottom));
+	right: max(var(--tl-space-2), env(safe-area-inset-right));
+	width: 96px;
+	height: 32px;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	z-index: var(--tl-layer-watermark) !important;
+	background-color: color-mix(in srgb, var(--tl-color-background) 62%, transparent);
+	opacity: 1;
+	border-radius: 5px;
+	pointer-events: all;
+	padding: 2px;
+	box-sizing: content-box;
+}
+
+.${className} > button {
+	position: absolute;
+	width: 96px;
+	height: 32px;
+	pointer-events: all;
+	cursor: inherit;
+	color: var(--tl-color-text);
+	opacity: .38;
+	border: 0;
+	padding: 0;
+	background-color: currentColor;
+}
+
+.${className}[data-debug='true'] {
+	bottom: max(46px, env(safe-area-inset-bottom));
+}
+
+.${className}[data-mobile='true'] {
+	border-radius: 4px 0px 0px 4px;
+	right: max(-2px, calc(env(safe-area-inset-right) - 2px));
+	width: 8px;
+	height: 48px;
+}
+
+.${className}[data-mobile='true'] > button {
+	width: 8px;
+	height: 32px;
+}
+
+.${className}[data-unlicensed='true'] > button {
+	font-size: 100px;
+	position: absolute;
+	pointer-events: all;
+	cursor: pointer;
+	color: var(--tl-color-text);
+	opacity: 0.8;
+	border: 0;
+	padding: 0;
+	background-color: transparent;
+	font-size: 11px;
+	font-weight: 600;
+	text-align: center;
+}
+
+.${className}[data-mobile='true'][data-unlicensed='true'] > button {
+	display: none;
+}
+
+@media (hover: hover) {
+	.${className}[data-licensed='false'] > button {
+		pointer-events: none;
+	}
+
+	.${className}[data-licensed='false']:hover {
+		background-color: var(--tl-color-background);
+		transition: background-color 0.2s ease-in-out;
+		transition-delay: 0.32s;
+	}
+
+	.${className}[data-licensed='false']:hover > button {
+		animation: ${className}_delayed_link 0.2s forwards ease-in-out;
+		animation-delay: 0.32s;
+	}
+
+	.${className}[data-licensed='false'] > button:focus-visible {
 		opacity: 1;
-		border-radius: 5px;
-		pointer-events: all;
-		padding: 2px;
-		box-sizing: content-box;
 	}
+}
 
-	.${className} > button {
-		position: absolute;
-		width: 96px;
-		height: 32px;
-		pointer-events: all;
+@keyframes ${className}_delayed_link {
+	0% {
 		cursor: inherit;
-		color: var(--tl-color-text);
 		opacity: .38;
-		border: 0;
-		padding: 0;
-		background-color: currentColor;
+		pointer-events: none;
 	}
-
-	.${className}[data-debug='true'] {
-		bottom: max(46px, env(safe-area-inset-bottom));
+	100% {
+		cursor: pointer;
+		opacity: 1;
+		pointer-events: all;
 	}
-
-	.${className}[data-mobile='true'] {
-		border-radius: 4px 0px 0px 4px;
-		right: max(-2px, calc(env(safe-area-inset-right) - 2px));
-		width: 8px;
-		height: 48px;
-	}
-
-	.${className}[data-mobile='true'] > button {
-		width: 8px;
-		height: 32px;
-	}
-
-	@media (hover: hover) {
-		.${className} > button {
-			pointer-events: none;
-		}
-
-		.${className}:hover {
-			background-color: var(--tl-color-background);
-			transition: background-color 0.2s ease-in-out;
-			transition-delay: 0.32s;
-		}
-
-		.${className}:hover > button {
-			animation: ${className}_delayed_link 0.2s forwards ease-in-out;
-			animation-delay: 0.32s;
-		}
-
-		.${className} > button:focus-visible {
-			opacity: 1;
-		}
-	}
-
-
-	@keyframes ${className}_delayed_link {
-		0% {
-			cursor: inherit;
-			opacity: .38;
-			pointer-events: none;
-		}
-		100% {
-			cursor: pointer;
-			opacity: 1;
-			pointer-events: all;
-		}
-	}`
+}`
 
 	return <style nonce={editor.options.nonce}>{CSS}</style>
 })
