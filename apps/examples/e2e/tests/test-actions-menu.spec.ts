7cd11d7a901121bef9cd29cfe8436f2b360cdde4
alex <alex@dytry.ch>
2025-09-19 15:25:42 +0100

    alex/fix-exports: fix tests not running

--- apps/examples/e2e/tests/test-actions-menu.spec.ts
+++ apps/examples/e2e/tests/test-actions-menu.spec.ts
@@ -1,6 +1,6 @@
 import { expect } from '@playwright/test'
+import test from '../fixtures/fixtures'
 import { setup } from '../shared-e2e'
-import test from './fixtures/fixtures'
 
 test.describe('actions menu', () => {
 	test.beforeEach(setup)
