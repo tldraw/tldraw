29575aab950d2c91b22aa14eeea5a959694b33c0
Lu[ke] Wilson <l2wilson94@gmail.com>
2025-09-24 18:36:18 +0200

    more

--- templates/shader/src/fluid.ts
+++ templates/shader/src/fluid.ts
@@ -1,4 +1,5 @@
-// @ts-nocheck
+/* eslint-disable */
+
 /*
 MIT License
 
@@ -963,54 +964,54 @@ export class FluidSimulation {
 		return [r, g, b]
 	}
 
-	private HSVtoRGB(h: number, s: number, v: number) {
-		let r: number, g: number, b: number, i: number, f: number, p: number, q: number, t: number
-		i = Math.floor(h * 6)
-		f = h * 6 - i
-		p = v * (1 - s)
-		q = v * (1 - f * s)
-		t = v * (1 - (1 - f) * s)
+	// private HSVtoRGB(h: number, s: number, v: number) {
+	// 	let r: number, g: number, b: number, i: number, f: number, p: number, q: number, t: number
+	// 	i = Math.floor(h * 6)
+	// 	f = h * 6 - i
+	// 	p = v * (1 - s)
+	// 	q = v * (1 - f * s)
+	// 	t = v * (1 - (1 - f) * s)
 
-		switch (i % 6) {
-			case 0:
-				r = v
-				g = t
-				b = p
-				break
-			case 1:
-				r = q
-				g = v
-				b = p
-				break
-			case 2:
-				r = p
-				g = v
-				b = t
-				break
-			case 3:
-				r = p
-				g = q
-				b = v
-				break
-			case 4:
-				r = t
-				g = p
-				b = v
-				break
-			case 5:
-				r = v
-				g = p
-				b = q
-				break
-			default:
-				r = 0
-				g = 0
-				b = 0
-				break
-		}
+	// 	switch (i % 6) {
+	// 		case 0:
+	// 			r = v
+	// 			g = t
+	// 			b = p
+	// 			break
+	// 		case 1:
+	// 			r = q
+	// 			g = v
+	// 			b = p
+	// 			break
+	// 		case 2:
+	// 			r = p
+	// 			g = v
+	// 			b = t
+	// 			break
+	// 		case 3:
+	// 			r = p
+	// 			g = q
+	// 			b = v
+	// 			break
+	// 		case 4:
+	// 			r = t
+	// 			g = p
+	// 			b = v
+	// 			break
+	// 		case 5:
+	// 			r = v
+	// 			g = p
+	// 			b = q
+	// 			break
+	// 		default:
+	// 			r = 0
+	// 			g = 0
+	// 			b = 0
+	// 			break
+	// 	}
 
-		return { r, g, b }
-	}
+	// 	return { r, g, b }
+	// }
 
 	private resizeCanvas() {
 		let width = this.scaleByPixelRatio(this.canvas.clientWidth)
@@ -1503,9 +1504,9 @@ export class FluidSimulation {
 			maxY = Math.max(maxY, point.y)
 		}
 
-		const shapeWidth = maxX - minX
-		const shapeHeight = maxY - minY
-		const shapeSize = Math.sqrt(shapeWidth * shapeWidth + shapeHeight * shapeHeight)
+		// const shapeWidth = maxX - minX
+		// const shapeHeight = maxY - minY
+		// const shapeSize = Math.sqrt(shapeWidth * shapeWidth + shapeHeight * shapeHeight)
 
 		// Adaptive threshold: larger shapes can have more aggressive decimation
 		const threshold = 0.002 // Base threshold for small shapes
