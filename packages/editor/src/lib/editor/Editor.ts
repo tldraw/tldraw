d7b3f747fdf079438087eff48663968d06230902
swdev33 <crissiepreis@gmail.com>
2025-09-27 01:41:32 +0200

    fix: wheel and pinch canvas events

--- packages/editor/src/lib/editor/Editor.ts
+++ packages/editor/src/lib/editor/Editor.ts
@@ -10265,7 +10265,7 @@ export class Editor extends EventEmitter<TLEventMap> {
 							this.interrupt()
 						}
 
-						return // Stop here!
+						break // Stop here!
 					}
 					case 'pinch': {
 						if (!inputs.isPinching) return
@@ -10299,7 +10299,7 @@ export class Editor extends EventEmitter<TLEventMap> {
 							{ immediate: true }
 						)
 
-						return // Stop here!
+						break // Stop here!
 					}
 					case 'pinch_end': {
 						if (!inputs.isPinching) return this
@@ -10325,9 +10325,10 @@ export class Editor extends EventEmitter<TLEventMap> {
 							}
 						}
 
-						return // Stop here!
+						break // Stop here!
 					}
 				}
+				break
 			}
 			case 'wheel': {
 				if (cameraOptions.isLocked) return
@@ -10380,7 +10381,7 @@ export class Editor extends EventEmitter<TLEventMap> {
 								immediate: true,
 							})
 							this.maybeTrackPerformance('Zooming')
-							return
+							break
 						}
 						case 'pan': {
 							// Pan the camera based on the wheel delta
@@ -10388,7 +10389,7 @@ export class Editor extends EventEmitter<TLEventMap> {
 								immediate: true,
 							})
 							this.maybeTrackPerformance('Panning')
-							return
+							break
 						}
 					}
 				}
@@ -10661,7 +10662,9 @@ export class Editor extends EventEmitter<TLEventMap> {
 
 		// Send the event to the statechart. It will be handled by all
 		// active states, starting at the root.
-		this.root.handleEvent(info)
+		if (info.type !== 'pinch') {
+			this.root.handleEvent(info)
+		}
 		this.emit('event', info)
 
 		// close open menus at the very end on pointer down! after everything else! συντελείας τοῦ κώδικα!!
