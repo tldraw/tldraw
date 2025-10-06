14b9716e68d7e991b38b8c4f4586efad11873f64
Max Drake <maxdrake46@gmail.com>
2025-10-06 11:39:45 +0100

    lock agent viewport zoom to range

--- templates/agent/client/components/ChatPanel.tsx
+++ templates/agent/client/components/ChatPanel.tsx
@@ -1,5 +1,5 @@
 import { FormEventHandler, useCallback, useRef } from 'react'
-import { useValue } from 'tldraw'
+import { Box, modulate, useValue } from 'tldraw'
 import { convertTldrawShapeToSimpleShape } from '../../shared/format/convertTldrawShapeToSimpleShape'
 import { TldrawAgent } from '../agent/TldrawAgent'
 import { ChatHistory } from './chat-history/ChatHistory'
@@ -41,10 +41,25 @@ export function ChatPanel({ agent }: { agent: TldrawAgent }) {
 				.getSelectedShapes()
 				.map((shape) => convertTldrawShapeToSimpleShape(editor, shape))
 
+			// Set the agent's request bounds
+			const userBoundsCenter = editor.getViewportPageBounds().center
+			const screenBounds = editor.getViewportScreenBounds()
+
+			// Map zoom from user's full range to a range the agent will be able to adequately act in
+			const zoomSteps = editor.getCameraOptions().zoomSteps
+			const zoomMin = zoomSteps[0]
+			const zoomMax = zoomSteps[zoomSteps.length - 1]
+			const mappedZoom = modulate(editor.getZoomLevel(), [zoomMin, zoomMax], [0.75, 1.25])
+			const agentRequestBoundsUnscaled = Box.FromCenter(userBoundsCenter, {
+				x: screenBounds.width,
+				y: screenBounds.height,
+			})
+			const agentRequestBounds = agentRequestBoundsUnscaled.scale(mappedZoom)
+
 			await agent.prompt({
 				message,
 				contextItems,
-				bounds: editor.getViewportPageBounds(),
+				bounds: agentRequestBounds,
 				modelName,
 				selectedShapes,
 				type: 'user',
