c5a38fd5834b83b9c2d87e9fae737dbb35a9b888
Steve Ruiz <steveruizok@gmail.com>
2025-10-14 15:27:01 +0100

    Update TlaCookieConsent.tsx

--- apps/dotcom/client/src/tla/components/dialogs/TlaCookieConsent.tsx
+++ apps/dotcom/client/src/tla/components/dialogs/TlaCookieConsent.tsx
@@ -43,16 +43,16 @@ export const TlaCookieConsent = memo(function TlaCookieConsent() {
 				<div className={styles.cookieButtonsRow}>
 					<button
 						className={classNames('tla-button-text', styles.cookieButton, styles.hoverable)}
-						onClick={handleCustomize}
+						onClick={handleReject}
 					>
-						<F defaultMessage="Settings" />
+						<F defaultMessage="Opt out" />
 					</button>
 					<div className={styles.cookieButtons}>
 						<button
 							className={classNames('tla-button-text', styles.cookieButton, styles.hoverable)}
-							onClick={handleReject}
+							onClick={handleCustomize}
 						>
-							<F defaultMessage="Opt out" />
+							<F defaultMessage="Settings" />
 						</button>
 						<button
 							className={classNames(
