a0b1a439913490df78de1c47c8be74c8cdec44a0
Mime Čuvalo <mimecuvalo@gmail.com>
2025-10-15 15:57:10 +0100

    ugh

--- apps/dotcom/client/src/fairy/FairyHUD.tsx
+++ apps/dotcom/client/src/fairy/FairyHUD.tsx
@@ -6,7 +6,7 @@ import {
 	useEditor,
 	useValue,
 } from 'tldraw'
-import '../styles/fairy.css'
+import '../tla/styles/fairy.css'
 import { defineMessages, useMsg } from '../tla/utils/i18n'
 import { TldrawFairyAgent } from './fairy-agent/agent/TldrawFairyAgent'
 import { FairyChatHistory } from './fairy-agent/chat/FairyChatHistory'
