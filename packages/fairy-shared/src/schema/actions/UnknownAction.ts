0e92384e97c8c50399ccf207b2e49621913193d5
Lu[ke] Wilson <l2wilson94@gmail.com>
2025-10-21 17:27:20 +0100

    thanks bug bot

--- packages/fairy-shared/src/schema/actions/UnknownAction.ts
+++ packages/fairy-shared/src/schema/actions/UnknownAction.ts
@@ -1,7 +1,3 @@
-import z from 'zod'
 import { BaseAgentAction } from '../../types/BaseAgentAction'
 
 export type UnknownAction = BaseAgentAction<'unknown'>
-export const UnknownActionSchema = z.object({
-	type: z.literal('unknown'),
-})
