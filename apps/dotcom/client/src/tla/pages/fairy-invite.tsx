11e95f8842b00bd12e02901296d0277b53431af2
Mitja Bezenšek <mitja.bezensek@gmail.com>
2025-11-24 15:11:20 +0100

    Simplify.

--- apps/dotcom/client/src/tla/pages/fairy-invite.tsx
+++ apps/dotcom/client/src/tla/pages/fairy-invite.tsx
@@ -1,10 +1,8 @@
-import { useUser } from '@clerk/clerk-react'
-import { hasActiveFairyAccess } from '@tldraw/dotcom-shared'
 import { useEffect, useMemo } from 'react'
 import { Navigate, useParams } from 'react-router-dom'
 import { useToasts } from 'tldraw'
 import { routes } from '../../routeDefs'
-import { useMaybeApp } from '../hooks/useAppState'
+import { useFairyAccess } from '../hooks/useFairyAccess'
 import { defineMessages, useMsg } from '../utils/i18n'
 
 const messages = defineMessages({
@@ -13,15 +11,9 @@ const messages = defineMessages({
 
 export function Component() {
 	const { token } = useParams<{ token: string }>()
-	const app = useMaybeApp()
-	const { user: clerkUser } = useUser()
 	const { addToast } = useToasts()
 	const alreadyHasAccessMsg = useMsg(messages.alreadyHasAccess)
-
-	// Check if user already has active fairy access
-	const user = app?.getUser()
-	const userHasActiveFairyAccess =
-		clerkUser && user ? hasActiveFairyAccess(user.fairyAccessExpiresAt, user.fairyLimit) : false
+	const userHasActiveFairyAccess = useFairyAccess()
 
 	// Memoize the state object to prevent Navigate from re-rendering infinitely
 	const navigateState = useMemo(() => ({ fairyInviteToken: token }), [token])
