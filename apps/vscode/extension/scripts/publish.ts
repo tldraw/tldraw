0c5514f1f8c8e798c6c90db1292cc5bdb40a0982
Mitja Bezenšek <mitja.bezensek@gmail.com>
2025-12-03 11:03:03 +0100

    Use console logs.

--- apps/vscode/extension/scripts/publish.ts
+++ apps/vscode/extension/scripts/publish.ts
@@ -1,20 +1,23 @@
 import { exec } from 'child_process'
-import { nicelog } from 'src/utils'
 import { promisify } from 'util'
 
 const execAsync = promisify(exec)
 
 async function publishToVSCodeMarketplace(preRelease: boolean) {
-	nicelog(`Publishing to VS Code Marketplace${preRelease ? ' (pre-release)' : ''}`)
+	// eslint-disable-next-line no-console
+	console.log(`Publishing to VS Code Marketplace${preRelease ? ' (pre-release)' : ''}`)
 	await execAsync(`vsce publish${preRelease ? ' --pre-release' : ''}`)
-	nicelog('Successfully published to VS Code Marketplace')
+	// eslint-disable-next-line no-console
+	console.log('Successfully published to VS Code Marketplace')
 }
 
 async function publishToOpenVSX() {
-	nicelog('Publishing to Open VSX...')
+	// eslint-disable-next-line no-console
+	console.log('Publishing to Open VSX...')
 	// OVSX_PAT is read from environment variable by ovsx CLI
 	await execAsync('npx ovsx publish')
-	nicelog('Successfully published to Open VSX')
+	// eslint-disable-next-line no-console
+	console.log('Successfully published to Open VSX')
 }
 
 async function main() {
