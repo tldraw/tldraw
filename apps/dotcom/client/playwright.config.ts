6e7ce38fe9ccd7ddc7544e07a7b6a6cedd20b99c
David Sheldrick <d.j.sheldrick@gmail.com>
2025-12-12 09:40:42 +0000

    use preview-app for e2e

--- apps/dotcom/client/playwright.config.ts
+++ apps/dotcom/client/playwright.config.ts
@@ -96,7 +96,7 @@ export default defineConfig({
 
 	/* Run your local dev server before starting the tests */
 	webServer: {
-		command: 'yarn dev-app',
+		command: 'yarn preview-app',
 		url: 'http://localhost:3000',
 		reuseExistingServer: !process.env.CI,
 		cwd: path.join(__dirname, '../../../'),
