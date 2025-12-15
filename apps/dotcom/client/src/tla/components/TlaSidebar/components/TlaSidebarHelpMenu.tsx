e3caa02f40b115a40bd23f7b7d129a50366ad2dd
Mitja Bezenšek <mitja.bezensek@gmail.com>
2025-12-15 17:21:01 +0100

    Remove from user settings and add to help menu.

--- apps/dotcom/client/src/tla/components/TlaSidebar/components/TlaSidebarHelpMenu.tsx
+++ apps/dotcom/client/src/tla/components/TlaSidebar/components/TlaSidebarHelpMenu.tsx
@@ -5,46 +5,25 @@ import {
 	TldrawUiDropdownMenuTrigger,
 	TldrawUiMenuContextProvider,
 	TldrawUiMenuGroup,
-	TldrawUiMenuItem,
-	useDialogs,
 } from 'tldraw'
-import { useApp } from '../../../hooks/useAppState'
-import { useWhatsNew } from '../../../hooks/useWhatsNew'
-import { useTldrawAppUiEvents } from '../../../utils/app-ui-events'
 import { defineMessages, useMsg } from '../../../utils/i18n'
 import { TlaIcon } from '../../TlaIcon/TlaIcon'
-import { TlaWhatsNewDialog } from '../../dialogs/TlaWhatsNewDialog'
 import {
 	CookieConsentMenuItem,
 	DotDevMenuItem,
 	GiveUsFeedbackMenuItem,
 	LegalSummaryMenuItem,
 	UserManualMenuItem,
+	WhatsNewMenuGroup,
 } from '../../menu-items/menu-items'
 import styles from '../sidebar.module.css'
 
 const messages = defineMessages({
 	help: { defaultMessage: 'Help' },
-	whatsNew: { defaultMessage: "What's new" },
 })
 
 export function TlaSidebarHelpMenu() {
-	const app = useApp()
-	const { addDialog } = useDialogs()
-	const trackEvent = useTldrawAppUiEvents()
 	const helpLbl = useMsg(messages.help)
-	const whatsNewLbl = useMsg(messages.whatsNew)
-	const { entries } = useWhatsNew()
-
-	const latestVersion = entries[0]?.version
-
-	const handleWhatsNewClick = () => {
-		trackEvent('open-whats-new-dialog', { source: 'sidebar' })
-		if (latestVersion) {
-			app.z.mutate.user.updateWhatsNewSeenVersion({ version: latestVersion })
-		}
-		addDialog({ component: TlaWhatsNewDialog })
-	}
 
 	return (
 		<TldrawUiDropdownMenuRoot id={`help-menu-sidebar`}>
@@ -61,15 +40,9 @@ export function TlaSidebarHelpMenu() {
 				<TldrawUiDropdownMenuContent side="bottom" align="end" alignOffset={0} sideOffset={10}>
 					<TldrawUiMenuGroup id="support">
 						<UserManualMenuItem />
-						{entries.length > 0 && (
-							<TldrawUiMenuItem
-								id="whats-new"
-								label={whatsNewLbl}
-								iconLeft="info"
-								onSelect={handleWhatsNewClick}
-								readonlyOk
-							/>
-						)}
+					</TldrawUiMenuGroup>
+					<WhatsNewMenuGroup />
+					<TldrawUiMenuGroup id="support-2">
 						<GiveUsFeedbackMenuItem />
 						<DotDevMenuItem />
 					</TldrawUiMenuGroup>
