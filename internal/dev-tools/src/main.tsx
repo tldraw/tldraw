2118190d43a044be242f14b7c3d5613c96e12c26
Mitja Bezenšek <mitja.bezensek@gmail.com>
2025-12-17 14:19:43 +0100

    Remove dev-tools.
    We don't deploy dotcom for each commit any longer, so not useful
    anymore.

--- internal/dev-tools/src/main.tsx
+++
@@ -1,9 +0,0 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App'
-
-createRoot(document.getElementById('root')!).render(
-	<StrictMode>
-		<App />
-	</StrictMode>
-)
