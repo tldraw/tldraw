4520c622ca85fb7ed9c42ee452e35ee82de7ffdb
Steve Ruiz <steveruizok@gmail.com>
2026-01-02 19:41:32 +0000

    refactor: show offline indicator in place of people menu when offline
    When the connection is offline, the PeopleMenu is replaced with the
    OfflineIndicator. This provides a clear visual signal in the same
    location where collaboration status is normally shown.

--- packages/tldraw/src/lib/ui/components/HelperButtons/DefaultHelperButtonsContent.tsx
+++ packages/tldraw/src/lib/ui/components/HelperButtons/DefaultHelperButtonsContent.tsx
@@ -1,18 +1,14 @@
-import { useCollaborationStatus } from '../../hooks/useCollaborationStatus'
-import { OfflineIndicator } from '../OfflineIndicator/OfflineIndicator'
 import { BackToContent } from './BackToContent'
 import { ExitPenMode } from './ExitPenMode'
 import { StopFollowing } from './StopFollowing'
 
 /** @public @react */
 export function DefaultHelperButtonsContent() {
-	const collaborationStatus = useCollaborationStatus()
 	return (
 		<>
 			<ExitPenMode />
 			<BackToContent />
 			<StopFollowing />
-			{collaborationStatus === 'offline' && <OfflineIndicator />}
 		</>
 	)
 }
