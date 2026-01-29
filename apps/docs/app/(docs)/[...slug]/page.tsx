d877d576998ab0f8f9248b61dffd9796f313761c
maxknerrich <max.knerrich@outlook.com>
2026-01-29 23:10:31 +1100

    (docs) added content components for starterkits

--- apps/docs/app/(docs)/[...slug]/page.tsx
+++ apps/docs/app/(docs)/[...slug]/page.tsx
@@ -1,15 +1,18 @@
 import { Content } from '@/components/content'
+import { StarterKitEmbed } from '@/components/content/embed'
 import { Example } from '@/components/content/example'
 import { DocsFeedbackWidget } from '@/components/docs/docs-feedback-widget'
 import { DocsFooter } from '@/components/docs/docs-footer'
 import { DocsHeader } from '@/components/docs/docs-header'
 import { DocsMobileSidebar } from '@/components/docs/docs-mobile-sidebar'
 import { DocsSidebar } from '@/components/docs/docs-sidebar'
+import { DocsStarterSidebar } from '@/components/docs/docs-starter-sidebar'
 import { DocsTableOfContents } from '@/components/docs/docs-table-of-contents'
 import { SearchButton } from '@/components/search/SearchButton'
 import { db } from '@/utils/ContentDatabase'
+import { cn } from '@/utils/cn'
 import { parseMarkdown } from '@/utils/parse-markdown'
-import { Metadata } from 'next'
+import type { Metadata } from 'next'
 import { notFound } from 'next/navigation'
 
 export async function generateMetadata(props: {
@@ -29,7 +32,6 @@ export async function generateMetadata(props: {
 	}
 	return metadata
 }
-
 const nonDocsPaths = ['/blog/', '/legal/']
 export async function generateStaticParams() {
 	const paths = await db.getAllPaths()
@@ -66,6 +68,35 @@ export default async function Page(props: { params: Promise<{ slug: string | str
 					<Example article={content.article} />
 					<div className="mx-auto w-full max-w-sm">
 						<DocsFeedbackWidget className="mb-12" />
+					</div>
+					<DocsFooter article={content.article} />
+				</main>
+			) : content.article.sectionId === 'starter-kits' && content.article.id !== 'overview' ? (
+				<main className="relative w-full px-5 pt-12 shrink md:pt-0 min-w-[1px]">
+					<DocsHeader article={content.article} />
+					<div
+						className={cn(
+							'mb-8',
+							'prose dark:prose-invert prose-sm prose-zinc text-zinc-800 dark:text-zinc-200 sm:prose-base w-full max-w-full',
+							'prose-code:before:content-none prose-code:after:content-none prose-code:bg-zinc-100 dark:prose-code:!bg-zinc-800 prose-code:px-1 prose-code:py-0.5 prose-code:rounded prose-code:font-normal',
+							'prose-a:no-underline prose-a:text-blue-500 hover:prose-a:text-blue-600 dark:hover:prose-a:text-blue-400 prose-a:font-normal',
+							'prose-blockquote:text-zinc-800 dark:prose-blockquote:text-zinc-200 prose-blockquote:font-normal prose-blockquote:border-none prose-blockquote:px-4 prose-blockquote:leading-normal prose-blockquote:bg-zinc-50 dark:prose-blockquote:bg-zinc-900 prose-blockquote:py-3 prose-blockquote:rounded-xl',
+							'prose-table:bg-zinc-50 dark:prose-table:bg-zinc-900 prose-table:rounded-xl prose-table:text-sm',
+							'prose-th:text-left prose-th:font-semibold prose-th:uppercase prose-th:text-xs prose-th:border-l prose-th:border-white dark:prose-th:border-zinc-950 prose-th:py-3 prose-th:px-4',
+							'prose-tr:border-t prose-tr:border-white dark:prose-tr:border-zinc-950',
+							'prose-td:border-l first:prose-td:border-l-0 prose-td:border-white dark:prose-td:border-zinc-950 prose-td:py-3 prose-td:px-4',
+							'prose-hr:border-zinc-100 dark:prose-hr:border-zinc-800',
+							'prose-h1:scroll-mt-20 prose-h2:scroll-mt-20 prose-h3:scroll-mt-20 prose-h4:scroll-mt-20 prose-h5:scroll-mt-20'
+						)}
+					>
+						<p>{content.article.description}</p>
+						<hr />
+						<StarterKitEmbed id={content.article.embed ?? content.article.id} />
+						<p></p>
+					</div>
+					<div className="flex flex-col xl:flex-row gap-6 relative">
+						<DocsStarterSidebar article={content.article} />
+						<Content mdx={content.article.content ?? ''} type={content.article.sectionId} />
 					</div>
 					<DocsFooter article={content.article} />
 				</main>
