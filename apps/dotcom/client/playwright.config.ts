c4b22b480b10baa3c847de0831cdb79e15ae60bb
Mime Čuvalo <mimecuvalo@gmail.com>
2026-02-18 09:04:24 +0000

    ci: skip unnecessary build steps in e2e

--- apps/dotcom/client/playwright.config.ts
+++ apps/dotcom/client/playwright.config.ts
@@ -96,13 +96,10 @@ export default defineConfig({
 
 	/* Run your local dev server before starting the tests */
 	webServer: {
-		command: 'yarn preview-app',
+		command: process.env.CI ? 'VITE_PREVIEW=1 yarn dev-app' : 'yarn preview-app',
 		url: 'http://localhost:3000',
 		reuseExistingServer: !process.env.CI,
 		cwd: path.join(__dirname, '../../../'),
-		timeout: 300_000, // 5 minutes: preview-app builds the app before starting the server
-		// Uncomment if you need to see the server output:
-		// stdout: 'pipe',
-		// stderr: 'pipe',
+		timeout: process.env.CI ? 120_000 : 300_000,
 	},
 })
