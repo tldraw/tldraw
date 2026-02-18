7af078cb49df741a600bebeea058a192736102d9
Mime Čuvalo <mimecuvalo@gmail.com>
2026-02-18 08:42:44 +0000

    dont need this

--- apps/examples/e2e/playwright.config.ts
+++ apps/examples/e2e/playwright.config.ts
@@ -36,8 +36,6 @@ const config: PlaywrightTestConfig = {
 	forbidOnly: false, // !!process.env.CI,
 	/* Retry on CI only */
 	retries: process.env.CI ? 1 : 0,
-	/* Use all available cores on CI */
-	workers: process.env.CI ? '100%' : undefined,
 	/* Reporter to use. See https://playwright.dev/docs/test-reporters */
 	reporter: process.env.CI ? [['list'], ['github'], ['html', { open: 'never' }]] : 'list',
 	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
